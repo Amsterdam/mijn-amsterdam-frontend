cc7bf5a159c4846aa84f47d6b81f83bdff78c326
Tim van Oostrom <tim@depulz.nl>
2019-03-22 10:38:41 +0100

    Add BRP api and global state
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/App.js
+++ src/App.js
@@ -8,23 +8,33 @@ import Dashboard from './pages/Dashboard/Dashboard.js';
 import NotFound from './pages/NotFound/NotFound.js';
 import styles from './App.module.scss';
 
+import AppContext from './App.context';
+import { useBrpApi } from 'hooks/brp-api.hook.js';
+
 export default function App() {
+  // State that needs to be available everywhere
+  const appState = {
+    BRP: useBrpApi(),
+  };
+
   return (
     <BrowserRouter>
-      <MainHeader />
-      <main className={styles.App}>
-        <Switch>
-          <Route exact path={AppRoutes.ROOT} component={Dashboard} />
-          {/* <Route path={AppRoutes.BURGERZAKEN} />
-            <Route path={AppRoutes.WONEN} />
-            <Route path={AppRoutes.BELASTINGEN} />
-            <Route path={AppRoutes.GEZONDHEID} />
-            <Route path={AppRoutes.INKOMEN} />
-            <Route path={AppRoutes.PROFILE} component={ProfileContainer} /> */}
-          <Route component={NotFound} />
-        </Switch>
-      </main>
-      <MainFooter />
+      <AppContext.Provider value={appState}>
+        <MainHeader />
+        <main className={styles.App}>
+          <Switch>
+            <Route exact path={AppRoutes.ROOT} component={Dashboard} />
+            {/* <Route path={AppRoutes.BURGERZAKEN} />
+              <Route path={AppRoutes.WONEN} />
+              <Route path={AppRoutes.BELASTINGEN} />
+              <Route path={AppRoutes.GEZONDHEID} />
+              <Route path={AppRoutes.INKOMEN} />
+              <Route path={AppRoutes.PROFILE} component={ProfileContainer} /> */}
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+        <MainFooter />
+      </AppContext.Provider>
     </BrowserRouter>
   );
 }
