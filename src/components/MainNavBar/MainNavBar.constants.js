15820c5e4e16697d47b7a7ca89af31e160ab3797
Tim van Oostrom <tim@depulz.nl>
2019-03-27 10:28:34 +0100

    Update submenu design
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/MainNavBar/MainNavBar.constants.js
+++ src/components/MainNavBar/MainNavBar.constants.js
@@ -1,20 +1,53 @@
-export const ButtonText = {
-  HOME: 'Home',
-  MIJN_THEMAS: "Mijn thema's",
-  BURGERZAKEN: 'Burgerzaken',
-  GEZONDHEID: 'Gezondheid',
-  WONEN: 'Wonen',
-  INKOMEN: 'Inkomen',
-  BELASTINGEN: 'Belastingen',
-  MIJN_BUURT: 'Mijn buurt',
-  MIJN_UPDATES: 'Mijn updates',
-};
+import { AppRoutes } from 'App.constants';
+import { ReactComponent as BurgerzakenIcon } from 'assets/images/burgerzaken.svg';
+import { ReactComponent as GezondheidIcon } from 'assets/images/gezondheid.svg';
+import { ReactComponent as WonenIcon } from 'assets/images/wonen.svg';
+import { ReactComponent as InkomenIcon } from 'assets/images/inkomen.svg';
+import { ReactComponent as BelastingenIcon } from 'assets/images/belastingen.svg';
 
-export const SubMenuItems = [
-  ButtonText.MIJN_THEMAS,
-  ButtonText.BURGERZAKEN,
-  ButtonText.GEZONDHEID,
-  ButtonText.WONEN,
-  ButtonText.INKOMEN,
-  ButtonText.BELASTINGEN,
+export const MenuConfig = [
+  {
+    label: 'Home',
+    id: 'home',
+    to: AppRoutes.ROOT,
+  },
+  {
+    label: "Mijn thema's",
+    id: 'mijn-themas',
+    to: AppRoutes.MIJN_THEMAS,
+    submenuItems: [
+      {
+        label: 'Burgerzaken',
+        id: 'burgerzaken',
+        to: AppRoutes.BURGERZAKEN,
+        Icon: BurgerzakenIcon,
+      },
+      {
+        label: 'Gezondheid',
+        id: 'gezondhied',
+        to: AppRoutes.GEZONDHEID,
+        Icon: GezondheidIcon,
+      },
+      {
+        label: 'Wonen',
+        id: 'wonen',
+        to: AppRoutes.WONEN,
+        Icon: WonenIcon,
+      },
+      {
+        label: 'Inkomen',
+        id: 'inkomen',
+        to: AppRoutes.INKOMEN,
+        Icon: InkomenIcon,
+      },
+      {
+        label: 'Belastingen',
+        id: 'belastingen',
+        to: AppRoutes.BELASTINGEN,
+        Icon: BelastingenIcon,
+      },
+    ],
+  },
+  { label: 'Mijn buurt', id: 'mijn-buurt', to: AppRoutes.MIJN_BUURT },
+  { label: 'Mijn updates', id: 'mijn-updates', to: AppRoutes.MIJN_UPDATES },
 ];
