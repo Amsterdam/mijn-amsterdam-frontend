3323d878fe830c124c198734cfe4d4cb2686dcb4
Tim van Oostrom <tim@depulz.nl>
2019-03-28 15:54:52 +0100

    Add login page and nginx conditional
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/auth/index.js
+++ mock-api/auth/index.js
@@ -2,7 +2,7 @@ module.exports = {
   path: '/api/auth',
   template: {
     isAuthenticated: (params, query, body, x, headers) => {
-      return true;
+      return false;
       // return (
       //   'x-saml-attribute-token1' in headers ||
       //   'x-saml-attribute-token2' in headers
