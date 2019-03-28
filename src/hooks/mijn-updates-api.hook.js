7d2176f943abcc3a6243a2abfd29baeef13024db
Tim van Oostrom <tim@depulz.nl>
2019-03-28 13:39:42 +0100

    Updates component + api hook
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/hooks/mijn-updates-api.hook.js
+++ src/hooks/mijn-updates-api.hook.js
@@ -1,37 +1,6 @@
 import { useDataApi } from './api.hook';
 import { format } from 'date-fns';
-import {
-  ApiUrls,
-  Chapters,
-  AppRoutes,
-  DEFAULT_DATE_FORMAT,
-} from 'App.constants';
-
-/**
- * This code is a reference implementation for specific api functionality.
- * TODO:
- * - Determine if link title and url are supplied by the api.
- * - Determine if we can use pagination or not.
- */
-
-function getLinkTitle({ chapter, linktTitle }) {
-  if (linktTitle) {
-    return linktTitle;
-  }
-
-  switch (chapter) {
-    case Chapters.BELASTINGEN:
-      return 'Betaal direct via Mijn Belastingen';
-    case Chapters.WONEN:
-      return 'Meer informatie';
-    default:
-      return 'Bekijk details';
-  }
-}
-
-function getLinkUrl({ chapter, linkUrl }) {
-  return linkUrl || AppRoutes[chapter];
-}
+import { ApiUrls, DEFAULT_DATE_FORMAT } from 'App.constants';
 
 export const useMijnUpdatesApi = (offset = 0, limit = 3) => {
   const options = { url: ApiUrls.MIJN_UPDATES, params: { offset, limit } };
@@ -41,9 +10,7 @@ export const useMijnUpdatesApi = (offset = 0, limit = 3) => {
     data: api.data.map(update => {
       return {
         ...update,
-        dateCreated: format(update.dateCreated, DEFAULT_DATE_FORMAT),
-        linkTitle: getLinkTitle(update),
-        linkUrl: getLinkUrl(update),
+        datePublished: format(update.dateCreated, DEFAULT_DATE_FORMAT),
       };
     }),
     refetch: page => api.refetch({ ...options, params: { offset, limit } }),
