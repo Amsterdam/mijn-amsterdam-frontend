85c134c1a5443872b1c1169726a87233a748aa5a
Tim van Oostrom <tim@depulz.nl>
2019-04-02 17:41:20 +0200

    Add Themas component
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

---
+++ src/components/MyChaptersPanel/MyChaptersPanel.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './MyChaptersPanel.module.scss';
+
+export default function MyChaptersPanel() {
+  return <div className={styles.MyChaptersPanel}>MyChaptersPanel</div>;
+}
