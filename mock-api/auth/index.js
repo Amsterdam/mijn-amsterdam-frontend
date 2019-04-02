44c99801c78437f3fb7323002c2464d7803dbafd
Tim van Oostrom <tim@depulz.nl>
2019-04-02 15:38:07 +0200

    Fix login flow mock api
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/auth/index.js
+++ mock-api/auth/index.js
@@ -1,33 +1,30 @@
-let isAuthenticated = false;
-let userType = null;
+const state = require('./state');
 
 const APP_PORT = process.env.FRONT_END_PORT || 3000;
 const APP_URL = `http://localhost:${APP_PORT}`;
 
 module.exports = {
-  path: '/api/auth|login|logout',
-  status: (req, res, next) => {
+  path: '/api/login|logout',
+  status: async (req, res, next) => {
     if (req.url.endsWith('login')) {
-      isAuthenticated = true;
-      userType = req.params.userType || 'BURGER';
+      await state.setAuth(true);
+      await state.setUserType(req.params.userType || 'BURGER');
       res.redirect(APP_URL);
       return;
-    } else if (req.url.endsWith('auth') && !isAuthenticated) {
-      res.status(403);
     } else if (req.url.endsWith('logout')) {
-      userType = null;
-      isAuthenticated = false;
+      await state.setUserType(null);
+      await state.setAuth(false);
       res.redirect(APP_URL);
       return;
     }
     next();
   },
   template: {
-    isAuthenticated: (params, query, body, x, headers) => {
-      return isAuthenticated;
+    isAuthenticated: async (params, query, body, x, headers) => {
+      return state.isAuthenticated();
     },
-    userType: (params, query, body, x, headers) => {
-      return userType;
+    userType: async (params, query, body, x, headers) => {
+      return state.getUserType();
     },
   },
 };
