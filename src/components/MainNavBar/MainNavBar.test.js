71ecfcb1ed2b44280876819335e911b36cc03a8f
Tim van Oostrom <tim@depulz.nl>
2019-04-02 13:20:26 +0200

    Add app state to tests
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/MainNavBar/MainNavBar.test.js
+++ src/components/MainNavBar/MainNavBar.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import MainNavBar from './MainNavBar';
+import AppState from 'AppState';
+
+const appState = { SESSION: { isAuthenticated: false } };
 
 it('Renders without crashing', () => {
-  shallow(<MainNavBar />);
+  shallow(
+    <AppState value={appState}>
+      <MainNavBar />
+    </AppState>
+  );
 });
