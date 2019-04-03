95d5a34f9e8b471f9c147da0a717932668d5cd49
Bennie van der Wel <bennievanderwel@gmail.com>
2019-04-03 10:37:47 +0200

    MIJN 310 removed redundant div

--- src/components/DirectLinks/DirectLinks.js
+++ src/components/DirectLinks/DirectLinks.js
@@ -6,22 +6,20 @@ import { ReactComponent as ExternalLinkIcon } from 'assets/icons/External-Link.s
 export default function DirectLinks() {
   return (
     <div className={styles.DirectLinks}>
-      <div className={styles.InnerContainer}>
-        <h2>Direct naar</h2>
-        <ul className={styles.LinkList}>
-          {Object.keys(LINKS).map(link => {
-            const { url, displayName } = LINKS[link];
-            return (
-              <li key={link}>
-                <a href={url}>
-                  <ExternalLinkIcon />
-                  {displayName}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <h2>Direct naar</h2>
+      <ul className={styles.LinkList}>
+        {Object.keys(LINKS).map(link => {
+          const { url, displayName } = LINKS[link];
+          return (
+            <li key={link}>
+              <a href={url}>
+                <ExternalLinkIcon />
+                {displayName}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
