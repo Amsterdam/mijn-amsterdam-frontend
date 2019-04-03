fbd0dd2e121e6e218acf78ad1dbb82f59247f53c
Bennie van der Wel <bennievanderwel@gmail.com>
2019-04-03 10:27:25 +0200

    MIJN 310 Added missing url's

--- src/components/DirectLinks/DirectLinks.constants.js
+++ src/components/DirectLinks/DirectLinks.constants.js
@@ -3,14 +3,14 @@ export const LINKS = {
     displayName: 'Mijn belastingen',
     url: 'https://belastingbalie.amsterdam.nl',
   },
-  // MIJN_PARKEREN: {
-  //   displayName: 'Mijn parkeren',
-  //   url: '',
-  // },
-  // MIJN_WERK_EN_INKOMEN: {
-  //   displayName: 'Mijn werk & inkomen',
-  //   url: '',
-  // },
+  MIJN_PARKEREN: {
+    displayName: 'Mijn parkeren',
+    url: 'https://www.amsterdam.nl/parkeren-verkeer/',
+  },
+  MIJN_WERK_EN_INKOMEN: {
+    displayName: 'Mijn werk & inkomen',
+    url: 'https://www.amsterdam.nl/werk-inkomen/',
+  },
   MIJN_ERFPACHT: {
     displayName: 'Mijn erfpacht',
     url: 'https://mijnerfpacht.amsterdam.nl',
