e6974d4a4456151c60ab4936a279649ee9ea4961
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-04-03 13:32:39 +0200

    Mijn 307 direct naar links blok proper (#20)
    * MIJN-310 Implemented the direct links block
    
    # Conflicts:
    #	src/pages/Dashboard/Dashboard.js
    
    * MIJN-310 Added some of the urls and some small fixes
    
    * MIJN-310 Used an existing icon for external links
    
    * MIJN 310 Added missing url's
    
    * MIJN 310 removed redundant div
    
    * MIJN 310 Small naming change
    
    * MIJN 310 simplified css
    
    * MIJN 310 Added new urls and did some small fixes
    
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>
    
    # Conflicts:
    #	src/components/DirectLinks/DirectLinks.js

---
+++ src/components/DirectLinks/DirectLinks.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './DirectLinks.module.scss';
+import { LINKS } from './DirectLinks.constants';
+import { ReactComponent as ExternalLinkIcon } from 'assets/icons/External-Link.svg';
+
+export default function DirectLinks() {
+  return (
+    <div className={styles.DirectLinks}>
+      <h2>Direct naar</h2>
+      <ul className={styles.LinkList}>
+        {Object.entries(LINKS).map(link => {
+          const [linkName, { url, title }] = link;
+          return (
+            <li key={linkName}>
+              <a href={url}>
+                <ExternalLinkIcon />
+                {title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
