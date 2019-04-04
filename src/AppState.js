ec0747ea12c2f8ce30444815923e0acebb4f0839
Tim van Oostrom <tim@depulz.nl>
2019-04-04 11:24:07 +0200

    Enhance api, move my updates to appState
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>
    
    # Conflicts:
    #	src/pages/Dashboard/Dashboard.js
    #	src/pages/MijnUpdates/MijnUpdates.js

--- src/AppState.js
+++ src/AppState.js
@@ -1,14 +1,16 @@
 import React, { createContext } from 'react';
 import { useBrpApi } from 'hooks/brp-api.hook.js';
 import useSessionApi from 'hooks/session.api.hook.js';
+import useMijnUpdatesApi from 'hooks/mijn-updates-api.hook';
 
 export const AppContext = createContext();
 
 export default function AppState({ children, value }) {
-  // State that needs to be available everywhere
+  // State that needs to be shared across multiple components.
   const appState = value || {
     BRP: useBrpApi(),
     SESSION: useSessionApi(),
+    MY_UPDATES: useMijnUpdatesApi(),
   };
 
   return <AppContext.Provider value={appState}>{children}</AppContext.Provider>;
