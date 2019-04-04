f743501309aa121808900b2546884c0c087c2f18
Tim van Oostrom <tim@depulz.nl>
2019-04-04 18:07:30 +0200

    Add focus to updates
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/hooks/my-cases-api.hook.js
+++ src/hooks/my-cases-api.hook.js
@@ -5,15 +5,17 @@ export default (offset, limit) => {
   const api = paginatedApiHook(ApiUrls.FOCUS, offset, limit);
 
   // NOTE: Temporary take data from focus api
-  const items = api.data.items.map(item => {
-    const latestType = item._meest_recent;
-    const dateModified = item.processtappen[latestType].datum;
-    return {
-      chapter: 'INKOMEN',
-      dateModified,
-      title: item.naam,
-      to: '/inkomen',
-    };
-  });
+  const items = api.data.items
+    .filter(item => !!item.processtappen.inBehandeling)
+    .map(item => {
+      const latestType = item._meest_recent;
+      const dateModified = item.processtappen[latestType].datum;
+      return {
+        chapter: 'INKOMEN',
+        dateModified,
+        title: item.naam,
+        to: '/inkomen',
+      };
+    });
   return { ...api, data: { items } };
 };
