8a024689ac4aea4a783007e3d99406e5f54ec839
Tim van Oostrom <tim@depulz.nl>
2019-04-04 11:24:07 +0200

    Enhance api, move my updates to appState
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PageContentMain from 'components/PageContentMain/PageContentMain';
 import PageContentMainHeading from 'components/PageContentMainHeading/PageContentMainHeading';
 import PageContentMainBody from 'components/PageContentMainBody/PageContentMainBody';
 import styles from './Dashboard.module.scss';
 import MijnUpdates from 'components/MijnUpdates/MijnUpdates';
-import { useMijnUpdatesApi } from 'hooks/mijn-updates-api.hook';
 import MyChaptersPanel from 'components/MyChaptersPanel/MyChaptersPanel';
 import DirectLinks from 'components/DirectLinks/DirectLinks';
+import { AppContext } from 'AppState';
+
+const MAX_UPDATES_VISIBLE = 3;
 
 export default () => {
   const {
-    data: { items, total },
-  } = useMijnUpdatesApi();
+    MY_UPDATES: {
+      data: { items, total },
+    },
+  } = useContext(AppContext);
 
   return (
     <PageContentMain className={styles.Dashboard}>
@@ -19,7 +23,10 @@ export default () => {
         Mijn updates ({total})
       </PageContentMainHeading>
       <PageContentMainBody>
-        <MijnUpdates total={total} items={items} />
+        <MijnUpdates
+          total={total}
+          items={items.slice(0, MAX_UPDATES_VISIBLE)}
+        />
         <MyChaptersPanel title="Mijn thema's" />
         <DirectLinks />
       </PageContentMainBody>
