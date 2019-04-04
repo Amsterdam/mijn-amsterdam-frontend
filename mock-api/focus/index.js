3a05750e16ca8cf41902d38a70f36907616fbce6
Tim van Oostrom <tim@depulz.nl>
2019-04-04 18:03:00 +0200

    Temporary add focus to cases
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/focus/index.js
+++ mock-api/focus/index.js
@@ -1,8 +1,14 @@
 const focus = require('../secure-data/focus.json');
+const items = Object.values(focus);
 
 module.exports = {
   path: '/api/focus/aanvragen',
-  template: {
-    ...focus,
+  template: (_, queryParams) => {
+    return {
+      total: items.length,
+      offset: queryParams.offset,
+      limit: queryParams.limit,
+      items: items.slice(queryParams.offset, queryParams.limit),
+    };
   },
 };
--- src/hooks/my-cases-api.hook.js
+++ src/hooks/my-cases-api.hook.js
@@ -2,5 +2,18 @@ import { ApiUrls } from 'App.constants';
 import paginatedApiHook from './paginated-api.hook';
 
 export default (offset, limit) => {
-  return paginatedApiHook(ApiUrls.MY_CASES, offset, limit);
+  const api = paginatedApiHook(ApiUrls.FOCUS, offset, limit);
+
+  // NOTE: Temporary take data from focus api
+  const items = api.data.items.map(item => {
+    const latestType = item._meest_recent;
+    const dateModified = item.processtappen[latestType].datum;
+    return {
+      chapter: 'INKOMEN',
+      dateModified,
+      title: item.naam,
+      to: '/inkomen',
+    };
+  });
+  return { ...api, data: { items } };
 };
