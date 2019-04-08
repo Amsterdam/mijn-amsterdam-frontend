177f23e6003613fb6ccc2a672f3c1f269a94284c
Tim van Oostrom <tim@depulz.nl>
2019-04-08 11:23:46 +0200

    Fix unused imports
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/pages/Burgerzaken/Burgerzaken.js
+++ src/pages/Burgerzaken/Burgerzaken.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PageContentMain from 'components/PageContentMain/PageContentMain';
 import PageContentMainHeading from 'components/PageContentMainHeading/PageContentMainHeading';
 import PageContentMainBody from 'components/PageContentMainBody/PageContentMainBody';
-import styles from './Burgerzaken.module.scss';
 
 export default () => {
   return (
--- src/pages/Zorg/Zorg.js
+++ src/pages/Zorg/Zorg.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PageContentMain from 'components/PageContentMain/PageContentMain';
 import PageContentMainHeading from 'components/PageContentMainHeading/PageContentMainHeading';
 import PageContentMainBody from 'components/PageContentMainBody/PageContentMainBody';
-import styles from './Zorg.module.scss';
 
 export default () => {
   return (
