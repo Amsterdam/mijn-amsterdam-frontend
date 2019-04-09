b1491936c63c5b4f5b7654c921d1dcd30c6868a8
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-04-09 10:35:46 +0200

    [MIJN-138] Afronding dashboard ticket (#34)
    * Rename terms, files
    * Rename some more
    * Remove unused dep
    * Temporary add focus to cases
    * Add focus to updates
    * Adapt to real api
    * Remove file
    * Remove log calls, beter filltering
    * Advanced state separation
    * TEMP: Format focus data
    * Add old date to compare for demo puposes
    * Case Labels + render condition
    * More info for title, remove condition
    * Add comment
    * Update my-cases-api.hook.js

--- src/AppState.js
+++ src/AppState.js
@@ -6,12 +6,22 @@ import useMyCasesApi from 'hooks/my-cases-api.hook';
 import useMyTipsApi from 'hooks/my-tips-api.hook';
 
 export const AppContext = createContext();
+export const SessionContext = createContext();
 
-export default function AppState({ children, value }) {
+export function SessionState({ render }) {
+  const session = useSessionApi();
+  return (
+    <SessionContext.Provider value={session}>
+      {render(session)}
+    </SessionContext.Provider>
+  );
+}
+
+export default function AppState({ render, children, value, session }) {
   // State that needs to be shared across multiple components.
   const appState = value || {
     BRP: useBrpApi(),
-    SESSION: useSessionApi(),
+    SESSION: session,
 
     // NOTE: If needed we can postpone immediate fetching of below data and start fetching in the component.
     MY_UPDATES: useMyUpdatesApi(),
@@ -19,5 +29,9 @@ export default function AppState({ children, value }) {
     MY_TIPS: useMyTipsApi(),
   };
 
-  return <AppContext.Provider value={appState}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={appState}>
+      {render ? render(appState) : children}
+    </AppContext.Provider>
+  );
 }
