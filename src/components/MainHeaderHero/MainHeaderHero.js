990672ef3875943ec6b982c77e0730ced5302442
Tim van Oostrom <tim@depulz.nl>
2019-04-09 15:33:34 +0200

    Header images, responsive images
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/MainHeaderHero/MainHeaderHero.js
+++ src/components/MainHeaderHero/MainHeaderHero.js
@@ -1,15 +1,59 @@
 import React from 'react';
 import styles from './MainHeaderHero.module.scss';
-import DEFAULT_IMG_SRC from 'assets/images/main-header-hero-default.png';
+
+// TODO: Enable if we have appropriate responsive images
+
+// function imgUrl(path, w, orientation = 'l', pd = 1) {
+//   const r = orientation === 'p' ? 0.4 : 0.25;
+//   return `${path}${Math.round(pd * w)}x${Math.round(pd * (w * r))}`;
+// }
 
 const DEFAULT_ALT = 'Sfeerbeeld kenmerkend voor de Amsterdammer';
+
+// TBD: Which size for mobile + tablet
+// const PORTRAIT_SMALL = imgUrl('//placehold.it/', 360, 'p');
+// const PORTRAIT_SMALL_2X = imgUrl('//placehold.it/', 360, 'p', 2);
+// const PORTRAIT_SMALL_3X = imgUrl('//placehold.it/', 360, 'p', 3);
+
+// const LANDSCAPE_SMALL = imgUrl('//placehold.it/', 1024);
+// const LANDSCAPE_MEDIUM = imgUrl('//placehold.it/', 1366);
+// const LANDSCAPE_LARGE = imgUrl('//placehold.it/', 1600);
+
+const DEFAULT_IMG_SRC = '/header/Header-Desktop-4.jpg';
 
 export default function MainHeaderHero(props) {
   const src = props.src || DEFAULT_IMG_SRC;
   const alt = props.alt || DEFAULT_ALT;
   return (
     <div className={styles.MainHeaderHero}>
-      <img src={src} alt={alt} />
+      <picture>
+        {/* <source
+          media="(orientation: portrait) and (max-width: 360px)"
+          srcset={PORTRAIT_SMALL}
+        />
+        <source
+          media="(orientation: portrait) and (-webkit-min-device-pixel-ratio: 2) and (min-width: 360px)"
+          srcset={PORTRAIT_SMALL_2X}
+        />
+        <source
+          media="(orientation: portrait) and (-webkit-min-device-pixel-ratio: 3) and (min-width: 360px)"
+          srcset={PORTRAIT_SMALL_3X}
+        />
+        <source
+          media="(orientation: landscape) and (max-width: 1024px)"
+          srcset={LANDSCAPE_SMALL}
+        />
+        <source
+          media="(orientation: landscape) and (min-width: 1440px)"
+          srcset={LANDSCAPE_LARGE}
+        />
+        <source
+          media="(orientation: landscape) and (min-width: 1200px)"
+          srcset={LANDSCAPE_MEDIUM}
+        />
+        */}
+        <img src={DEFAULT_IMG_SRC} className={styles.TheImg} alt={alt} />
+      </picture>
     </div>
   );
 }
