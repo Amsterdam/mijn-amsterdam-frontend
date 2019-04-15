fa69b1a599c4ba5764e93e0c75b6e5d96232ae82
Tim van Oostrom <tim@depulz.nl>
2019-04-15 12:55:39 +0200

    Rename to .tsx
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/ChapterHeadingIcon/ChapterHeadingIcon.tsx
+++ src/components/ChapterHeadingIcon/ChapterHeadingIcon.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import styles from './ChapterHeadingIcon.module.scss';
 import ChapterIcon from 'components/ChapterIcon/ChapterIcon';
 
-export default function ChapterHeadingIcon({ className, ...rest }) {
+export interface ChapterHeadingIconProps {
+  className: any;
+}
+
+export default function ChapterHeadingIcon({
+  className,
+  ...rest
+}: ChapterHeadingIconProps) {
   return (
     <ChapterIcon
       aria-hidden="true"
