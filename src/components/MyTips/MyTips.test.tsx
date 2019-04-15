a1af9801527b614ea9cd6d3b40172944a8f7af33
Tim van Oostrom <tim@depulz.nl>
2019-04-15 16:59:25 +0200

    Fix test prop
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/MyTips/MyTips.test.tsx
+++ src/components/MyTips/MyTips.test.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import MyTips from './MyTips';
+import { MyTip } from 'hooks/api/my-tips-api.hook';
+
+const MY_TIPS: MyTip[] = [];
 
 it('Renders without crashing', () => {
-  shallow(<MyTips />);
+  shallow(<MyTips items={MY_TIPS} />);
 });
