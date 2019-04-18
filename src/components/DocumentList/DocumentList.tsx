f7e01cd47289d32a69afaf8e8b570e047a279bf2
Tim van Oostrom <tim@depulz.nl>
2019-04-18 13:10:16 +0200

    Document list component
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/DocumentList/DocumentList.tsx
+++ src/components/DocumentList/DocumentList.tsx
@@ -3,53 +3,46 @@ import styles from './DocumentList.module.scss';
 import ButtonLink from 'components/ButtonLink/ButtonLink';
 import { defaultDateFormat } from 'helpers/App';
 import { AppRoutes, Colors } from 'App.constants';
-import ChapterIcon from 'components/ChapterIcon/ChapterIcon';
+import { ReactComponent as DocumentIcon } from 'assets/icons/Document.svg';
+import { ReactComponent as DownloadIcon } from 'assets/icons/Download.svg';
 import Heading from 'components/Heading/Heading';
-import { MyUpdate } from '../../hooks/api/my-updates-api.hook';
 
-export interface MyUpdatesProps {
-  items: MyUpdate[];
-  total: number;
+export interface Document {
+  id: string;
+  title: string;
+  url: string;
+  type: string;
+  datePublished: string;
 }
 
-export default function DocumentList({ items = [], total = 0 }: MyUpdatesProps) {
+export interface DocumentListProps {
+  items: Document[];
+}
+
+export default function DocumentList({ items = [] }: DocumentListProps) {
   return (
     <div className={styles.DocumentList}>
       <ul>
         {items.map(item => {
           return (
-            <li key={item.id} className={styles.MyUpdateItem}>
-              <ChapterIcon
-                fill={Colors.primaryRed}
-                className={styles.Icon}
-                chapter={item.chapter}
-              />
-              <aside>
-                <em className={styles.ChapterIndication}>
-                  {item.chapter.toLowerCase()}
-                </em>
+            <li key={item.id} className={styles.DocumentListItem}>
+              <aside className={styles.MetaInfo}>
+                <em className={styles.TypeIndication}>{item.type}</em>
                 <time className={styles.Datum} dateTime={item.datePublished}>
                   {defaultDateFormat(item.datePublished)}
                 </time>
               </aside>
-              <Heading el="h4" size="small">
-                {item.title}
-              </Heading>
-              {!!item.description && (
-                <p className={styles.Description}>{item.description}</p>
-              )}
-              <p className={styles.Action}>
-                <ButtonLink to={item.link.to}>{item.link.title}</ButtonLink>
-              </p>
+              <a className={styles.DownloadLink} href={item.url}>
+                <DocumentIcon className={styles.Icon} />
+                <Heading el="h4" size="small">
+                  {item.title} <DownloadIcon className={styles.DownloadIcon} />
+                </Heading>
+                <div className={styles.FileType}>PDF</div>
+              </a>
             </li>
           );
         })}
       </ul>
-      {total > items.length && (
-        <p className={styles.FooterLink}>
-          <ButtonLink to={AppRoutes.MY_UPDATES}>Alle updates</ButtonLink>
-        </p>
-      )}
     </div>
   );
 }
