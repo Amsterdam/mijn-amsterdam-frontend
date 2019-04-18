c6b0cd29ec5ad38f7e952e4a9ae1548c2de9ec56
Tim van Oostrom <tim@depulz.nl>
2019-04-18 13:07:32 +0200

    Add ZorgDetail page
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/pages/ZorgDetail/ZorgDetail.test.tsx
+++ src/pages/ZorgDetail/ZorgDetail.test.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ZorgDetail from './ZorgDetail';
+import AppState from 'AppState';
+
+const APP_STATE = { WMO: { data: { items: [] } } };
 
 it('Renders without crashing', () => {
-  shallow(<ZorgDetail />);
+  shallow(
+    <AppState value={APP_STATE}>
+      <ZorgDetail />
+    </AppState>
+  );
 });
--- src/pages/ZorgDetail/ZorgDetail.tsx
+++ src/pages/ZorgDetail/ZorgDetail.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PageContentMain from 'components/PageContentMain/PageContentMain';
 import PageContentMainHeading from 'components/PageContentMainHeading/PageContentMainHeading';
 import PageContentMainBody from 'components/PageContentMainBody/PageContentMainBody';
 import styles from './ZorgDetail.module.scss';
 import ChapterHeadingIcon from 'components/ChapterHeadingIcon/ChapterHeadingIcon';
 import { Chapters } from 'App.constants';
-import { IconButtonLink } from '../../components/ButtonLink/ButtonLink';
-import { AppRoutes } from '../../App.constants';
+import { IconButtonLink } from 'components/ButtonLink/ButtonLink';
+import { AppRoutes } from 'App.constants';
 import { ReactComponent as CaretLeft } from 'assets/icons/Chevron-Left.svg';
+import { AppContext } from 'AppState';
+import useReactRouter from 'use-react-router';
+import { WmoItem } from '../../data-formatting/wmo';
+import DocumentList from 'components/DocumentList/DocumentList';
+import Heading from '../../components/Heading/Heading';
 
 export default () => {
+  const {
+    WMO: {
+      data: { items },
+    },
+  } = useContext(AppContext);
+  const {
+    match: {
+      params: { id },
+    },
+  } = useReactRouter();
+  const WmoItem = items.find(item => item.id === id);
+  const documents = [
+    {
+      id: 'doc-1',
+      title: 'Documentje',
+      type: 'beschikking',
+      datePublished: '2019-10-12',
+      url: 'http://www.download',
+    },
+    {
+      id: 'doc-1',
+      title: 'Documentje',
+      type: 'beschikking',
+      datePublished: '2019-10-12',
+      url: 'http://www.download',
+    },
+    {
+      id: 'doc-1',
+      title: 'Documentje',
+      type: 'beschikking',
+      datePublished: '2019-10-12',
+      url: 'http://www.download',
+    },
+    {
+      id: 'doc-1',
+      title: 'Documentje',
+      type: 'beschikking',
+      datePublished: '2019-10-12',
+      url: 'http://www.download',
+    },
+  ];
   return (
     <PageContentMain variant="full" className={styles.ZorgDetail}>
       <PageContentMainHeading el="header" variant="boxedWithIcon">
         <ChapterHeadingIcon chapter={Chapters.ZORG} />
         <IconButtonLink to={AppRoutes.ZORG}>
-          <CaretLeft /> Terug
+          <CaretLeft /> Zorg
         </IconButtonLink>
-        <h2 className={styles.PageHeading}>ZorgDetail</h2>
+        <Heading el="h2" className={styles.PageHeading}>
+          {WmoItem && WmoItem.title}
+        </Heading>
       </PageContentMainHeading>
       <PageContentMainBody variant="boxed">
-        <p>ZorgDetail body</p>
+        <Heading size="mediumLarge" className={styles.DocumentListHeading}>
+          Mijn brieven
+        </Heading>
+        <DocumentList items={documents} />
       </PageContentMainBody>
     </PageContentMain>
   );
