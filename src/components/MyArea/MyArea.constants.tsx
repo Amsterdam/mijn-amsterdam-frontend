f6204cc9a29e0e2d04bfa60ad816e7e59aae063a
Bennie van der Wel <bennievanderwel@gmail.com>
2019-04-23 13:41:08 +0200

    MIJN-528 Processed review feedback

--- src/components/MyArea/MyArea.constants.tsx
+++ src/components/MyArea/MyArea.constants.tsx
@@ -1,2 +1,2 @@
-export const MAP_URL: string =
+export const MAP_URL =
   'https://data.amsterdam.nl/data/?modus=kaart&achtergrond=topo_rd_zw&center=52.3717228%2C4.8927377&embed=true&legenda=true&zoom=10';
--- src/components/MyArea/MyArea.tsx
+++ src/components/MyArea/MyArea.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter, NavLink, RouteComponentProps } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './MyArea.module.scss';
 import { MAP_URL } from './MyArea.constants';
 import { AppRoutes } from 'App.constants';
@@ -7,7 +7,7 @@ import { ReactComponent as Logo } from 'assets/images/logo-amsterdam.svg';
 import { ReactComponent as CloseIcon } from 'assets/icons/Close.svg';
 import Heading from 'components/Heading/Heading';
 
-export const MyAreaHeader = () => {
+export function MyAreaHeader() {
   return (
     <div className={styles.Header}>
       <Logo />
@@ -19,37 +19,32 @@ export const MyAreaHeader = () => {
       </NavLink>
     </div>
   );
-};
+}
 
-export const MyAreaMap = () => {
+export function MyAreaMap() {
   return (
     <iframe
       id="mapIframe"
-      title="Map of my area"
+      title="Kaart van mijn buurt"
       src={MAP_URL}
       className={styles.Map}
     />
   );
-};
+}
 
-const MyArea: React.SFC<RouteComponentProps> = ({ history }) => {
+export default function MyArea() {
   return (
     <div className={styles.MyArea}>
       <MyAreaMap />
-      <div
-        onClick={() => history.push(AppRoutes.MY_AREA)}
-        className={styles.Overlay}
-      >
-        <div className={styles.Content}>
+      <NavLink to={AppRoutes.MY_AREA} className={styles.Overlay}>
+        <div>
           <Heading size="large">Mijn Buurt</Heading>
           <p>
             Klik voor een overzicht van gemeentelijke informatie rond uw eigen
             woning
           </p>
         </div>
-      </div>
+      </NavLink>
     </div>
   );
-};
-
-export default withRouter(MyArea);
+}
