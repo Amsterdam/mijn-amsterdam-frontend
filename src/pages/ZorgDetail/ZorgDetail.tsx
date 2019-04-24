0ea6f0e6b173be9ddd8d9a87ee38de76aaa202cf
Tim van Oostrom <tim@depulz.nl>
2019-04-24 15:04:06 +0200

    Add Detailed info
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/pages/ZorgDetail/ZorgDetail.tsx
+++ src/pages/ZorgDetail/ZorgDetail.tsx
@@ -3,12 +3,14 @@ import PageContentMain from 'components/PageContentMain/PageContentMain';
 import PageContentMainHeading from 'components/PageContentMainHeading/PageContentMainHeading';
 import styles from './ZorgDetail.module.scss';
 import ChapterHeadingIcon from 'components/ChapterHeadingIcon/ChapterHeadingIcon';
-import { Chapters } from 'App.constants';
+import { Chapters, AppRoutes } from 'App.constants';
 import { AppContext } from 'AppState';
 import useReactRouter from 'use-react-router';
 import Heading from 'components/Heading/Heading';
 import PageContentMainHeadingBackLink from 'components/PageContentMainHeadingBackLink/PageContentMainHeadingBackLink';
-import { AppRoutes } from '../../App.constants';
+import PageContentMainBody from 'components/PageContentMainBody/PageContentMainBody';
+import { ButtonLinkExternal } from 'components/ButtonLink/ButtonLink';
+import classnames from 'classnames';
 
 export default () => {
   const {
@@ -34,6 +36,39 @@ export default () => {
           {WmoItem && WmoItem.title}
         </Heading>
       </PageContentMainHeading>
+      <PageContentMainBody variant="regularBoxed">
+        <Heading className={styles.ListHeading}>Mijn gegevens</Heading>
+        <ul className={styles.List}>
+          {WmoItem && WmoItem.dateStart && (
+            <li className={classnames(styles.ListItem, styles.DatesInfo)}>
+              <p>
+                <strong>Startdatum indicatie</strong>
+                <time>{WmoItem.dateStart}</time>
+              </p>
+              {WmoItem && WmoItem.dateFinish && (
+                <p>
+                  <strong>Einddatum indicatie</strong>
+                  <time>{WmoItem.dateFinish}</time>
+                </p>
+              )}
+            </li>
+          )}
+          {WmoItem && WmoItem.supplier && (
+            <li className={classnames(styles.ListItem, styles.SupplierInfo)}>
+              <p>
+                <strong>Leverancier</strong>
+                <span>{WmoItem.supplier}</span>
+              </p>
+              <p>
+                <strong>&nbsp;</strong>
+                <ButtonLinkExternal to={WmoItem.supplierUrl}>
+                  {WmoItem.supplierUrl}
+                </ButtonLinkExternal>
+              </p>
+            </li>
+          )}
+        </ul>
+      </PageContentMainBody>
     </PageContentMain>
   );
 };
