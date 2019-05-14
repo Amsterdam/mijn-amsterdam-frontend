84524d48a1ffc69e23c612950cd2d7b68bb1b56b
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-05-14 16:41:27 +0200

    [Mijn-675] focus updates (#57)
    * Implementation of Generic focus updates

--- src/components/StatusLine/StatusLine.test.tsx
+++ src/components/StatusLine/StatusLine.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import StatusLine, { StatusLineItem } from './StatusLine';
+import { BrowserRouter } from 'react-router-dom';
 
 const ITEMS: StatusLineItem[] = [];
 
 it('Renders without crashing', () => {
-  shallow(<StatusLine items={ITEMS} />);
+  shallow(
+    <BrowserRouter>
+      <StatusLine items={ITEMS} />
+    </BrowserRouter>
+  );
 });
