eb688afa5a108ee721af1a50619ed51e13a05823
Tim van Oostrom <tim@depulz.nl>
2019-05-23 14:39:54 +0200

    Fix test
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/ErrorMessages/ErrorMessages.test.tsx
+++ src/components/ErrorMessages/ErrorMessages.test.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ErrorMessages from './ErrorMessages';
 
+const DUMMY_ERRORS = [
+  {
+    name: 'Een api naam',
+    error: 'De server reageert niet',
+  },
+];
+
 it('Renders without crashing', () => {
-  shallow(<ErrorMessages />);
+  shallow(<ErrorMessages errors={DUMMY_ERRORS} />);
 });
