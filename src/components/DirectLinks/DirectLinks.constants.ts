2f48bc1f421667431392d0f2a1a2073f9ae5a2b6
Tim van Oostrom <tim@depulz.nl>
2019-07-02 10:19:10 +0200

    [MIJN-961] - Direct links changes
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/DirectLinks/DirectLinks.constants.ts
+++ src/components/DirectLinks/DirectLinks.constants.ts
@@ -1,11 +1,9 @@
 import { AppRoutes } from 'App.constants';
 export type DirectLinkType =
-  | 'MIJN_BELASTINGEN'
   | 'MIJN_SUBSIDIE'
   | 'MIJN_WERK_EN_INKOMEN'
   | 'MY_AREA'
-  | 'MY_TIPS'
-  | 'MIJN_ERFPACHT';
+  | 'MY_TIPS';
 
 export interface DirectLink {
   title: string;
@@ -25,24 +23,14 @@ export const LINKS: { [key in DirectLinkType]: DirectLink } = {
     url: AppRoutes.MY_AREA,
     isPhoneScreenLink: true,
   },
-  MIJN_BELASTINGEN: {
-    title: 'Mijn belastingen',
-    url: 'https://belastingbalie.amsterdam.nl',
-    isExternalLink: true,
-  },
-  MIJN_SUBSIDIE: {
-    title: 'Mijn subsidie',
-    url: 'https://mijnsubsidies.amsterdam.nl/loket/',
-    isExternalLink: true,
-  },
   MIJN_WERK_EN_INKOMEN: {
     title: 'Mijn werk & inkomen',
     url: 'https://edison.amsterdam.nl/SignIn?ReturnUrl=%2F',
     isExternalLink: true,
   },
-  MIJN_ERFPACHT: {
-    title: 'Mijn erfpacht',
-    url: 'https://mijnerfpacht.amsterdam.nl',
+  MIJN_SUBSIDIE: {
+    title: 'Mijn subsidies',
+    url: 'https://mijnsubsidies.amsterdam.nl/loket/',
     isExternalLink: true,
   },
 };
