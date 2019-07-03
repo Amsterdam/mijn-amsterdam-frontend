134c29ad8ec9c665090492a4a451072c32ff99ad
Tim van Oostrom <tim@depulz.nl>
2019-07-03 14:38:35 +0200

    [MIJN-997] Add Sentry to front-end
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'styles/main.scss';
+import * as Sentry from '@sentry/browser';
+
+const sentryDSN = process.env.REACT_APP_SENTRY_DSN;
+if (sentryDSN) {
+  Sentry.init({
+    dsn: sentryDSN,
+    environment: process.env.REACT_APP_SENTRY_ENV || 'development',
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
