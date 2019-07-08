5016fa4d279c8f78df5b282d5e2d57f0773600f4
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-07-08 16:11:50 +0200

    Disable Sentry for development (#87)
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,14 @@ import 'styles/main.scss';
 import * as Sentry from '@sentry/browser';
 
 const sentryDSN = process.env.REACT_APP_SENTRY_DSN;
-if (sentryDSN) {
+if (
+  sentryDSN &&
+  process.env.REACT_APP_SENTRY_ENV &&
+  process.env.REACT_APP_SENTRY_ENV !== 'development'
+) {
   Sentry.init({
     dsn: sentryDSN,
-    environment: process.env.REACT_APP_SENTRY_ENV || 'development',
+    environment: process.env.REACT_APP_SENTRY_ENV,
   });
 }
 
