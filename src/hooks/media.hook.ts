e5c4637a351201a76ab40cc33dee58a6ff497b02
Bennie van der Wel <bennievanderwel@gmail.com>
2019-07-18 15:18:58 +0200

    MIJN-999 Disabled responsive css for IE11

--- src/hooks/media.hook.ts
+++ src/hooks/media.hook.ts
@@ -1,14 +1,21 @@
 // Helper functions to determine screen size in JS
 import { useMedia } from 'use-media';
+import { detect } from 'detect-browser';
+
+const isIE11 = detect()!.name === 'ie';
+
+export function useIE11() {
+  return isIE11;
+}
 
 export function useDesktopScreen(): boolean {
-  return useMedia({ minWidth: 1025 });
+  return useMedia({ minWidth: 1025 }) || useIE11();
 }
 
 export function useTabletScreen(): boolean {
-  return useMedia({ maxWidth: 1024 }); // including Nexus 10
+  return useMedia({ maxWidth: 1024 }) && !isIE11; // including Nexus 10
 }
 
 export function usePhoneScreen(): boolean {
-  return useMedia({ maxWidth: 640 });
+  return useMedia({ maxWidth: 640 }) && !isIE11;
 }
