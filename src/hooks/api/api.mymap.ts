5e80438838d027c4c7472fe502f6e3dd7631d8e6
Bennie van der Wel <bennievanderwel@gmail.com>
2019-07-22 10:27:11 +0200

    Back to prod embed map

--- src/hooks/api/api.mymap.ts
+++ src/hooks/api/api.mymap.ts
@@ -5,7 +5,9 @@ import { usePhoneScreen } from 'hooks/media.hook';
 
 export const BAG_SEARCH_ENDPOINT_URL = `${ApiUrls.BAG}?q=`;
 
-export const MAP_URL = process.env.REACT_APP_EMBED_MAP_URL;
+// export const MAP_URL = process.env.REACT_APP_EMBED_MAP_URL;
+export const MAP_URL =
+  'https://data.amsterdam.nl/data/?modus=kaart&achtergrond=topo_rd_zw&embed=true';
 export const DEFAULT_LAT = 52.3717228;
 export const DEFAULT_LON = 4.8927377;
 export const DEFAULT_ZOOM = 8;
