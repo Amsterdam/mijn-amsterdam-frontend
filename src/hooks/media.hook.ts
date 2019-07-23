4719c2d8d89d154ad0750dcdc113944401bbf289
Bennie van der Wel <bennievanderwel@gmail.com>
2019-07-23 15:38:20 +0200

    MIJN-999 Disabled responsive css for IE11 (#112)
    * MIJN-999 Disabled responsive css for IE11
    
    * MIJN-999 Fixed review feedback
    
    * MIJN-999 uninstalled unneeded lib and replaced magic numbers

--- src/hooks/media.hook.ts
+++ src/hooks/media.hook.ts
@@ -1,15 +1,17 @@
-import { Breakpoints } from './../App.constants';
 // Helper functions to determine screen size in JS
+import { Breakpoints } from './../App.constants';
 import { useMedia } from 'use-media';
 
+var isIE11 = !!window.navigator.userAgent.indexOf('MSIE ');
+
 export function useDesktopScreen(): boolean {
-  return useMedia({ minWidth: Breakpoints.tablet + 1 });
+  return useMedia({ minWidth: Breakpoints.tablet + 1 }) || isIE11;
 }
 
 export function useTabletScreen(): boolean {
-  return useMedia({ maxWidth: Breakpoints.tablet }); // including Nexus 10
+  return useMedia({ maxWidth: Breakpoints.tablet }) && !isIE11;
 }
 
 export function usePhoneScreen(): boolean {
-  return useMedia({ maxWidth: Breakpoints.phone });
+  return useMedia({ maxWidth: Breakpoints.phone }) && !isIE11;
 }
