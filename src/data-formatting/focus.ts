010dc26086966a779b46b5814e91784d7b4ff741
Bennie van der Wel <bennievanderwel@gmail.com>
2019-07-29 13:40:34 +0200

    DIrty fix for faulty test data (#117)
    * DIrty fix for faulty test data
    
    * Removed debugger
    
    * Removed console log

--- src/data-formatting/focus.ts
+++ src/data-formatting/focus.ts
@@ -558,7 +558,7 @@ function formatStepData(
 
 // This function transforms the source data from the api into readable/presentable messages for the client.
 function formatFocusProduct(product: FocusProduct): FocusItem {
-  const {
+  let {
     _id: id,
     _meest_recent: latestStep,
     soortProduct: productOrigin,
@@ -568,9 +568,37 @@ function formatFocusProduct(product: FocusProduct): FocusItem {
     dienstverleningstermijn: daysSupplierActionRequired,
     inspanningsperiode: daysUserActionRequired,
   } = product;
-
   const inProgress = isInProgess(decision, steps);
   const stepData = steps[latestStep];
+
+  // FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  // ################################
+  // HACK ###########################
+  // ################################
+
+  // Due to an error in the test data we get back from the WPI we need to
+  // overwrite the data in order for it to work. This was a super dirty quick
+  // fix and needed since a team was testing the app at the moment it broke and
+  // they needed it to work, badly.
+
+  // TODO: DO NOT PUSH TO PRODUCTION
+  // TODO: DO NOT PUSH TO PRODUCTION
+  // TODO: DO NOT PUSH TO PRODUCTION
+
+  if (
+    !inProgress &&
+    (decision === 'Afwijzing' || decision === 'Toekenning') &&
+    latestStep === 'aanvraag'
+  ) {
+    // @ts-ignore
+    latestStep = 'beslissing';
+  }
+
+  // ################################
+  // HACK ###########################
+  // ################################
+
   const stepLabels = inProgress
     ? (Labels[productOrigin][latestStep] as Info)
     : (Labels[productOrigin][latestStep] as InfoExtended)[decision];
