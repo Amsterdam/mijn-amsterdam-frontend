587b3d9030a3c69e5442cdfb737fc22b168e6957
Bennie van der Wel <bennievanderwel@gmail.com>
2019-08-06 13:24:52 +0200

    MIJN-1112 Fixed the isIE check which caused that some elements were not responsive

--- src/hooks/media.hook.ts
+++ src/hooks/media.hook.ts
@@ -2,16 +2,17 @@
 import { Breakpoints } from './../App.constants';
 import { useMedia } from 'use-media';
 
-var isIE11 = !!window.navigator.userAgent.indexOf('MSIE ');
+var ua = window.navigator.userAgent;
+var isIE = /MSIE|Trident/.test(ua);
 
 export function useDesktopScreen(): boolean {
-  return useMedia({ minWidth: Breakpoints.tablet + 1 }) || isIE11;
+  return useMedia({ minWidth: Breakpoints.tablet + 1 }) || isIE;
 }
 
 export function useTabletScreen(): boolean {
-  return useMedia({ maxWidth: Breakpoints.tablet }) && !isIE11;
+  return useMedia({ maxWidth: Breakpoints.tablet }) && !isIE;
 }
 
 export function usePhoneScreen(): boolean {
-  return useMedia({ maxWidth: Breakpoints.phone }) && !isIE11;
+  return useMedia({ maxWidth: Breakpoints.phone }) && !isIE;
 }
