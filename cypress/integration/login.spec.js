071c0c0f056a4e0178105fdd106fda81f3ea1256
Tim van Oostrom <tim@depulz.nl>
2019-08-27 16:48:38 +0200

    Change test
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -5,16 +5,16 @@ describe('Landing page and login', function() {
   const LOGIN_URL = 'localhost:3000/api/login';
   const LOGOUT_URL = 'localhost:3000/api/logout';
 
-  // it('shows login button', function() {
-  //   // Make sure the user is in an logged out state
-  //   cy.request(LOGOUT_URL);
-  //   cy.visit(URL);
-  //   cy.contains(LOGIN_BTN_TEXT);
-  // });
-
-  it('redirects to the dashboard when logged in', function() {
-    cy.request(LOGIN_URL);
-    // cy.visit(URL);
-    cy.url().should('eq', URL);
+  it('shows login button', function() {
+    // Make sure the user is in an logged out state
+    // cy.request(LOGOUT_URL);
+    cy.visit(URL);
+    cy.contains(LOGIN_BTN_TEXT);
   });
+
+  // it('redirects to the dashboard when logged in', function() {
+  //   cy.request(LOGIN_URL);
+  //   // cy.visit(URL);
+  //   cy.url().should('eq', URL);
+  // });
 });
