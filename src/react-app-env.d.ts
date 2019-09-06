6497bb1faa0855e00429e2f9d36a6512aecf2187
Tim van Oostrom <tim@depulz.nl>
2019-09-06 15:07:46 +0200

    Restore types
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,11 +4,3 @@ type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
 
 // TODO: Add typings for this function
 declare module 'use-debounce/lib/callback';
-
-export function useDebounce(value: any, delay: any, ...args: any[]): any;
-export function useDebouncedCallback(
-  callback: any,
-  delay: any,
-  deps: any,
-  ...args: any[]
-): any;
