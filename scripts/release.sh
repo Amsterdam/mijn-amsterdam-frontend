275b347d0cda72a10fe59766f3e61c8d2555698e
Tim van Oostrom <tim@depulz.nl>
2019-09-17 17:36:26 +0200

    Revert "Push release to master"
    This reverts commit 1c472c689cf6d0727032890bab6585a1a69f5986.

--- scripts/release.sh
+++ scripts/release.sh
@@ -50,6 +50,6 @@ npm --no-git-tag-version --allow-same-version version "$MAJ.$MIN.$BUG" && \
 git add package.json package-lock.json && \
 git commit -m "Bump! $NEWTAG" && \
 git tag -a "$NEWTAG" -m  "Production ${NEWTAG}" && \
-git push -u --follow-tags origin master && \
+git push -u --follow-tags origin "$BRANCH" && \
 
 echo "Don't forget to Pull Request the release!"
