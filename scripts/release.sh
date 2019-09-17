f5796a70017a7fceb5b0f31668822d20f3da54b2
Tim van Oostrom <tim@depulz.nl>
2019-09-17 15:51:15 +0200

    other tag
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- scripts/release.sh
+++ scripts/release.sh
@@ -46,8 +46,8 @@ echo "Adding Tag: $NEWTAG";
 git branch -m "production-${NEWTAG}" && \
 npm --no-git-tag-version --allow-same-version version "$MAJ.$MIN.$BUG" && \
 git add package.json package-lock.json && \
+git tag "$NEWTAG" -a && \
 git commit -m "Bump! $NEWTAG" && \
-git tag "$NEWTAG" && \
 git push -u origin "$NEWTAG" && \
 
 echo "Don't forget to Pull Request the release!"
