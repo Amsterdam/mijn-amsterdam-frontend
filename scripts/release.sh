c5223c6bb56a7f820df8f7201b705ac81e32d742
Tim van Oostrom <tim@depulz.nl>
2019-09-17 14:32:36 +0200

    snap!
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- scripts/release.sh
+++ scripts/release.sh
@@ -54,7 +54,7 @@ git branch -m $NEWTAG &&
 git tag -a $NEWTAG -m $NEWTAG &&
 npm --no-git-tag-version version "$MAJ.$MIN.$BUG" &&
 # git add package.json package-lock.json &&
-# git commit -m "Bump! $NEWTAG"
+git commit -m "Bump! $NEWTAG"
 # git push -u origin $NEWTAG
 
 echo "Don't forget to Pull Request the release!"
