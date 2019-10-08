0e44128fd99b2a82f7ef9a55d75fb03da3514771
Tim van Oostrom <tim@depulz.nl>
2019-10-08 18:13:14 +0200

    Test simple spec
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- cypress/integration/landing-and-login.spec.js
+++ cypress/integration/landing-and-login.spec.js
@@ -1,24 +1,7 @@
 import { assertAtHome } from '../support/helpers';
 
 describe('Landing page and login', function() {
-  function atHome() {
-    assertAtHome('Welkom op Mijn Amsterdam');
-  }
-
-  it('Visit logout url always returns to landing page', function() {
-    cy.request('/api/logout');
-    cy.visit('/');
-    atHome();
-  });
-
-  it('Login button can be clicked, user is redirected to dashboard', function() {
-    cy.contains('Inloggen met DigiD').click();
-    cy.location('pathname').should('eq', '/');
-    cy.contains('Actueel');
-  });
-
-  it('Clicking logout button sends us back to the landing page', function() {
-    cy.contains('Uitloggen').click();
-    atHome();
+  it('works', () => {
+    cy.request('http://api:5000/api/login');
   });
 });
