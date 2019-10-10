a8d298cca51d99f48e1d87df33fdc461c9b92412
Tim van Oostrom <tim@depulz.nl>
2019-10-10 17:48:13 +0200

    Change nodemon setup
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/index.js
+++
@@ -1,9 +0,0 @@
-const ip = require('./get-ip');
-
-const [dir, port] = process.argv.slice(2);
-const apiPort = process.env.MOCK_API_PORT || port || 5000;
-const configDir = process.cwd() + '/' + dir;
-
-console.log(
-  `Dyson listening on ${ip}:${apiPort} and serving from: ${configDir}`
-);
