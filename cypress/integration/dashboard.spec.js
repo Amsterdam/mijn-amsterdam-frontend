d9e9ea675043c75731926b2c3908b467834c7e5e
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-10-14 14:29:53 +0200

    Mijn 931 additional e2e tests (#214)

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -1,18 +1,34 @@
 import { assertAtHome, selectComponent } from '../support/helpers';
 
-describe('Dashboard', function() {
-  it('Visit login url always redirects user to Dashboard page', function() {
+describe('Dashboard', () => {
+  it('Visit login url always redirects user to Dashboard page', () => {
     cy.visit('/api/login');
     assertAtHome('Actueel');
   });
 
-  it("Shows the Chapters Panel (Thema's)", function() {
+  it("Shows the Chapters Panel (Thema's)", () => {
     selectComponent('MyChaptersPanel_MyChaptersPanel').contains("Mijn thema's");
   });
 
-  it('Belastingen Chapter is always visible', function() {
+  it('Belastingen Chapter is always visible', () => {
     selectComponent('MainNavSubmenu_MainNavSubmenuLink').contains(
       'Belastingen'
     );
+  });
+
+  it('Shows MyCases', () => {
+    selectComponent('MyCases_MyCases').should('exist');
+  });
+
+  it('Shows MyArea', () => {
+    selectComponent('MyArea_MyArea').should('exist');
+  });
+
+  it('Shows MyTips', () => {
+    selectComponent('MyTips_MyTips').should('exist');
+  });
+
+  it('DirectLinks', () => {
+    selectComponent('DirectLinks_DirectLinks').should('exist');
   });
 });
