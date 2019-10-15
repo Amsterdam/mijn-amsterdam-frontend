2c34dc13802c0457f1ef0e5d8f6908102086075e
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-10-15 11:30:39 +0200

    WMO Conditional feature flag

--- src/App.constants.ts
+++ src/App.constants.ts
@@ -94,7 +94,7 @@ export const ApiConfig: TypeIndex<ApiName, ApiConfig> = {
     postponeFetch: isProduction(),
   },
   WMO: {
-    postponeFetch: false,
+    postponeFetch: isProduction(),
   },
   MY_TIPS: {
     postponeFetch: true,
