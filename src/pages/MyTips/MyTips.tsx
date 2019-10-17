94fd6e3ab7ddafd6580471fcf84e5c7f44016f5f
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-10-17 16:19:11 +0200

    OptInOut modal implementation (#221)

--- src/pages/MyTips/MyTips.tsx
+++ src/pages/MyTips/MyTips.tsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import { Chapters, ChapterTitles } from 'App.constants';
+import { AppContext } from 'AppState';
+import classnames from 'classnames';
+import Alert from 'components/Alert/Alert';
+import ChapterIcon from 'components/ChapterIcon/ChapterIcon';
+import MyTips from 'components/MyTips/MyTips';
 import { OverviewPage, PageContent } from 'components/Page/Page';
 import PageHeading from 'components/PageHeading/PageHeading';
-import MyTips from 'components/MyTips/MyTips';
-import { AppContext } from 'AppState';
-import { Chapters, ChapterTitles } from 'App.constants';
-import ChapterIcon from 'components/ChapterIcon/ChapterIcon';
+import React, { useContext, useState } from 'react';
 import styles from './MyTips.module.scss';
-import Alert from 'components/Alert/Alert';
+import Modal from 'components/Modal/Modal';
 
 export default () => {
   const {
@@ -15,8 +17,13 @@ export default () => {
       isLoading: isMyTipsLoading,
       isError,
       isPristine,
+      isOptIn,
+      optIn,
+      optOut,
     },
   } = useContext(AppContext);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
     <OverviewPage className={styles.MyTips}>
@@ -25,10 +32,73 @@ export default () => {
       </PageHeading>
       <PageContent>
         <p>
-          Hier ziet u een overzicht van alle tips. U kunt ook alleen tips zien
-          die bij u passen. Om uw gegevens daarvoor te gebruiken hebben we uw
-          toestemming nodig.
+          {!isOptIn ? (
+            <>
+              U ziet nu algemene tips over voorzieningnen en activiteiten in
+              Amsterdam. Op basis van uw informatie die bij de gemeente bekend
+              is kunnen we u ook informatie tonen die beter bij uw persoonlijk
+              situatie past.
+            </>
+          ) : (
+            <>
+              U ziet nu persoonlijke tips over voorzieningen en activiteiten in
+              Amsterdam. We kunnen u ook algemene informatie tonen waarbij geen
+              gebruik gemaakt word van persoonlijke informatie.
+            </>
+          )}
+          <button
+            className={classnames('action-button', styles.OptInOutButton)}
+            onClick={() => setModalIsOpen(true)}
+          >
+            Toon {isOptIn && 'geen '}persoonlijke tips
+          </button>
         </p>
+        <Modal
+          isOpen={modalIsOpen}
+          onClose={() => setModalIsOpen(false)}
+          title="Persoonlijke tips"
+          contentWidth={620}
+        >
+          <>
+            <p className={styles.OptInOutInfo}>
+              {!isOptIn ? (
+                <>
+                  U ziet nu algemene tips over voorzieningnen en activiteiten in
+                  Amsterdam. Op basis van uw informatie die bij de gemeente
+                  bekend is kunnen we u ook informatie tonen die beter bij uw
+                  persoonlijk situatie past.
+                </>
+              ) : (
+                <>
+                  U ziet nu persoonlijke tips over voorzieningen en activiteiten
+                  in Amsterdam. We kunnen u ook algemene informatie tonen
+                  waarbij geen gebruik gemaakt word van persoonlijke informatie.
+                </>
+              )}
+            </p>
+            <p className={styles.OptInOutButtons}>
+              <button
+                className={classnames('action-button', 'secondary', 'link')}
+                onClick={() => setModalIsOpen(false)}
+              >
+                Nee bedankt
+              </button>
+              <button
+                className={classnames('action-button', 'secondary')}
+                onClick={() => {
+                  if (isOptIn) {
+                    optOut();
+                  } else {
+                    optIn();
+                  }
+                  setModalIsOpen(false);
+                }}
+              >
+                Toon {isOptIn && 'geen '}persoonlijke tips
+              </button>
+            </p>
+          </>
+        </Modal>
         {isError && (
           <Alert type="warning">
             <p>We kunnen op dit moment geen gegevens tonen.</p>
