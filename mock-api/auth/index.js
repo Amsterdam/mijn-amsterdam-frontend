308e6a02f33f7f380a18726386440f6349f803fc
Tim van Oostrom <tim@depulz.nl>
2019-10-31 17:04:18 +0100

    Correct urls
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/auth/index.js
+++ mock-api/auth/index.js
@@ -2,7 +2,7 @@ const state = require('./state');
 const ip = require('../get-ip') || 'localhost';
 const APP_HOST = process.env.APP_HOST || ip;
 const APP_PORT = process.env.APP_PORT || 3000;
-const APP_URL = `http://${APP_HOST}:${APP_PORT}`;
+const APP_URL = `//${APP_HOST}:${APP_PORT}`;
 
 module.exports = {
   path: '/api/login|logout',
