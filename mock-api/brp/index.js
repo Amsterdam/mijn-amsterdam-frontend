def8113ddff9c2e19a240eb1cf9527a6c7164ae8
Tim van Oostrom <tim@depulz.nl>
2019-11-01 12:43:42 +0100

    Disable error status
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/brp/index.js
+++ mock-api/brp/index.js
@@ -3,10 +3,10 @@ const user = require('../json/brp.json');
 module.exports = {
   path: '/api/brp/brp',
   delay: 500,
-  status: (req, res, next) => {
-    res.status(502);
-    next();
-  },
+  // status: (req, res, next) => {
+  //   res.status(502);
+  //   next();
+  // },
   template: {
     ...user,
   },
