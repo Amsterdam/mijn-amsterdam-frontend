e21ed8c63a979d980f0927d885585b629743e227
Tim van Oostrom <tim@depulz.nl>
2019-11-11 18:16:44 +0100

    Form logout tryout
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/hooks/api/session.api.hook.ts
+++ src/hooks/api/session.api.hook.ts
@@ -1,6 +1,6 @@
 import { AUTOLOGOUT_DIALOG_LAST_CHANCE_COUNTER_SECONDS } from 'components/AutoLogoutDialog/AutoLogoutDialog';
 import { getApiUrl } from 'helpers/App';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useDataApi } from './api.hook';
 import { ApiRequestOptions, ApiState } from './api.types';
 
@@ -23,6 +23,51 @@ const requestOptions: ApiRequestOptions = {
 };
 
 export type SessionApiState = Omit<ApiState, 'data'> & SessionState;
+
+function post(path: string, params: any, method = 'post') {
+  // The rest of this code assumes you are not using a library.
+  // It can be made less wordy if you use one.
+  const form = document.createElement('form');
+  form.method = method;
+  form.action = path;
+
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      const hiddenField = document.createElement('input');
+      hiddenField.type = 'hidden';
+      hiddenField.name = key;
+      hiddenField.value = params[key];
+
+      form.appendChild(hiddenField);
+    }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
+
+export function useTMALogout() {
+  const [tgtBlob, setTgtBlob] = useState('');
+  useEffect(() => {
+    if (!tgtBlob) {
+      const url = new URL(
+        'https://mijn.amsterdam.nl/api/login?aselect_credentials=5pLdp0PtVW8aS4Psyc7papbZ81stmuQi5a*GmTWWvM9Zj1lXlD*RSWFiJPx4FhzROO7yUCWzMR-qtQdTuVYA3JdKr01vxpZxsH1WLsqW24Y5WLENF159iEZx2EQEr-26nvpqBPk6kODGl-M-I0XDXxjqjM9G6VyIvxz5UMXlRU4_&rid=R2F3E2DDDAFF7E61DC0117E7A9B31926E6C19B977&a-select-server=tma.amsterdam.nl&language=nl'
+      );
+      const queryParams: any = new URLSearchParams(url.search);
+      setTgtBlob(queryParams.get('aselect_credentials'));
+    }
+  }, []);
+
+  return () => {
+    const params = {
+      request: 'logout',
+      tgt_blob: tgtBlob,
+      'a-select-server': 'tma.amsterdam.nl',
+      logoutbutton: ' Log Out ',
+    };
+    post('https://tma.amsterdam.nl/aselectserver/server', params);
+  };
+}
 
 export default function useSessionApi(
   initialData = INITIAL_SESSION_STATE
