f6dc36038cbf88bfe525dd9b12b5d2c53988523d
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-12-03 17:15:08 +0100

    Change data (#285)
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/hooks/api/api.garbage.ts
+++ src/hooks/api/api.garbage.ts
@@ -236,56 +236,56 @@ export default function useGarbageApi({
     {
       naam: 'Afvalpunt De Faas (Oost, alleen voor bewoners)',
       centroid: [4.97158187910041, 52.3710300144489] as Centroid,
-      adres: 'Faas Wilkesstraat 1201095 MD Amsterdam',
+      adres: 'Faas Wilkesstraat 120<br>1095 MD Amsterdam',
       telefoon: '020 5876145',
       email: 'afvalpunten@aebamsterdam.nl',
       openingstijden:
-        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\r\n\r\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
+        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\n\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
     },
     {
       naam: 'Afvalpunt Henk Sneevlietweg (Nieuw-West)',
       centroid: [4.83347291303415, 52.3433575455427] as Centroid,
-      adres: 'Henk Sneevlietweg 22 1066 VH  Amsterdam',
+      adres: 'Henk Sneevlietweg 22&#xA0;<br>1066 VH&#xA0;&#xA0;Amsterdam',
       telefoon: '020 587 6126',
       email: 'afvalpunten@aebamsterdam.nl',
       openingstijden:
-        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\r\n\r\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
+        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\n\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
     },
     {
       naam: 'Afvalpunt Meerkerkdreef (Zuidoost)',
       centroid: [4.97786715111811, 52.302520488967] as Centroid,
-      adres: 'Meerkerkdreef 31 1106 GZ  Amsterdam',
+      adres: 'Meerkerkdreef 31&#xA0;<br>1106 GZ &#xA0;Amsterdam',
       telefoon: '020 587 6116',
       email: 'afvalpunten@aebamsterdam.nl',
       openingstijden:
-        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\r\n\r\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
+        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\n\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
     },
     {
       naam: 'Afvalpunt Rozenburglaan (Oost)',
       centroid: [4.93782666381194, 52.3388475386366] as Centroid,
-      adres: 'Rozenburglaan 1 1097 HK  Amsterdam',
+      adres: 'Rozenburglaan 1&#xA0;<br>1097 HK &#xA0;Amsterdam',
       telefoon: '020 587 6114',
       email: 'afvalpunten@aebamsterdam.nl',
       openingstijden:
-        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\r\n\r\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
+        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\n\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
     },
     {
       naam: 'Afvalpunt Seineweg (Nieuw-West)',
       centroid: [4.82017500674289, 52.386058385908] as Centroid,
-      adres: 'Seineweg 1 1043 BE  Amsterdam',
+      adres: 'Seineweg 1&#xA0;<br>1043 BE&#xA0; Amsterdam',
       telefoon: '020 587 6144',
       email: 'afvalpunten@aebamsterdam.nl',
       openingstijden:
-        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\r\n\r\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
+        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\n\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
     },
     {
       naam: 'Afvalpunt Struisgrasstraat (Noord)',
       centroid: [4.90786592615004, 52.3957390236765] as Centroid,
-      adres: 'Struisgrasstraat 33a 1032 KE  Amsterdam',
+      adres: 'Struisgrasstraat 33a <br>1032 KE &#xA0;Amsterdam',
       telefoon: '020 587 6122',
       email: 'afvalpunten@aebamsterdam.nl',
       openingstijden:
-        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\r\n\r\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
+        'De Afvalpunten zijn open van maandag tot en met zaterdag van 08.00 tot 17.00 uur.\n\nHet Afvalpunt op de Henk Sneevlietweg 22 is elke zondag van 10.00 uur tot 16.00 uur open.',
     },
   ].map(item => {
     return {
