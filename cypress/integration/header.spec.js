9902a4221b3a400772d59eb93a4eaf67a64ef77b
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-12-05 10:32:50 +0100

    Mijn 1573 e2e tests (#283)

--- cypress/integration/header.spec.js
+++ cypress/integration/header.spec.js
@@ -16,22 +16,36 @@ describe('Header', () => {
   it('(PHONE) Should show the burger menu when on a small screen', () => {
     cy.viewport('iphone-6');
     selectComponent('MainNavBar_BurgerMenuToggleBtn').should('exist');
+    selectComponent('MainNavBar_Backdrop').should('exist');
   });
 
   it('(PHONE) Should toggle the menu overlay', () => {
     cy.viewport('iphone-6');
-    selectComponent('MainNavBarAnim-enter').should('not.exist');
+    selectComponent('MainNavBar_LinkContainer').should('exist');
+    selectComponent('MainNavBar_LinkContainer').should(
+      'have.css',
+      'left',
+      '-400px'
+    );
     selectComponent('MainNavBar_BurgerMenuToggleBtn').click();
-    selectComponent('MainNavBarAnim-enter').should('exist');
+    selectComponent('MainNavBar_LinkContainer').should(
+      'have.css',
+      'left',
+      '0px'
+    );
     selectComponent('MainNavBar_BurgerMenuToggleBtn').click();
-    selectComponent('MainNavBarAnim-enter').should('not.exist');
+    selectComponent('MainNavBar_LinkContainer').should(
+      'have.css',
+      'left',
+      '-400px'
+    );
   });
 
   it('Should toggle the tutorial', () => {
     selectComponent('Tutorial_Tutorial').should('not.exist');
     selectComponent('MainNavBar_TutorialBtn').click();
     selectComponent('Tutorial_Tutorial').should('exist');
-    selectComponent('MainNavBar_TutorialBtn').click();
+    selectComponent('Tutorial_CloseTutorial').click();
     selectComponent('Tutorial_Tutorial').should('not.exist');
   });
 });
