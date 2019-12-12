0737052e0d61f725d62d182fde153709dc3cc6a3
Tim van Oostrom <tim@depulz.nl>
2019-12-12 16:43:59 +0100

    Re-add test env
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,11 @@ import 'styles/main.scss';
 import * as Sentry from '@sentry/browser';
 
 const sentryDSN = process.env.REACT_APP_SENTRY_DSN;
+const notInExcludedEnvironment = !['development', 'test'].includes(
+  process.env.REACT_APP_BUILD_ENV!
+);
 
-if (
-  sentryDSN &&
-  process.env.REACT_APP_BUILD_ENV &&
-  !['development', 'test'].includes(process.env.REACT_APP_BUILD_ENV)
-) {
+if (sentryDSN && process.env.REACT_APP_BUILD_ENV && notInExcludedEnvironment) {
   Sentry.init({
     dsn: sentryDSN,
     environment: process.env.REACT_APP_BUILD_ENV,
