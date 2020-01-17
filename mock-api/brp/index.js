5fb2d01a2dae95f928c7a448094234b9a6c7bcb5
Tim van Oostrom <tim@depulz.nl>
2020-01-17 10:07:33 +0100

    Remove delays
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/brp/index.js
+++ mock-api/brp/index.js
@@ -2,7 +2,7 @@ const user = require('../json/brp.json');
 
 module.exports = {
   path: '/api/brp/brp',
-  delay: 3000,
+  // delay: 0,
   // status: (req, res, next) => {
   //   res.status(502);
   //   next();
