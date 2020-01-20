ba179e853ddcffa00553279fd14fc41581288aac
Tim van Oostrom <tim@depulz.nl>
2020-01-20 16:35:23 +0100

    Fix paths
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/ChapterIcon/ChapterIcon.test.tsx
+++ src/components/ChapterIcon/ChapterIcon.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ChapterIcon from './ChapterIcon';
-import { Chapters } from 'config/App.constants';
+import { Chapters } from 'config/Chapter.constants';
 import { BrowserRouter } from 'react-router-dom';
 
 it('Renders without crashing', () => {
