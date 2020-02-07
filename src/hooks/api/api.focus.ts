4b53ca103af455fc7a9f1be9a3e48ef16428a013
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2020-02-07 16:07:33 +0100

    Add inkomen specificatie hook (#339)

--- src/hooks/api/api.focus.ts
+++ src/hooks/api/api.focus.ts
@@ -10,6 +10,57 @@ import { useDataApi } from './api.hook';
 import { ApiState } from './api.types';
 import { MyNotification } from './my-notifications-api.hook';
 
+export type FocusInkomenSpecificatieType =
+  | 'IOAZ'
+  | 'BBS'
+  | 'WKO'
+  | 'IOAW'
+  | 'STIMREG'
+  | 'BIBI'
+  | 'PART'
+  | 'BBZ';
+
+export const focusInkomenSpecificatieTypes: {
+  [type in FocusInkomenSpecificatieType]: string;
+} = {
+  IOAZ: 'IOAZ',
+  BBS: 'Bijzonder bijstand en stimuleringsregelingen',
+  WKO: 'Wet kinderopvang',
+  IOAW: 'IOAW',
+  STIMREG: 'Stimuleringsregelingen',
+  BIBI: 'Bijzonder bijstand',
+  PART: 'Participatiewet',
+  BBZ: 'BBZ',
+};
+
+export interface FocusInkomenSpecificatie {
+  title: string;
+  datePublished: string;
+  id: string;
+  url: string;
+  type: FocusInkomenSpecificatieType;
+}
+
+export type FocusInkomenSpecificatiesApiState = ApiState<
+  FocusInkomenSpecificatie[]
+>;
+
+export function useFocusInkomenSpecificatiesApi(): FocusInkomenSpecificatiesApiState {
+  const [api] = useDataApi<FocusInkomenSpecificatie[]>(
+    {
+      url: getApiUrl('FOCUS_INKOMEN_SPECIFICATIES'),
+      postpone: getApiConfigValue(
+        'FOCUS_INKOMEN_SPECIFICATIES',
+        'postponeFetch',
+        false
+      ),
+    },
+    []
+  );
+
+  return api;
+}
+
 export interface FocusData {
   items: FocusItem[];
   recentCases: FocusItem[];
