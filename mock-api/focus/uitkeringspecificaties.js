7ea97abdcfaaef3d3642b0baec6a080fc608e43b
Tim van Oostrom <tim@depulz.nl>
2020-02-07 16:05:08 +0100

    Add inkomen specificatie hook
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/focus/uitkeringspecificaties.js
+++
@@ -1,9 +0,0 @@
-const uitkeringspecificaties = require('../json/uitkeringspecificaties.json');
-
-module.exports = {
-  path: '/api/focus/specificaties',
-  // delay: 11000,
-  template: (_, queryParams) => {
-    return uitkeringspecificaties;
-  },
-};
--- src/config/Api.constants.ts
+++ src/config/Api.constants.ts
@@ -18,6 +18,7 @@ export const ApiUrls: TypeIndex<ApiName, string> = {
   BRP: `${API_BASE_URL}/brp/brp`,
   WMO: `${API_BASE_URL}/wmoned/voorzieningen`,
   FOCUS: `${API_BASE_URL}/focus/aanvragen`,
+  FOCUS_INKOMEN_SPECIFICATIES: `${API_BASE_URL}/focus/inkomen-specificaties`,
   AUTH: `${API_BASE_URL}/auth/check`,
   ERFPACHT: `${API_BASE_URL}/erfpacht/check-erfpacht`,
   BAG: `${DATAPUNT_API_BASE_URL}/atlas/search/adres/`,
