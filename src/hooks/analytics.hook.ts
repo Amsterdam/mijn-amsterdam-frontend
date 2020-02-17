b058082accdff47045fba1f8d66b9626fcb42c61
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2020-02-17 16:48:09 +0100

    Disable tracking cookies (#343)
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/hooks/analytics.hook.ts
+++ src/hooks/analytics.hook.ts
@@ -14,6 +14,9 @@ const MatomoTrackerConfig = {
 // Initialize connection with analytics
 export function useAnalytics(isEnabled: boolean = true) {
   if (isEnabled && hasSiteId && !MatomoInstance) {
+    // Instruct Matomo to not use cookies for tracking
+    const win = window as any;
+    (win._paq || (win._paq = [])).push(['disableCookies']);
     MatomoInstance = new MatomoTracker(MatomoTrackerConfig);
   }
 }
