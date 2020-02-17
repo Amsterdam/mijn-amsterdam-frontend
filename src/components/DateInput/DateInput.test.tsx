bb3d8ff65d5f91e019aabf225458677cb916d0fe
Tim van Oostrom <tim@depulz.nl>
2020-02-17 10:37:17 +0100

    Update snap, test Date input
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/components/DateInput/DateInput.test.tsx
+++ src/components/DateInput/DateInput.test.tsx
@@ -3,5 +3,15 @@ import { shallow } from 'enzyme';
 import DateInput from './DateInput';
 
 it('Renders without crashing', () => {
-  shallow(<DateInput />);
+  const minDate = new Date('2019-01-01');
+  const maxDate = new Date('2020-01-01');
+  const onChange = () => void 0;
+  shallow(
+    <DateInput
+      value="2019-11-11"
+      minDate={minDate}
+      maxDate={maxDate}
+      onChange={onChange}
+    />
+  );
 });
