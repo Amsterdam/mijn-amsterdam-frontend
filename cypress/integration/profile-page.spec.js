f9da88cd49fff66ee237ce3a2c3eff34f0370c67
Tim van Oostrom <tim@depulz.nl>
2020-03-19 10:21:27 +0100

    Change chapter title
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- cypress/integration/profile-page.spec.js
+++ cypress/integration/profile-page.spec.js
@@ -5,7 +5,7 @@ describe('Profile page', () => {
 
   it('Clicking the profile link in the header shows the profile page', () => {
     selectComponent('MainNavBar_ProfileLink').click();
-    assertAtPage('Mijn gegevens', '/persoonlijke-gegevens');
+    assertAtPage('Persoonsgegevens', '/persoonlijke-gegevens');
   });
 
   it('Shows profile data', () => {
