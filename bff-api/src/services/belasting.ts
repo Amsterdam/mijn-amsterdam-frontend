d50a4d2ed12229cf67264cf60dfb9906b18e6ae7
Tim van Oostrom <tim@depulz.nl>
2020-04-02 09:45:21 +0200

    Add belasting api
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- bff-api/src/services/belasting.ts
+++ bff-api/src/services/belasting.ts
@@ -1,7 +1,57 @@
+import { AxiosResponse } from 'axios';
+import { MyNotification } from '../../../src/hooks/api/my-notifications-api.hook';
+import { ApiUrls } from '../config/app';
+import { requestSourceData } from '../helpers/requestSourceData';
+import { MyTip } from './services-tips';
+
 export interface BELASTINGData {}
 
+interface BELASTINGSourceDataContent {
+  isKnown: boolean;
+  meldingen: MyNotification[];
+  tips: MyTip[];
+}
+
+interface BELASTINGSourceData {
+  status: 'OK' | 'ERROR';
+  content?: BELASTINGSourceDataContent;
+  message?: string;
+}
+
+interface BelastingApiContent {
+  isKnown: boolean;
+  notifications: MyNotification[];
+  tips: MyTip[];
+}
+
+function formatBelastingNotifications(notifications?: MyNotification[]) {
+  return Array.isArray(notifications)
+    ? notifications.map(notification => ({
+        ...notification,
+        chapter: 'BELASTINGEN',
+      }))
+    : [];
+}
+
+function formatBELASTINGData(sourceData: AxiosResponse<BELASTINGSourceData>) {
+  const { meldingen, tips, ...restData } = sourceData.data?.content || {
+    meldingen: [],
+    tips: [],
+    isKnown: false,
+  };
+
+  // TEMP HACK! OVERWRITE PRIORITY
+  const prioritzedTips = tips.map(tip => Object.assign(tip, { priority: 100 }));
+
+  return {
+    ...restData,
+    tips: prioritzedTips,
+    notifications: formatBelastingNotifications(meldingen),
+  };
+}
+
 export function fetch() {
-  return Promise.resolve({
-    belasting: true,
-  });
+  return requestSourceData<BELASTINGSourceData>({
+    url: ApiUrls.BELASTING,
+  }).then(data => formatBELASTINGData(data));
 }
