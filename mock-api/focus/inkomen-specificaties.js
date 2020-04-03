032891c58961f2b603d2d9c332e018cb8db12bea
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2020-04-03 13:24:01 +0200

    GAT updates (#370)
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/focus/inkomen-specificaties.js
+++ mock-api/focus/inkomen-specificaties.js
@@ -4,27 +4,6 @@ module.exports = {
   path: '/api/focus/combined',
   // delay: 11000,
   template: (_, queryParams) => {
-    const jaaropgaven = [];
-    const uitkeringsspecificaties = [];
-
-    data.forEach((item, index) => {
-      if (index % 12 === 0) {
-        jaaropgaven.push({
-          ...item,
-          title: 'Jaaropgave',
-        });
-      }
-      uitkeringsspecificaties.push({
-        ...item,
-        isAnnualStatement: false,
-      });
-    });
-
-    return {
-      content: {
-        jaaropgaven,
-        uitkeringsspecificaties,
-      },
-    };
+    return data;
   },
 };
