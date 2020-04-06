01d368cc2ed5f8574c3a5707dc4f72ba9502fa3a
Tim van Oostrom <tim@depulz.nl>
2020-04-06 13:23:47 +0200

    Remove mock-api, adapt to updated gateway
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mock-api/afvalophaalgebieden/index.js
+++
@@ -1,9 +0,0 @@
-const data = require('../json/afvalophaalgebieden.json');
-
-module.exports = {
-  path: '/api/afvalophaalgebieden/search',
-  // delay: 5000,
-  template: (_, queryParams) => {
-    return data;
-  },
-};
--- mock-api/focus/inkomen-specificaties.js
+++
@@ -1,9 +0,0 @@
-const data = require('../json/inkomen-specificaties.json');
-
-module.exports = {
-  path: '/api/focus/combined',
-  // delay: 11000,
-  template: (_, queryParams) => {
-    return data;
-  },
-};
