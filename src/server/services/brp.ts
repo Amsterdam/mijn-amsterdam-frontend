a915c90077d6058585c5ca1f96d39698bbda78b4
Tim van Oostrom <tim@depulz.nl>
2020-04-16 17:35:49 +0200

    Add brp notifications
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/server/services/brp.ts
+++ src/server/services/brp.ts
@@ -1,8 +1,10 @@
-import { ApiUrls, AppRoutes, Chapter } from '../../universal/config';
+import { ApiUrls, AppRoutes, Chapter, Chapters } from '../../universal/config';
 
 import { defaultDateFormat } from '../../universal/helpers';
 import { requestData } from '../helpers';
 import { MyNotification } from '../../universal/types/App.types';
+import { differenceInCalendarDays } from 'date-fns';
+import { generatePath } from 'react-router-dom';
 
 export interface ReisDocument {
   documentNummer: string;
@@ -75,6 +77,34 @@ export interface BRPData {
   reisDocumenten?: ReisDocument[];
 }
 
+const DAYS_BEFORE_EXPIRATION = 120;
+
+const BrpDocumentTitles: Record<string, string> = {
+  paspoort: 'paspoort',
+  identiteitskaart: 'ID-kaart',
+  rijbewijs: 'rijbewijs',
+};
+
+const BrpDocumentCallToAction: Record<
+  'isExpired' | 'willExpire',
+  Record<string, string>
+> = {
+  isExpired: {
+    paspoort:
+      'https://www.amsterdam.nl/burgerzaken/paspoort-en-idkaart/paspoort-aanvragen/',
+    identiteitskaart:
+      'https://www.amsterdam.nl/burgerzaken/paspoort-en-idkaart/id-kaart-aanvragen/',
+    rijbewijs: '',
+  },
+  willExpire: {
+    paspoort:
+      'https://www.amsterdam.nl/burgerzaken/paspoort-en-idkaart/paspoort-aanvragen/',
+    identiteitskaart:
+      'https://www.amsterdam.nl/burgerzaken/paspoort-en-idkaart/id-kaart-aanvragen/',
+    rijbewijs: '',
+  },
+};
+
 export function getFullAddress(adres: Adres) {
   return `${adres.straatnaam} ${adres.huisnummer || ''} ${adres.huisletter ||
     ''} ${adres.huisnummertoevoeging || ''}`.trim();
@@ -85,6 +115,7 @@ export function getBagSearchAddress(adres: Adres) {
 }
 
 export function formatBRPNotifications(data: BRPData) {
+  const now = new Date();
   const inOnderzoek = data?.adres?.inOnderzoek || false;
   const isOnbekendWaarheen = data?.persoon?.vertrokkenOnbekendWaarheen || false;
   const dateLeft = data?.persoon?.datumVertrekUitNederland
@@ -93,10 +124,67 @@ export function formatBRPNotifications(data: BRPData) {
 
   const notifications: MyNotification[] = [];
 
+  const expiredDocuments =
+    !!data.reisDocumenten &&
+    data.reisDocumenten.filter(
+      document => new Date(document.datumAfloop) < now
+    );
+
+  const willExpireSoonDocuments =
+    !!data.reisDocumenten &&
+    data.reisDocumenten.filter(document => {
+      const days = differenceInCalendarDays(
+        new Date(document.datumAfloop),
+        now
+      );
+
+      return days <= DAYS_BEFORE_EXPIRATION && days > 0;
+    });
+
+  if (!!expiredDocuments && expiredDocuments.length) {
+    expiredDocuments.forEach(document => {
+      const docTitle = BrpDocumentTitles[document.documentType];
+      notifications.push({
+        chapter: Chapters.BURGERZAKEN,
+        datePublished: now.toISOString(),
+        hideDatePublished: true,
+        id: `${docTitle}-datum-afloop-verstreken`,
+        title: `Uw ${docTitle} is verlopen`,
+        description: `Sinds ${defaultDateFormat(
+          document.datumAfloop
+        )} is uw ${docTitle} niet meer geldig.`,
+        link: {
+          to: BrpDocumentCallToAction.isExpired[document.documentType],
+          title: `Vraag snel uw nieuwe ${docTitle} aan`,
+        },
+      });
+    });
+  }
+
+  if (!!willExpireSoonDocuments && willExpireSoonDocuments.length) {
+    willExpireSoonDocuments.forEach(document => {
+      const docTitle = BrpDocumentTitles[document.documentType];
+      notifications.push({
+        chapter: Chapters.BURGERZAKEN,
+        datePublished: now.toISOString(),
+        hideDatePublished: true,
+        id: `${document.documentType}-datum-afloop-binnekort`,
+        title: `Uw ${docTitle} verloopt binnenkort`,
+        description: `Vanaf ${defaultDateFormat(
+          document.datumAfloop
+        )} is uw ${docTitle} niet meer geldig.`,
+        link: {
+          to: BrpDocumentCallToAction.isExpired[document.documentType],
+          title: `Vraag snel uw nieuwe ${docTitle} aan`,
+        },
+      });
+    });
+  }
+
   if (inOnderzoek) {
     notifications.push({
-      chapter: 'BURGERZAKEN' as Chapter,
-      datePublished: new Date().toISOString(),
+      chapter: Chapters.BURGERZAKEN,
+      datePublished: now.toISOString(),
       id: 'brpAdresInOnderzoek',
       title: 'Adres in onderzoek',
       description:
@@ -110,8 +198,8 @@ export function formatBRPNotifications(data: BRPData) {
 
   if (isOnbekendWaarheen) {
     notifications.push({
-      chapter: 'BURGERZAKEN' as Chapter,
-      datePublished: new Date().toISOString(),
+      chapter: Chapters.BURGERZAKEN,
+      datePublished: now.toISOString(),
       id: 'brpVertrokkenOnbekendWaarheen',
       title: 'Vertrokken - onbekend waarheen',
       description: `U staat sinds ${dateLeft} in Basisregistratie Personen (BRP) geregistreerd als 'vertrokken onbekend waarheen'.`,
@@ -125,10 +213,33 @@ export function formatBRPNotifications(data: BRPData) {
   return notifications;
 }
 
+function transformBRPData(responseData: BRPData) {
+  if (responseData.reisDocumenten) {
+    Object.assign(responseData, {
+      reisDocumenten: responseData.reisDocumenten.map(document => {
+        const route = generatePath(AppRoutes.BURGERZAKEN_DOCUMENT, {
+          id: document.documentNummer,
+        });
+        return Object.assign({}, document, {
+          title: BrpDocumentTitles[document.documentType],
+          datumAfloop: defaultDateFormat(document.datumAfloop),
+          datumUitgifte: defaultDateFormat(document.datumUitgifte),
+          link: {
+            to: route,
+            title: document.documentType,
+          },
+        });
+      }),
+    });
+  }
+  return responseData;
+}
+
 export function fetchBRP(sessionID: SessionID) {
   return requestData<BRPData>(
     {
       url: ApiUrls.BRP,
+      transformResponse: transformBRPData,
     },
     sessionID
   );
