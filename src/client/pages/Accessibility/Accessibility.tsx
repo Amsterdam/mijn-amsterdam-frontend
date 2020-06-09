5b66af6fe3abaa49033730e04fc906b82998964a
Tim van Oostrom <tim@depulz.nl>
2020-06-09 16:15:45 +0200

    Add content
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/pages/Accessibility/Accessibility.tsx
+++ src/client/pages/Accessibility/Accessibility.tsx
@@ -4,6 +4,7 @@ import {
   PageHeading,
   PageContent,
   LinkdInline,
+  Heading,
 } from '../../components';
 
 export default () => {
@@ -12,13 +13,58 @@ export default () => {
       <PageHeading>Toegankelijkheidsverklaring</PageHeading>
       <PageContent>
         <p>
-          Mijn Amsterdam is toegankelijk
+          De gemeente Amsterdam wil dat iedereen Mijn Amsterdam kan gebruiken.
+          Komt u toch een pagina tegen die u niet kunt lezen of gebruiken? Meld
+          het via ons contactformulier.
+        </p>
+
+        <Heading el="h3" size="small">
+          Toegankelijk Mijn Amsterdam
+        </Heading>
+        <p>
+          Iedereen moet websites van de overheid kunnen gebruiken. Dit is wat
+          wij doen om Mijn Amsterdam toegankelijk te maken en te houden voor
+          iedereen:
+        </p>
+        <ul>
+          <li>Wij ontwikkelen Mijn Amsterdam samen met u, de Amsterdammer. </li>
+          <li>
+            We doen regelmatig gebruikersonderzoek en testen Mijn Amsterdam op
+            toegankelijkheid.
+          </li>
+          <li>We lossen knelpunten op.</li>
+          <li>
+            Onze medewerkers houden hun kennis over toegankelijkheid op peil.
+          </li>
+        </ul>
+        <Heading el="h3" size="small">
+          Onderdelen die nog niet toegankelijk zijn
+        </Heading>
+
+        <ul>
+          <li>Pdf-bestanden zijn nog niet altijd toegankelijk.</li>
+          <li>
+            De persoonlijke plattegrond ‘Mijn Buurt’ is nog niet goed
+            toegankelijk. We werken eraan om deze zo snel mogelijk aan de
+            vereisten te laten voldoen.
+          </li>
+        </ul>
+        <Heading el="h3" size="small">
+          Toegankelijkheidsverklaring
+        </Heading>
+
+        <p>
+          De eisen voor een toegankelijke website staan in het Tijdelijk besluit
+          digitale toegankelijkheid overheid. In dat besluit staat ook dat wij
+          een toegankelijkheidsverklaring op Mijn Amsterdam moeten zetten. Lees
+          hier onze volledige{' '}
           <LinkdInline
             external={true}
             href={'/toegankelijkheidsverklaring-mijn-amsterdam-20200520.html'}
           >
-            Toegankelijkheidsverklaring
+            toegankelijkheidsverklaring
           </LinkdInline>
+          .
         </p>
       </PageContent>
     </TextPage>
