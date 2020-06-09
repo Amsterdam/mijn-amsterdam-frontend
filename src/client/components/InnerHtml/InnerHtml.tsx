a192eeaecbfc1b34e8e3b00d5df0a502d3b5899e
Tim van Oostrom <tim@depulz.nl>
2020-06-09 15:38:50 +0200

    Remove obsolete config
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/components/InnerHtml/InnerHtml.tsx
+++ src/client/components/InnerHtml/InnerHtml.tsx
@@ -1,18 +1,4 @@
-// import sanitizeHtml from 'sanitize-html';
 import React, { memo } from 'react';
-
-function sanitizeHtml(items: any, config: any) {
-  return items;
-}
-
-const TAGS_ALLOWED = ['a', 'p', 'br', 'strong', 'em', 'i', 'b', 'div', 'u'];
-const ATTR_ALLOWED = {
-  a: ['href', 'name', 'target', 'rel'],
-};
-const DEFAULT_CONFIG = {
-  allowedSchemes: ['https'],
-  disallowedTagsMode: 'escape',
-};
 
 interface InnerHtmlTagProps {
   wrapWithTagName?: keyof JSX.IntrinsicElements;
@@ -25,20 +11,11 @@ interface InnerHtmlTagProps {
 function InnerHtmlTag({
   wrapWithTagName = 'div',
   children,
-  allowedTags = TAGS_ALLOWED,
-  allowedAttributes = ATTR_ALLOWED,
   className,
 }: InnerHtmlTagProps) {
-  const config = Object.assign(DEFAULT_CONFIG, {
-    allowedTags,
-    allowedAttributes,
-  });
   const Tag = wrapWithTagName;
   return (
-    <Tag
-      className={className}
-      dangerouslySetInnerHTML={{ __html: sanitizeHtml(children, config) }}
-    />
+    <Tag className={className} dangerouslySetInnerHTML={{ __html: children }} />
   );
 }
 
