a7b0b55cf06b95acaddd1c041d8b32d6cb8075ba
Tim van Oostrom <tim@depulz.nl>
2020-06-12 10:44:21 +0200

    Add icon, add sample data
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/pages/Vergunningen/Vergunningen.tsx
+++ src/client/pages/Vergunningen/Vergunningen.tsx
@@ -1,15 +1,18 @@
-import { Page, PageContent, PageHeading } from '../../components';
-
 import React from 'react';
+import { ChapterTitles } from '../../../universal/config/index';
+import { ChapterIcon, PageContent, PageHeading } from '../../components';
+import { OverviewPage } from '../../components/Page/Page';
 import styles from './Vergunningen.module.scss';
 
 export default () => {
   return (
-    <Page className={styles.Vergunningen}>
-      <PageHeading>Vergunningen</PageHeading>
+    <OverviewPage className={styles.Vergunningen}>
+      <PageHeading icon={<ChapterIcon />}>
+        {ChapterTitles.VERGUNNINGEN}
+      </PageHeading>
       <PageContent>
         <p>Vergunningen body</p>
       </PageContent>
-    </Page>
+    </OverviewPage>
   );
 };
