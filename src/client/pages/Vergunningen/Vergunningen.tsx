7f34095b5b006141587f910397f0114ba22ca118
Tim van Oostrom <tim@depulz.nl>
2020-06-12 10:56:19 +0200

    Add text and error
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/pages/Vergunningen/Vergunningen.tsx
+++ src/client/pages/Vergunningen/Vergunningen.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ChapterTitles } from '../../../universal/config/index';
-import { ChapterIcon, PageContent, PageHeading } from '../../components';
+import { isError } from '../../../universal/helpers';
+import { AppContext } from '../../AppState';
+import {
+  Alert,
+  ChapterIcon,
+  Linkd,
+  PageContent,
+  PageHeading,
+} from '../../components';
 import { OverviewPage } from '../../components/Page/Page';
 import styles from './Vergunningen.module.scss';
-import { Colors } from '../../config/app';
 
 export default () => {
+  const { VERGUNNINGEN } = useContext(AppContext);
+
   return (
     <OverviewPage className={styles.Vergunningen}>
       <PageHeading icon={<ChapterIcon />}>
         {ChapterTitles.VERGUNNINGEN}
       </PageHeading>
       <PageContent>
-        <p>Vergunningen body</p>
+        <p>
+          Wilt u een evenement organiseren op straat of in een gebouw? Dan hebt
+          u meestal een evenementenvergunning nodig, maar soms is een melding
+          voldoende. Kijk hier hoe het werkt.
+        </p>
+        <p>
+          <Linkd external={true} href="https://mijn.amsterdam.nl/">
+            Meer informatie op amsterdam.nl
+          </Linkd>
+        </p>
+        {isError(VERGUNNINGEN) && (
+          <Alert type="warning">
+            <p>We kunnen op dit moment geen gegevens tonen.</p>
+          </Alert>
+        )}
       </PageContent>
     </OverviewPage>
   );
