90b5f4df72f237135597b51c705a0753f0b5cf81
Tim van Oostrom <tim@depulz.nl>
2020-06-16 14:40:38 +0200

    Change zoom levels
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/universal/config/map.ts
+++ src/universal/config/map.ts
@@ -6,5 +6,5 @@ export const MAP_URL = process.env.REACT_APP_EMBED_MAP_URL;
 export const LAYERS_CONFIG =
   'lagen=gembek-overig%3A1%7Cgembek-verreg%3A1%7Cgembek-verbes%3A1%7Cgembek-terras%3A1%7Cgembek-splits%3A1%7Cgembek-speela%3A1%7Cgembek-rectif%3A1%7Cgembek-optijd%3A1%7Cgembek-onttre%3A1%7Cgembek-omgver%3A1%7Cgembek-meldin%3A1%7Cgembek-medede%3A1%7Cgembek-ligpla%3A1%7Cgembek-kapver%3A1%7Cgembek-inspra%3A1%7Cgembek-exploi%3A1%7Cgembek-evever%3A1%7Cgembek-drahor%3A1%7Cgembek-bespla%3A1%7Cafvlc-wlokca%3A1%7Cafvlc-wlotxtl%3A1%7Cafvlc-wlopls%3A1%7Cafvlc-wlogls%3A1%7Cafvlc-wloppr%3A1%7Cafvlc-wlorst%3A1%7Cevnmt-tcevt%3A1%7Cparkrn-pvrgeb%3A0';
 
-export const DEFAULT_ZOOM = 8;
-export const LOCATION_ZOOM = 13;
+export const DEFAULT_ZOOM = 12;
+export const LOCATION_ZOOM = 15;
