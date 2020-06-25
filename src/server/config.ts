f048940ea8c55aa105b19fd8bbf71e6487f5826f
Tim van Oostrom <tim@depulz.nl>
2020-06-25 11:40:14 +0200

    Change TTL
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/server/config.ts
+++ src/server/config.ts
@@ -37,8 +37,8 @@ const ONE_SECOND_MS = 1000;
 const ONE_MINUTE_MS = 60 * ONE_SECOND_MS;
 const ONE_HOUR_MS = 60 * ONE_MINUTE_MS;
 
-export const DEFAULT_API_CACHE_TTL_MS = 30 * ONE_SECOND_MS; // 30 seconds
-export const DEFAULT_CANCEL_TIMEOUT_MS = 20 * ONE_SECOND_MS; // 20 seconds
+export const DEFAULT_API_CACHE_TTL_MS = 30 * ONE_SECOND_MS;
+export const DEFAULT_CANCEL_TIMEOUT_MS = 10 * ONE_SECOND_MS;
 
 export const DEFAULT_REQUEST_CONFIG: DataRequestConfig = {
   cancelTimeout: DEFAULT_CANCEL_TIMEOUT_MS,
