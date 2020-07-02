d76719554293c37c148f7e5402876e4b46e3717c
Tim van Oostrom <tim@depulz.nl>
2020-07-02 09:50:21 +0200

    Move var
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/hooks/useAppState.ts
+++ src/client/hooks/useAppState.ts
@@ -16,14 +16,13 @@ const sauronRequestOptions = {
   ],
 };
 
-const hasEventSourceSupport = !('EventSource' in window); // IE11 and early edge versions don't have EventSource support. These browsers will use the the Sauron endpoint.
-
 /**
  * The primary communication is the EventSource. In the case the EventSource can't connect to the server, a number of retries will take place.
  * If the EventSource fails the Sauron endpoint /all will be used in a last attempt to fetch the data needed to display a fruity application.
  * If that fails we just can't connect to the server for whatever reason. Sentry error handling might have information about this scenario.
  */
 export function useAppState() {
+  const hasEventSourceSupport = !('EventSource' in window); // IE11 and early edge versions don't have EventSource support. These browsers will use the the Sauron endpoint.
   const { TIPS, fetch: fetchTips } = useTipsApi();
   const [isTheOneEndpoint, setSauronFallback] = useState(hasEventSourceSupport);
   const [isDataRequested, setIsDateRequested] = useState(false);
