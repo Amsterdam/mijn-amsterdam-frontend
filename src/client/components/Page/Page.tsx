e166b235a8c6640766cd8a34d96e738be2644421
Tim van Oostrom <tim@depulz.nl>
2020-07-27 10:07:23 +0200

    Remove antipattern
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/components/Page/Page.tsx
+++ src/client/components/Page/Page.tsx
@@ -9,63 +9,39 @@ export interface PageProps extends HTMLProps<HTMLDivElement> {
   children: ComponentChildren;
 }
 
-export default function Page({
-  className,
-  children,
-  ...otherProps
-}: PageProps) {
+export default function Page({ className, children }: PageProps) {
   const classNames = composeClassNames(styles.Page, className);
 
-  return (
-    <main {...otherProps} className={classNames}>
-      {children}
-    </main>
-  );
+  return <main className={classNames}>{children}</main>;
 }
 
-export function TextPage({ children, className, ...otherProps }: PageProps) {
+export function TextPage({ children, className }: PageProps) {
   return (
-    <Page
-      {...otherProps}
-      className={composeClassNames(styles.TextPage, className)}
-    >
+    <Page className={composeClassNames(styles.TextPage, className)}>
       {children}
     </Page>
   );
 }
 
-export function OverviewPage({
-  children,
-  className,
-  ...otherProps
-}: PageProps) {
+export function OverviewPage({ children, className }: PageProps) {
   return (
-    <Page
-      {...otherProps}
-      className={composeClassNames(styles.OverviewPage, className)}
-    >
+    <Page className={composeClassNames(styles.OverviewPage, className)}>
       {children}
     </Page>
   );
 }
 
-export function DetailPage({ children, className, ...otherProps }: PageProps) {
+export function DetailPage({ children, className }: PageProps) {
   return (
-    <Page
-      {...otherProps}
-      className={composeClassNames(styles.DetailPage, className)}
-    >
+    <Page className={composeClassNames(styles.DetailPage, className)}>
       {children}
     </Page>
   );
 }
 
-export function PageContent({ children, className, ...otherProps }: PageProps) {
+export function PageContent({ children, className }: PageProps) {
   return (
-    <div
-      {...otherProps}
-      className={composeClassNames(styles.PageContent, className)}
-    >
+    <div className={composeClassNames(styles.PageContent, className)}>
       {children}
     </div>
   );
