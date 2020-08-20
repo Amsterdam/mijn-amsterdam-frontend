a96f2e9d0d1b5af42faf8ff4293cf7c21849e1c9
Tim van Oostrom <tim@depulz.nl>
2020-08-20 09:09:49 +0200

    Add aanvraag to beginning
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/server/services/focus/focus-tozo-helpers.ts
+++ src/server/services/focus/focus-tozo-helpers.ts
@@ -223,12 +223,12 @@ export function createTozoResult(
   const tozo2Steps = otherSteps.filter(step => step.product === 'Tozo 2');
 
   if (aanvraagSteps['Tozo 1']) {
-    tozo1Steps.push(aanvraagSteps['Tozo 1']);
+    tozo1Steps.unshift(aanvraagSteps['Tozo 1']);
   }
   const tozo1Item = tozo1Steps.length && createTozoItem('Tozo 1', tozo1Steps);
 
   if (aanvraagSteps['Tozo 2']) {
-    tozo2Steps.push(aanvraagSteps['Tozo 2']);
+    tozo2Steps.unshift(aanvraagSteps['Tozo 2']);
   }
   const tozo2Item = tozo2Steps.length && createTozoItem('Tozo 2', tozo2Steps);
 
