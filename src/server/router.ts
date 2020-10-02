1fe2123eb75cd211a03c25dd080f60375668b9bf
Tim van Oostrom <tim@depulz.nl>
2020-10-02 16:04:22 +0200

    Add router
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/server/router.ts
+++ src/server/router.ts
@@ -1,6 +1,10 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { BffEndpoints } from './config';
 import {
+  loadServicesMapDatasetItem,
+  loadServicesMapDatasets,
+} from './services';
+import {
   loadServicesAll,
   loadServicesSSE,
   loadServicesTips,
@@ -18,6 +22,37 @@ router.get(
 );
 router.get(BffEndpoints.SERVICES_STREAM, loadServicesSSE);
 router.get(BffEndpoints.SERVICES_TIPS, loadServicesTips);
+
+router.get(
+  BffEndpoints.MAP_DATASETS,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const datasets = await loadServicesMapDatasets(res.locals.sessionID);
+      res.json(datasets);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get(
+  BffEndpoints.MAP_DATASETS_ITEM,
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log('params', req.params);
+    try {
+      const datasetItem = await loadServicesMapDatasetItem(
+        res.locals.sessionID,
+        req.params.dataset,
+        req.params.id
+      );
+      res.json(datasetItem);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.get(
   BffEndpoints.HEALTH,
