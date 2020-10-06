f05f5a7f1b2d34d721f9d3d5a7d679407d300791
Tim van Oostrom <tim@depulz.nl>
2020-10-06 12:49:17 +0200

    Fix exitEarly
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/server/helpers/app.ts
+++ src/server/helpers/app.ts
@@ -1,11 +1,11 @@
 import * as Sentry from '@sentry/node';
 import { NextFunction, Request, Response } from 'express';
 import npath from 'path';
+import { matchPath } from 'react-router-dom';
 import uid from 'uid-safe';
 import { IS_AP } from '../../universal/config';
 import {
   BffEndpoints,
-  BffProfileTypePathSegment,
   BFF_BASE_PATH,
   DEV_USER_TYPE_HEADER,
   PUBLIC_BFF_ENDPOINTS,
@@ -14,17 +14,11 @@ import {
 import { clearSessionCache } from './source-api-request';
 
 export function isValidRequestPath(requestPath: string, path: string) {
-  return (
-    requestPath === npath.join(BFF_BASE_PATH, path) ||
-    requestPath ===
-      npath.join(
-        BFF_BASE_PATH,
-        BffProfileTypePathSegment.privateCommercial,
-        path
-      ) ||
-    requestPath ===
-      npath.join(BFF_BASE_PATH, BffProfileTypePathSegment.commercial, path)
-  );
+  const isRouteMatch = !!matchPath(requestPath, {
+    path: npath.join(BFF_BASE_PATH, path),
+    exact: true,
+  });
+  return isRouteMatch;
 }
 
 export function isBffEndpoint(requestPath: string) {
