cb03b3fef8ed2e84e399e9ec310a6a0ce84dfcae
Tim van Oostrom <tim@depulz.nl>
2020-10-08 10:54:19 +0200

    Update page titles
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/config/pages.ts
+++ src/client/config/pages.ts
@@ -6,14 +6,28 @@ export const PageTitleMain = 'Mijn Amsterdam';
 // Used in <html><head><title>{PageTitle}</title></head>
 export const PageTitles = {
   [AppRoutes.ROOT]: 'Home | Dashboard',
-  [AppRoutes.BURGERZAKEN]: ChapterTitles.BURGERZAKEN,
+  [AppRoutes.BURGERZAKEN]: `${ChapterTitles.BURGERZAKEN} overzicht`,
+  [AppRoutes.BURGERZAKEN_DOCUMENT]: `Document | ${ChapterTitles.BURGERZAKEN}`,
   [AppRoutes.ZORG]: `${ChapterTitles.ZORG} overzicht`,
   [AppRoutes['ZORG/VOORZIENINGEN']]: `Voorziening | ${ChapterTitles.ZORG}`,
   [AppRoutes.INKOMEN]: `${ChapterTitles.INKOMEN} | overzicht`,
-  [AppRoutes['INKOMEN/BIJSTANDSUITKERING']]: `Bijstandsuitkering`,
+  [AppRoutes[
+    'INKOMEN/BIJSTANDSUITKERING'
+  ]]: `Bijstandsuitkering | ${ChapterTitles.INKOMEN}`,
   [AppRoutes['INKOMEN/STADSPAS']]: `Stadspas | ${ChapterTitles.INKOMEN}`,
   [AppRoutes['INKOMEN/TOZO']]: `Tozo | ${ChapterTitles.INKOMEN}`,
-  [AppRoutes.BRP]: `Profiel`,
+  [AppRoutes[
+    'INKOMEN/SPECIFICATIES'
+  ]]: `Uitkeringsspecificaties | ${ChapterTitles.INKOMEN}`,
+  [`${AppRoutes['INKOMEN/SPECIFICATIES']}/jaaropgaven`]: `Jaaropgaven | ${ChapterTitles.INKOMEN}`,
+  [AppRoutes.BRP]: `Mijn gegevens`,
+  [AppRoutes.ACCESSIBILITY]: `Toegankelijkheidsverklaring`,
+  [AppRoutes.GENERAL_INFO]: `Uitleg`,
+  [AppRoutes.VERGUNNINGEN]: `${ChapterTitles.VERGUNNINGEN} overzicht`,
+  [AppRoutes[
+    'VERGUNNINGEN/DETAIL'
+  ]]: `Vergunning | ${ChapterTitles.VERGUNNINGEN}`,
+  [AppRoutes.KVK]: `Mijn onderneming`,
   [AppRoutes.BUURT]: `Mijn buurt`,
   [AppRoutes.TIPS]: `Mijn Tips | overzicht`,
   [AppRoutes.NOTIFICATIONS]: `${ChapterTitles.NOTIFICATIONS} | overzicht`,
