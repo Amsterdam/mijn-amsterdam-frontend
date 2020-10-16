7307f60b2a2f3aec8efbc5fe9fdc264918be4564
Tim van Oostrom <tim@depulz.nl>
2020-10-16 14:51:40 +0200

    Update snaps
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/hooks/useNotifications.ts
+++ src/client/hooks/useNotifications.ts
@@ -2,24 +2,25 @@ import { selectorFamily, useRecoilValue } from 'recoil';
 import { Chapters } from '../../universal/config';
 import { appStateAtom } from './useAppState';
 import { useProfileTypeValue } from './useProfileType';
+import { WelcomeNotification } from '../config/staticData';
 
 const appStateNotificationsSelector = selectorFamily({
   key: 'appStateNotifications',
   get: (profileType: ProfileType) => ({ get }) => {
     const appState = get(appStateAtom);
-
+    let notifications = appState.NOTIFICATIONS.content || [];
     if (
       profileType === 'private-commercial' &&
       appState.NOTIFICATIONS.content
     ) {
-      return appState.NOTIFICATIONS.content.filter(
+      notifications = appState.NOTIFICATIONS.content.filter(
         notification =>
           notification.chapter !== Chapters.BRP &&
           notification.chapter !== Chapters.BURGERZAKEN
       );
     }
 
-    return appState.NOTIFICATIONS.content || [];
+    return [...notifications, WelcomeNotification];
   },
 });
 
