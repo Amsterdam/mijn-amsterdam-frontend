18a0e6a3fa5cabc1b29bee0ef6b50ca26894422d
Tim van Oostrom <tim@depulz.nl>
2020-10-22 11:11:02 +0200

    Add download error message
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/components/DocumentList/DocumentList.tsx
+++ src/client/components/DocumentList/DocumentList.tsx
@@ -1,9 +1,10 @@
 import * as Sentry from '@sentry/browser';
 import classnames from 'classnames';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { GenericDocument } from '../../../universal/types/App.types';
-import { IconDownload } from '../../assets/icons';
+import { IconAlert, IconDownload } from '../../assets/icons';
 import { trackPageView } from '../../hooks/analytics.hook';
+import Alert from '../Alert/Alert';
 import Linkd from '../Button/Button';
 import styles from './DocumentList.module.scss';
 
@@ -40,39 +41,58 @@ function addFileType(url: string, type: string = '') {
 }
 
 export function DocumentLink({ document, label }: DocumentLinkProps) {
-  return (
-    <Linkd
-      className={styles.DocumentLink}
-      icon={IconDownload}
-      href={document.url}
-      onClick={event => {
-        event.preventDefault();
-        fetch(document.url).then(response => {
-          const trackingUrl =
-            window.location.pathname +
-            addFileType(
-              `/downloads/${document.download || document.title}`,
-              document.type
-            );
+  const [isErrorVisible, setErrorVisible] = useState(false);
 
-          if (response.status !== 200) {
-            Sentry.captureException('Could not download document', {
-              extra: {
-                title: document.title,
-                url: document.url,
-              },
-            });
-          } else {
-            // Tracking pageview here because trackDownload doesn't work properly in Matomo
-            trackPageView(document.title, trackingUrl);
-            downloadFile(document);
-          }
-        });
-        return false;
-      }}
-    >
-      {label || document.title}
-    </Linkd>
+  const onClickDocumentLink = useCallback(
+    (event) => {
+      event.preventDefault();
+      // First check to see if the request will succeed or not.
+      fetch(document.url).then((response) => {
+        const trackingUrl =
+          window.location.pathname +
+          addFileType(
+            `/downloads/${document.download || document.title}`,
+            document.type
+          );
+
+        // Show error if request fails
+        if (response.status !== 200) {
+          Sentry.captureException('Could not download document', {
+            extra: {
+              title: document.title,
+              url: document.url,
+            },
+          });
+          setErrorVisible(true);
+        } else {
+          // Tracking pageview here because trackDownload doesn't work properly in Matomo.
+          trackPageView(document.title, trackingUrl);
+          // Make the file download available to the browser.
+          downloadFile(document);
+        }
+      });
+      return false;
+    },
+    [document]
+  );
+
+  return (
+    <>
+      <Linkd
+        className={classnames(
+          styles.DocumentLink,
+          isErrorVisible && styles.DocumentLinkError
+        )}
+        icon={isErrorVisible ? IconAlert : IconDownload}
+        href={document.url}
+        onClick={onClickDocumentLink}
+      >
+        {label || document.title}
+      </Linkd>
+      {isErrorVisible && (
+        <span className={styles.DownloadError}>Download niet beschikbaar</span>
+      )}
+    </>
   );
 }
 
@@ -87,7 +107,7 @@ export default function DocumentList({
         isExpandedView && styles[`DocumentList--expandedView`]
       )}
     >
-      {documents.map(document => (
+      {documents.map((document) => (
         <li className={styles.DocumentListItem} key={document.id}>
           {isExpandedView ? (
             <>
