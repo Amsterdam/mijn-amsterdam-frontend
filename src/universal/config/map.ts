cff08ca1311dc99f7f47b073ea54f852ee099cdd
Tim van Oostrom <tim@depulz.nl>
2020-10-23 16:07:26 +0200

    Change zoom levels
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/universal/config/map.ts
+++ src/universal/config/map.ts
@@ -11,8 +11,8 @@ export const CITY_LAYERS_CONFIG =
   'lagen=gembek-overig%3A0|gembek-verreg%3A0|gembek-verbes%3A0|gembek-terras%3A0|gembek-splits%3A0|gembek-speela%3A0|gembek-rectif%3A0|gembek-optijd%3A0|gembek-onttre%3A0|gembek-omgver%3A0|gembek-meldin%3A0|gembek-medede%3A0|gembek-ligpla%3A0|gembek-kapver%3A0|gembek-inspra%3A0|gembek-exploi%3A0|gembek-evever%3A0|gembek-drahor%3A0|gembek-bespla%3A0|afvlc-wlokca%3A0|afvlc-wlotxtl%3A0|afvlc-wlopls%3A0|afvlc-wlogls%3A0|afvlc-wloppr%3A0|afvlc-wlorst%3A0|evnmt-tcevt%3A0|parkrn-pvrgeb%3A0';
 
 export const CITY_ZOOM = 8;
-export const HOOD_ZOOM = 13;
-export const LOCATION_ZOOM = 15;
+export const HOOD_ZOOM = 12;
+export const LOCATION_ZOOM = 16;
 
 export const projDefinition = `+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.4171,50.3319,465.5524,-0.398957,0.343988,-1.87740,4.0725 +units=m +no_defs`;
 export const proj4RD = proj4('WGS84', projDefinition);
