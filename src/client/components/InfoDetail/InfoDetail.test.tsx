9773d85d719ef0d479c4ead67fb61385aa5802a8
Tim van Oostrom <tim@depulz.nl>
2021-01-14 10:02:18 +0100

    Add InfoDetail test
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/components/InfoDetail/InfoDetail.test.tsx
+++ src/client/components/InfoDetail/InfoDetail.test.tsx
@@ -1,7 +1,12 @@
 import InfoDetail from './InfoDetail';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
 it('Renders without crashing', () => {
-  shallow(<InfoDetail />);
+  const { container } = render(
+    <InfoDetail label="Foo" value="Bar" el="article" />
+  );
+  expect(screen.getByText('Foo')).toBeInTheDocument();
+  expect(screen.getByText('Bar')).toBeInTheDocument();
+  expect((container.firstChild as any).tagName).toBe('ARTICLE');
 });
