0c7da3ee646fd7a2d6c9a3fb1029baeda7c8fab8
Tim van Oostrom <tim@depulz.nl>
2021-01-14 13:14:50 +0100

    Add MyCases test
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/components/MyCases/MyCases.test.tsx
+++ src/client/components/MyCases/MyCases.test.tsx
@@ -1,7 +1,48 @@
+import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 import MyCases from './MyCases';
+const ITEMS = [
+  {
+    id: 'test-id',
+    datePublished: '2020-10-10',
+    title: 'My test case',
+    chapter: 'INKOMEN',
+    link: {
+      to: '/',
+      title: 'Link title',
+    },
+  },
+];
 
-it('Renders without crashing', () => {
-  shallow(<MyCases title="testje" items={[]} isLoading={false} />);
+describe('<MyCases/>', () => {
+  it('Shows cases when provided', () => {
+    render(
+      <MemoryRouter>
+        <MyCases title="testje" items={ITEMS} isLoading={false} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('testje')).toBeInTheDocument();
+    expect(screen.getByText('My test case')).toBeInTheDocument();
+
+    const { container } = render(
+      <MemoryRouter>
+        <MyCases title="testje" items={ITEMS} isLoading={true} />
+      </MemoryRouter>
+    );
+
+    expect(
+      container.querySelector('[class*="LoadingContent"]')
+    ).toBeInTheDocument();
+  });
+  it('Shows a fallback text when no cases provided', () => {
+    render(
+      <MemoryRouter>
+        <MyCases title="testje" items={[]} isLoading={false} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText(/geen aanvragen/)).toBeInTheDocument();
+  });
 });
