9465dce2866b2e2ab51dc6a5004ea87293b34982
Tim van Oostrom <tim@depulz.nl>
2021-01-14 14:49:54 +0100

    Add alphapage test
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/pages/AlphaPage/AlphaPage.test.tsx
+++ src/client/pages/AlphaPage/AlphaPage.test.tsx
@@ -1,39 +1,11 @@
-import { shallow, mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { generatePath } from 'react-router-dom';
-import { MutableSnapshot } from 'recoil';
-import { AppRoutes } from '../../../universal/config/routing';
-import { appStateAtom } from '../../hooks/useAppState';
-import MockApp from '../MockApp';
 import AlphaPage from './AlphaPage';
 
-const testState = {
-  STATE_SLICE1: { status: 'OK', content: null },
-};
-
-function initializeState(snapshot: MutableSnapshot) {
-  snapshot.set(appStateAtom, testState);
-}
-
 describe('<AlphaPage />', () => {
-  const routeEntry = generatePath(AppRoutes.ROOT);
-  const routePath = AppRoutes.ROOT;
-
-  const Component = () => (
-    <MockApp
-      routeEntry={routeEntry}
-      routePath={routePath}
-      component={AlphaPage}
-      initializeState={initializeState}
-    />
-  );
-
   it('Renders without crashing', () => {
-    shallow(<Component />);
-  });
-
-  it('Matches the Full Page snapshot', () => {
-    const html = mount(<Component />).html();
-    expect(html).toMatchSnapshot();
+    render(<AlphaPage />);
+    expect(screen.getByText('AlphaPage title')).toBeInTheDocument();
+    expect(screen.getByText('AlphaPage body')).toBeInTheDocument();
   });
 });
--- src/client/pages/AlphaPage/AlphaPage.tsx
+++ src/client/pages/AlphaPage/AlphaPage.tsx
@@ -8,7 +8,7 @@ export default function AlphaPage() {
   // const { STATE_SLICE } = useAppStateGetter();
   return (
     <Page className={styles.AlphaPage}>
-      <PageHeading>AlphaPage</PageHeading>
+      <PageHeading>AlphaPage title</PageHeading>
       <PageContent>
         <p>AlphaPage body</p>
       </PageContent>
