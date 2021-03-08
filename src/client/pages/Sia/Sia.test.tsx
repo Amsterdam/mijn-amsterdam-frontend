6505a5fd07d26e7da6e035dc02b16aa222571a3c
Tim van Oostrom <tim@depulz.nl>
2021-03-08 09:39:21 +0100

    Add SIA tests, update snaps
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- src/client/pages/Sia/Sia.test.tsx
+++ src/client/pages/Sia/Sia.test.tsx
@@ -1,11 +1,36 @@
 import { render, screen } from '@testing-library/react';
-
+import { generatePath } from 'react-router-dom';
+import { MutableSnapshot } from 'recoil';
+import { AppRoutes } from '../../../universal/config';
+import { appStateAtom } from '../../hooks/useAppState';
+import MockApp from '../MockApp';
 import Sia from './Sia';
 
+const testState: any = {
+  SIA: {
+    content: [],
+  },
+};
+
+function initializeState(snapshot: MutableSnapshot) {
+  snapshot.set(appStateAtom, testState);
+}
+
 describe('<Sia />', () => {
+  const routeEntry = generatePath(AppRoutes.SIA);
+  const routePath = AppRoutes.SIA;
+
+  const Component = () => (
+    <MockApp
+      routeEntry={routeEntry}
+      routePath={routePath}
+      component={Sia}
+      initializeState={initializeState}
+    />
+  );
   it('Renders without crashing', () => {
-    render(<Sia />);
-    expect(screen.getByText('Sia title')).toBeInTheDocument();
-    expect(screen.getByText('Sia body')).toBeInTheDocument();
+    render(<Component />);
+    expect(screen.getByText('Meldingen')).toBeInTheDocument();
+    // expect(screen.getByText('Sia body')).toBeInTheDocument();
   });
 });
