d3e2d02772a2d13f23b224953c5fecea8a2b07b2
MokumJ <33216498+MokumJ@users.noreply.github.com>
2021-06-23 15:35:39 +0200

    update teksten conform design

--- src/server/services/vergunningen.ts
+++ src/server/services/vergunningen.ts
@@ -288,44 +288,43 @@ export function createVergunningNotification(
     );
     const GPKForm =
       'https://formulieren.amsterdam.nl/TripleForms/DirectRegelen/formulier/nl-NL/evAmsterdam/GehandicaptenParkeerKaartAanvraag.aspx/Inleiding';
+    const fullName = 'Europese gehandicaptenparkeerkaart (GPK)'; // change this later to title property
     switch (true) {
       case item.decision === 'Verleend' &&
         isNearEndDate(item) &&
         !hasOtherValidVergunningOfSameType(allGPKItems, item):
-        title = `Uw vergunning loopt af`;
-        description = `Uw vergunning ${item.caseType} loopt binnenkort af. Vraag tijdig een nieuwe vergunning aan.`;
-        cta = `Vergunning aanvragen`;
+        title = `${item.caseType} loopt af`;
+        description = `Uw ${fullName} loopt binnenkort af. Vraag tijdig een nieuwe vergunning aan.`;
+        cta = `Vraag op tijd een nieuwe ${item.caseType} aan`;
         linkTo = GPKForm;
         datePublished = format(new Date(), 'yyyy-MM-dd');
         break;
       case item.decision === 'Verleend' &&
         isExpired(item) &&
         !hasOtherValidVergunningOfSameType(allGPKItems, item):
-        title = `Uw vergunning is verlopen`;
-        description = `Uw vergunning ${item.caseType} is verlopen. U kunt een nieuwe vergunning aanvragen.`;
-        cta = 'Vergunning aanvragen';
+        title = `${item.caseType} is verlopen`;
+        description = `Uw ${fullName} is verlopen.`;
+        cta = `Vraag een nieuwe ${item.caseType} aan`;
         linkTo = GPKForm;
         datePublished = format(new Date(), 'yyyy-MM-dd');
         break;
       case item.status !== 'Afgehandeld':
-        description = `Uw vergunningsaanvraag ${item.caseType} is in behandeling`;
+        description = `Uw aanvraag voor een ${fullName} is in behandeling`;
         break;
       case item.status === 'Afgehandeld':
-        description = `Uw vergunningsaanvraag ${item.caseType} is afgehandeld`;
+        description = `Uw aanvraag voor een ${fullName} is afgehandeld`;
         break;
     }
   } else {
     switch (true) {
       case item.status !== 'Afgehandeld':
+        title = `${item.caseType} in behandeling`;
         description = `Uw vergunningsaanvraag ${item.caseType} is in behandeling`;
         break;
       case item.status === 'Afgehandeld':
+        title = `${item.caseType} fgehandeld`;
         description = `Uw vergunningsaanvraag ${item.caseType} is afgehandeld`;
         break;
-      // case new Date() >= dateEnd:
-      //   title = 'Uw vergunning is verlopen';
-      //   description = `Uw vergunningsaanvraag ${item.caseType} is afgehandeld`;
-      //   break;
     }
   }
 
