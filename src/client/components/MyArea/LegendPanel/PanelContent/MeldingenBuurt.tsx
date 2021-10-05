a60554ae30eda0ceaa38e1438d8eb3fa27ae589c
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-10-05 17:33:35 +0200

    Change category title

--- src/client/components/MyArea/LegendPanel/PanelContent/MeldingenBuurt.tsx
+++ src/client/components/MyArea/LegendPanel/PanelContent/MeldingenBuurt.tsx
@@ -1,10 +1,21 @@
 import { InfoDetail } from '../../..';
 import GenericBase from './GenericBase';
 import { dateTimeFormatYear } from '../../../../../universal/helpers/date';
+import { DATASETS } from '../../../../../universal/config/buurt';
+import { capitalizeFirstLetter } from '../../../../../universal/helpers';
 
 interface MyArePanelContentMeldingenBuurtProps {
   panelItem: any;
   datasetId: string;
+}
+
+function displayCategoryTitle(category: string) {
+  const displayCategory = capitalizeFirstLetter(category);
+  const config =
+    DATASETS.meldingenBuurt.datasets.meldingenBuurt?.filters?.categorie
+      ?.valueConfig?.[displayCategory];
+
+  return config?.title ?? displayCategory;
 }
 
 export default function MyArePanelContentMeldingenBuurt({
@@ -12,7 +23,10 @@ export default function MyArePanelContentMeldingenBuurt({
   panelItem,
 }: MyArePanelContentMeldingenBuurtProps) {
   return (
-    <GenericBase title={panelItem.categorie} supTitle="Meldingen">
+    <GenericBase
+      title={displayCategoryTitle(panelItem.categorie)}
+      supTitle="Meldingen"
+    >
       {!!panelItem.datumCreatie && (
         <InfoDetail
           label="Datum en tijd melding"
