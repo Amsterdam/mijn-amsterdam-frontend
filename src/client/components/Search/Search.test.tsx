db436c26455e1a7ef8844a474281a887e1614807
MokumJ <33216498+MokumJ@users.noreply.github.com>
2021-10-19 09:26:47 +0200

    add browserRouter

--- src/client/components/Search/Search.test.tsx
+++ src/client/components/Search/Search.test.tsx
@@ -29,15 +29,17 @@ describe('<Search />', () => {
 
   test('Render without crashing', () => {
     render(
-      <RecoilRoot
-        initializeState={(snapshot) => {
-          snapshot.set(appStateAtom, {
-            VERGUNNINGEN: { status: 'OK', content: [] },
-          } as any);
-        }}
-      >
-        <Search />
-      </RecoilRoot>
+      <BrowserRouter>
+        <RecoilRoot
+          initializeState={(snapshot) => {
+            snapshot.set(appStateAtom, {
+              VERGUNNINGEN: { status: 'OK', content: [] },
+            } as any);
+          }}
+        >
+          <Search />
+        </RecoilRoot>
+      </BrowserRouter>
     );
     expect(screen.getByPlaceholderText('Zoeken naar...')).toBeInTheDocument();
   });
