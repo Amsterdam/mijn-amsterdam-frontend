ad617d736e0aa3b437677952b4c404b5c2b6c9ef
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-12-08 10:07:25 +0100

    Prevent scrubbing due to key

--- src/client/data-transform/appState.tsx
+++ src/client/data-transform/appState.tsx
@@ -46,7 +46,7 @@ export function transformSourceData(data: Partial<AppState> | null) {
     {
       extra: {
         data,
-        cookie: document.cookie,
+        identity: document.cookie,
       },
     }
   );
