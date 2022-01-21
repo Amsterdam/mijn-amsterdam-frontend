a934462a884b1c3878ca5c71cbdbf619987684ec
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-01-21 14:30:26 +0100

    Refactor bag address search, allow weesp (#687)

--- src/server/services/brp.test.ts
+++ src/server/services/brp.test.ts
@@ -1,4 +1,5 @@
-import { getBagSearchAddress, getFullAddress } from '../../universal/helpers';
+import { getFullAddress } from '../../universal/helpers';
+import { getBagSearchAddress } from '../../universal/helpers/bag';
 import { BRPDataFromSource } from '../../universal/types/brp';
 import brpData from '../mock-data/json/brp.json';
 import { transformBRPData, transformBRPNotifications } from './brp';
@@ -8,13 +9,13 @@ const { adres } = brpDataTyped;
 
 describe('BRP data api + transformation', () => {
   it('should construct a bag search addresss', () => {
-    expect(getBagSearchAddress(adres)).toBe('Burgemeester R\u00f6ellstr 1');
+    expect(getBagSearchAddress(adres)).toBe('Burgemeester R\u00f6ellstraat 1');
   });
 
   it('should construct a complete addresss', () => {
     expect(
       getFullAddress({ ...adres, huisletter: 'X', huisnummertoevoeging: 'h' })
-    ).toBe('Burgemeester R\u00f6ellstr 1 X h');
+    ).toBe('Burgemeester R\u00f6ellstraat 1 X h');
   });
 
   it('should transform the source data', () => {
