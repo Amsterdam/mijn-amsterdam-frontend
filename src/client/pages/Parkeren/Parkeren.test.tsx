a20ae2d3209042606011ec8010bdbbda8c835bf5
janthijs <92784122+janthijs@users.noreply.github.com>
2022-01-26 14:10:11 +0100

    tests: update and add

---
+++ src/client/pages/Parkeren/Parkeren.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import { generatePath } from 'react-router-dom';
+
+import { AppRoutes, ChapterTitles } from '../../../universal/config';
+
+import MockApp from '../MockApp';
+import Parkeren from './Parkeren';
+
+describe('Parkeren', () => {
+  beforeAll(() => {
+    (window.scrollTo as any) = jest.fn();
+  });
+
+  const routeEntry = generatePath(AppRoutes.TIPS);
+  const routePath = AppRoutes.TIPS;
+
+  const Component = () => (
+    <MockApp
+      routeEntry={routeEntry}
+      routePath={routePath}
+      component={Parkeren}
+    />
+  );
+
+  it('should contain the correct links', () => {
+    render(<Component />);
+
+    expect(screen.getByText(ChapterTitles.PARKEREN)).toBeInTheDocument();
+
+    expect(
+      screen.getByText('Lees hier meer over parkeren')
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText('Log voor uw vergunning in op Mijn Parkeren')
+    ).toBeInTheDocument();
+  });
+});
