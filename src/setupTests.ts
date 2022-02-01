e2164fcb31b57fa666caf19462d52257bee29f12
janthijs <92784122+janthijs@users.noreply.github.com>
2022-02-01 09:49:23 +0100

    test: add new test case

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,9 @@
+import axios from 'axios';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
+
+// Configure axios to use the node adapter.
+axios.defaults.adapter = require('axios/lib/adapters/http');
 
 global.matchMedia =
   global.matchMedia ||
