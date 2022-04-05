b5b176f1face79f356ca9f01fc0c0b0d90a38541
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-04-05 14:27:27 +0200

    Use decode

--- src/server/helpers/app.ts
+++ src/server/helpers/app.ts
@@ -138,10 +138,7 @@ export interface TokenData {
 }
 
 export function decodeOIDCToken(token: string): TokenData {
-  return jose.JWT.verify(token, OIDC_SECRET, {
-    complete: true,
-    algorithms: ['HS256'],
-  }) as unknown as TokenData;
+  return jose.JWT.decode(token, { complete: true }) as unknown as TokenData;
 }
 
 interface DevSessionData {
