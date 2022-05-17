3a2db566eed2fec51b9797210b3ce9070af2ffdd
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-05-17 16:56:47 +0200

    Add try catch for opaque error handling

--- src/client/hooks/media.hook.ts
+++ src/client/hooks/media.hook.ts
@@ -42,16 +42,28 @@ export function useMediaLayout(query: MediaQueryObject): boolean {
   });
 
   useLayoutEffect(() => {
-    const media = window.matchMedia(queryString);
-    if (media.matches !== matches) {
-      setMatches(!!media.matches);
+    const mediaQuery = window.matchMedia(queryString);
+
+    if (mediaQuery.matches !== matches) {
+      setMatches(!!mediaQuery.matches);
     }
-    const listener = () => {
-      setMatches(!!media.matches);
+
+    const handler = () => {
+      setMatches(!!mediaQuery.matches);
     };
 
-    (media.addEventListener || media.addListener)('change', listener);
-    return () => media.removeEventListener('change', listener);
+    try {
+      mediaQuery.addEventListener('change', handler);
+    } catch (e) {
+      mediaQuery.addListener?.(handler);
+    }
+    return () => {
+      try {
+        mediaQuery.removeEventListener('change', handler);
+      } catch (e) {
+        mediaQuery.removeListener?.(handler);
+      }
+    };
   }, [matches, queryString]);
 
   return matches;
