0e34f45aa2810bfdaa88d75ae7fbbdcdb55c2678
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-05-23 20:14:00 +0200

    Change node, add explicit cert

--- src/server/services/simple-connect/belastingen.ts
+++ src/server/services/simple-connect/belastingen.ts
@@ -1,17 +1,26 @@
+import fs from 'fs';
+import https from 'https';
 import { AuthProfileAndToken } from '../../helpers/app';
 import { fetchService } from './api-service';
+
+const httpsAgent = new https.Agent({
+  ca: fs.readFileSync('/etc/ssl/certs/ca-certificates.crt'),
+});
 
 export function fetchBelastingen(
   requestID: requestID,
   authProfileAndToken: AuthProfileAndToken
 ) {
   const requestConfig = {
-    // (verify = '/etc/ssl/certs/ca-certificates.crt')
+    url: process.env.BFF_BELASTINGEN_ENDPOINT,
+    httpsAgent,
     headers: {
       Authorization: `Bearer ${process.env.BFF_BELASTINGEN_TOKEN}`,
       subjid: authProfileAndToken.profile.id || '',
     },
   };
+
+  console.log(requestConfig);
 
   return fetchService(requestID, authProfileAndToken, requestConfig);
 }
