2c5ade3bd1ec200021607b7d4cc419bf3c31af01
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-05-24 10:53:09 +0200

    Some response debugging

--- src/server/services/simple-connect/belastingen.ts
+++ src/server/services/simple-connect/belastingen.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import https from 'https';
+import { DataRequestConfig } from '../../config';
 import { AuthProfileAndToken } from '../../helpers/app';
 import { fetchService } from './api-service';
 
@@ -7,20 +8,30 @@ const httpsAgent = new https.Agent({
   ca: fs.readFileSync('/etc/ssl/certs/ca-certificates.crt'),
 });
 
+function getBsnTranslations() {
+  const translationsJson = process.env.BFF_BELASTINGEN_BSN_TRANSLATIONS;
+  console.log('translationsJson', translationsJson);
+}
+
 export function fetchBelastingen(
   requestID: requestID,
   authProfileAndToken: AuthProfileAndToken
 ) {
-  const requestConfig = {
+  const requestConfig: DataRequestConfig = {
     url: process.env.BFF_BELASTINGEN_ENDPOINT,
     httpsAgent,
     headers: {
-      Authorization: `Bearer ${process.env.BFF_BELASTINGEN_TOKEN}`,
+      Authorization: `Bearer ${process.env.BFF_BELASTINGEN_BEARER_TOKEN}`,
       subjid: authProfileAndToken.profile.id || '',
+    },
+    transformResponse: (response) => {
+      getBsnTranslations();
+      console.log('resp', response);
+      return response;
     },
   };
 
-  console.log(requestConfig);
+  // console.log(requestConfig);
 
   return fetchService(requestID, authProfileAndToken, requestConfig);
 }
