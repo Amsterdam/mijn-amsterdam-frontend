d9c6daa42f4c6c17cb86160ad92520071a2b64b2
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-05-27 16:54:45 +0200

    ADd test

--- src/server/services/simple-connect/test-encrypt.ts
+++ src/server/services/simple-connect/test-encrypt.ts
@@ -1,5 +1,13 @@
-import { encryptPayload } from './milieuzone';
+import { encryptPayload, fetchMilieuzone } from './milieuzone';
+const [id] = process.argv.slice(2);
 
 const payload = 'test 1 2 3';
 
 console.log('result payload', encryptPayload(payload));
+
+fetchMilieuzone('xxxx111', {
+  profile: { id, authMethod: 'digid', profileType: 'private' },
+  token: '',
+})
+  .then((r) => console.log('mzone', r))
+  .catch((error) => console.log('mzoneerrr', error));
