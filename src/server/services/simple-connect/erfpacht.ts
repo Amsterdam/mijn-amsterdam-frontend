3e798e4d9ef76c144e7c8b55d8825f81cd05dcb1
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-05-27 16:15:29 +0200

    Add erfpacht

--- src/server/services/simple-connect/erfpacht.ts
+++ src/server/services/simple-connect/erfpacht.ts
@@ -1,1 +1,71 @@
-import { fetchService } from './api-service';
+import { Chapters } from '../../../universal/config';
+import { DataRequestConfig, getApiConfig } from '../../config';
+import { AuthProfileAndToken } from '../../helpers/app';
+import { fetchGenerated, fetchService } from './api-service';
+import { encrypt } from './subsidie';
+
+function encryptPayload(payload: string) {
+  return encrypt(payload, process.env.BFF_MIJN_ERFPACHT_ENCRYPTION_KEY_V2 + '');
+}
+
+type ErfpachtSourceResponse = 'true' | 'false';
+
+function transformErfpachtResponse(response: ErfpachtSourceResponse) {
+  console.log('erfpacht response', response);
+  return {
+    isKnown: response === 'true',
+  };
+}
+
+function getConfigMain(
+  authProfileAndToken: AuthProfileAndToken
+): DataRequestConfig {
+  const profile = authProfileAndToken.profile;
+  const [payload, iv] = encryptPayload(profile.id + '');
+  const type = profile.profileType === 'commercial' ? 'company' : 'private';
+
+  return getApiConfig('ERFPACHT', {
+    url: `${process.env.BFF_MIJN_ERFPACHT_API_URL}/api/v2/check/groundlease/${type}/${payload}`,
+    headers: {
+      'X-RANDOM-IV': iv,
+      'X-API-KEY': process.env.BFF_MIJN_ERFPACHT_API_KEY + '',
+    },
+    transformResponse: transformErfpachtResponse,
+  });
+}
+
+export async function fetchErfpacht(
+  requestID: requestID,
+  authProfileAndToken: AuthProfileAndToken
+) {
+  return fetchService(requestID, getConfigMain(authProfileAndToken), false);
+}
+
+function getConfigNotifications(
+  authProfileAndToken: AuthProfileAndToken
+): DataRequestConfig {
+  const profile = authProfileAndToken.profile;
+  const [payload, iv] = encryptPayload(profile.id + '');
+  const type = profile.profileType === 'commercial' ? 'kvk' : 'bsn';
+
+  return getApiConfig('ERFPACHT', {
+    url: `${process.env.BFF_MIJN_ERFPACHT_API_URL}/api/v2/check/notifications/${type}/${payload}`,
+    headers: {
+      'X-RANDOM-IV': iv,
+      'X-API-KEY': process.env.BFF_MIJN_ERFPACHT_API_KEY + '',
+    },
+  });
+}
+
+export async function fetchErfpachtGenerated(
+  requestID: requestID,
+  authProfileAndToken: AuthProfileAndToken
+) {
+  const response = await fetchGenerated(
+    requestID,
+    getConfigNotifications(authProfileAndToken),
+    Chapters.ERFPACHT
+  );
+
+  return response;
+}
