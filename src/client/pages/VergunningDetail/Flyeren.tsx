de0f605d52293ff580ffe2ea2a951be21ac254b6
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2022-08-18 13:32:25 +0200

    MIJN-4170 - Updates after FAT Flyeren (#771)

--- src/client/pages/VergunningDetail/Flyeren.tsx
+++ src/client/pages/VergunningDetail/Flyeren.tsx
@@ -5,31 +5,61 @@ import { InfoDetailGroup } from '../../components/InfoDetail/InfoDetail';
 import { Location } from './Location';
 import styles from './VergunningDetail.module.scss';
 
+// Controleren of van/tot dezelfde datum is, in dat geval niet de velden van/tot tonen.
+// In dat geval allen de datum tonen.
 export function Flyeren({ vergunning }: { vergunning: FlyerenVergunning }) {
+  const isVerleend = vergunning.decision === 'Verleend';
+  const isSameDate = vergunning.dateStart === vergunning.dateEnd;
+
   return (
     <>
       <InfoDetail label="Kenmerk" value={vergunning?.identifier || '-'} />
-      {!!vergunning.location && <Location location={vergunning.location} />}
-      <InfoDetailGroup className={styles.Flyeren_DateAndTime}>
-        <InfoDetail
-          label="Van"
-          value={
-            vergunning?.dateStart
-              ? defaultDateFormat(vergunning.dateStart)
-              : '-'
-          }
-        />
-        <InfoDetail
-          label="Tot en met"
-          value={
-            vergunning?.dateEnd ? defaultDateFormat(vergunning.dateEnd) : '-'
-          }
-        />
-        <InfoDetail
-          label="Tussen"
-          value={`${vergunning?.timeStart} - ${vergunning?.timeEnd}`}
-        />
-      </InfoDetailGroup>
+      {isVerleend && <Location location={vergunning.location} />}
+      {isVerleend && !isSameDate && (
+        <InfoDetailGroup className={styles.Flyeren_DateAndTime}>
+          <InfoDetail
+            label="Van"
+            value={
+              vergunning?.dateStart
+                ? defaultDateFormat(vergunning.dateStart)
+                : '-'
+            }
+          />
+          <InfoDetail
+            label="Tot en met"
+            value={
+              vergunning?.dateEnd ? defaultDateFormat(vergunning.dateEnd) : '-'
+            }
+          />
+          <InfoDetail
+            label="Tussen"
+            value={`${vergunning?.timeStart} - ${vergunning?.timeEnd}`}
+          />
+        </InfoDetailGroup>
+      )}
+      {isVerleend && isSameDate && (
+        <InfoDetailGroup className={styles.Flyeren_DateAndTime}>
+          <InfoDetail
+            label="Op"
+            value={
+              vergunning?.dateStart
+                ? defaultDateFormat(vergunning.dateStart)
+                : '-'
+            }
+          />
+          <InfoDetail
+            label="Van"
+            value={vergunning?.timeStart ? `${vergunning.timeStart} uur` : '-'}
+          />
+          <InfoDetail
+            label="Tot"
+            value={vergunning?.timeEnd ? `${vergunning.timeEnd} uur` : '-'}
+          />
+        </InfoDetailGroup>
+      )}
+      {isVerleend && (
+        <InfoDetail label="Resultaat" value={vergunning.decision} />
+      )}
     </>
   );
 }
