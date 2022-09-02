f350bde2dc9d89fc8db9c7d276e56e6a86ea16e0
janthijs <92784122+janthijs@users.noreply.github.com>
2022-09-02 09:43:36 +0200

    fix: review comments

--- src/server/services/buurt/meldingen-subcategories.ts
+++ src/server/services/buurt/meldingen-subcategories.ts
@@ -1,5 +1,17 @@
+import { capitalizeFirstLetter } from '../../../universal/helpers';
+
 export function transformSlugToCategorie(slug: string | undefined) {
-  return meldingenSubCategories.find((c) => c.id === slug)?.name;
+  if (!slug) {
+    return slug;
+  }
+
+  let name = meldingenSubCategories.find((c) => c.id === slug)?.name;
+
+  if (!name) {
+    name = capitalizeFirstLetter(slug?.replaceAll(/-/g, ' '));
+  }
+
+  return name;
 }
 
 export const meldingenSubCategories = [
@@ -14,26 +26,6 @@ export const meldingenSubCategories = [
   {
     id: 'bruin-en-witgoed',
     name: 'Bruin- en witgoed',
-  },
-  {
-    id: 'container-bijplaatsing',
-    name: 'Container bijplaatsing',
-  },
-  {
-    id: 'container-glas-kapot',
-    name: 'Container glas kapot',
-  },
-  {
-    id: 'container-glas-vol',
-    name: 'Container glas vol',
-  },
-  {
-    id: 'container-is-kapot',
-    name: 'Container is kapot',
-  },
-  {
-    id: 'container-is-vol',
-    name: 'Container is vol',
   },
   {
     id: 'container-voor-papier-is-stuk',
@@ -52,76 +44,20 @@ export const meldingenSubCategories = [
     name: 'Container plastic kapot',
   },
   {
-    id: 'grofvuil',
-    name: 'Grofvuil',
-  },
-  {
-    id: 'handhaving-op-afval',
-    name: 'Handhaving op afval',
-  },
-  {
-    id: 'huisafval',
-    name: 'Huisafval',
-  },
-  {
-    id: 'kerstbomen',
-    name: 'Kerstbomen',
-  },
-  {
-    id: 'overig-afval',
-    name: 'Overig afval',
-  },
-  {
-    id: 'prullenbak-is-kapot',
-    name: 'Prullenbak is kapot',
-  },
-  {
-    id: 'prullenbak-is-vol',
-    name: 'Prullenbak is vol',
-  },
-  {
     id: 'puin-sloopafval',
     name: 'Puin- / sloopafval',
-  },
-  {
-    id: 'rolcontainer-is-kapot',
-    name: 'Rolcontainer is kapot',
-  },
-  {
-    id: 'rolcontainer-is-vol',
-    name: 'Rolcontainer is vol',
   },
   {
     id: 'veeg-zwerfvuil',
     name: 'Veeg- / zwerfvuil',
   },
   {
-    id: 'afwatering-brug',
-    name: 'Afwatering brug',
-  },
-  {
-    id: 'brug-bediening',
-    name: 'Beweegbare brug',
-  },
-  {
     id: 'kades',
     name: 'Kademuren',
   },
   {
-    id: 'oevers',
-    name: 'Oevers',
-  },
-  {
     id: 'riolering-verstopte-kolk',
     name: 'Riolering - verstopte kolk',
-  },
-  {
-    id: 'sluis',
-    name: 'Sluis',
-  },
-  {
-    id: 'steiger',
-    name: 'Steiger',
   },
   {
     id: 'bruggen',
@@ -130,18 +66,6 @@ export const meldingenSubCategories = [
   {
     id: 'verzakking-kades',
     name: 'Verzakking van kades',
-  },
-  {
-    id: 'watergangen',
-    name: 'Watergangen',
-  },
-  {
-    id: 'vermoeden',
-    name: 'Vermoeden',
-  },
-  {
-    id: 'beplanting',
-    name: 'Beplanting',
   },
   {
     id: 'boom-boomstob',
@@ -192,14 +116,6 @@ export const meldingenSubCategories = [
     name: 'Drijfvuil bevaarbaar water',
   },
   {
-    id: 'eikenprocessierups',
-    name: 'Eikenprocessierups',
-  },
-  {
-    id: 'japanse-duizendknoop',
-    name: 'Japanse duizendknoop',
-  },
-  {
     id: 'maaien-snoeien',
     name: 'Maaien',
   },
@@ -216,26 +132,6 @@ export const meldingenSubCategories = [
     name: 'Overig groen en water',
   },
   {
-    id: 'snoeien',
-    name: 'Snoeien',
-  },
-  {
-    id: 'overig',
-    name: 'Overig',
-  },
-  {
-    id: 'overige-dienstverlening',
-    name: 'Overige dienstverlening',
-  },
-  {
-    id: 'geluidsoverlast-installaties',
-    name: 'Geluidsoverlast installaties',
-  },
-  {
-    id: 'geluidsoverlast-muziek',
-    name: 'Geluidsoverlast muziek',
-  },
-  {
     id: 'overig-horecabedrijven',
     name: 'Overig bedrijven / horeca',
   },
@@ -244,16 +140,8 @@ export const meldingenSubCategories = [
     name: 'Overlast door bezoekers (niet op terras)',
   },
   {
-    id: 'overlast-terrassen',
-    name: 'Overlast terrassen',
-  },
-  {
     id: 'stank-horecabedrijven',
     name: 'Stank horeca/bedrijven',
-  },
-  {
-    id: 'stankoverlast',
-    name: 'Stankoverlast',
   },
   {
     id: 'auto-scooter-bromfietswrak',
@@ -268,10 +156,6 @@ export const meldingenSubCategories = [
     name: 'Deelscooters en -(bak)fietsen',
   },
   {
-    id: 'fietswrak',
-    name: 'Fietswrak',
-  },
-  {
     id: 'graffiti-wildplak',
     name: 'Graffiti / wildplak',
   },
@@ -284,28 +168,8 @@ export const meldingenSubCategories = [
     name: 'Lozing / dumping / bodemverontreiniging',
   },
   {
-    id: 'markten',
-    name: 'Markten',
-  },
-  {
-    id: 'overig-openbare-ruimte',
-    name: 'Overig openbare ruimte',
-  },
-  {
-    id: 'parkeeroverlast',
-    name: 'Parkeeroverlast',
-  },
-  {
     id: 'stank-geluidsoverlast',
     name: 'Stank- / geluidsoverlast',
-  },
-  {
-    id: 'hondenpoep',
-    name: 'Uitwerpselen',
-  },
-  {
-    id: 'wegsleep',
-    name: 'Wegsleep',
   },
   {
     id: 'blokkade-van-de-vaarweg',
@@ -344,26 +208,6 @@ export const meldingenSubCategories = [
     name: 'Wrak in het water',
   },
   {
-    id: 'dode-dieren',
-    name: 'Dode dieren',
-  },
-  {
-    id: 'duiven',
-    name: 'Duiven',
-  },
-  {
-    id: 'ganzen',
-    name: 'Ganzen',
-  },
-  {
-    id: 'meeuwen',
-    name: 'Meeuwen',
-  },
-  {
-    id: 'overig-dieren',
-    name: 'Overig dieren',
-  },
-  {
     id: 'ratten',
     name: 'Ratten – in de openbare ruimte',
   },
@@ -372,20 +216,12 @@ export const meldingenSubCategories = [
     name: 'Ratten – in en rond een woning',
   },
   {
-    id: 'wespen',
-    name: 'Wespen',
-  },
-  {
     id: 'daklozen-bedelen',
     name: 'Daklozen / bedelen',
   },
   {
     id: 'drank-en-drugsoverlast',
     name: 'Drank- / drugsoverlast',
-  },
-  {
-    id: 'jongerenoverlast',
-    name: 'Jongerenoverlast',
   },
   {
     id: 'loslopende-agressieve-honden',
@@ -404,20 +240,8 @@ export const meldingenSubCategories = [
     name: "Overlast van taxi's, bussen en fietstaxi's",
   },
   {
-    id: 'personen-op-het-water',
-    name: 'Personen op het water',
-  },
-  {
-    id: 'vuurwerkoverlast',
-    name: 'Vuurwerkoverlast',
-  },
-  {
     id: 'wildplassen-poepen-overgeven',
     name: 'Wildplassen / poepen / overgeven',
-  },
-  {
-    id: 'drijfvuil-bevaarbaar-water',
-    name: 'Drijfvuil bevaarbaar water',
   },
   {
     id: 'drijfvuil-niet-bevaarbaar-water',
@@ -430,10 +254,6 @@ export const meldingenSubCategories = [
   {
     id: 'gladheid-olie',
     name: 'Gladheid door olie op de weg',
-  },
-  {
-    id: 'gladheid-winterdienst',
-    name: 'Gladheid winterdienst',
   },
   {
     id: 'graffitiwildplak',
@@ -456,10 +276,6 @@ export const meldingenSubCategories = [
     name: 'Put / riolering verstopt',
   },
   {
-    id: 'uitwerpselen',
-    name: 'Uitwerpselen',
-  },
-  {
     id: 'veegzwerfvuil',
     name: 'Veeg- / zwerfvuil',
   },
@@ -472,18 +288,6 @@ export const meldingenSubCategories = [
     name: 'Belijning',
   },
   {
-    id: 'bewegwijzering',
-    name: 'Bewegwijzering',
-  },
-  {
-    id: 'brug',
-    name: 'Brug',
-  },
-  {
-    id: 'camerasystemen',
-    name: 'Camerasystemen',
-  },
-  {
     id: 'fietsrek-nietje',
     name: 'Fietsrek / nietje',
   },
@@ -492,36 +296,12 @@ export const meldingenSubCategories = [
     name: 'Gladheid winterdienst',
   },
   {
-    id: 'klok',
-    name: 'Klok',
-  },
-  {
-    id: 'lichthinder',
-    name: 'Lichthinder',
-  },
-  {
-    id: 'onderhoud-fietspad',
-    name: 'Onderhoud fietspad',
-  },
-  {
     id: 'onderhoud-stoep-straat-en-fietspad',
     name: 'Onderhoud stoep, rijweg en parkeerplaats',
   },
   {
-    id: 'oplaadpunt',
-    name: 'Oplaadpunt',
-  },
-  {
     id: 'overig-wegen-verkeer-straatmeubilair',
     name: 'Overig Wegen, verkeer, straatmeubilair',
-  },
-  {
-    id: 'parkeer-verwijssysteem',
-    name: 'Parkeer verwijssysteem',
-  },
-  {
-    id: 'parkeerautomaten',
-    name: 'Parkeerautomaten',
   },
   {
     id: 'prullenbakkapot',
@@ -534,22 +314,6 @@ export const meldingenSubCategories = [
   {
     id: 'put-riolering-verstopt',
     name: 'Put / riolering verstopt',
-  },
-  {
-    id: 'speelplaats',
-    name: 'Speelplaats',
-  },
-  {
-    id: 'sportvoorziening',
-    name: 'Sportvoorziening',
-  },
-  {
-    id: 'stadsplattegronden',
-    name: 'Stadsplattegronden',
-  },
-  {
-    id: 'straatmeubilair',
-    name: 'Straatmeubilair',
   },
   {
     id: 'lantaarnpaal-straatverlichting',
@@ -572,56 +336,20 @@ export const meldingenSubCategories = [
     name: 'Verkeersbord',
   },
   {
-    id: 'verkeerslicht',
-    name: 'Verkeerslicht',
-  },
-  {
-    id: 'verkeersoverlast',
-    name: 'Verkeersoverlast',
-  },
-  {
     id: 'verkeersoverlast-verkeerssituaties',
     name: 'Verkeersoverlast / Verkeerssituaties',
-  },
-  {
-    id: 'verkeerssituaties',
-    name: 'Verkeerssituaties',
-  },
-  {
-    id: 'verlichting-netstoring',
-    name: 'Verlichting netstoring',
   },
   {
     id: 'omleiding',
     name: 'Wegwerkzaamheden',
   },
   {
-    id: 'leegstand',
-    name: 'Leegstand',
-  },
-  {
-    id: 'onderhuur-en-adreskwaliteit',
-    name: 'Onderhuur en adreskwaliteit',
-  },
-  {
     id: 'wonen-overig',
     name: 'Overige Wonen',
   },
   {
-    id: 'vakantieverhuur',
-    name: 'Vakantieverhuur',
-  },
-  {
-    id: 'wonen-ondermijning',
-    name: 'Wonen ondermijning',
-  },
-  {
     id: 'woningdelen-spookburgers',
     name: 'Woningdelen',
-  },
-  {
-    id: 'woningkwaliteit',
-    name: 'Woningkwaliteit',
   },
   {
     id: 'fraude',
