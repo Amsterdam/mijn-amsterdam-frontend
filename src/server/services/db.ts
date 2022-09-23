57b43009f9b76f2dba5fd24b57d7a9cbfbd3ca7d
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-09-23 13:59:41 +0200

    Add counter overview
    # Conflicts:
    #	package-lock.json
    #	package.json

---
+++ src/server/services/db.ts
@@ -0,0 +1,32 @@
+import * as Sentry from '@sentry/node';
+import { Client, ClientConfig } from 'pg';
+import { IS_AP } from '../../universal/config';
+
+export const pgDbConfig: ClientConfig = {
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: IS_AP },
+};
+
+console.log('pgDbConfig', pgDbConfig);
+
+const client = new Client(pgDbConfig);
+let isConnected = false;
+
+export async function query(query: string, values?: any[]) {
+  let result = null;
+  try {
+    if (!isConnected) {
+      await client.connect();
+      isConnected = true;
+    }
+    result = await client.query(query, values);
+    // await pool.end();
+  } catch (error) {
+    Sentry.captureException(error);
+  }
+  return result;
+}
