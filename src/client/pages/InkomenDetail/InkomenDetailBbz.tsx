33d6fd0e23df0648a8aa0987300ddd9f7983faa9
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2022-09-27 12:25:32 +0200

    MIJN-4915 - Oplossen bevindingen uit FAT BBZ (#791)
    * fix: updates
    
    * fix: fixed issues
    
    * Fix bug
    
    * Fix component
    
    * Fix erfpacht
    
    * Update src/client/pages/InkomenDetail/InkomenDetailBbz.tsx
    
    Co-authored-by: Tim van Oostrom <t.van.oostrom@amsterdam.nl>
    
    * fix: re-add prop
    
    * Update src/server/services/wpi/content/bbz.ts
    
    Co-authored-by: Tim van Oostrom <t.van.oostrom@amsterdam.nl>
    
    Co-authored-by: Tim van Oostrom <t.van.oostrom@amsterdam.nl>

--- src/client/pages/InkomenDetail/InkomenDetailBbz.tsx
+++ src/client/pages/InkomenDetail/InkomenDetailBbz.tsx
@@ -9,15 +9,19 @@ export default function InkomenDetailBbz() {
   const { WPI_BBZ } = useAppStateGetter();
   const { id } = useParams<{ id: string }>();
   const statusItem = WPI_BBZ.content?.find((item) => item.id === id);
-  const hasDecisionStep = statusItem?.steps.some(step => step.id === "besluit") ?? false;
+  const hasDecisionStep =
+    statusItem?.steps.some((step) => step.id.includes('besluit')) ?? false;
+
   const pageContent = useCallback((isLoading, inkomenItem) => {
     return (
       <>
         <p>
           Hieronder ziet u de status van uw aanvraag voor een uitkering of
           lening van het Bbz. Ook als u een IOAZ uitkering heeft aangevraagd
-          ziet u de status hieronder. Het duurt maximaal 3 werkdagen voordat uw
-          documenten over het Bbz in Mijn Amsterdam staan.
+          ziet u de status hieronder. Als u meerdere aanvragen voor het Bbz hebt
+          gedaan, dan krijgt u 1 besluit als antwoord op al uw aanvragen voor
+          het Bbz. Het duurt maximaal 3 werkdagen voordat uw documenten over het
+          Bbz in Mijn Amsterdam staan.
         </p>
         <p>
           Hebt u schuldhulp aangevraagd? Dan wordt daarover contact met u
@@ -42,10 +46,10 @@ export default function InkomenDetailBbz() {
       highlightKey={false}
       statusLabel={() => `Bbz-aanvraag`}
       showStatusLineConnection={!hasDecisionStep} // There is no logical connection between the historic BBZ steps, therefor we do not show the checkmarks as they imply a linear proces.
-      reverseSteps={hasDecisionStep} // For an unknown business reason, the historic steps of BBZ are shown in reverse.
       documentPathForTracking={(document) =>
         `/downloads/inkomen/bbz/${document.title.split(/\n/)[0]}`
       }
+      reverseSteps={hasDecisionStep} // For an unknown business reason, the historic steps of BBZ are shown in reverse.
     />
   );
 }
