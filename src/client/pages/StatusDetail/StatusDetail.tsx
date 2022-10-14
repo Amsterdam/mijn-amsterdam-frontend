359928d6de9630c3eee4a542dc8de08f4152823f
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-10-14 18:03:02 +0200

    Change import

--- src/client/pages/StatusDetail/StatusDetail.tsx
+++ src/client/pages/StatusDetail/StatusDetail.tsx
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react';
 import { ReactNode, useEffect, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppRoutes, Chapter, ChapterTitles } from '../../../universal/config';
-import { isError, isLoading, relayApiUrl } from '../../../universal/helpers';
+import { isError, isLoading } from '../../../universal/helpers';
 import {
   GenericDocument,
   StatusLine,
@@ -19,6 +19,7 @@ import {
   StatusLine as StatusLineComponent,
 } from '../../components';
 import { LinkdInline } from '../../components/Button/Button';
+import { relayApiUrl } from '../../config/api';
 import { useAppStateGetter } from '../../hooks/useAppState';
 import styles from './StatusDetail.module.scss';
 
