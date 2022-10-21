e65f9040811e5773966a29ff6b7d0b82c8ec74f1
janthijs <92784122+janthijs@users.noreply.github.com>
2022-10-21 10:15:51 +0200

    fix: show time separate from date

--- src/client/pages/VergunningDetail/Nachtwerkontheffing.tsx
+++ src/client/pages/VergunningDetail/Nachtwerkontheffing.tsx
@@ -7,19 +7,25 @@ import InfoDetail, {
   InfoDetailGroup,
 } from '../../components/InfoDetail/InfoDetail';
 import { Location } from './Location';
+import styles from './VergunningDetail.module.scss';
 
 export function Nachtwerkontheffing({
   vergunning,
 }: {
   vergunning: NachtwerkontheffingType;
 }) {
+  const isVerleend = vergunning.decision === 'Verleend';
+  const isAfgehandeld = vergunning.status === 'Afgehandeld';
+  const isSameDate =
+    vergunning.dateStart === vergunning.dateEnd || vergunning.dateEnd === null;
+
   return (
     <>
       <InfoDetail label="Kenmerk" value={vergunning.identifier || '-'} />
-      {!!vergunning.location && <Location location={vergunning.location} />}
+      {isVerleend && <Location location={vergunning.location} />}
 
-      {!!vergunning.decision && (
-        <InfoDetailGroup>
+      {isVerleend && !isSameDate && (
+        <InfoDetailGroup className={styles.DateAndTime_SingleLine}>
           <InfoDetail
             label="Vanaf"
             value={
@@ -44,9 +50,33 @@ export function Nachtwerkontheffing({
                 : '-'
             }
           />
+          <InfoDetail
+            label="Tussen"
+            value={`${vergunning?.timeStart} - ${vergunning?.timeEnd}`}
+          />
         </InfoDetailGroup>
       )}
-      {!!vergunning.decision && (
+      {isVerleend && isSameDate && (
+        <InfoDetailGroup className={styles.DateAndTime_SingleLine}>
+          <InfoDetail
+            label="Op"
+            value={
+              vergunning?.dateStart
+                ? defaultDateFormat(vergunning.dateStart)
+                : '-'
+            }
+          />
+          <InfoDetail
+            label="Van"
+            value={vergunning?.timeStart ? `${vergunning.timeStart} uur` : '-'}
+          />
+          <InfoDetail
+            label="Tot"
+            value={vergunning?.timeEnd ? `${vergunning.timeEnd} uur` : '-'}
+          />
+        </InfoDetailGroup>
+      )}
+      {isAfgehandeld && (
         <InfoDetail label="Resultaat" value={vergunning.decision} />
       )}
     </>
