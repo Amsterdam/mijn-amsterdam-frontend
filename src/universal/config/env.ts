3be29fec0f9d46f49a6f480060e591aada97c8df
janthijs <92784122+janthijs@users.noreply.github.com>
2022-11-01 15:21:21 +0100

    fix: add analytics for dev/acc

--- src/universal/config/env.ts
+++ src/universal/config/env.ts
@@ -35,6 +35,8 @@ export const IS_AP = IS_ACCEPTANCE || IS_PRODUCTION;
 
 const otapServerEnv: OtapEnv = {
   development: {
+    analyticsId: 'c166e22f-2fb6-4cbe-bb98-b7e3053c2150',
+    analyticsUrlBase: 'https://dap.amsterdam.nl/',
     krefiaDirectLink: 'https://krefia.amsterdam.nl',
     ssoErfpachtUrl:
       'https://mijnerfpacht.amsterdam.nl/saml/login/alias/mijnErfpachtBurger',
@@ -51,6 +53,8 @@ const otapServerEnv: OtapEnv = {
     bagUrl: 'https://api.data.amsterdam.nl/atlas/search/adres/?features=2&q=',
   },
   acceptance: {
+    analyticsId: 'c166e22f-2fb6-4cbe-bb98-b7e3053c2150',
+    analyticsUrlBase: 'https://dap.amsterdam.nl/',
     sentryDsn:
       'https://d9bff634090c4624bce9ba7d8f0875dd@sentry.data.amsterdam.nl/13',
     bffSentryDsn: process.env.BFF_SENTRY_DSN || '',
