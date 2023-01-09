c9c4235d2425202130e90c2a02f171911b720a4c
janthijs <92784122+janthijs@users.noreply.github.com>
2023-01-09 16:16:36 +0100

    test: add test case

--- src/client/pages/ApplicationError/ApplicationError.test.tsx
+++ src/client/pages/ApplicationError/ApplicationError.test.tsx
@@ -1,10 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import ApplicationError from './ApplicationError';
 
-it('Renders without crashing', () => {
-  const error = new Error('There is an error');
+describe('ApplicationError', () => {
+  it('Renders without crashing', () => {
+    const error = new Error('There is an error');
 
-  // @ts-ignore
-  render(<ApplicationError error={error} />);
-  expect(screen.getByText(/There is an error/)).toBeInTheDocument();
+    // @ts-ignore
+    render(<ApplicationError error={error} />);
+    expect(screen.getByText(/There is an error/)).toBeInTheDocument();
+  });
+
+  it('Render without crashing when location is not home', () => {
+    const location = new URL('https://mijn.amsterdam.nl/klachten/1');
+
+    delete (window as any).location;
+    window.location = location as unknown as Location;
+
+    const error = new Error('There is a different error');
+
+    // @ts-ignore
+    render(<ApplicationError error={error} />);
+    expect(screen.getByText(/There is a different error/)).toBeInTheDocument();
+  });
 });
