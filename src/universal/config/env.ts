f0701f74e836e79c244bba264884315b385001d9
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-04-18 11:10:28 +0200

    Change dsn

--- src/universal/config/env.ts
+++ src/universal/config/env.ts
@@ -40,8 +40,7 @@ const otapServerEnv: OtapEnv = {
       'https://mijnerfpacht.amsterdam.nl/saml/login/alias/mijnErfpachtBurger',
     ssoErfpachtUrlEH:
       'https://mijnerfpacht.amsterdam.nl/saml/login/alias/mijnErfpachtZakelijk',
-    bffSentryDsn:
-      'https://d9bff634090c4624bce9ba7d8f0875dd@sentry.data.amsterdam.nl/13',
+    bffSentryDsn: process.env.BFF_SENTRY_DSN || '',
     ssoMilieuzoneUrl: 'https://ontheffingen.amsterdam.nl/publiek/aanvragen',
     ssoSubsidiesUrl: 'https://acc.mijnsubsidies.amsterdam.nl/dashboard',
     bagUrl: 'https://api.data.amsterdam.nl/atlas/search/adres/?features=2&q=', // features=2 is een Feature flag zodat ook Weesp resultaten worden weergegeven.
@@ -54,7 +53,7 @@ const otapServerEnv: OtapEnv = {
     analyticsId: 'c166e22f-2fb6-4cbe-bb98-b7e3053c2150',
     analyticsUrlBase: 'https://dap.amsterdam.nl/',
     sentryDsn:
-      'https://d9bff634090c4624bce9ba7d8f0875dd@sentry.data.amsterdam.nl/13',
+      'https://d9bff634090c4624bce9ba7d8f0875dd@sentry-new.data.amsterdam.nl/13',
     bffSentryDsn: process.env.BFF_SENTRY_DSN || '',
     ssoErfpachtUrl:
       'https://mijnerfpacht.acc.amsterdam.nl/saml/login/alias/mijnErfpachtBurger',
@@ -69,7 +68,7 @@ const otapServerEnv: OtapEnv = {
     analyticsId: 'f558164e-e388-49e0-864e-5f172552789c',
     analyticsUrlBase: 'https://dap.amsterdam.nl/',
     sentryDsn:
-      'https://d9bff634090c4624bce9ba7d8f0875dd@sentry.data.amsterdam.nl/13',
+      'https://d9bff634090c4624bce9ba7d8f0875dd@sentry-new.data.amsterdam.nl/13',
     bffSentryDsn: process.env.BFF_SENTRY_DSN || '',
     ssoErfpachtUrl:
       'https://mijnerfpacht.amsterdam.nl/saml/login/alias/mijnErfpachtBurger',
