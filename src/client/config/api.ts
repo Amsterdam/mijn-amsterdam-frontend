fb2fb9bb39f98f58c598c6e85def1d4cc7474e47
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2023-05-23 15:12:02 +0200

    Update src/client/config/api.ts
    Co-authored-by: Tim van Oostrom <t.van.oostrom@amsterdam.nl>

--- src/client/config/api.ts
+++ src/client/config/api.ts
@@ -78,7 +78,7 @@ export const ErrorNames: Record<string /* ApiStateKey */, string> = {
   BEZWAREN: 'Ingediende bezwaren',
   HORECA: 'Horeca vergunningen',
   AVG: 'Ingediende AVG verzoeken',
-  BODEM: 'Bodem',
+  BODEM: 'Bodem: loodmetingen',
 };
 
 export function createErrorDisplayData(
