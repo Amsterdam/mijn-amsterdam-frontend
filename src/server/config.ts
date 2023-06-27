6ee4e8ad9c24e0115591498cd48133345f840287
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-06-27 11:24:49 +0200

    Change back value

--- src/server/config.ts
+++ src/server/config.ts
@@ -78,7 +78,7 @@ const ONE_MINUTE_MS = 60 * ONE_SECOND_MS;
 const ONE_HOUR_MS = 60 * ONE_MINUTE_MS;
 
 export const DEFAULT_API_CACHE_TTL_MS = 45 * ONE_SECOND_MS; // This means that every request that depends on the response of another will use the cached version of the response for a maximum of 45 seconds.
-export const DEFAULT_CANCEL_TIMEOUT_MS = 35 * ONE_SECOND_MS; // This means a request will be aborted after 35 seconds without a response.
+export const DEFAULT_CANCEL_TIMEOUT_MS = 20 * ONE_SECOND_MS; // This means a request will be aborted after 20 seconds without a response.
 
 export const DEFAULT_REQUEST_CONFIG: DataRequestConfig = {
   cancelTimeout: DEFAULT_CANCEL_TIMEOUT_MS,
