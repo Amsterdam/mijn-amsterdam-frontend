f7e8729d123efc3933b17aa0ca86fce8bdbddf66
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-07-07 15:04:34 +0200

    Add request debugging

--- src/server/config.ts
+++ src/server/config.ts
@@ -133,21 +133,25 @@ export const ApiConfig: ApiDataRequestConfig = {
   WPI_E_AANVRAGEN: {
     url: `${process.env.BFF_WPI_API_BASE_URL}/wpi/e-aanvragen`,
     passthroughOIDCToken: true,
+    debugRequestConfig: true,
     cancelTimeout: 5 * ONE_SECOND_MS,
   },
   WPI_AANVRAGEN: {
     url: `${process.env.BFF_WPI_API_BASE_URL}/wpi/uitkering-en-stadspas/aanvragen`,
     passthroughOIDCToken: true,
+    debugRequestConfig: true,
     cancelTimeout: 5 * ONE_SECOND_MS,
   },
   WPI_SPECIFICATIES: {
     url: `${process.env.BFF_WPI_API_BASE_URL}/wpi/uitkering/specificaties-en-jaaropgaven`,
     passthroughOIDCToken: true,
+    debugRequestConfig: true,
     cancelTimeout: 5 * ONE_SECOND_MS,
   },
   WPI_STADSPAS: {
     url: `${process.env.BFF_WPI_API_BASE_URL}/wpi/stadspas`,
     passthroughOIDCToken: true,
+    debugRequestConfig: true,
     cancelTimeout: 5 * ONE_SECOND_MS,
   },
   BEZWAREN_LIST: {
