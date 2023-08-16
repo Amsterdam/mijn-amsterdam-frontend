cca3451d91bfabebb1937ba68df8f61b0cf29e48
janthijs <92784122+janthijs@users.noreply.github.com>
2023-08-16 17:47:36 +0200

    fix: remove comment

--- src/client/hooks/useNotifications.ts
+++ src/client/hooks/useNotifications.ts
@@ -12,7 +12,6 @@ const appStateNotificationsSelector = selectorFamily({
   get:
     (profileType: ProfileType) =>
     ({ get }) => {
-      //a
       const appState = get(appStateAtom);
       let notifications = appState.NOTIFICATIONS.content || [];
       let welcomeNotification = WelcomeNotification;
