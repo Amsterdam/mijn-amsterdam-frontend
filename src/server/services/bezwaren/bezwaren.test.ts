657e44e4c62395603d7fe72b2ca83a5f1054db46
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-08-25 12:06:20 +0200

    Update test

--- src/server/services/bezwaren/bezwaren.test.ts
+++ src/server/services/bezwaren/bezwaren.test.ts
@@ -9,6 +9,7 @@ import {
   fetchBezwaren,
   fetchBezwarenNotifications,
 } from './bezwaren';
+import { encrypt } from '../../../universal/helpers/encrypt-decrypt';
 
 describe('Bezwaren', () => {
   const requestId = '456';
@@ -41,11 +42,7 @@ describe('Bezwaren', () => {
         .reply(200, bezwarenDocumenten)
         .get((uri) => uri.includes('/statussen'))
         .times(4)
-        .reply(200, bezwarenStatus)
-        .get(
-          `/bezwaren/zgw/v1/enkelvoudiginformatieobjecten/${documentId}/download`
-        )
-        .reply(200, 'wat-document-data');
+        .reply(200, bezwarenStatus);
     });
 
     it('should return data in expected format', async () => {
@@ -66,46 +63,46 @@ describe('Bezwaren', () => {
               {
                 "chapter": "BEZWAREN",
                 "datePublished": "2023-04-25",
-                "description": "Wij hebben uw bezwaar BI.21.014121.001 ontvangen.",
+                "description": "Wij hebben uw bezwaar BI.21.014121.001 in behandeling genomen.",
                 "id": "BI.21.014121.001",
                 "link": {
-                  "title": "Bekijk details",
+                  "title": "Bekijk uw bezwaar",
                   "to": "/bezwaren/68cdd171-b4fd-44cc-a4d3-06b77341f20a",
                 },
-                "title": "Bezwaar ontvangen",
+                "title": "Bezwaar in behandeling",
               },
               {
                 "chapter": "BEZWAREN",
                 "datePublished": "2023-04-03",
-                "description": "Wij hebben uw bezwaar JB.22.000076.001 ontvangen.",
+                "description": "Wij hebben uw bezwaar JB.22.000076.001 in behandeling genomen.",
                 "id": "JB.22.000076.001",
                 "link": {
-                  "title": "Bekijk details",
+                  "title": "Bekijk uw bezwaar",
                   "to": "/bezwaren/9804b064-90a3-43b0-bc7c-924f9939888d",
                 },
-                "title": "Bezwaar ontvangen",
+                "title": "Bezwaar in behandeling",
               },
               {
                 "chapter": "BEZWAREN",
                 "datePublished": "2022-11-04",
-                "description": "Wij hebben uw bezwaar ZAAK2 ontvangen.",
+                "description": "Wij hebben uw bezwaar ZAAK2 in behandeling genomen.",
                 "id": "ZAAK2",
                 "link": {
-                  "title": "Bekijk details",
+                  "title": "Bekijk uw bezwaar",
                   "to": "/bezwaren/cc117d91-1b00-4bae-bbdd-9ea3a6d6d185",
                 },
-                "title": "Bezwaar ontvangen",
+                "title": "Bezwaar in behandeling",
               },
               {
                 "chapter": "BEZWAREN",
-                "datePublished": "2023-01-04",
-                "description": "Er is een besluit over uw bezwaar ZAAK3.",
+                "datePublished": "2022-11-04",
+                "description": "Wij hebben uw bezwaar ZAAK3 in behandeling genomen.",
                 "id": "ZAAK3",
                 "link": {
-                  "title": "Bekijk details",
+                  "title": "Bekijk uw bezwaar",
                   "to": "/bezwaren/956541b6-7a25-4132-9592-0a509bc7ace0",
                 },
-                "title": "Bezwaar afgehandeld",
+                "title": "Bezwaar in behandeling",
               },
             ],
           },
@@ -115,16 +112,25 @@ describe('Bezwaren', () => {
     });
 
     it('should be possible to download a document', async () => {
+      remoteApi
+        .get(
+          `/bezwaren/zgw/v1/enkelvoudiginformatieobjecten/${documentId}/download`
+        )
+        .reply(200);
+
       const documentResponse = await fetchBezwaarDocument(
         requestId,
         profileAndToken,
-        documentIdEncrypted
+        documentIdEncrypted,
+        true
       );
 
       //@ts-ignore
-      expect(documentResponse?.message).toEqual(undefined);
-      expect(documentResponse.status).toEqual('OK');
-      expect(documentResponse.content).toEqual('wat-document-data');
+      expect(documentResponse.status).toEqual(200);
+      expect(documentResponse.request.headers['content-type']).toEqual(
+        'aplication/pdf'
+      );
+      // expect(documentResponse).toEqual(X);
     });
   });
 });
