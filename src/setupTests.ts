d8dfbe95dcfb7a3d9c1a85a3960e778bfdb336ff
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-09-01 10:38:36 +0200

    Rename

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -41,7 +41,7 @@ export const remoteApiHost = 'http://remote-api-host';
 process.env.REACT_APP_BFF_AUTH_PATH = '';
 
 process.env.REACT_APP_BFF_API_URL = bffApiHost;
-process.env.BFF_MS_API_HOST = remoteApiHost;
+process.env.BFF_API_DATA_HOST = remoteApiHost;
 
 process.env.BFF_DISABLE_MOCK_ADAPTER = 'true';
 process.env.BFF_REQUEST_CACHE_ENABLED = 'false';
