e804fed17df0c85075325047b8e041b407c08edf
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-09-05 16:42:11 +0200

    Move out of page content

--- src/client/pages/Bezwaren/Bezwaren.tsx
+++ src/client/pages/Bezwaren/Bezwaren.tsx
@@ -64,39 +64,38 @@ export default function BEZWAREN() {
             Meer informatie over Bezwaar maken
           </Linkd>
         </p>
-
-        <SectionCollapsible
-          id="SectionCollapsible-complaints"
-          title="Lopende bezwaren"
-          noItemsMessage="U heeft geen lopende zaken. Het kan zijn dat een ingediend bezwaar nog niet in behandeling is genomen."
-          startCollapsed={false}
-          hasItems={!!ingediendeBezwaren?.length}
-          isLoading={isLoading(BEZWAREN)}
-          className={styles.SectionCollapsibleFirst}
-        >
-          <Table
-            className={styles.DocumentsTable}
-            displayProps={DISPLAY_PROPS_BEZWAREN_LOPEND}
-            items={ingediendeBezwaren}
-          />
-        </SectionCollapsible>
-
-        <SectionCollapsible
-          id="SectionCollapsible-complaints"
-          title="Afgehandelde bezwaren"
-          noItemsMessage="U hebt nog geen afgehandelde bezwaren."
-          startCollapsed={false}
-          hasItems={!!afgehandeldeBezwaren?.length}
-          isLoading={isLoading(BEZWAREN)}
-          className={styles.SectionCollapsibleFirst}
-        >
-          <Table
-            className={styles.DocumentsTable}
-            displayProps={DISPLAY_PROPS_BEZWAREN_AFGEROND}
-            items={afgehandeldeBezwaren}
-          />
-        </SectionCollapsible>
       </PageContent>
+      <SectionCollapsible
+        id="SectionCollapsible-complaints"
+        title="Lopende bezwaren"
+        noItemsMessage="U heeft geen lopende zaken. Het kan zijn dat een ingediend bezwaar nog niet in behandeling is genomen."
+        startCollapsed={false}
+        hasItems={!!ingediendeBezwaren?.length}
+        isLoading={isLoading(BEZWAREN)}
+        className={styles.SectionCollapsibleFirst}
+      >
+        <Table
+          className={styles.DocumentsTable}
+          displayProps={DISPLAY_PROPS_BEZWAREN_LOPEND}
+          items={ingediendeBezwaren}
+        />
+      </SectionCollapsible>
+
+      <SectionCollapsible
+        id="SectionCollapsible-complaints"
+        title="Afgehandelde bezwaren"
+        noItemsMessage="U hebt nog geen afgehandelde bezwaren."
+        startCollapsed={false}
+        hasItems={!!afgehandeldeBezwaren?.length}
+        isLoading={isLoading(BEZWAREN)}
+        className={styles.SectionCollapsibleFirst}
+      >
+        <Table
+          className={styles.DocumentsTable}
+          displayProps={DISPLAY_PROPS_BEZWAREN_AFGEROND}
+          items={afgehandeldeBezwaren}
+        />
+      </SectionCollapsible>
     </OverviewPage>
   );
 }
