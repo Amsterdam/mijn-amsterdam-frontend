2f5499dddc701cf461a21c24bf29cd558a3626ce
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-09-07 13:37:56 +0200

    Change MainHeader

--- src/client/components/MainHeader/MainHeader.tsx
+++ src/client/components/MainHeader/MainHeader.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-
+import { Heading } from '@amsterdam/design-system-react';
 import { AppRoutes } from '../../../universal/config';
 import { ReactComponent as AmsterdamLogoLarge } from '../../assets/images/logo-amsterdam-large.svg';
 import { ReactComponent as AmsterdamLogo } from '../../assets/images/logo-amsterdam.svg';
@@ -9,7 +9,6 @@ import { useDesktopScreen, usePhoneScreen } from '../../hooks/media.hook';
 import { useAppStateGetter } from '../../hooks/useAppState';
 import Linkd from '../Button/Button';
 import ErrorMessages from '../ErrorMessages/ErrorMessages';
-import Heading from '../Heading/Heading';
 import MainHeaderHero from '../MainHeaderHero/MainHeaderHero';
 import MainNavBar from '../MainNavBar/MainNavBar';
 import styles from './MainHeader.module.scss';
@@ -52,13 +51,13 @@ export default function MainHeader({
             />
           </a>
           {location.pathname !== AppRoutes.ROOT ? (
-            <Heading size="large" el="h1">
+            <Heading level={2} size="level-1">
               <Link to={AppRoutes.ROOT} title="Terug naar home">
                 Mijn Amsterdam
               </Link>
             </Heading>
           ) : (
-            <Heading size="large" el="h1">
+            <Heading level={2} size="level-1">
               Mijn Amsterdam
             </Heading>
           )}
