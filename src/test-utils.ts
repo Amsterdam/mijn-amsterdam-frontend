46b3109365a17c7c842172ad37a9b382a7d40453
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-09-12 10:41:04 +0200

    Restore headers

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -2,10 +2,8 @@ import nock from 'nock';
 import { bffApiHost, remoteApiHost } from './setupTests';
 
 const defaultReplyHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS',
-  'Access-Control-Allow-Headers':
-    'Content-Type, Origin, Accept, Authorization, User-Agent, Content-Length, X-Requested-With',
+  'access-control-allow-origin': '*',
+  'access-control-allow-credentials': 'true',
 };
 
 export const bffApi = nock(`${bffApiHost}:80`).defaultReplyHeaders(
