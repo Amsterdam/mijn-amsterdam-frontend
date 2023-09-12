5b39e5fd0c499bef5b192f28216df2aaace254b1
janthijs <92784122+janthijs@users.noreply.github.com>
2023-09-12 17:47:36 +0200

    fix: ctd script

--- release.js
+++ release.js
@@ -1,5 +1,7 @@
 const child_process = require('child_process');
 
+const RELEASE_MERGE_MESSAGE = 'Bump!';
+
 function executeCommandSync(command) {
   return child_process.execSync(command).toString();
 }
@@ -21,4 +23,19 @@ const lines = executeCommandSync(
   `git log --pretty="%s %cI" --no-merges --since="${dateOfLastTag}"`
 );
 
-console.log(lines);
+const splitLines = lines.trim().split(/(?:\r\n|\r|\n)/g);
+const jiraTicketRegex = /(mijn(-| )\d+|bump!)/gi;
+
+splitLines.map((line) => {
+  if (line.startsWith(RELEASE_MERGE_MESSAGE)) {
+    return; // The release is merged after the release tag is created.
+    // So we might (should) encounter the previous release merge commit.
+  }
+
+  const match = jiraTicketRegex.exec(line.trim());
+
+  if (!!match) {
+    // each line is a commit message since the last release
+    console.log(line, match[0]);
+  }
+});
