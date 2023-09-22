c9ab1d5167b1c02aff02c31f4ea3b30b3cc4553a
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2023-09-22 15:41:16 +0200

    fix: update

--- src/client/pages/BezwarenDetail/BezwarenStatusLines.tsx
+++ src/client/pages/BezwarenDetail/BezwarenStatusLines.tsx
@@ -5,7 +5,7 @@ import { StatusLine } from '../../components';
 function transformBezwaarStatusToStatusLines(statussen: BezwaarStatus[]) {
   const index = statussen.findIndex((s) => s.uuid === EMPTY_UUID);
   const activeIndex =
-    index > 0 ? index - 1 : index === 0 ? index : statussen.length;
+    index > 0 ? index - 1 : index === 0 ? index : statussen.length - 1;
 
   return statussen.map((status, index) => ({
     id: status.statustoelichting,
