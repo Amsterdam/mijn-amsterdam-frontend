2cdc01b72b90b02235eb939e8f75f0d6f10574dc
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2023-10-03 14:51:38 +0200

    try this

--- scripts/release-notifications/echo.js
+++ scripts/release-notifications/echo.js
@@ -1,6 +1,56 @@
 const args = process.argv.slice(2);
 
-const WEBHOOK = args[0].split('=')[1];
+const PATH = args[0].split('=')[1];
 const HOST = args[1].split('=')[1];
 
-console.log(HOST, WEBHOOK);
+const post_data = {
+  type: 'message',
+  attachments: [
+    {
+      contentType: 'application/vnd.microsoft.card.adaptive',
+      content: {
+        type: 'AdaptiveCard',
+        body: [
+          {
+            type: 'TextBlock',
+            text: 'Message Text',
+          },
+        ],
+        $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+        version: '1.0',
+      },
+    },
+  ],
+};
+
+const post_options = {
+  host: HOST,
+  port: 443,
+  path: PATH,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(post_data),
+    'User-Agent': 'nodejs',
+  },
+};
+
+try {
+  // Set up the request
+  const post_req = https.request(post_options, function (res) {
+    res.setEncoding('utf8');
+    res.on('error', function (err) {
+      console.error(err.stack);
+      console.log('Node NOT Exiting...');
+    });
+    res.on('data', function (chunk) {
+      console.log('Response: ' + chunk);
+    });
+  });
+
+  post_req.write(post_data);
+  post_req.end();
+} catch (e) {
+  console.error(e);
+  console.log('Node NOT Exiting...');
+}
