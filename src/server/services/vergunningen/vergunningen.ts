025d614d02d866915c53d7e26d3a5b87a678d459
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2023-10-13 15:05:17 +0200

    fix: remove ?

--- src/server/services/vergunningen/vergunningen.ts
+++ src/server/services/vergunningen/vergunningen.ts
@@ -322,7 +322,7 @@ export function transformVergunningenData(
     return [];
   }
 
-  let vergunningen: Vergunning[] = responseData?.content?.map((item) => {
+  let vergunningen: Vergunning[] = responseData.content.map((item) => {
     const id = hash(
       `vergunning-${(item.identifier || item.caseType) + item.dateRequest}`
     );
