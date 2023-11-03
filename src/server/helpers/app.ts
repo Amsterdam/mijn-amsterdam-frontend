b84f035f0f72c353126e20704751bdcad331f187
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-11-03 15:51:42 +0100

    Api

--- src/server/helpers/app.ts
+++ src/server/helpers/app.ts
@@ -213,7 +213,7 @@ const getJWKSKey = memoize(async () => {
         url: process.env.BFF_OIDC_JWKS_URL,
         responseType: 'json',
       })
-      .then((response) => jose.createLocalJWKSet(response.data));
+      .then((response) => jose.createLocalJWKSet(response.data)());
   }
 
   return getPublicKeyForDevelopment();
