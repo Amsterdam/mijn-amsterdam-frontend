668e123bd5fb042a16e23629976281fe0a9a958f
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-11-14 14:26:20 +0100

    Change flag

--- src/server/services/db/config.ts
+++ src/server/services/db/config.ts
@@ -1,4 +1,4 @@
-import { IS_AP, IS_DEVELOPMENT, IS_OT } from '../../../universal/config';
+import { IS_AP, IS_OT } from '../../../universal/config';
 
-export const IS_PG = IS_DEVELOPMENT;
+export const IS_PG = IS_AP;
 export const IS_VERBOSE = IS_OT;
