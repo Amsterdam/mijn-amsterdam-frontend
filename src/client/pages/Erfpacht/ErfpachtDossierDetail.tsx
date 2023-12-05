ee244f7300348d5f7796acd203c80cb8ff2adc9b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-12-05 15:09:16 +0100

    Add panel component, add panels to detailpage

--- src/client/pages/Erfpacht/ErfpachtDossierDetail.tsx
+++ src/client/pages/Erfpacht/ErfpachtDossierDetail.tsx
@@ -1,34 +1,24 @@
 import {
   Alert,
-  Heading,
   Grid,
-  Screen,
+  Heading,
   Link,
-  UnorderedList,
-  Paragraph,
   OrderedList,
+  Paragraph,
+  Screen,
 } from '@amsterdam/design-system-react';
-import { AppRoutes, ChapterTitles } from '../../../universal/config';
-import {
-  ApiResponse,
-  apiPristineResult,
-  isError,
-  isLoading,
-} from '../../../universal/helpers';
-import { ChapterIcon, DetailPage, PageHeading } from '../../components';
-import { useAppStateGetter } from '../../hooks';
-import { TableV2 } from '../../components/Table/TableV2';
-import { MaParagraph } from '../../components/Paragraph/Paragraph';
-import { MAX_TABLE_ROWS_ON_THEMA_PAGINA } from '../../config/app';
-import { LinkToListPage } from '../../components/LinkToListPage/LinkToListPage';
-import { addLinkElementToProperty } from '../../components/Table/Table';
-import { useErfpachtV2Data } from './Erfpacht';
+import classname from 'classnames';
 import { useParams } from 'react-router-dom';
-import { useDataApi } from '../../hooks/api/useDataApi';
+import { AppRoutes, ChapterTitles } from '../../../universal/config';
+import { ApiResponse, apiPristineResult } from '../../../universal/helpers';
+import { ChapterIcon, DetailPage, PageHeading } from '../../components';
+import { TableV2 } from '../../components/Table/TableV2';
 import { BFFApiUrls } from '../../config/api';
+import { useDataApi } from '../../hooks/api/useDataApi';
 
-import styles from './ErfpachtDossierDetail.module.scss';
 import type { ErfpachtV2DossiersDetail } from '../../../server/services/simple-connect/erfpacht';
+import { CollapsiblePanel } from '../../components/CollapsiblePanel/CollapsiblePanel';
+import styles from './ErfpachtDossierDetail.module.scss';
 
 export default function ErfpachtDossierDetail() {
   const { id } = useParams<{ id: string }>();
@@ -57,14 +47,14 @@ export default function ErfpachtDossierDetail() {
       </PageHeading>
       <Screen>
         <style>
-          {/* {`
+          {`
           :root {
             --mams-font-size: initial;
             --mams-line-height: initial;
           }
-          `} */}
+          `}
         </style>
-        <Grid>
+        <Grid className={styles.Grid}>
           {(api.isError || noContent) && (
             <Grid.Cell fullWidth>
               <Alert title="Foutmelding" icon severity="error">
@@ -117,127 +107,171 @@ export default function ErfpachtDossierDetail() {
                     </OrderedList>
                   </dd>
                 </dl>
-                <Heading level={3} size="level-2">
-                  Juridisch
-                </Heading>
-                <dl>
-                  <dt>{dossier.juridisch.titelAlgemeneBepaling}</dt>
-                  <dd>
-                    <Link href="">{dossier.juridisch.algemeneBepaling}</Link>
-                  </dd>
-                  <dt>{dossier.juridisch.titelIngangsdatum}</dt>
-                  <dd>{dossier.juridisch.ingangsdatum}</dd>
-                  <dt>{dossier.juridisch.titelSoortErfpacht}</dt>
-                  <dd>{dossier.juridisch.soortErfpacht}</dd>
-                </dl>
-                <Heading level={3} size="level-2">
-                  Bijzondere bepalingen
-                </Heading>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>
-                        {dossier.bestemmingen?.[0].titelBestemmingOmschrijving}
-                      </th>
-                      <th>Oppervlakte</th>
-                    </tr>
-                  </thead>
-                  {dossier.bestemmingen.map((bestemming) => {
-                    return (
+              </Grid.Cell>
+              <Grid.Cell fullWidth className={styles.Section}>
+                <CollapsiblePanel title="Juridisch">
+                  <dl>
+                    <dt>{dossier.juridisch.titelAlgemeneBepaling}</dt>
+                    <dd>
+                      <Link href="">{dossier.juridisch.algemeneBepaling}</Link>
+                    </dd>
+                    <dt>{dossier.juridisch.titelIngangsdatum}</dt>
+                    <dd>{dossier.juridisch.ingangsdatum}</dd>
+                    <dt>{dossier.juridisch.titelSoortErfpacht}</dt>
+                    <dd>{dossier.juridisch.soortErfpacht}</dd>
+                  </dl>
+                </CollapsiblePanel>
+              </Grid.Cell>
+              <Grid.Cell fullWidth className={styles.Section}>
+                <CollapsiblePanel title="Bijzondere bepalingen">
+                  <table>
+                    <thead>
                       <tr>
-                        <td>{bestemming.samengesteldeOmschrijving}</td>
-                        <td>
-                          {bestemming.oppervlakte}
-                          {bestemming.eenheid}
-                        </td>
+                        <th>
+                          {
+                            dossier.bestemmingen?.[0]
+                              .titelBestemmingOmschrijving
+                          }
+                        </th>
+                        <th>Oppervlakte</th>
                       </tr>
+                    </thead>
+                    {dossier.bestemmingen.map((bestemming) => {
+                      return (
+                        <tr>
+                          <td>{bestemming.samengesteldeOmschrijving}</td>
+                          <td>
+                            {bestemming.oppervlakte}
+                            {bestemming.eenheid}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </table>
+                </CollapsiblePanel>
+              </Grid.Cell>
+              <Grid.Cell
+                className={classname(styles.Section, styles.Section_Financieel)}
+                fullWidth
+              >
+                <CollapsiblePanel title="Financieel">
+                  <Heading level={3} className={styles.Section__heading}>
+                    {
+                      dossier.financieel.huidigePeriode
+                        .titelFinancieelPeriodeVan
+                    }{' '}
+                    {dossier.financieel.huidigePeriode.periodeVan} t/m{' '}
+                    {dossier.financieel.huidigePeriode.periodeTm}
+                  </Heading>
+                  <dl>
+                    <dt>
+                      {
+                        dossier.financieel.huidigePeriode
+                          .titelFinancieelAlgemeneBepaling
+                      }
+                    </dt>
+                    <dd>
+                      {dossier.financieel.huidigePeriode.algemeneBepaling}
+                    </dd>
+                    <dt>
+                      {dossier.financieel.huidigePeriode.titelFinancieelRegime}
+                    </dt>
+                    <dd>{dossier.financieel.huidigePeriode.regime}</dd>
+                    <dt>
+                      {dossier.financieel.huidigePeriode.titelFinancieelCanon}
+                    </dt>
+                    <dd>
+                      <OrderedList markers={false}>
+                        {dossier.financieel.huidigePeriode.canons.map(
+                          (canon) => {
+                            return (
+                              <OrderedList.Item>
+                                {canon.samengesteld}
+                              </OrderedList.Item>
+                            );
+                          }
+                        )}
+                      </OrderedList>
+                    </dd>
+                  </dl>
+                  {dossier.financieel.toekomstigePeriodeList.map((periode) => {
+                    return (
+                      <>
+                        <Heading
+                          level={3}
+                          className={classname(
+                            styles.Section__heading,
+                            styles.Section__heading_nested
+                          )}
+                        >
+                          {periode.titelFinancieelToekomstigePeriodeVan}{' '}
+                          {periode.periodeVan} t/m {periode.periodeTm}
+                        </Heading>
+                        <dl>
+                          <dt>
+                            {periode.titelFinancieelToekomstigeAlgemeneBepaling}
+                          </dt>
+                          <dd>{periode.algemeneBepaling}</dd>
+                          <dt>{periode.titelFinancieelToekomstigeRegime}</dt>
+                          <dd>{periode.regime}</dd>
+                          <dt>{periode.titelFinancieelToekomstigeCanon}</dt>
+                          <dd>
+                            <OrderedList markers={false}>
+                              {periode.canonOmschrijvingen.map(
+                                (omschrijving) => {
+                                  return (
+                                    <OrderedList.Item>
+                                      {omschrijving}
+                                    </OrderedList.Item>
+                                  );
+                                }
+                              )}
+                            </OrderedList>
+                          </dd>
+                        </dl>
+                      </>
                     );
                   })}
-                </table>
-                <Heading level={2}>Financieel</Heading>
-                <Heading level={3}>
-                  {dossier.financieel.huidigePeriode.titelFinancieelPeriodeVan}{' '}
-                  {dossier.financieel.huidigePeriode.periodeVan} t/m{' '}
-                  {dossier.financieel.huidigePeriode.periodeTm}
-                </Heading>
-                <dl>
-                  <dt>
-                    {dossier.financieel.huidigePeriode.titelFinancieelRegime}
-                  </dt>
-                  <dd>{dossier.financieel.huidigePeriode.regime}</dd>
-                  <dt>
-                    {dossier.financieel.huidigePeriode.titelFinancieelCanon}
-                  </dt>
-                  <dd>
-                    <OrderedList>
-                      {dossier.financieel.huidigePeriode.canons.map((canon) => {
-                        return (
-                          <OrderedList.Item>
-                            {canon.samengesteld}
-                          </OrderedList.Item>
-                        );
-                      })}
-                    </OrderedList>
-                  </dd>
-                </dl>
-                {dossier.financieel.toekomstigePeriodeList.map((periode) => {
-                  return (
-                    <>
-                      <Heading level={3}>
-                        {periode.titelFinancieelToekomstigePeriodeVan}{' '}
-                        {periode.periodeVan} t/m {periode.periodeTm}
-                      </Heading>
+                </CollapsiblePanel>
+              </Grid.Cell>
+              <Grid.Cell className={styles.Section} fullWidth>
+                <CollapsiblePanel title="Facturen">
+                  <Grid className={styles.GridNested}>
+                    <Grid.Cell span={3} start={1}>
                       <dl>
-                        <dt>{periode.titelFinancieelToekomstigeRegime}</dt>
-                        <dd>{periode.regime}</dd>
-                        <dt>{periode.titelFinancieelToekomstigeCanon}</dt>
-                        <dd>
-                          <OrderedList>
-                            {periode.canonOmschrijvingen.map((omschrijving) => {
-                              return (
-                                <OrderedList.Item>
-                                  {omschrijving}
-                                </OrderedList.Item>
-                              );
-                            })}
-                          </OrderedList>
-                        </dd>
+                        <dt>{dossier.titelBetaler}</dt>
+                        <dd>{dossier.facturen.betaler}</dd>
                       </dl>
-                    </>
-                  );
-                })}
-                <Heading level={3} size="level-2">
-                  Facturen
-                </Heading>
-              </Grid.Cell>
-              <Grid.Cell span={3} start={1}>
-                <dl>
-                  <dt>{dossier.titelBetaler}</dt>
-                  <dd>{dossier.facturen.betaler}</dd>
-                </dl>
-              </Grid.Cell>
-              <Grid.Cell span={3} start={4}>
-                <dl>
-                  <dt>Debiteurnummer</dt>
-                  <dd>{dossier.facturen.debiteurnummer}</dd>
-                </dl>
-              </Grid.Cell>
-              <Grid.Cell fullWidth>
-                {!!dossier.facturen.notas.length && (
-                  <TableV2
-                    titleKey="dossieradres"
-                    items={dossier.facturen.notas}
-                    displayProps={{
-                      notaNummer:
-                        dossier.facturen.notas?.[0].titelFacturenNotaNummer,
-                      formattedFactuurBedrag:
-                        dossier.facturen.notas?.[0].titelFacturenFactuurBedrag,
-                      status: dossier.facturen.notas?.[0].titelFacturenStatus,
-                      vervalDatum:
-                        dossier.facturen.notas?.[0].titelFacturenVervaldatum,
-                    }}
-                  />
-                )}
+                    </Grid.Cell>
+                    <Grid.Cell span={3} start={4}>
+                      <dl>
+                        <dt>Debiteurnummer</dt>
+                        <dd>{dossier.facturen.debiteurnummer}</dd>
+                      </dl>
+                    </Grid.Cell>
+                    <Grid.Cell fullWidth>
+                      {!!dossier.facturen.notas.length && (
+                        <TableV2
+                          titleKey="dossieradres"
+                          items={dossier.facturen.notas}
+                          displayProps={{
+                            notaNummer:
+                              dossier.facturen.notas?.[0]
+                                .titelFacturenNotaNummer,
+                            formattedFactuurBedrag:
+                              dossier.facturen.notas?.[0]
+                                .titelFacturenFactuurBedrag,
+                            status:
+                              dossier.facturen.notas?.[0].titelFacturenStatus,
+                            vervalDatum:
+                              dossier.facturen.notas?.[0]
+                                .titelFacturenVervaldatum,
+                          }}
+                        />
+                      )}
+                    </Grid.Cell>
+                  </Grid>
+                </CollapsiblePanel>
               </Grid.Cell>
             </>
           )}
