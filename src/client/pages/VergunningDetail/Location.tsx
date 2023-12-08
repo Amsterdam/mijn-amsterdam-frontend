01ea5c57fc3c1aae34d4e146dc5a4bfdd094d2e3
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-12-08 12:35:53 +0100

    Update env reference

--- src/client/pages/VergunningDetail/Location.tsx
+++ src/client/pages/VergunningDetail/Location.tsx
@@ -51,7 +51,7 @@ export function Location({
       url: '',
       postpone: true,
       headers: {
-        'X-Api-Key': process.env.BFF_DATA_AMSTERDAM_API_KEY,
+        'X-Api-Key': import.meta.env.BFF_DATA_AMSTERDAM_API_KEY,
       },
     },
     null
