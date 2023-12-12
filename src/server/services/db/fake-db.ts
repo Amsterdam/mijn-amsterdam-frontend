baa8febf7175f192750a7bd0e2a2edac4bbc0801
oscarbakker <bakkeroscar@gmail.com>
2023-12-12 14:27:04 +0100

    Add tests for fake-db

--- src/server/services/db/fake-db.ts
+++ src/server/services/db/fake-db.ts
@@ -1,11 +1,11 @@
 export async function query(queryString: string, values?: any[]) {
-    return null;
+  return null
 }
 
 export async function queryGET(queryString: string, values?: any[]) {
-    return null;
+  return null
 }
 
 export async function queryALL(queryString: string, values?: any[]) {
-    return [];
+  return []
 }
