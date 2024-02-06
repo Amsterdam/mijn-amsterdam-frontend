f6cad352d0f02df686e86495c590b3da75c89917
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-02-06 15:46:36 +0100

    Disable headers

--- src/server/app.ts
+++ src/server/app.ts
@@ -82,7 +82,7 @@ app.set('views', `./${viewDir}/server/views`);
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  standardHeaders: false, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
