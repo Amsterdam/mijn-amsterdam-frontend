dc703c28260802341d986f8d37860a1d250ab820
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-02-09 08:59:33 +0100

    Remove icon attr

--- src/client/pages/Erfpacht/Erfpacht.tsx
+++ src/client/pages/Erfpacht/Erfpacht.tsx
@@ -83,7 +83,7 @@ export default function Erfpacht() {
 
           {isError(ERFPACHTv2) && (
             <Grid.Cell span="all">
-              <Alert title="Foutmelding" icon severity="error">
+              <Alert title="Foutmelding" severity="error">
                 <Paragraph>
                   We kunnen op dit moment geen erfpachtrechten tonen.
                 </Paragraph>
