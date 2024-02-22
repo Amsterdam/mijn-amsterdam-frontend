9fbc652eeabfe1df31315c03536c62fbe275e72a
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-02-22 12:42:25 +0100

    Remove index prop

--- src/client/pages/Erfpacht/DossierDetail/ErfpachtersList.tsx
+++ src/client/pages/Erfpacht/DossierDetail/ErfpachtersList.tsx
@@ -64,7 +64,7 @@ export function ErfpachtersList({
                     : undefined
                 }
               >
-                {index}-{relatie.relatieNaam}{' '}
+                {relatie.relatieNaam}{' '}
                 {relatie.betaler && hasBetalerWijzigenLink ? (
                   <WijzigenLink
                     linkVariant="inList"
