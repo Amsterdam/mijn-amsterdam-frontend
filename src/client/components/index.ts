e691f2cbe64afa83f004028035b53a401b79bcc5
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-03-06 16:39:37 +0100

    Temp fix for DX

--- src/client/components/index.ts
+++ src/client/components/index.ts
@@ -1,4 +1,5 @@
 export { ErrorAlert } from './Alert/Alert';
+export { default as Alert } from './Alert/Alert';
 export { default as AlphaComponent } from './AlphaComponent/AlphaComponent';
 export { default as ApplicationError } from '../pages/ApplicationError/ApplicationError';
 export { default as AutoLogoutDialog } from './AutoLogoutDialog/AutoLogoutDialog';
