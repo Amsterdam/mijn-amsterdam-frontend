611f38442022ba6ea0ba10eda8266e394c20f419
Yacine <yacineh@hotmail.com>
2024-03-28 17:18:27 +0100

    Added severity as possible variable

--- src/client/components/Alert/Alert.tsx
+++ src/client/components/Alert/Alert.tsx
@@ -1,31 +1,22 @@
-import { Alert as DSAlert, Paragraph } from '@amsterdam/design-system-react';
+import { Alert as DSAlert, Paragraph, AlertProps } from '@amsterdam/design-system-react';
 import { ComponentChildren } from '../../../universal/types';
 
 export interface ComponentProps {
   children?: ComponentChildren;
   title?: string;
+  severityInput?: any
 }
 
 export function ErrorAlert({
   children,
   title = 'Foutmelding',
+  severityInput="error"
+
 }: ComponentProps) {
   return (
-    <DSAlert title={title} severity="error">
+    <DSAlert title={title} severity={severityInput}>
       <Paragraph>{children}</Paragraph>
     </DSAlert>
   );
 }
 
-export default function OUDE_ALERT_WEGHALEN({
-  children,
-  type = 'success',
-  className,
-}: any) {
-  return (
-    <div style={{ border: '5px solid red' }}>
-      <strong>WEGHALEN!!!?</strong>
-      {children}
-    </div>
-  );
-}
