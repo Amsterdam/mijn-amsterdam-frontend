108aa71e1d2f0e764d6197b368227b0a69aea2d9
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-04-15 09:37:30 +0200

    Mijn-7888 alert maken (#1213)

--- src/client/components/ListPagePaginated/ListPagePaginated.tsx
+++ src/client/components/ListPagePaginated/ListPagePaginated.tsx
@@ -1,7 +1,7 @@
-import { Alert, Grid, Paragraph, Screen } from '@amsterdam/design-system-react';
+import { Grid, Paragraph, Screen } from '@amsterdam/design-system-react';
 import { ReactNode, useMemo } from 'react';
 import { generatePath, useHistory, useParams } from 'react-router-dom';
-import { ChapterIcon, LoadingContent, OverviewPage, PageHeading } from '..';
+import { ChapterIcon, ErrorAlert, LoadingContent, OverviewPage, PageHeading } from '..';
 import { Chapter } from '../../../universal/config';
 import { PaginationV2 } from '../Pagination/PaginationV2';
 import { TableV2 } from '../Table/TableV2';
@@ -77,9 +77,7 @@ export function ListPagePaginated({
           {!!body && <Grid.Cell span="all">{body}</Grid.Cell>}
           {isError && (
             <Grid.Cell span="all">
-              <Alert title="Foutmelding" severity="error">
-                <Paragraph>{errorText}</Paragraph>
-              </Alert>
+              <ErrorAlert>{errorText}</ErrorAlert>
             </Grid.Cell>
           )}
           <Grid.Cell span="all">
