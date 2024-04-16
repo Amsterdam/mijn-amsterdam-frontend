713157fb79765478d3fd70eab8233c17b67fcd68
Oscar <bakkeroscar@gmail.com>
2024-04-16 10:17:20 +0200

    MIJN-7472/Add tip when oldest kid is born after 2016 and parents are married or living together by contract  (#1230)
    * Add tip when oldest kid is born after 2016 and couple is married or geregistreerd partnerschap
    * Change predicate, change tips and change brp json for tests
    * Improve readability
    * Update bff tests
    
    ---------
    
    Co-authored-by: Tim van Oostrom <t.van.oostrom@amsterdam.nl>

--- src/server/services/tips/predicates.test.ts
+++ src/server/services/tips/predicates.test.ts
@@ -12,6 +12,7 @@ import {
   hasBnBVergunning,
   hasDutchNationality,
   hasKidsBetweenAges2And18,
+  hasOldestKidBornFrom2016,
   hasStadspasGroeneStip,
   hasToeristicheVerhuurVergunningen,
   hasTozo,
@@ -21,6 +22,7 @@ import {
   is18OrOlder,
   isBetween17and18,
   isLivingInAmsterdamLessThanNumberOfDays,
+  isMarriedOrLivingTogether,
   isReceivingSubsidy,
   not,
   or,
@@ -232,6 +234,56 @@ describe('predicates', () => {
         (expected, birthdate1, birthdate2) => {
           expect(
             hasKidsBetweenAges2And18(getMockAppState(birthdate1, birthdate2))
+          ).toBe(expected);
+        }
+      );
+    });
+
+    describe('hasOldestKidBornFrom2016', () => {
+      const getMockAppState = (
+        geboortedatumKind1: string,
+        geboortedatumKind2: string
+      ) => {
+        const BRPCopy = { ...BRP };
+
+        BRP.content.kinderen[0].geboortedatum = geboortedatumKind1;
+        BRP.content.kinderen[1].geboortedatum = geboortedatumKind2;
+
+        return getBRPAppState(BRPCopy);
+      };
+
+      it.each([
+        [false, '2019-01-04', '2015-05-05'],
+        [true, '2016-01-01', '2021-12-25'],
+        [false, '1990-03-20', '2000-10-29'],
+        [false, '2024-07-26', '2024-07-24'],
+      ])(
+        'should return %s for kids with birthdays at %s and %s',
+        (expected, birthdate1, birthdate2) => {
+          expect(
+            hasOldestKidBornFrom2016(getMockAppState(birthdate1, birthdate2))
+          ).toBe(expected);
+        }
+      );
+    });
+
+    describe('isMarriedOrLivingTogether', () => {
+      const getMockAppState = (burgerlijkeStaat: string) => {
+        const BRPCopy = { ...BRP };
+
+        BRPCopy.content.verbintenis.soortVerbintenis = burgerlijkeStaat;
+
+        return getBRPAppState(BRPCopy);
+      };
+
+      it.each([
+        [true, 'h'],
+        [false, ''],
+      ])(
+        'should return %s for burgerlijkeStaat %s',
+        (expected, burgerlijkeStaat) => {
+          expect(
+            isMarriedOrLivingTogether(getMockAppState(burgerlijkeStaat))
           ).toBe(expected);
         }
       );
