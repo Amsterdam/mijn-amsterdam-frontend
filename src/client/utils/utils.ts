c2725dfe4eb09fb20751f6ad851befa6a6e16ec7
oscarbakker <bakkeroscar@gmail.com>
2024-05-06 16:54:15 +0200

    Remove import

--- src/client/utils/utils.ts
+++ src/client/utils/utils.ts
@@ -1,5 +1,4 @@
 import { BFF_API_BASE_URL } from '../config/api';
-import { WerkzaamhedenEnVervoerOpStraat } from '../../server/services';
 
 export function relayApiUrl(path: string) {
   return `${BFF_API_BASE_URL}/relay${path}`;
