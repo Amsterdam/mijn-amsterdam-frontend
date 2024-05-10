39b3edac0bbf7f789e934b79eb021a39895d6f8d
Anouk Boukema <boukema.a@gmail.com>
2024-05-10 14:21:10 +0200

    If 2 locations show in columns

--- src/client/pages/VergunningDetail/EigenParkeerplaats.tsx
+++ src/client/pages/VergunningDetail/EigenParkeerplaats.tsx
@@ -1,9 +1,12 @@
 import { Link } from '@amsterdam/design-system-react';
 import type { EigenParkeerplaats as EigenParkeerplaatsType } from '../../../server/services';
 import { defaultDateFormat } from '../../../universal/helpers';
-import InfoDetail from '../../components/InfoDetail/InfoDetail';
+import InfoDetail, {
+  InfoDetailGroup,
+} from '../../components/InfoDetail/InfoDetail';
 import { Location } from './Location';
 import styles from '../../components/LocationModal/LocationModal.module.scss';
+import { JSX } from 'react/jsx-runtime';
 
 export function EigenParkeerplaats({
   vergunning,
@@ -12,11 +15,34 @@ export function EigenParkeerplaats({
 }) {
   const isVerleend = vergunning.processed && vergunning.decision === 'Verleend';
   const isAfgehandeld = vergunning.processed;
-  let location1 = null;
-  let location2 = null;
+
+  let locationHTMLs: JSX.Element[] = [];
 
   if (Array.isArray(vergunning.locations)) {
-    [location1, location2] = vergunning.locations;
+    vergunning.locations.forEach((location, i) => {
+      let name = 'Adres';
+      // If two addresses: indicate the difference by adding 1 or 2.
+      if (vergunning.locations!.length > 1) name = `Adres ${i + 1}`;
+      locationHTMLs.push(
+        <Location
+          label={name}
+          location={`${location.street} ${location.houseNumber}`}
+        />,
+        <InfoDetail label="Soort plek" value={location.type} />,
+        <InfoDetail
+          label="Parkeervak"
+          value={
+            <Link
+              className={styles.LocationModalLink}
+              variant="inline"
+              href={location.url}
+            >
+              Bekijk parkeervak
+            </Link>
+          }
+        />
+      );
+    });
   }
 
   return (
@@ -24,82 +50,44 @@ export function EigenParkeerplaats({
       <InfoDetail label="Kenmerk" value={vergunning.identifier || '-'} />
       <InfoDetail
         label="Verzoek"
+        valueWrapperElement="div"
         value={
           <ul>
             {vergunning.requestTypes.map((d, i) => (
-              <li>{d}</li>
+              <li key={i}>{d}</li>
             ))}
           </ul>
         }
       />
 
-      {!!location1 && (
-        <>
-          <Location
-            label="Adres"
-            location={`${location1.street} ${location1.houseNumber}`}
-          />
-          <InfoDetail label="Soort plek" value={location1.type} />
-          <InfoDetail
-            label="Parkeervak"
-            value={
-              <Link
-                className={styles.LocationModalLink}
-                variant="inline"
-                href={location1.url}
-              >
-                Bekijk parkeervak
-              </Link>
-            }
-          />
-        </>
-      )}
-
-      {!!location2 && (
-        <>
-          <Location
-            label="Adres"
-            location={`${location2.street} ${location2.houseNumber}`}
-          />
-          <InfoDetail label="Soort plek" value={location2.type} />
-          <InfoDetail
-            label="Parkeervak"
-            value={
-              <Link
-                className={styles.LocationModalLink}
-                variant="inline"
-                href={location2.url}
-              >
-                Bekijk parkeervak
-              </Link>
-            }
-          />
-        </>
-      )}
+      {/* Bc above exactly 3 different InfoDetails are defined we can loop over
+      them and put them together in a a row/InfoDetailGroup. */}
+      {[0, 1, 2].map((i) => (
+        <InfoDetailGroup key={i}>
+          {locationHTMLs[i]}
+          {locationHTMLs[i + 3]}
+        </InfoDetailGroup>
+      ))}
 
       <InfoDetail label="Kenteken(s)" value={vergunning.licensePlates} />
-
       {vergunning.requestTypes.some((type) => type === 'Kentekenwijziging') && (
         <InfoDetail
           label="Oude kenteken"
           value={vergunning.previousLicensePlates}
         />
       )}
-
       {isVerleend && vergunning.dateStart && (
         <InfoDetail
           label="Startdatum"
           value={defaultDateFormat(vergunning.dateStart)}
         />
       )}
-
       {isVerleend && vergunning.dateEnd && (
         <InfoDetail
           label="Vervaldatum"
           value={defaultDateFormat(vergunning.dateEnd)}
         />
       )}
-
       {isAfgehandeld && (
         <InfoDetail label="Resultaat" value={vergunning.decision} />
       )}
