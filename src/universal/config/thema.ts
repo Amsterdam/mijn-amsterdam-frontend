746506f121ebec3dce07e29802f5e2e4596e187b
Yacine <yacineh@hotmail.com>
2024-05-25 15:39:00 +0200

    Nog wat themas verbouwd > werkt nog niet routes wordt nog aangeroepen per thema

--- src/universal/config/thema.ts
+++ src/universal/config/thema.ts
@@ -2,47 +2,58 @@ import { generatePath } from 'react-router-dom';
 import { LinkProps } from '../types/App.types';
 import { ExternalUrls } from './app';
 import { AppRoute, AppRoutes, TrackingConfig } from './routes';
-// import { AppState } from '../../client/AppState';
-// import { SVGComponent } from '../types';
-// import { IconAVG, IconBezwaren } from '../../client/assets/icons';
+import { AppState } from '../../client/AppState';
+import { Match } from '../../universal/types';
 
 //nieuw toegevoegd
-//import { SVGComponent } from '../../universal/types';
-//import { IconAVG, IconBezwaren } from '../../client/assets/icons';
+// import { SVGComponent } from '../../universal/types';
+// import { IconAVG, IconBezwaren } from '../../client/assets/icons';
 
 // Within the team we call these Themes / Thema's
 export type Thema =
-  | 'AFVAL'
-  | 'BELASTINGEN'
-  | 'BURGERZAKEN'
-  | 'BUURT'
-  | 'BEZWAREN'
-  | 'INKOMEN'
+  //  | 'AFVAL'
+
+  //  | 'BELASTINGEN'
+  //| 'BURGERZAKEN'
+  | 'BUURT' //  | 'BEZWAREN'
+  // | 'INKOMEN'
   | 'STADSPAS'
-  | 'BRP'
+  // | 'BRP'
   | 'MILIEUZONE'
   | 'OVERTREDINGEN'
   | 'NOTIFICATIONS'
   | 'ROOT'
   | 'ERFPACHT'
   | 'ERFPACHTv2'
-  | 'ZORG'
+  // | 'ZORG'
   | 'VERGUNNINGEN'
   | 'SVWI'
   | 'KVK'
   | 'SIA'
-  | 'TOERISTISCHE_VERHUUR'
+  // | 'TOERISTISCHE_VERHUUR'
   | 'SEARCH'
   | 'SUBSIDIE'
   | 'PARKEREN'
-  | 'KLACHTEN'
-  | 'HORECA'
+  // | 'KLACHTEN'
+  //| 'HORECA'
   | 'KREFIA'
-  | 'AVG'
-  | 'BODEM'
+  //  | 'AVG'
+  // | 'BODEM'
   | string;
 
-export type ThemaIDs = 'BEZWAREN' | 'AVG';
+export type ThemaIDs =
+  | 'AVG'
+  | 'AFVAL'
+  | 'BELASTINGEN'
+  | 'BEZWAREN'
+  | 'BODEM'
+  | 'BRP'
+  | 'BURGERZAKEN'
+  | 'HORECA'
+  | 'INKOMEN'
+  | 'KLACHTEN'
+  | 'TOERISTISCHE_VERHUUR'
+  | 'ZORG';
 export type inlogType = 'private' | 'commercial';
 
 // export enum nums {
@@ -56,30 +67,144 @@ type ThemaConfig = {
   appRoute: string; //dit is wat je ziet in de url ziet nadat je op thema klikt (klopt)
   appRouteDetail: string; //dit is wat je ziet in de url ziet wanneer je op een detailpagina bent
   profileTypes: inlogType[]; //dit zijn de inlogtypes (DIGID/EHERK of BEIDE)
-  // icon: SVGComponent; // Add the themaIcon in '../../client/assets/icons' and import it.
+  //icon: SVGComponent; // Add the themaIcon in '../../client/assets/icons' and import it.
   //isThemaActive: (state: AppState) => { return: true };
 };
 
 export const themas2: Record<ThemaIDs, ThemaConfig> = {
+  AFVAL: {
+    title: 'Afval',
+    browserTabName: '| rond uw adres',
+    browserTabNameDetail: 'none',
+    appRoute: '/afval',
+    appRouteDetail: 'none',
+    profileTypes: ['private', 'commercial'],
+    //icon: IconAVG,
+    //isThemaActive: (state: AppState) => { return: true },
+  },
+
+  AVG: {
+    title: 'AVG persoonsgegevens',
+    browserTabName: '| verzoeken',
+    browserTabNameDetail: '| verzoek',
+    appRoute: generatePath(AppRoutes.AVG, { page: 1 }), //waarom niet gewoon '/avg'? (zie routes) en zie ook klachten?
+    appRouteDetail: '/avg/verzoek/:id',
+    profileTypes: ['private', 'commercial'],
+    //icon: IconAVG,
+    //isThemaActive: (state: AppState) => { return: true },
+  },
+  BELASTINGEN: {
+    //nog een voor Eherk
+    title: 'Belastingen',
+    browserTabName: 'none',
+    browserTabNameDetail: 'none',
+    appRoute: ExternalUrls.SSO_BELASTINGEN,
+    appRouteDetail: 'none',
+    //moet nog ergens >   rel: 'external',
+    profileTypes: ['private'],
+    //icon: IconBelastingen,
+    //isThemaActive: (state: AppState) => { return: true }
+  },
   BEZWAREN: {
-    title: 'Bezwaren3',
+    title: 'Bezwaren',
     browserTabName: '| overzicht',
     browserTabNameDetail: '| bezwaar',
     appRoute: '/bezwaren',
     appRouteDetail: '/bezwaren/:uuid',
     profileTypes: ['private', 'commercial'],
-    // icon: IconBezwaren,
+    //icon: IconBezwaren,
     //isThemaActive: (state: AppState) => { return: true }
   },
-  AVG: {
-    title: 'AVG persoonsgegevens',
-    browserTabName: '| verzoeken',
-    browserTabNameDetail: '| verzoek',
-    appRoute: generatePath(AppRoutes.AVG, { page: 1 }),
-    appRouteDetail: '/avg/verzoek/:id',
+  BODEM: {
+    title: 'Bodem',
+    browserTabName: '| overzicht',
+    browserTabNameDetail: '| lood in de bodem-check',
+    appRoute: '/bodem',
+    appRouteDetail: '/lood-meting/:id',
+    profileTypes: ['private', 'commercial'],
+    //icon: IconBodem,
+    //isThemaActive: (state: AppState) => { return: true }
+  },
+  BRP: {
+    title: 'Mijn gegevens',
+    browserTabName: '',
+    browserTabNameDetail: 'none',
+    appRoute: '/persoonlijke-gegevens',
+    appRouteDetail: 'none',
     profileTypes: ['private'],
-    // icon: IconAVG,
-    //isThemaActive: (state: AppState) => { return: true },
+    //icon: IconMijnGegevens,
+    //isThemaActive: (state: AppState) => { return: true }
+  },
+  BURGERZAKEN: {
+    title: 'Burgerzaken',
+    browserTabName: '| overzicht',
+    browserTabNameDetail: '| ID-kaart',
+    appRoute: '/burgerzaken',
+    appRouteDetail: '/burgerzaken/id-kaart/:id',
+    profileTypes: ['private'],
+    //icon: IconBurgerzaken,
+    //isThemaActive: (state: AppState) => { return: true }
+  },
+  HORECA: {
+    title: 'Horeca',
+    browserTabName: '| overzicht',
+    browserTabNameDetail: '| Vergunning',
+    appRoute: '/horeca/',
+    appRouteDetail: '/horeca/:title/:id',
+    profileTypes: ['private', 'commercial'],
+    //icon: IconHoreca,
+    //isThemaActive: (state: AppState) => { return: true }
+  },
+  INKOMEN: {
+    title: 'Inkomen',
+    browserTabName: '| overzicht',
+    browserTabNameDetail: '| Bijstandsuitkering', //Bijstandsuitkering | Tozo | Bbz | TONK
+    appRoute: '/inkomen',
+    appRouteDetail: '/inkomen/bijstandsuitkering/:id', // 'INKOMEN/BIJSTANDSUITKERING': '/inkomen/bijstandsuitkering/:id',
+    // 'INKOMEN/SPECIFICATIES': '/inkomen/specificaties/:variant/:page?',
+    // 'INKOMEN/TOZO': '/inkomen/tozo/:version/:id',
+    // 'INKOMEN/TONK': '/inkomen/tonk/:version/:id',
+    // 'INKOMEN/BBZ': '/inkomen/bbz/:version/:id',
+    profileTypes: ['private'],
+    //icon: IconWior,
+    //isThemaActive: (state: AppState) => { return: true }
+  },
+  KLACHTEN: {
+    title: 'Klachten',
+    browserTabName: '| overzicht',
+    browserTabNameDetail: '| klacht',
+    appRoute: '/klachten/:page?',
+    appRouteDetail: '/klachten/klacht/:id',
+    profileTypes: ['private'],
+    //icon: IconKlachten,
+    //isThemaActive: (state: AppState) => { return: true }
+  },
+  TOERISTISCHE_VERHUUR: {
+    title: 'Klachten',
+    browserTabName: '| overzicht',
+    browserTabNameDetail: '| Vergunning', //maar je hebt ook Vergunning vakantieverhuur en Vergunning Bed & Breakfast
+    appRoute: '/toeristische-verhuur',
+    appRouteDetail: '/toeristische-verhuur/vergunning/:id',
+    //maar je hebt ook nog
+    // 'TOERISTISCHE_VERHUUR/VERGUNNING/BB':
+    //   '/toeristische-verhuur/vergunning/bed-and-breakfast/:id',
+    // 'TOERISTISCHE_VERHUUR/VERGUNNING/VV':
+    //   '/toeristische-verhuur/vergunning/vakantieverhuur/:id',
+    profileTypes: ['private', 'commercial'],
+    //icon: IconToeristischeVerhuur,
+    //isThemaActive: (state: AppState) => { return: true }
+  },
+
+  ZORG: {
+    //zie ik niet terug > testen of je deze wel op de main bij Anouk ziet..
+    title: 'Zorg en ondersteuning',
+    browserTabName: '| overzicht',
+    browserTabNameDetail: '| Voorziening',
+    appRoute: '/zorg-en-ondersteuning',
+    appRouteDetail: '/zorg-en-ondersteuning/voorzieningen/:id',
+    profileTypes: ['private'],
+    //icon: IconZorg,
+    //isThemaActive: (state: AppState) => { return: true }
   },
 };
 
@@ -200,23 +325,26 @@ export const DocumentTitles: {
         return 'Inloggen | Mijn Amsterdam';
     }
   },
-  [AppRoutes.BURGERZAKEN]: `${ThemaTitles.BURGERZAKEN} | overzicht`,
-  [AppRoutes['BURGERZAKEN/ID-KAART']]: `ID-Kaart | ${ThemaTitles.BURGERZAKEN}`,
-  [AppRoutes.ZORG]: `${ThemaTitles.ZORG} | overzicht`,
-  [AppRoutes['ZORG/VOORZIENINGEN']]: `Voorziening | ${ThemaTitles.ZORG}`,
+  // [AppRoutes.BURGERZAKEN]: `${ThemaTitles.BURGERZAKEN} | overzicht`,
+  // [AppRoutes['BURGERZAKEN/ID-KAART']]: `ID-Kaart | ${ThemaTitles.BURGERZAKEN}`,
+  // [AppRoutes.ZORG]: `${ThemaTitles.ZORG} | overzicht`,
+  // [AppRoutes['ZORG/VOORZIENINGEN']]: `Voorziening | ${ThemaTitles.ZORG}`,
+  //inkomen moet nog verwerkt worden
   [AppRoutes.INKOMEN]: `${ThemaTitles.INKOMEN} | overzicht`,
   [AppRoutes['INKOMEN/BIJSTANDSUITKERING']]:
     `Bijstandsuitkering | ${ThemaTitles.INKOMEN}`,
-  [AppRoutes.STADSPAS]: `Stadspas | overzicht`,
-  [AppRoutes['STADSPAS/AANVRAAG']]: `Stadspas | ${ThemaTitles.INKOMEN}`,
-  [AppRoutes['STADSPAS/SALDO']]: `Stadspas saldo | ${ThemaTitles.INKOMEN}`,
   [AppRoutes['INKOMEN/TOZO']]: `Tozo | ${ThemaTitles.INKOMEN}`,
   [AppRoutes['INKOMEN/TONK']]: `TONK | ${ThemaTitles.INKOMEN}`,
   [AppRoutes['INKOMEN/BBZ']]: `Bbz | ${ThemaTitles.INKOMEN}`,
+  //einde inkomen moet verwerkt worden
+  [AppRoutes.STADSPAS]: `Stadspas | overzicht`,
+  [AppRoutes['STADSPAS/AANVRAAG']]: `Stadspas | ${ThemaTitles.INKOMEN}`,
+  [AppRoutes['STADSPAS/SALDO']]: `Stadspas saldo | ${ThemaTitles.INKOMEN}`,
+
   [AppRoutes['INKOMEN/SPECIFICATIES']]:
     `Uitkeringsspecificaties | ${ThemaTitles.INKOMEN}`,
   [`${AppRoutes['INKOMEN/SPECIFICATIES']}/jaaropgaven`]: `Jaaropgaven | ${ThemaTitles.INKOMEN}`,
-  [AppRoutes.BRP]: `Mijn gegevens`,
+  //[AppRoutes.BRP]: `Mijn gegevens`,
   [AppRoutes.ACCESSIBILITY]: `Toegankelijkheidsverklaring`,
   [AppRoutes.GENERAL_INFO]: `Dit ziet u in Mijn Amsterdam`,
   [AppRoutes.VERGUNNINGEN]: `${ThemaTitles.VERGUNNINGEN} | overzicht`,
@@ -225,30 +353,30 @@ export const DocumentTitles: {
   [AppRoutes.KVK]: `Mijn onderneming`,
   [AppRoutes.BUURT]: `Mijn buurt`,
   [AppRoutes.NOTIFICATIONS]: `${ThemaTitles.NOTIFICATIONS} | overzicht`,
-  [AppRoutes.AFVAL]: `${ThemaTitles.AFVAL} rond uw adres`,
+
   [AppRoutes.SIA]: `${ThemaTitles.SIA} overzicht`,
   [AppRoutes['SIA/DETAIL/OPEN']]: `Melding open | ${ThemaTitles.SIA}`,
   [AppRoutes['SIA/DETAIL/CLOSED']]: `Melding afgesloten | ${ThemaTitles.SIA}`,
   [AppRoutes.SIA_OPEN]: `Meldingen | Alle openstaande meldingen`,
   [AppRoutes.SIA_CLOSED]: `Meldingen | Alle afgesloten meldingen`,
-  [AppRoutes.TOERISTISCHE_VERHUUR]: `${ThemaTitles.TOERISTISCHE_VERHUUR} | overzicht`,
-  [AppRoutes['TOERISTISCHE_VERHUUR/VERGUNNING']]:
-    `Vergunning | ${ThemaTitles.TOERISTISCHE_VERHUUR}`,
-  [AppRoutes['TOERISTISCHE_VERHUUR/VERGUNNING/BB']]:
-    `Vergunning Bed & Breakfast | ${ThemaTitles.TOERISTISCHE_VERHUUR}`,
-  [AppRoutes['TOERISTISCHE_VERHUUR/VERGUNNING/VV']]:
-    `Vergunning vakantieverhuur | ${ThemaTitles.TOERISTISCHE_VERHUUR}`,
+  // [AppRoutes.TOERISTISCHE_VERHUUR]: `${ThemaTitles.TOERISTISCHE_VERHUUR} | overzicht`,
+  // [AppRoutes['TOERISTISCHE_VERHUUR/VERGUNNING']]:
+  //   `Vergunning | ${ThemaTitles.TOERISTISCHE_VERHUUR}`,
+  // [AppRoutes['TOERISTISCHE_VERHUUR/VERGUNNING/BB']]:
+  //   `Vergunning Bed & Breakfast | ${ThemaTitles.TOERISTISCHE_VERHUUR}`,
+  // [AppRoutes['TOERISTISCHE_VERHUUR/VERGUNNING/VV']]:
+  //   `Vergunning vakantieverhuur | ${ThemaTitles.TOERISTISCHE_VERHUUR}`,
   [AppRoutes.KREFIA]: `${ThemaTitles.KREFIA}`,
   [AppRoutes.SEARCH]: `Zoeken`,
   [AppRoutes.PARKEREN]: 'Parkeren',
-  [AppRoutes.KLACHTEN]: `${ThemaTitles.KLACHTEN} | overzicht`,
-  [AppRoutes['KLACHTEN/KLACHT']]: `${ThemaTitles.KLACHTEN} | klacht`,
-  [AppRoutes.HORECA]: 'Horeca | overzicht',
-  [AppRoutes['HORECA/DETAIL']]: 'Vergunning | Horeca',
+  // [AppRoutes.KLACHTEN]: `${ThemaTitles.KLACHTEN} | overzicht`,
+  // [AppRoutes['KLACHTEN/KLACHT']]: `${ThemaTitles.KLACHTEN} | klacht`,
+  // [AppRoutes.HORECA]: 'Horeca | overzicht',
+  // [AppRoutes['HORECA/DETAIL']]: 'Vergunning | Horeca',
   [AppRoutes.YIVI_LANDING]: 'Inloggen met yivi | Mijn Amsterdam',
   [AppRoutes.BFF_500_ERROR]: '500 Server Error | Mijn Amsterdam',
-  [AppRoutes.BODEM]: 'Bodem | overzicht',
-  [AppRoutes['BODEM/LOOD_METING']]: 'Bodem | lood in de bodem-check',
+  //[AppRoutes.BODEM]: 'Bodem | overzicht',
+  //[AppRoutes['BODEM/LOOD_METING']]: 'Bodem | lood in de bodem-check',
   [AppRoutes.ERFPACHTv2]: 'Erfpacht | overzicht',
   [AppRoutes['ERFPACHTv2/DOSSIERS']]: 'Erfpacht | Lijst met dossiers',
   [AppRoutes['ERFPACHTv2/OPEN_FACTUREN']]: 'Erfpacht | Lijst met open facturen',
@@ -258,6 +386,7 @@ export const DocumentTitles: {
   [AppRoutes.API1_LOGIN]: 'Inloggen | Mijn Amsterdam',
   [AppRoutes.API2_LOGIN]: 'Inloggen | Mijn Amsterdam',
   ...browserTabNames,
+  // [AppRoutes.AFVAL]: `${ThemaTitles.AFVAL} rond uw adres`
   // [AppRoutes.BEZWAREN]: `${ThemaTitles.BEZWAREN} | overzicht`,
   // [AppRoutes['BEZWAREN/DETAIL']]: `${ThemaTitles.BEZWAREN} | bezwaar`,
   // [AppRoutes.AVG]: `${ThemaTitles.AVG} | verzoeken`,
@@ -270,9 +399,9 @@ export interface ThemaMenuItem extends LinkProps {
   isAlwaysVisible?: boolean;
   hasAppStateValue?: boolean;
 }
+///nieuw  moet ThemamenuItem vervangen.
 
 let themaMenuItems = [];
-
 for (const [key, value] of Object.entries(themas2)) {
   // consol¿e.log(value);
   themaMenuItems.push({
@@ -282,26 +411,13 @@ for (const [key, value] of Object.entries(themas2)) {
     profileTypes: value.profileTypes,
   });
 }
-
+//einde nieuw
 export const myThemasMenuItems: ThemaMenuItem[] = [
-  {
-    title: ThemaTitles.BRP,
-    id: Themas.BRP,
-    to: AppRoutes.BRP,
-    profileTypes: ['private'],
-  },
   {
     title: ThemaTitles.KVK,
     id: Themas.KVK,
     to: AppRoutes.KVK,
     profileTypes: ['commercial', 'private'],
-  },
-  {
-    title: ThemaTitles.BELASTINGEN,
-    id: Themas.BELASTINGEN,
-    to: ExternalUrls.SSO_BELASTINGEN,
-    rel: 'external',
-    profileTypes: ['private'],
   },
 
   {
@@ -312,12 +428,7 @@ export const myThemasMenuItems: ThemaMenuItem[] = [
     profileTypes: ['commercial'],
     isAlwaysVisible: true,
   },
-  {
-    title: ThemaTitles.BURGERZAKEN,
-    id: Themas.BURGERZAKEN,
-    to: AppRoutes.BURGERZAKEN,
-    profileTypes: ['private'],
-  },
+
   {
     title: ThemaTitles.ERFPACHT,
     id: Themas.ERFPACHT,
@@ -359,18 +470,7 @@ export const myThemasMenuItems: ThemaMenuItem[] = [
     rel: 'external',
     profileTypes: ['commercial'],
   },
-  {
-    title: ThemaTitles.ZORG,
-    id: Themas.ZORG,
-    to: AppRoutes.ZORG,
-    profileTypes: ['private'],
-  },
-  {
-    title: ThemaTitles.INKOMEN,
-    id: Themas.INKOMEN,
-    to: AppRoutes.INKOMEN,
-    profileTypes: ['private'],
-  },
+
   {
     title: ThemaTitles.SVWI,
     id: Themas.SVWI,
@@ -384,12 +484,7 @@ export const myThemasMenuItems: ThemaMenuItem[] = [
     to: AppRoutes.STADSPAS,
     profileTypes: ['private'],
   },
-  {
-    title: ThemaTitles.AFVAL,
-    id: Themas.AFVAL,
-    to: AppRoutes.AFVAL,
-    profileTypes: ['private', 'commercial'],
-  },
+
   {
     title: ThemaTitles.VERGUNNINGEN,
     id: Themas.VERGUNNINGEN,
@@ -416,12 +511,7 @@ export const myThemasMenuItems: ThemaMenuItem[] = [
     to: AppRoutes.SIA,
     profileTypes: ['private-attributes'],
   },
-  {
-    title: ThemaTitles.TOERISTISCHE_VERHUUR,
-    id: Themas.TOERISTISCHE_VERHUUR,
-    to: AppRoutes.TOERISTISCHE_VERHUUR,
-    profileTypes: ['private', 'commercial'],
-  },
+
   {
     title: ThemaTitles.KREFIA,
     id: Themas.KREFIA,
@@ -435,24 +525,7 @@ export const myThemasMenuItems: ThemaMenuItem[] = [
     profileTypes: ['private', 'commercial'],
     hasAppStateValue: false,
   },
-  {
-    title: ThemaTitles.KLACHTEN,
-    id: Themas.KLACHTEN,
-    to: generatePath(AppRoutes.KLACHTEN, { page: 1 }),
-    profileTypes: ['private'],
-  },
-  {
-    title: ThemaTitles.HORECA,
-    id: Themas.HORECA,
-    to: AppRoutes.HORECA,
-    profileTypes: ['private', 'commercial'],
-  },
-  {
-    title: ThemaTitles.BODEM,
-    id: Themas.BODEM,
-    to: AppRoutes.BODEM,
-    profileTypes: ['private', 'commercial'],
-  },
+
   ...themaMenuItems,
   // {
   //   title: ThemaTitles.AVG,
@@ -460,16 +533,71 @@ export const myThemasMenuItems: ThemaMenuItem[] = [
   //   to: generatePath(AppRoutes.AVG, { page: 1 }),
   //   profileTypes: ['private'],
   // },
+  //
   // {
-  //   title: themas2.BEZWAREN.title,
-  //   id: nums.BEZWAREN,
-  //   to: themas2.BEZWAREN.appRoute,
-  //   profileTypes: ['private', 'commercial'],
+  //   title: ThemaTitles.BELASTINGEN,
+  //   id: Themas.BELASTINGEN,
+  //   to: ExternalUrls.SSO_BELASTINGEN,
+  //   rel: 'external',
+  //   profileTypes: ['private'],
   // },
   // {
   //   title: ThemaTitles.BEZWAREN,
   //   id: Themas.BEZWAREN,
   //   to: AppRoutes.BEZWAREN,
+  //   profileTypes: ['private', 'commercial'],
+  // },
+  // ];  {
+  //     title: ThemaTitles.AFVAL,
+  //     id: Themas.AFVAL,
+  //     to: AppRoutes.AFVAL,
+  //     profileTypes: ['private', 'commercial'],
+  //   }
+  // {
+  //   title: ThemaTitles.BODEM,
+  //   id: Themas.BODEM,
+  //   to: AppRoutes.BODEM,
+  //   profileTypes: ['private', 'commercial'],
+  // },
+  // {
+  //   title: ThemaTitles.BURGERZAKEN,
+  //   id: Themas.BURGERZAKEN,
+  //   to: AppRoutes.BURGERZAKEN,
+  //   profileTypes: ['private'],
+
+  //   title: ThemaTitles.INKOMEN,
+  //   id: Themas.INKOMEN,
+  //   to: AppRoutes.INKOMEN,
+  //   profileTypes: ['private'],
+  // },
+  // {
+  //   title: ThemaTitles.ZORG,
+  //   id: Themas.ZORG,
+  //   to: AppRoutes.ZORG,
+  //   profileTypes: ['private'],
+  // },
+  // {
+  //   title: ThemaTitles.KLACHTEN,
+  //   id: Themas.KLACHTEN,
+  //   to: generatePath(AppRoutes.KLACHTEN, { page: 1 }),
+  //   profileTypes: ['private'],
+  // },
+  // {
+  //   title: ThemaTitles.BRP,
+  //   id: Themas.BRP,
+  //   to: AppRoutes.BRP,
+  //   profileTypes: ['private'],
+  // },
+  // {
+  //   title: ThemaTitles.HORECA,
+  //   id: Themas.HORECA,
+  //   to: AppRoutes.HORECA,
+  //   profileTypes: ['private', 'commercial'],
+  // },
+  // {
+  //   title: ThemaTitles.TOERISTISCHE_VERHUUR,
+  //   id: Themas.TOERISTISCHE_VERHUUR,
+  //   to: AppRoutes.TOERISTISCHE_VERHUUR,
   //   profileTypes: ['private', 'commercial'],
   // },
 ];
