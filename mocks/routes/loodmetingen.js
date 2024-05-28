53f99e889ae3e3e1808826aba100d4dd91cbf108
Roan Paulus <roan.paulus@amsterdam.com>
2024-05-28 14:07:34 +0200

    loodmetingen toegevoegd

---
+++ mocks/routes/loodmetingen.js
@@ -0,0 +1,50 @@
+const LOODMETINGEN_RESPONSE = require('../fixtures/loodmetingen.json');
+const LOODMETINGEN_RAPPORT_RESPONSE = require('../fixtures/loodmeting_rapport.json');
+
+module.exports = [
+  {
+    id: 'get-loodmetingen-details',
+    url: '/be_getrequestdetails',
+    method: 'POST',
+    variants: [
+      {
+        id: 'standard',
+        type: 'json',
+        options: {
+          status: 200,
+          body: LOODMETINGEN_RESPONSE,
+        },
+      },
+    ],
+  },
+  {
+    id: 'get-loodmetingen-rapport',
+    url: '/be_downloadleadreport',
+    method: 'POST',
+    variants: [
+      {
+        id: 'standard',
+        type: 'json',
+        options: {
+          status: 200,
+          body: LOODMETINGEN_RAPPORT_RESPONSE,
+        },
+      },
+    ],
+  },
+  {
+    id: 'get-loodmetingen-auth',
+    url: '/tenant123/oauth2/v2.0/token',
+    method: 'POST',
+    variants: [
+      {
+        id: 'standard',
+        type: 'json',
+        options: {
+          status: 200,
+          body: { access_token: 'foo-bar' },
+        },
+      },
+    ],
+  },
+];
