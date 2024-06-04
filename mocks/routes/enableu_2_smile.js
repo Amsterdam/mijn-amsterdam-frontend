9ef7ecb838cccff38d1b7ef64ff3d10e878c7a27
Roan Paulus <roan.paulus@amsterdam.com>
2024-06-04 17:27:05 +0200

    removed comment

--- mocks/routes/enableu_2_smile.js
+++ mocks/routes/enableu_2_smile.js
@@ -2,7 +2,6 @@ const KLACHTEN_RESPONSE = require('../fixtures/klachten.json');
 const AVG_RESPONSE = require('../fixtures/avg.json');
 const AVG_THEMAS_RESPONSE = require('../fixtures/avg-themas.json');
 
-// RP TODO: AVG takes long to load? seems to be blocked by get-kvk, because this appears after it's delay
 module.exports = [
   {
     id: 'get-enableu2smile-klachten',
