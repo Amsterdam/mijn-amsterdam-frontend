060d4b72329417f84a542b556220ca89571dd488
Roan Paulus <roan.paulus@amsterdam.nl>
2024-06-11 10:56:23 +0200

    bezwaren documents now in mocks server, but crashes on a 404 when requesting the document

--- mocks/routes/bezwaren/documenten.js
+++ mocks/routes/bezwaren/documenten.js
@@ -1,3 +1,4 @@
+const path = require('node:path');
 const BEZWAREN_DOCUMENTEN_RESPONSE = require('../../fixtures/bezwaren-documents.json');
 
 module.exports = [
@@ -12,6 +13,20 @@ module.exports = [
         options: {
           status: 200,
           body: BEZWAREN_DOCUMENTEN_RESPONSE,
+        },
+      },
+    ],
+  },
+  {
+    id: 'get-bezwaren-download-document',
+    url: '/zgw/v1/enkelvoudiginformatieobjecten/:id/download',
+    variants: [
+      {
+        id: 'standard',
+        type: 'file',
+        options: {
+          status: 200,
+          path: path.resolve('/mocks/fixtures/documents/document.pdf'),
         },
       },
     ],
