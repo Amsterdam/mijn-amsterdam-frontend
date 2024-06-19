ea8ebd3418aa20c137ac9937e007a6689ec1f6a4
Roan Paulus <roan.paulus@amsterdam.nl>
2024-06-19 16:13:11 +0200

    generalized it for all files

--- mocks/loadJSON.js
+++ mocks/loadJSON.js
@@ -1,16 +1,12 @@
 const fs = require('node:fs');
 const settings = require('./settings.js');
+const path = require('node:path');
 
 /** Load in a JSON file and replace all occurences of '{{origin}}' with Mocks localhost */
 function loadJSON(jsonFilePath) {
-  const jsonFile = fs.readFileSync(jsonFilePath, { encoding: 'utf8' });
-
-  return JSON.parse(jsonFile, (key, value) => {
-    if (key === 'url') {
-      return value.replace('{{origin}}', settings.MOCK_ORIGIN);
-    }
-    return value;
-  });
+  let jsonFile = fs.readFileSync(jsonFilePath, { encoding: 'utf8' });
+  jsonFile = jsonFile.replaceAll('{{origin}}', settings.MOCK_ORIGIN);
+  return JSON.parse(jsonFile);
 }
 
 module.exports = { loadJSON };
