858b693eb4a7241b5bcf9e4bc5830e051a1b1e40
Roan Paulus <roan.paulus@amsterdam.nl>
2024-06-24 14:29:43 +0200

    commit voor debug hulp oscar

--- src/server/services/vergunningen-v2/decos-service.test.ts
+++ src/server/services/vergunningen-v2/decos-service.test.ts
@@ -1,33 +1,43 @@
 import { remoteApi } from '../../../test-utils';
-import { forTesting as decos } from './decos-service';
+import { forTesting as decos, fetchDecosDocument } from './decos-service';
 import { getAuthProfile } from '../../helpers/app';
 import nock from 'nock';
 
 describe('decos-service api test', () => {
-  beforeEach(() => {
-    // RP TODO: Nock adds port :80 after origin, other tests also have this problem
-    // If unless that's not a problem?
-    remoteApi
-      .post('/decos/search/books?properties=false&select=key')
-      .reply(200, {});
-    console.log(nock.activeMocks());
-  });
+  // describe('getUserKey tests', () => {
+  //   beforeEach(() => {
+  //     // RP TODO: Nock adds port :80 after origin, other tests also have this problem
+  //     // If unless that's not a problem?
+  //     remoteApi
+  //       .post('/decos/search/books?properties=false&select=key')
+  //       .reply(200, {});
+  //     console.log(nock.activeMocks());
+  //   });
+  //   it('Regular happy path', async () => {
+  //     const profile = getAuthProfile({
+  //       authMethod: 'digid',
+  //       profileType: 'commercial',
+  //       aud: 'test1',
+  //       sid: 'test5',
+  //       sub: '',
+  //     });
+  //     const authProfileAndToken = { profile, token: '111222333' };
+  //     const requestID = '456';
+  //     const userKeys = await decos.getUserKeys(requestID, authProfileAndToken);
+  //
+  //     // RP TODO: Still have to determine a good response after fixing nock problem
+  //     expect(userKeys).toEqual(['somekey']);
+  //   });
+  // });
 
-  describe('getUserKey tests', () => {
-    test('Regular happy path', async () => {
-      const profile = getAuthProfile({
-        authMethod: 'digid',
-        profileType: 'commercial',
-        aud: 'test1',
-        sid: 'test5',
-        sub: '',
-      });
-      const authProfileAndToken = { profile, token: '111222333' };
-      const requestID = '456';
-      const userKeys = await decos.getUserKeys(requestID, authProfileAndToken);
+  describe('fetchDecosDocument tests', () => {
+    beforeEach(() => {
+      remoteApi.get('/items/:docId/content').reply(200, {});
+    });
 
-      // RP TODO: Still have to determine a good response after fixing nock problem
-      expect(userKeys).toEqual(['somekey']);
+    it('fetches a document', async () => {
+      const doc = await fetchDecosDocument('456', '123456789');
+      expect(doc).toBe('jjj');
     });
   });
 });
