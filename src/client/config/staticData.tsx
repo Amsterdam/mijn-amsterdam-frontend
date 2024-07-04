0fdb5b9ebf40bf8efcc6fb53d597c1ba3b611236
Anouk Boukema <boukema.a@gmail.com>
2024-07-04 13:50:35 +0200

    1 welcome message

--- src/client/config/staticData.tsx
+++ src/client/config/staticData.tsx
@@ -5,30 +5,9 @@ import { ExternalUrls } from './app';
 export const WelcomeNotification: MyNotification = {
   id: 'welcome01',
   thema: Themas.NOTIFICATIONS,
-  datePublished: new Date(2019, 10, 11).toISOString(),
-  title: 'Welkom op Mijn Amsterdam!',
-  hideDatePublished: true,
-  description:
-    'Mijn Amsterdam is nog volop in ontwikkeling. Er komt steeds meer informatie bij.',
-  customLink: {
-    callback: () => {
-      const usabilla = (window as any).usabilla_live;
-      if (usabilla) {
-        usabilla('click');
-      } else {
-        window.location.href = ExternalUrls.CONTACT_FORM;
-      }
-    },
-    title: 'Laat ons weten wat u ervan vindt',
-  },
-};
-
-export const WelcomeNotification2: MyNotification = {
-  id: 'welcome02',
-  thema: Themas.NOTIFICATIONS,
   datePublished: new Date(2022, 0, 20).toISOString(),
   title: 'Welkom op Mijn Amsterdam!',
-  hideDatePublished: false,
+  hideDatePublished: true,
   description: `
       <p>
         Welkom op Mijn Amsterdam; uw persoonlijke digitale pagina bij de
@@ -41,12 +20,7 @@ export const WelcomeNotification2: MyNotification = {
           wat de status is en hoe u het kunt doorgeven als er iets niet klopt.
         </li>
       </ul>
-      <p>
-        <strong>Voor Weesp nog niet alles zichtbaar op Mijn Amsterdam</strong><br/>
-        Voor Weespers is nog niet alle informatie via Mijn Amsterdam zichtbaar. In de aanloop van
-        de fusie tussen gemeente Amsterdam en Weesp op 24 maart 2022 en de
-        maanden erna komen er steeds meer producten en diensten bij.
-      </p>
+
     `,
   customLink: {
     callback: () => {
@@ -66,7 +40,7 @@ export const WelcomeNotification2Commercial: MyNotification = {
   thema: Themas.NOTIFICATIONS,
   datePublished: new Date(2022, 0, 20).toISOString(),
   title: 'Welkom op Mijn Amsterdam!',
-  hideDatePublished: false,
+  hideDatePublished: true,
   description: `<p>
         Welkom op Mijn Amsterdam; de persoonlijke digitale pagina voor burgers
         en ondernemers bij de gemeente Amsterdam. Hier ziet u op 1 centrale
@@ -78,13 +52,7 @@ export const WelcomeNotification2Commercial: MyNotification = {
         <li>
           wat de status is en hoe u het kunt doorgeven als er iets niet klopt.
         </li>
-      </ul>
-      <p>
-        <strong>Voor Weesp nog niet alles zichtbaar op Mijn Amsterdam</strong><br/>
-        Voor ondernemers uit Weesp is nog niet alle informatie via Mijn Amsterdam zichtbaar. In
-        de aanloop van de fusie tussen gemeente Amsterdam en Weesp op 24 maart
-        2022 en de maanden erna komen er steeds meer producten en diensten bij.
-      </p>`,
+      </ul>`,
   customLink: {
     callback: () => {
       const usabilla = (window as any).usabilla_live;
--- src/client/hooks/useNotifications.ts
+++ src/client/hooks/useNotifications.ts
@@ -1,7 +1,6 @@
 import { selectorFamily, useRecoilValue } from 'recoil';
 import {
   WelcomeNotification,
-  WelcomeNotification2,
   WelcomeNotification2Commercial,
 } from '../config/staticData';
 import { appStateAtom } from './useAppState';
@@ -14,18 +13,13 @@ const appStateNotificationsSelector = selectorFamily({
     ({ get }) => {
       const appState = get(appStateAtom);
       let notifications = appState.NOTIFICATIONS.content || [];
-      let welcomeNotification = WelcomeNotification;
 
-      if (appState.BRP?.content?.adres?.woonplaatsNaam === 'Weesp') {
-        welcomeNotification =
-          profileType === 'private'
-            ? WelcomeNotification2
-            : WelcomeNotification2Commercial;
-
-        return [welcomeNotification, ...notifications];
-      }
-
-      return [...notifications, welcomeNotification];
+      return [
+        ...notifications,
+        profileType === 'private'
+          ? WelcomeNotification
+          : WelcomeNotification2Commercial,
+      ];
     },
 });
 
