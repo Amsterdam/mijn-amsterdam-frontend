fcebca42ace4a0387e5dc6fa64d5d80ab837b333
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-07-09 09:41:22 +0200

    Make returnto variable

--- src/server/services/hli/router-external-consumer.ts
+++ src/server/services/hli/router-external-consumer.ts
@@ -1,23 +1,43 @@
 export const router = express.Router();
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import { BffEndpoints } from '../../config';
 import {
+  generateFullApiUrlBFF,
   getAuth,
-  isRequestAuthenticated,
   sendUnauthorized,
 } from '../../helpers/app';
+import { RETURNTO_AMSAPP_STADSPAS_CLIENTNUMMER } from '../../helpers/auth';
 import { fetchClientNummer } from './hli-zorgned-service';
 
 router.get(
+  BffEndpoints.STADSPAS_AMSAPP_LOGIN,
+  async (req: Request, res: Response) => {
+    return res.redirect(
+      generateFullApiUrlBFF(BffEndpoints.AUTH_LOGIN_DIGID) +
+        `?returnTo=${RETURNTO_AMSAPP_STADSPAS_CLIENTNUMMER}`
+    );
+  }
+);
+
+router.get(
   BffEndpoints.STADSPAS_CLIENTNUMMER,
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response) => {
     const authProfileAndToken = await getAuth(req);
+
     if (
       authProfileAndToken.profile.id &&
       authProfileAndToken.profile.profileType === 'private'
     ) {
-      return fetchClientNummer(res.locals.requestID, authProfileAndToken);
+      const clientNummerResponse = await fetchClientNummer(
+        res.locals.requestID,
+        authProfileAndToken
+      );
+
+      if (clientNummerResponse.status === 'OK') {
+        return res.send(clientNummerResponse);
+      }
     }
+
     return sendUnauthorized(res);
   }
 );
