8f603e60ea16e7ab0d194d8bce80d2fe55e0809a
oscarbakker <bakkeroscar@gmail.com>
2024-07-18 15:36:40 +0200

    Add document DocumentListV2 with documents

--- src/client/pages/ZorgDetail/ZorgDetail.tsx
+++ src/client/pages/ZorgDetail/ZorgDetail.tsx
@@ -1,7 +1,10 @@
 import { Grid } from '@amsterdam/design-system-react';
-import { InfoDetail } from '../../components';
+import { DocumentList, InfoDetail } from '../../components';
 import { Datalist } from '../../components/Datalist/Datalist';
 import StatusDetail from '../StatusDetail/StatusDetail';
+import { InfoDetailGroup } from '../../components/InfoDetail/InfoDetail';
+import { defaultDateFormat } from '../../../universal/helpers/date';
+import DocumentListV2 from '../../components/DocumentList/DocumentListV2';
 
 export default function ZorgDetail() {
   return (
@@ -9,14 +12,21 @@ export default function ZorgDetail() {
       stateKey="WMO"
       thema="ZORG"
       pageContent={(isLoading, statusItem) => {
+        let documents = statusItem?.steps?.flatMap((step) => step.documents);
+        const dates = statusItem?.steps?.map((step) => step.datePublished);
         return (
-          statusItem?.supplier && (
+          <>
+            {statusItem?.supplier && (
+              <Grid.Cell span="all">
+                <Datalist
+                  rows={[{ content: statusItem.supplier, label: 'Aanbieder' }]}
+                />
+              </Grid.Cell>
+            )}
             <Grid.Cell span="all">
-              <Datalist
-                rows={[{ content: statusItem.supplier, label: 'Aanbieder' }]}
-              />
-            </Grid.Cell>
-          )
+              <DocumentListV2 documents={documents} />
+            </Grid.Cell>{' '}
+          </>
         );
       }}
       documentPathForTracking={(document) =>
