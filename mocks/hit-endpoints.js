4d67138f747be819671e976a9fdc51f0e3f958a4
Roan Paulus <roan.paulus@amsterdam.nl>
2024-08-09 14:56:31 +0200

    Added README and fixed remaining endpoint, and also added a missing endpoint

--- mocks/hit-endpoints.js
+++ mocks/hit-endpoints.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+
 const settings = require('./settings');
 
 sendRequestToAllEndpoints();
@@ -11,8 +12,16 @@ async function sendRequestToAllEndpoints() {
   for (let i = 0; i < routeResponse.data.length - 1; i++) {
     const entry = routeResponse.data[i];
 
-    const requestUrl = `${settings.MOCK_ORIGIN}${replaceParametersWithConcreteData(entry.url)}`;
-    console.log(requestUrl);
+    if (entry.id in CUSTOM_REQUESTS) {
+      CUSTOM_REQUESTS[entry.id]();
+      continue;
+    }
+
+    // Regex to replace all url parameters, like for example '/:something/' becomes '/1/'.
+    const paramReplacingPattern = new RegExp('(?<=\/):.+?(?=\/|$)', 'g'); // prettier-ignore
+    // reason: It will remove all escape sequences in the regular expression which is undesired.
+
+    const requestUrl = `${settings.MOCK_ORIGIN}${entry.url.replaceAll(paramReplacingPattern, '1')}`;
 
     if (entry.method === 'get') {
       axios.get(requestUrl);
@@ -22,10 +31,46 @@ async function sendRequestToAllEndpoints() {
   }
 }
 
-// Ignore or it will remove all escape sequences in the regular expression.
-// prettier-ignore
-function replaceParametersWithConcreteData(url) {
-  // Regex to replace all url parameters, like for example '/:something/' becomes '/1/'.
-  url = url.replaceAll(new RegExp('(?<=\/):.+?(?=\/|$)', 'g'), '1');
-  return url.replace('*', 'wildcard');
-}
+const ORIGIN = settings.MOCK_API_BASE_URL;
+
+/** Add handlers here that need custom behavior.
+ * In the form of `[route-id]: [function]`.
+ *
+ * Some routes have custom logic depending on the header/body.
+ * This requires manually crafted requests.
+ */
+const CUSTOM_REQUESTS = {
+  'get-erfpacht': () => {
+    const variations = ['user', 'company'];
+    variations.forEach((variation) =>
+      axios.get(
+        `${ORIGIN}/erfpacht/api/v2/check/groundlease/${variation}/wildcard`
+      )
+    );
+  },
+  'get-erfpacht-notifications': () => {
+    const variations = ['bsn', 'kvk'];
+    variations.forEach((variation) =>
+      axios.get(`${ORIGIN}/erfpacht/api/v2/notifications/${variation}/wildcard`)
+    );
+  },
+  'get-subsidie': () => {
+    const variations = ['citizen', 'company'];
+    variations.forEach((variation) =>
+      axios.get(`${ORIGIN}/subsidies/${variation}/12token34`)
+    );
+  },
+  'post-enableu2smile-klachten': () => {
+    const readKlachtForm = new FormData();
+    readKlachtForm.append('function', 'readKlacht');
+    axios.post(`${ORIGIN}/smile`, readKlachtForm);
+
+    const readAVGForm = new FormData();
+    readAVGForm.append('function', 'readAVGverzoek');
+    axios.post(`${ORIGIN}/smile`, readAVGForm);
+
+    const readThemaPerAVGVerzoekForm = new FormData();
+    readThemaPerAVGVerzoekForm.append('function', 'readthemaperavgverzoek');
+    axios.post(`${ORIGIN}/smile`, readThemaPerAVGVerzoekForm);
+  },
+};
