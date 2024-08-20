4d899ec5145a8760b7cf65f2fb885a7c48527f53
Roan Paulus <roan.paulus@amsterdam.nl>
2024-08-20 10:09:15 +0200

    Fixed bug in aanbiedingen transacties

--- src/server/config.ts
+++ src/server/config.ts
@@ -416,7 +416,7 @@ export const ExternalConsumerEndpoints = {
   // Privately accessible
   private: {
     STADSPAS_PASSEN: `${BFF_BASE_PATH_PRIVATE}/services/amsapp/stadspas/passen/:${STADSPASSEN_ENDPOINT_PARAMETER}`,
-    STADSPAS_AANBIEDINGEN_TRANSACTIES: `${BFF_BASE_PATH_PRIVATE}/services/amsapp/stadspas/aanbiedingen/transactions/:pasNummer`,
+    STADSPAS_AANBIEDINGEN_TRANSACTIES: `${BFF_BASE_PATH_PRIVATE}/services/amsapp/stadspas/aanbiedingen/transactions/:transactionsKeyEncrypted`,
     STADPAS_BUDGET_TRANSACTIES: `${BFF_BASE_PATH_PRIVATE}/services/amsapp/stadspas/budget/transactions/:transactionsKeyEncrypted`,
   },
 };
