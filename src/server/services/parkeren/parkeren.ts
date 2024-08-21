26e82706799d36dbf7ac64a7f9eecbbe68335a53
Roan Paulus <123072030+RoanPaulus@users.noreply.github.com>
2024-08-21 09:19:05 +0200

    Suggetion: Simplified creating request URL
    Co-authored-by: Tim van Oostrom <t.van.oostrom@amsterdam.nl>

--- src/server/services/parkeren/parkeren.ts
+++ src/server/services/parkeren/parkeren.ts
@@ -11,30 +11,14 @@ export async function fetchSSOParkerenURL(
   requestID: requestID,
   authProfileAndToken: AuthProfileAndToken
 ) {
-  const profileType = authProfileAndToken.profile.profileType;
   const config = getApiConfig('PARKEREN', {
-    transformResponse: (response: ParkerenUrlResponse) => {
+    formatUrl(requestConfig) {
+      return `${requestConfig.url}/sso/get_authentication_url?service=${authProfileAndToken.profile.authMethod}`;
+    },
+    transformResponse: (response: ParkerenUrlResponseSource) => {
       return response.url;
     },
   });
-  const base_route = '/sso/get_authentication_url?service=';
 
-  switch (profileType) {
-    case 'private': {
-      config.url = `${config.url}${base_route}digid`;
-      break;
-    }
-    case 'commercial': {
-      config.url = `${config.url}${base_route}eherkenning`;
-      break;
-    }
-    default: {
-      const errorMessage = 'No profile type found for Parkeren.';
-      console.error(errorMessage);
-      return apiErrorResult(errorMessage, null, 400);
-    }
-  }
-
-  const response = await requestData<ParkerenUrlResponse>(config, requestID);
-  return response;
+  return requestData<ParkerenUrlResponse>(config, requestID);
 }
