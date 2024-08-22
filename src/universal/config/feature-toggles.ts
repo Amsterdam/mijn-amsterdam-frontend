4e917c9f4f391ad5dfd2ede92c15787fb3bef216
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-08-22 15:50:41 +0200

    Disable DB

--- src/universal/config/feature-toggles.ts
+++ src/universal/config/feature-toggles.ts
@@ -10,7 +10,7 @@ export const FeatureToggle = {
   bodemActive: !IS_PRODUCTION, // NOTE: Temporarily disabled
   cleopatraApiActive: true,
   cmsFooterActive: true,
-  dbDisabled: false,
+  dbDisabled: true,
   decosServiceActive: IS_OT,
   ehKetenmachtigingActive: !IS_PRODUCTION,
   eherkenningActive: true,
