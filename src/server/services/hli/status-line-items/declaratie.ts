6d6125caf5aead06c342b7973f17a875d14ee45c
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-08-26 16:01:37 +0200

    Change text

--- src/server/services/hli/status-line-items/declaratie.ts
+++ src/server/services/hli/status-line-items/declaratie.ts
@@ -6,12 +6,9 @@ import {
 import { BESLUIT } from './generic';
 
 function getDeclaratieBesluitDescription(regeling: ZorgnedAanvraagTransformed) {
-  return `<p>
-    ${
-      regeling.resultaat === 'toegewezen'
-        ? `U heeft recht op ${regeling.titel} per ${regeling.datumIngangGeldigheid ? defaultDateFormat(regeling.datumIngangGeldigheid) : ''}`
-        : `U heeft geen recht op ${regeling.titel}`
-    }.
+  return `
+    <p>
+      Uw declaratie is ${regeling.resultaat}.
     </p>
     <p>
       In de brief leest u ook hoe u bezwaar kunt maken of een klacht kan
