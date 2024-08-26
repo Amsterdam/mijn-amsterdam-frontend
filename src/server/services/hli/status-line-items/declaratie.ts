23be4912b3c4c66eeb87f75c62207db1e7aad061
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-08-26 15:34:23 +0200

    Mijn-9086-Feature - fat bevindingen (#1445)
    * Add condition for regelingen
    
    * Prevent Einde recht for products that don’t have that status
    
    * Update einde recht
    
    * Add test
    
    * Change description, add niet voldaan
    
    * Hide cursus stap

--- src/server/services/hli/status-line-items/declaratie.ts
+++ src/server/services/hli/status-line-items/declaratie.ts
@@ -1,5 +1,28 @@
 import { defaultDateFormat } from '../../../../universal/helpers/date';
-import { ZorgnedStatusLineItemTransformerConfig } from '../../zorgned/zorgned-config-and-types';
+import {
+  ZorgnedAanvraagTransformed,
+  ZorgnedStatusLineItemTransformerConfig,
+} from '../../zorgned/zorgned-config-and-types';
 import { BESLUIT } from './generic';
 
-export const DECLARATIE: ZorgnedStatusLineItemTransformerConfig[] = [BESLUIT];
+function getDeclaratieBesluitDescription(regeling: ZorgnedAanvraagTransformed) {
+  return `<p>
+    ${
+      regeling.resultaat === 'toegewezen'
+        ? `U heeft recht op ${regeling.titel} per ${regeling.datumIngangGeldigheid ? defaultDateFormat(regeling.datumIngangGeldigheid) : ''}`
+        : `U heeft geen recht op ${regeling.titel}`
+    }.
+    </p>
+    <p>
+      In de brief leest u ook hoe u bezwaar kunt maken of een klacht kan
+      indienen.
+    </p>
+  `;
+}
+
+export const DECLARATIE: ZorgnedStatusLineItemTransformerConfig[] = [
+  {
+    ...BESLUIT,
+    description: (regeling) => getDeclaratieBesluitDescription(regeling),
+  },
+];
