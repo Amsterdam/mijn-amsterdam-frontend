0a7777e64fe5b60fdda0731817bf3cd0a8a1cc6a
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-09-09 15:04:36 +0200

    Change blacklist tests

--- src/server/services/session-blacklist.test.ts
+++ src/server/services/session-blacklist.test.ts
@@ -1,27 +1,45 @@
-import express, { Response, Request } from 'express';
-import { OIDC_SESSION_COOKIE_NAME } from '../auth/auth-config';
-import { generateDevSessionCookieValue } from '../auth/auth-helpers-development';
-import { isBlacklistedHandler } from '../routing/route-handlers';
 import {
-  getIsBlackListed,
   addToBlackList,
   cleanupSessionIds,
+  getIsBlackListed,
 } from './session-blacklist';
+
+const mocks = vi.hoisted(() => {
+  return {
+    db: {
+      id: 'x',
+      query: vi.fn(),
+      queryGET: vi.fn(),
+      queryALL: vi.fn(),
+    },
+  };
+});
+
+vi.mock('../services/db/db', async () => {
+  return {
+    db: async () => mocks.db,
+  };
+});
 
 describe('Session-blacklist', () => {
   const sessionID = 'test123';
 
   test('Is NOT blacklisted', async () => {
     const rs = await getIsBlackListed(sessionID);
-
+    expect(mocks.db.queryGET).toHaveBeenCalled();
     expect(rs).toBe(false);
   });
 
   test('Add to black list', async () => {
     await addToBlackList(sessionID);
+    expect(mocks.db.query).toHaveBeenCalledWith(
+      'INSERT INTO acc_session_blacklist (session_id) VALUES ($1) RETURNING id',
+      ['test123']
+    );
   });
 
   test('Is blacklisted', async () => {
+    mocks.db.queryGET.mockResolvedValueOnce({ count: 1 });
     const rs = await getIsBlackListed(sessionID);
     expect(rs).toBe(true);
   });
