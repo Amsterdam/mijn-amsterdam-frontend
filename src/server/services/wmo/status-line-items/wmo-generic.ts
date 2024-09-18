fe673d7f04784c170d39947dd72a4e5043336325
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-09-18 15:18:22 +0200

    Update texts

--- src/server/services/wmo/status-line-items/wmo-generic.ts
+++ src/server/services/wmo/status-line-items/wmo-generic.ts
@@ -177,18 +177,15 @@ export const EINDE_RECHT: ZorgnedStatusLineItemTransformerConfig = {
       ${
         aanvraag.datumEindeGeldigheid
           ? aanvraag.isActueel
-            ? `Uw recht op ${aanvraag.titel} stopt op ${
-                aanvraag.datumEindeGeldigheid
-                  ? `${defaultDateFormat(aanvraag.datumEindeGeldigheid)}.`
-                  : ''
-              }`
-            : `Uw recht op ${aanvraag.titel} is beëindigd ${
-                aanvraag.datumEindeGeldigheid
-                  ? `per ${defaultDateFormat(aanvraag.datumEindeGeldigheid)}.`
-                  : ''
-              }`
-          : ``
-      }
+            ? `Als uw recht op ${aanvraag.titel} stopt, krijgt u hiervan bericht`
+            : `Uw recht op ${aanvraag.titel} is beëindigd
+          ${
+            aanvraag.datumEindeGeldigheid
+              ? ` per ${defaultDateFormat(aanvraag.datumEindeGeldigheid)}`
+              : ''
+          }`
+          : ''
+      }.
     </p>
     `,
 };
@@ -200,14 +197,17 @@ export const EINDE_RECHT_PGB: ZorgnedStatusLineItemTransformerConfig = {
       ${
         aanvraag.datumEindeGeldigheid
           ? aanvraag.isActueel
-            ? `Als uw recht op ${aanvraag.titel} stopt, krijgt u hiervan bericht`
-            : `Uw recht op ${aanvraag.titel} is beëindigd
-          ${
-            aanvraag.datumEindeGeldigheid
-              ? ` per ${defaultDateFormat(aanvraag.datumEindeGeldigheid)}`
-              : ''
-          }`
-          : ''
+            ? `Uw recht op ${aanvraag.titel} stopt op ${
+                aanvraag.datumEindeGeldigheid
+                  ? `${defaultDateFormat(aanvraag.datumEindeGeldigheid)}.`
+                  : ''
+              }`
+            : `Uw recht op ${aanvraag.titel} is beëindigd ${
+                aanvraag.datumEindeGeldigheid
+                  ? `per ${defaultDateFormat(aanvraag.datumEindeGeldigheid)}.`
+                  : ''
+              }`
+          : ``
       }.
     </p>
     ${
