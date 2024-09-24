7010441f4bd6f371b0fb6dada615fa00e41dc509
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-09-24 20:24:05 +0200

    Update test

--- src/client/pages/Afis/Afis.test.tsx
+++ src/client/pages/Afis/Afis.test.tsx
@@ -1,17 +1,16 @@
 import { render, waitFor } from '@testing-library/react';
-
 import { generatePath } from 'react-router-dom';
 import { MutableSnapshot } from 'recoil';
+import {
+  AfisBusinessPartnerDetailsTransformed,
+  AfisFacturenByStateResponse,
+} from '../../../server/services/afis/afis-types';
+import { bffApi } from '../../../test-utils';
 import { AppRoutes } from '../../../universal/config/routes';
 import { AppState } from '../../../universal/types';
 import { appStateAtom } from '../../hooks/useAppState';
 import MockApp from '../MockApp';
 import { AfisThemaPagina } from './Afis';
-import { bffApi } from '../../../test-utils';
-import {
-  AfisBusinessPartnerDetailsTransformed,
-  AfisFactuur,
-} from '../../../server/services/afis/afis-types';
 
 const businessPartnerIdEncrypted = 'xxx-123-xxx';
 const testState = {
@@ -31,68 +30,75 @@ function initializeState(snapshot: MutableSnapshot) {
 describe('<Afis />', () => {
   const businessPartnerDetails: AfisBusinessPartnerDetailsTransformed = {
     addressId: 999,
-    businessPartnerId: 515177,
+    businessPartnerId: '000515177',
     fullName: 'Taxon Expeditions BV',
     phone: null,
     email: null,
   };
 
-  const mockFacturen = {
-    open: [
-      {
-        id: 'F001',
-        title: 'Invoice F001',
-        afzender: 'Company A',
-        datePublished: '2023-01-15',
-        datePublishedFormatted: '15 januari 2023',
-        paymentDueDate: '2023-02-15',
-        paymentDueDateFormatted: '15 februari 2023',
-        debtClearingDate: null,
-        debtClearingDateFormatted: null,
-        amountOwed: 1000,
-        amountOwedFormatted: '€ 1.000,00',
-        factuurNummer: 'F001',
-        status: 'openstaand',
-        paylink: 'https://payment.example.com/F001',
-        documentDownloadLink: 'https://download.example.com/F001',
-      },
-      {
-        id: 'F003',
-        title: 'Invoice F003',
-        afzender: 'Company C',
-        datePublished: '2023-03-10',
-        datePublishedFormatted: '10 maart 2023',
-        paymentDueDate: '2023-04-10',
-        paymentDueDateFormatted: '10 april 2023',
-        debtClearingDate: null,
-        debtClearingDateFormatted: null,
-        amountOwed: 2000,
-        amountOwedFormatted: '€ 2.000,00',
-        factuurNummer: 'F003',
-        status: 'openstaand',
-        paylink: 'https://payment.example.com/F003',
-        documentDownloadLink: 'https://download.example.com/F003',
-      },
-    ],
-    closed: [
-      {
-        id: 'F002',
-        title: 'Invoice F002',
-        afzender: 'Company B',
-        datePublished: '2023-02-01',
-        datePublishedFormatted: '1 februari 2023',
-        paymentDueDate: '2023-03-01',
-        paymentDueDateFormatted: '1 maart 2023',
-        debtClearingDate: '2023-02-15',
-        debtClearingDateFormatted: '15 februari 2023',
-        amountOwed: 0,
-        amountOwedFormatted: '€ 0,00',
-        factuurNummer: 'F002',
-        status: 'betaald',
-        paylink: null,
-        documentDownloadLink: 'https://download.example.com/F002',
-      },
-    ],
+  const mockFacturen: AfisFacturenByStateResponse = {
+    open: {
+      count: 2,
+      facturen: [
+        {
+          afzender: 'Company A',
+          datePublished: '2023-01-15',
+          datePublishedFormatted: '15 januari 2023',
+          paymentDueDate: '2023-02-15',
+          paymentDueDateFormatted: '15 februari 2023',
+          debtClearingDate: null,
+          debtClearingDateFormatted: null,
+          amountOwed: 1000,
+          amountOwedFormatted: '€ 1.000,00',
+          factuurNummer: 'F001',
+          status: 'openstaand',
+          statusDescription: 'openstaand',
+          paylink: 'https://payment.example.com/F001',
+          documentDownloadLink: 'https://download.example.com/F001',
+          factuurDocumentId: '1',
+        },
+        {
+          afzender: 'Company C',
+          datePublished: '2023-03-10',
+          datePublishedFormatted: '10 maart 2023',
+          paymentDueDate: '2023-04-10',
+          paymentDueDateFormatted: '10 april 2023',
+          debtClearingDate: null,
+          debtClearingDateFormatted: null,
+          amountOwed: 2000,
+          amountOwedFormatted: '€ 2.000,00',
+          factuurNummer: 'F003',
+          status: 'openstaand',
+          statusDescription: 'openstaand',
+          paylink: 'https://payment.example.com/F003',
+          documentDownloadLink: 'https://download.example.com/F003',
+          factuurDocumentId: '2',
+        },
+      ],
+    },
+    afgehandeld: {
+      count: 1,
+      facturen: [
+        {
+          afzender: 'Company B',
+          datePublished: '2023-02-01',
+          datePublishedFormatted: '1 februari 2023',
+          paymentDueDate: '2023-03-01',
+          paymentDueDateFormatted: '1 maart 2023',
+          debtClearingDate: '2023-02-15',
+          debtClearingDateFormatted: '15 februari 2023',
+          amountOwed: 0,
+          amountOwedFormatted: '€ 0,00',
+          factuurNummer: 'F002',
+          status: 'betaald',
+          statusDescription: 'betaald',
+          paylink: null,
+          documentDownloadLink: 'https://download.example.com/F002',
+          factuurDocumentId: '3',
+        },
+      ],
+    },
+    overgedragen: { count: 0, facturen: [] },
   };
 
   bffApi
