ad941f6285bd6c2338f0807ee51b2c7fc20ac01b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-10-01 11:39:13 +0200

    Make texts conditional (#1521)

--- src/server/services/wmo/status-line-items/wmo-diensten.ts
+++ src/server/services/wmo/status-line-items/wmo-diensten.ts
@@ -28,6 +28,18 @@ function isActive(
   );
 }
 
+function isLeveringGestopt(
+  stepIndex: number,
+  aanvraag: ZorgnedAanvraagTransformed,
+  today: Date
+) {
+  return (
+    isDelivered(aanvraag, today) &&
+    (isDeliveryStopped(aanvraag, today) ||
+      isEindeGeldigheidVerstreken(aanvraag.datumEindeGeldigheid, today))
+  );
+}
+
 export const diensten: ZorgnedStatusLineItemTransformerConfig[] = [
   AANVRAAG,
   IN_BEHANDELING,
@@ -41,22 +53,23 @@ export const diensten: ZorgnedStatusLineItemTransformerConfig[] = [
       isDelivered(aanvraag, today),
     isActive: (stepIndex, aanvraag, today: Date) =>
       aanvraag.isActueel && isDeliveredStatusActive(aanvraag, today),
-    description: (aanvraag) =>
-      `<p>U krijgt nu ${aanvraag.titel} van ${aanvraag.leverancier}.</p>`,
+    description: (aanvraag, today) =>
+      isDelivered(aanvraag, today)
+        ? `<p>U krijgt nu ${aanvraag.titel} van ${aanvraag.leverancier}.</p>`
+        : '',
   },
   {
     status: 'Levering gestopt',
     datePublished: (aanvraag) => aanvraag.datumEindeLevering ?? '',
     isVisible: isDeliveryStepVisible,
-    isChecked: (stepIndex, aanvraag, today: Date) =>
-      isDelivered(aanvraag, today) &&
-      (isDeliveryStopped(aanvraag, today) ||
-        isEindeGeldigheidVerstreken(aanvraag.datumEindeGeldigheid, today)),
+    isChecked: isLeveringGestopt,
     isActive: (stepIndex, aanvraag, today) =>
       isDeliveryStopped(aanvraag, today) &&
       !isEindeGeldigheidVerstreken(aanvraag.datumEindeGeldigheid, today),
-    description: (aanvraag) =>
-      `<p>${`${aanvraag.leverancier} heeft ons laten weten dat u geen ${aanvraag.titel} meer krijgt.`}</p>`,
+    description: (aanvraag, today) =>
+      isLeveringGestopt(0, aanvraag, today)
+        ? `<p>${`${aanvraag.leverancier} heeft ons laten weten dat u geen ${aanvraag.titel} meer krijgt.`}</p>`
+        : '',
   },
   EINDE_RECHT,
 ];
