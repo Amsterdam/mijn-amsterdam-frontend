2773e228dcc4cbdd7762fb523e0a21e301417b04
Anouk Boukema <boukema.a@gmail.com>
2024-10-21 16:15:35 +0200

    changed info text thema parkeren

--- src/client/pages/Parkeren/Parkeren.tsx
+++ src/client/pages/Parkeren/Parkeren.tsx
@@ -1,10 +1,11 @@
-import { Button, Icon, Paragraph } from '@amsterdam/design-system-react';
+import { Button, Icon } from '@amsterdam/design-system-react';
 import { ExternalLinkIcon } from '@amsterdam/design-system-react-icons';
 import { generatePath } from 'react-router-dom';
 
 import { useParkerenData } from './useParkerenData.hook';
 import { Vergunning } from '../../../server/services';
 import { AppRoutes } from '../../../universal/config/routes';
+import { ErrorAlert } from '../../components';
 import { MaLink } from '../../components/MaLink/MaLink';
 import { ThemaTitles } from '../../config/thema';
 import { useAppStateGetter } from '../../hooks/useAppState';
@@ -51,22 +52,19 @@ export default function Parkeren() {
 
   const pageContentTop = (
     <>
-      <Paragraph className="ams-mb--sm">
-        Alle informatie over parkeren in Amsterdam vindt u op . Daar kunt u ook
-        terecht voor informatie over fietskelders, laadpalen voor elektrische
-        auto&apos;s en andere vragen die U heeft over parkeren of vervoer. Het
-        aanvragen of wijzigen van een parkeervergunning voor bewoners kan via
-        Mijn Parkeren. U moet hier wel opnieuw inloggen.
-      </Paragraph>
-      <MaLink
-        href={`${parkerenUrl || import.meta.env.REACT_APP_SSO_URL_PARKEREN}`}
-        rel="external"
-      >
-        <Button>
-          Log in op Mijn Parkeren
-          <Icon svg={ExternalLinkIcon} size={'level-5'} />
-        </Button>
-      </MaLink>
+      <ErrorAlert severity="info" title="Let op">
+        Het inzien, aanvragen of wijzigen van een parkeervergunning voor
+        bewoners kan via Mijn Parkeren.
+        <MaLink
+          href={`${parkerenUrl || import.meta.env.REACT_APP_SSO_URL_PARKEREN}`}
+          rel="external"
+        >
+          <Button>
+            Log in op Mijn Parkeren
+            <Icon svg={ExternalLinkIcon} size={'level-5'} />
+          </Button>
+        </MaLink>
+      </ErrorAlert>
     </>
   );
 
