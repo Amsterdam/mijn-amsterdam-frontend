4ed502d90dd74eca0580b2c273201030f721a46d
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-10-28 10:26:46 +0100

    Mijn-9451-Feature-Chore - hli deploy zorgned disclaimer (#1564)
    * Chores: Test, Add Historic items mention,
    
    * Change color
    
    * Enable HLI for prod
    
    * Add no-explicit-any for test files as well
    
    * Change typing

--- src/client/pages/Zorg/Zorg.tsx
+++ src/client/pages/Zorg/Zorg.tsx
@@ -1,110 +1,82 @@
-import { useMemo } from 'react';
+import { Link, Paragraph } from '@amsterdam/design-system-react';
+import { generatePath } from 'react-router-dom';
 
+import { useZorgThemaData } from './useZorgThemaData';
 import styles from './Zorg.module.scss';
-import { AppRoutes } from '../../../universal/config/routes';
-import { isError, isLoading } from '../../../universal/helpers/api';
-import {
-  ErrorAlert,
-  Linkd,
-  LinkdInline,
-  MaintenanceNotifications,
-  OverviewPage,
-  PageContent,
-  PageHeading,
-  SectionCollapsible,
-  Table,
-  ThemaIcon,
-  addTitleLinkComponent,
-} from '../../components';
-import { ExternalUrls } from '../../config/app';
-import { ThemaTitles } from '../../config/thema';
-import { useAppStateGetter } from '../../hooks/useAppState';
+import { WMOVoorzieningFrontend } from '../../../server/services/wmo/wmo-config-and-types';
+import { LinkProps } from '../../../universal/types/App.types';
+import { ExternalUrls } from '../../config/external-urls';
+import ThemaPagina from '../ThemaPagina/ThemaPagina';
+import ThemaPaginaTable from '../ThemaPagina/ThemaPaginaTable';
 
-const DISPLAY_PROPS = {
-  title: '',
-  status: 'Status',
-  dateDecision: 'Datum',
-};
+export function HistoricItemsMention() {
+  return (
+    <Paragraph className={styles.HistoricItemsMention}>
+      U ziet hier informatie vanaf 1 januari 2018. Wilt u iets weten van
+      daarvoor? Bel dan de Wmo Helpdesk:{' '}
+      <Link href="tel:08000643" rel="noreferrer">
+        0800 0643
+      </Link>
+      .<br />U ziet ook geen begeleid thuis en beschermd verblijf (vroeger
+      maatschappelijke opvang en beschermd wonen). Hier zijn we nog mee bezig.
+    </Paragraph>
+  );
+}
 
-export default function Zorg() {
-  const { WMO } = useAppStateGetter();
+export default function ThemaPaginaZorg() {
+  const { isError, isLoading, regelingen, title, routes, tableConfig } =
+    useZorgThemaData();
 
-  const itemsActual = useMemo(() => {
-    if (!WMO.content?.length) {
-      return [];
+  const pageContentTop = (
+    <Paragraph>
+      Hieronder ziet u uw voorzieningen vanuit de Wet maatschappelijke
+      ondersteuning (Wmo). Heeft u vragen of wilt u een wijziging doorgeven? Bel
+      dan gratis de Wmo Helpdesk:{' '}
+      <Link rel="noreferrer" href="tel:08000643" variant="inline">
+        0800 0643
+      </Link>{' '}
+      (maandag tot en met vrijdag van 08.00 tot 18.00 uur) of ga langs bij het
+      Sociaal Loket.
+    </Paragraph>
+  );
+
+  const linkListItems: LinkProps[] = [
+    {
+      to: ExternalUrls.ZORG_LEES_MEER,
+      title: 'Lees hier meer over zorg en ondersteuning',
+    },
+  ];
+
+  const tables = Object.entries(tableConfig).map(
+    ([kind, { title, displayProps, filter, maxItems, className }]) => {
+      return (
+        <ThemaPaginaTable<WMOVoorzieningFrontend>
+          key={kind}
+          title={title}
+          className={className}
+          zaken={regelingen.filter(filter)}
+          listPageRoute={generatePath(routes.listPage, {
+            kind,
+          })}
+          displayProps={displayProps}
+          textNoContent={`U heeft geen ${title.toLowerCase()}`}
+          maxItems={maxItems}
+        />
+      );
     }
-    return addTitleLinkComponent(WMO.content?.filter((item) => item.isActual));
-  }, [WMO]);
-
-  const itemsPrevious = useMemo(() => {
-    if (!WMO.content?.length) {
-      return [];
-    }
-    return addTitleLinkComponent(WMO.content?.filter((item) => !item.isActual));
-  }, [WMO]);
+  );
 
   return (
-    <OverviewPage className={styles.ZorgOverviewPage}>
-      <PageHeading
-        backLink={{
-          to: AppRoutes.HOME,
-          title: 'Home',
-        }}
-        icon={<ThemaIcon />}
-      >
-        {ThemaTitles.ZORG}
-      </PageHeading>
-      <PageContent>
-        <p>
-          Hieronder ziet u uw regelingen en hulpmiddelen vanuit de Wmo. Hebt u
-          vragen of wilt u een wijziging doorgeven? Bel dan gratis de Wmo
-          Helpdesk:{' '}
-          <LinkdInline external={true} href="tel:08000643">
-            0800 0643
-          </LinkdInline>
-          . Of ga langs bij het Sociaal Loket.
-        </p>
-        <p>
-          <Linkd external={true} href={ExternalUrls.ZORG_LEES_MEER}>
-            Lees hier meer over zorg en ondersteuning
-          </Linkd>
-        </p>
-        <p>
-          <Linkd external={true} href={ExternalUrls.ZORG_LEES_MEER}>
-            Documenten uploaden voor de Wmo
-          </Linkd>
-        </p>
-        <MaintenanceNotifications page="zorg" />
-        {isError(WMO) && (
-          <ErrorAlert>We kunnen op dit moment geen gegevens tonen.</ErrorAlert>
-        )}
-      </PageContent>
-      <SectionCollapsible
-        id="SectionCollapsible-healthcare-granted"
-        title="Huidige regelingen en hulpmiddelen"
-        noItemsMessage="U heeft geen huidige regelingen en hulpmiddelen."
-        hasItems={!!itemsActual.length}
-        startCollapsed={false}
-        className={styles.SectionCollapsibleCurrent}
-        isLoading={isLoading(WMO)}
-      >
-        <Table displayProps={DISPLAY_PROPS} items={itemsActual} />
-      </SectionCollapsible>
-
-      <SectionCollapsible
-        id="SectionCollapsible-healthcare-previous"
-        title="Eerdere regelingen en hulpmiddelen"
-        noItemsMessage="U heeft geen eerdere regelingen en hulpmiddelen."
-        hasItems={!!itemsPrevious.length}
-        startCollapsed={true}
-        isLoading={isLoading(WMO)}
-      >
-        <Table displayProps={DISPLAY_PROPS} items={itemsPrevious} />
-      </SectionCollapsible>
-      <p className={styles.HistoricItemsMention}>
-        U ziet hier alleen informatie vanaf 1 januari 2018. Bel voor informatie
-        van eerdere jaren de Wmo Helpdesk: 0800 0643.
-      </p>
-    </OverviewPage>
+    <>
+      <ThemaPagina
+        title={title}
+        pageContentTop={pageContentTop}
+        linkListItems={linkListItems}
+        pageContentMain={tables}
+        isError={isError}
+        isLoading={isLoading}
+      />
+      <HistoricItemsMention />
+    </>
   );
 }
