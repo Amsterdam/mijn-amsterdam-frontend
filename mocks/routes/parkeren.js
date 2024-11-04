d66747baeda02863b25bdcc6340af8f1dd4aa274
oscarbakker <bakkeroscar@gmail.com>
2024-11-04 13:39:24 +0100

    Add parkeren mock data to mocks

--- mocks/routes/parkeren.js
+++ mocks/routes/parkeren.js
@@ -16,4 +16,55 @@ module.exports = [
       },
     ],
   },
+  {
+    id: 'get-private-active-permit-request',
+    url: `${settings.MOCK_API_BASE_URL}/private/active_permit_request`,
+    method: 'GET',
+    variants: [
+      {
+        id: 'standard',
+        type: 'json',
+        options: {
+          status: 200,
+          body: {
+            data: {
+              client_product_id: 8703,
+              object: 'client_product',
+              client_id: 8703,
+              status: 'ACTIVE',
+              started_at: '2024-10-01T12:00:00+00:00',
+              ended_at: '2025-04-30T23:59:59+00:00',
+              zone: 'WM55 Oost-5a',
+              link: 'demo01.ams.fo.geodeci.frpermit/8703',
+              vrns: '999123',
+            },
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: 'get-company-active-permit-request',
+    url: `${settings.MOCK_API_BASE_URL}/company/active_permit_request`,
+    method: 'GET',
+    variants: [
+      {
+        id: 'standard',
+        type: 'json',
+        options: {
+          status: 200,
+          body: {
+            data: {
+              link: 'demo01.ams.fo.geodeci.fr/permits',
+              id: 8702,
+              client_id: 8702,
+              status: 'in_progress',
+              permit_name: 'Bewonersvergunning',
+              permit_zone: 'CE02C Centrum-2c',
+            },
+          },
+        },
+      },
+    ],
+  },
 ];
