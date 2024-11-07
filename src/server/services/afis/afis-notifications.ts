529d4209f549ae1816ad5b7141fcaa7eae6e78dc
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-11-07 17:27:31 +0100

    MIJN-9508 - meldingtekst

--- src/server/services/afis/afis-notifications.ts
+++ src/server/services/afis/afis-notifications.ts
@@ -34,9 +34,10 @@ export function createAfisFacturenNotification(
   ).length;
 
   const title = 'Betaal tijdig om extra kosten te voorkomen';
+  const isMulti = openFacturenCount > 1;
   const description = htmlTextContent(`
     U heeft ${openFacturenCount} openstaande facturen.
-    ${openFacturenHerinneringCount ? `Van ${openFacturenHerinneringCount} factuur${openFacturenHerinneringCount > 1 ? 'en' : ''} heeft u inmiddels een herinnering ontvangen per e-mail of post.` : ''}
+    ${openFacturenHerinneringCount ? `Van ${openFacturenHerinneringCount} ${isMulti ? 'facturen' : 'factuur'} heeft u inmiddels een herinnering ontvangen per e-mail of post.` : ''}
   `);
 
   const datePublished = new Date().toISOString();
