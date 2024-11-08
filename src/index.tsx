bcd3896ad4ab3b2c791ff2514a730ee389806f71
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-11-08 13:23:05 +0100

    Fix Modal/Dialog styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import { ErrorInfo } from 'react';
-
 import { AppInsightsErrorBoundary } from '@microsoft/applicationinsights-react-js';
 import { createRoot } from 'react-dom/client';
 
 import App from './client/App';
 import ApplicationError from './client/pages/ApplicationError/ApplicationError';
 import './client/styles/main.scss';
-import { captureMessage, reactPlugin } from './client/utils/monitoring';
+import { reactPlugin } from './client/utils/monitoring';
 
 if (
   /MSIE (\d+\.\d+);/.test(navigator.userAgent) ||
@@ -23,15 +21,6 @@ console.info(
   MA_GIT_SHA ?? '-1',
   MA_BUILD_ID ?? '-1'
 );
-
-const sendToMonitoring = (error: Error, info: ErrorInfo) => {
-  captureMessage(`Kritieke applicatie fout: ${error.message}`, {
-    severity: 'critical',
-    properties: {
-      componentStack: info.componentStack,
-    },
-  });
-};
 
 const root = createRoot(document.getElementById('root')!);
 
