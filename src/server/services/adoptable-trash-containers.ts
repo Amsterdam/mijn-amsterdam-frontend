5d69e083ce56d61bd102cc9da07d45799a19c623
Roan Paulus <roan.paulus@amsterdam.nl>
2024-11-19 16:59:52 +0100

    fix latlng not coming in correct order

--- src/server/services/adoptable-trash-containers.ts
+++ src/server/services/adoptable-trash-containers.ts
@@ -1,4 +1,4 @@
-import { LatLngBoundsLiteral } from 'leaflet';
+import { LatLngTuple } from 'leaflet';
 
 import { FeatureToggle } from '../../universal/config/feature-toggles';
 import { Themas } from '../../universal/config/thema';
@@ -9,16 +9,13 @@ import {
 import { AuthProfileAndToken } from '../auth/auth-types';
 import { fetchDataset } from './buurt/buurt';
 import { datasetEndpoints } from './buurt/datasets';
-import {
-  filterDatasetFeatures,
-  filterFeaturesinRadius,
-  getBboxFromFeatures,
-} from './buurt/helpers';
+import { filterDatasetFeatures, filterFeaturesinRadius } from './buurt/helpers';
 import { fetchMyLocation } from './home';
+import { captureMessage } from './monitoring';
+import { getLatLngCoordinates } from '../../universal/helpers/bag';
+import { MyNotification } from '../../universal/types';
 
-const WITHIN_RADIUS_KM = 0.1;
-
-function getNotification(bbox: LatLngBoundsLiteral) {
+function getNotification(containerID: string) {
   return {
     id: 'adoptable-trash-container-notification',
     datePublished: new Date().toISOString(),
@@ -27,8 +24,7 @@ function getNotification(bbox: LatLngBoundsLiteral) {
     description: `Help mee om uw eigen buurt schoon te houden en adopteer een afvalcontainer.
  Liever op een andere manier bijdragen? Leen dan een afvalgrijper!`,
     link: {
-      // RP TODO: Change link to aim at trash container
-      to: `/buurt?datasetIds=["wior"]&filters={"wior":{"datumStartUitvoering":{"values":{"Binnen enkele maanden":1}},"duur":{"values":{"Meerdaags":1}}}}&bbox=[[${bbox[0]}],[${bbox[1]}]]`,
+      to: `http://localhost:3000/buurt?datasetIds=%5B%22afvalcontainers%22%5D&zoom=15&filters=&loadingFeature=%7B%22datasetId%22%3A%22afvalcontainers%22%2C%22id%22%3A%22${containerID}%22%7D&s=1`,
       title: 'Bekijk de werkzaamheden op kaart',
     },
   };
@@ -58,27 +54,43 @@ export async function fetchAdoptableTrashContainers(
     MY_LOCATION.content?.[0]?.latlng &&
     meldingen?.status === 'OK'
   ) {
-    // Take first address
+    // Put all the Lat Lng coordinates in the right order.
+    // Because other functions operating on the data expect them in a certain order.
+    for (const feature of meldingen.content.features) {
+      if (feature.geometry.type === 'Point') {
+        const { lat, lng } = getLatLngCoordinates(
+          feature.geometry.coordinates as LatLngTuple
+        );
+        feature.geometry.coordinates = [lat, lng];
+      } else {
+        captureMessage(`Unexpected geometry type: '${feature.geometry.type}'`, {
+          severity: 'error',
+        });
+      }
+    }
+
     const latlng = MY_LOCATION.content[0].latlng;
+    const WITHIN_RADIUS_KM = 0.1;
     const featuresInRadius = filterFeaturesinRadius(
       latlng,
       meldingen.content.features,
       WITHIN_RADIUS_KM
     );
+
     const filteredFeatures = filterDatasetFeatures(
       featuresInRadius,
       [datasetId],
       filters
     );
-    const bbox = getBboxFromFeatures(filteredFeatures, latlng);
-    const notification = getNotification(bbox);
 
-    return apiSuccessResult({
-      tips:
-        FeatureToggle.wiorMeldingen && filteredFeatures.length >= 2
-          ? [notification]
-          : [],
-    });
+    const tips: MyNotification[] =
+      FeatureToggle.adopteerbareAfvalContainerMeldingen
+        ? filteredFeatures.map((feature) => {
+            return getNotification(feature.properties.id);
+          })
+        : [];
+
+    return apiSuccessResult({ tips });
   }
 
   return apiDependencyError({ MY_LOCATION });
