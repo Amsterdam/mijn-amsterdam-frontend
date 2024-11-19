3a836f77afc042969ba2a1fd2717a44510ecef55
YacineHallo <y.hesseling@amsterdam.nl>
2024-11-19 09:44:28 +0100

    Wat overbodige code weggehaald

--- src/client/pages/Burgerzaken/helpers.ts
+++ src/client/pages/Burgerzaken/helpers.ts
@@ -1,21 +1,5 @@
-import {
-  IdentiteitsbewijsFromSource,
-  Identiteitsbewijs,
-} from '../../../universal/types';
 import { AppState } from '../../../universal/types/App.types';
 import { ThemaTitles } from '../../config/thema';
-
-function getThemaTitleConditions( //deze hoeft niet wanneer ik het daaronder aan de praat krijg regel 36 en 37
-  hasIDKaart: boolean,
-  hasPaspoort: boolean,
-  document: IdentiteitsbewijsFromSource
-) {
-  if (document.documentType === 'europese identiteitskaart') {
-    hasIDKaart === true;
-  } else if (document.documentType === 'paspoort') {
-    hasPaspoort === true;
-  } else return ThemaTitles.BURGERZAKEN;
-}
 
 export function getThemaTitleBurgerzaken(
   hasIDKaart: boolean,
