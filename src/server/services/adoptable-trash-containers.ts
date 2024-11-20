4445429288a66bc785b825d767599af9168b1eae
Roan Paulus <roan.paulus@amsterdam.nl>
2024-11-20 15:41:39 +0100

    Prepartion for description texts

--- src/server/services/adoptable-trash-containers.ts
+++ src/server/services/adoptable-trash-containers.ts
@@ -19,21 +19,6 @@ import { captureMessage } from './monitoring';
 import { getLatLngCoordinates } from '../../universal/helpers/bag';
 import { MyNotification } from '../../universal/types';
 
-function getNotification(containerID: string) {
-  return {
-    id: 'adoptable-trash-container-notification',
-    datePublished: new Date().toISOString(),
-    thema: Themas.BUURT,
-    title: 'Adopteer een afvalcontainer',
-    description: `Help mee om uw eigen buurt schoon te houden en adopteer een afvalcontainer.
- Liever op een andere manier bijdragen? Leen dan een afvalgrijper!`,
-    link: {
-      to: `http://localhost:3000/buurt?datasetIds=%5B%22afvalcontainers%22%5D&zoom=15&filters=&loadingFeature=%7B%22datasetId%22%3A%22afvalcontainers%22%2C%22id%22%3A%22${containerID}%22%7D&s=1`,
-      title: 'Bekijk de werkzaamheden op kaart',
-    },
-  };
-}
-
 export async function fetchAdoptableTrashContainers(
   requestID: RequestID,
   authProfileAndToken: AuthProfileAndToken
@@ -54,54 +39,84 @@ export async function fetchAdoptableTrashContainers(
   const MY_LOCATION = await fetchMyLocation(requestID, authProfileAndToken);
 
   if (
-    MY_LOCATION.status === 'OK' &&
-    MY_LOCATION.content?.[0]?.latlng &&
-    meldingen?.status === 'OK'
+    MY_LOCATION.status !== 'OK' ||
+    !MY_LOCATION.content?.[0]?.latlng ||
+    meldingen?.status !== 'OK'
   ) {
-    // Put all the Lat Lng coordinates in the right order.
-    for (const feature of meldingen.content.features) {
-      if (feature.geometry.type === 'Point') {
-        const { lat, lng } = getLatLngCoordinates(
-          feature.geometry.coordinates as LatLngTuple
-        );
-        feature.geometry.coordinates = [lat, lng];
-      } else {
-        captureMessage(`Unexpected geometry type: '${feature.geometry.type}'`, {
-          severity: 'error',
-        });
-      }
-    }
-
-    const latlng = MY_LOCATION.content[0].latlng;
-    const WITHIN_RADIUS_KM = 0.1;
-    const featuresInRadius = filterFeaturesinRadius(
-      latlng,
-      meldingen.content.features,
-      WITHIN_RADIUS_KM
-    );
-
-    const filteredFeatures: MaPointFeature<AfvalFeatureProperties>[] =
-      filterDatasetFeatures<AfvalFeatureProperties>(
-        featuresInRadius,
-        [datasetId],
-        filters
-      ).filter((feature) => {
-        return feature.properties.geadopteerd_ind === 'Nee';
-      });
-
-    const tips: MyNotification[] =
-      FeatureToggle.adopteerbareAfvalContainerMeldingen
-        ? filteredFeatures.map((feature) => {
-            return getNotification(feature.properties.id);
-          })
-        : [];
-
-    return apiSuccessResult({ tips });
+    return apiDependencyError({ MY_LOCATION });
   }
 
-  return apiDependencyError({ MY_LOCATION });
+  // Put all the Lat Lng coordinates in the right order.
+  for (const feature of meldingen.content.features) {
+    if (feature.geometry.type === 'Point') {
+      const { lat, lng } = getLatLngCoordinates(
+        feature.geometry.coordinates as LatLngTuple
+      );
+      feature.geometry.coordinates = [lat, lng];
+    } else {
+      captureMessage(`Unexpected geometry type: '${feature.geometry.type}'`, {
+        severity: 'error',
+      });
+    }
+  }
+
+  const latlng = MY_LOCATION.content[0].latlng;
+  const WITHIN_RADIUS_KM = 0.1;
+  const featuresInRadius = filterFeaturesinRadius(
+    latlng,
+    meldingen.content.features,
+    WITHIN_RADIUS_KM
+  );
+
+  const filteredFeatures: MaPointFeature<AfvalFeatureProperties>[] =
+    filterDatasetFeatures<AfvalFeatureProperties>(
+      featuresInRadius,
+      [datasetId],
+      filters
+    ).filter((feature) => {
+      return feature.properties.geadopteerd_ind === 'Nee';
+    });
+
+  return apiSuccessResult({ tips: getNotifications(filteredFeatures) });
 }
 
 type AfvalFeatureProperties = DatasetFeatureProperties & {
   geadopteerd_ind: 'Ja' | 'Nee';
+};
+
+function getNotifications(
+  features: MaPointFeature<AfvalFeatureProperties>[]
+): MyNotification[] {
+  if (!FeatureToggle.adopteerbareAfvalContainerMeldingen) {
+    return [];
+  }
+
+  return features.map((feature) => buildNotification(feature.properties.id));
+}
+
+function buildNotification(containerID: string): MyNotification {
+  return {
+    id: 'adoptable-trash-container-notification',
+    datePublished: new Date().toISOString(),
+    thema: Themas.BUURT,
+    title: 'Adopteer een afvalcontainer',
+    description: `Help mee om uw eigen buurt schoon te houden en adopteer een afvalcontainer.
+ Liever op een andere manier bijdragen? Leen dan een afvalgrijper!`,
+    link: {
+      to: `http://localhost:3000/buurt?datasetIds=%5B%22afvalcontainers%22%5D&zoom=15&filters=&loadingFeature=%7B%22datasetId%22%3A%22afvalcontainers%22%2C%22id%22%3A%22${containerID}%22%7D&s=1`,
+      title: 'Bekijk de werkzaamheden op kaart',
+    },
+  };
+}
+
+type Description = {
+  adult: string;
+  teen: string;
+};
+
+const description: Description = {
+  adult: `Help mee om uw eigen buurt schoon te houden en adopteer een afvalcontainer.
+ Liever op een andere manier bijdragen? Leen dan een afvalgrijper!`,
+  teen: `Help mee om je eigen buurt schoon te houden en adopteer een afvalcontainer.
+ Wil je liever iets anders doen? Leen dan een afvalgrijper!`,
 };
