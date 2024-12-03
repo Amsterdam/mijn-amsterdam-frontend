ea4ed0606cac943cbd6931d2dc6c587e3f930596
YacineHallo <108801103+YacineHallo@users.noreply.github.com>
2024-12-03 11:27:18 +0100

    Update src/client/config/menuItems.ts
    Co-authored-by: Tim van Oostrom <t.van.oostrom@amsterdam.nl>

--- src/client/config/menuItems.ts
+++ src/client/config/menuItems.ts
@@ -17,9 +17,6 @@ export const themasByProfileType: (
       .map((item) => buildThemaMenuItem(item, 'commercial')),
   })[profileType];
 
-console.log('Test2');
-console.log(myThemasMenuItems);
-// , themasByProfileType('commercial'));
 
 function buildThemaMenuItem(item: ThemaMenuItem, profileType: ProfileType) {
   const appState = useAppStateGetter();
