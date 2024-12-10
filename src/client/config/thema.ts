240f91d7e07662d8a27c9adebf1e15b445a1b464
Roan Paulus <roan.paulus@amsterdam.nl>
2024-12-10 15:27:21 +0100

    Suggestion 1

--- src/client/config/thema.ts
+++ src/client/config/thema.ts
@@ -333,7 +333,7 @@ export const myThemasMenuItems: ThemaMenuItem[] = [
     title: ThemaTitles.PARKEREN,
     id: Themas.PARKEREN,
     to: (appState: AppState) => {
-      const hasDecosParkeerVergunningen = (
+      const hasOtherParkeerVegunningen = (
         appState.VERGUNNINGEN?.content ?? []
       ).some((vergunning) =>
         PARKEER_CASE_TYPES.has(vergunning.caseType as DecosCaseType)
@@ -341,7 +341,7 @@ export const myThemasMenuItems: ThemaMenuItem[] = [
       const urlExternal =
         appState.PARKEREN.content?.url ??
         import.meta.env.REACT_APP_SSO_URL_PARKEREN;
-      return hasDecosParkeerVergunningen ? AppRoutes.PARKEREN : urlExternal;
+      return hasOtherParkeerVegunningen ? AppRoutes.PARKEREN : urlExternal;
     },
     profileTypes: ['private', 'commercial'],
   },
