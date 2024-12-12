f54d1cfab1d867849ff3880024758891560bf833
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-12-12 15:14:34 +0100

    Add transform hook, refine types

---
+++ src/client/pages/Vergunningen/useVergunningenTransformed.hook.ts
@@ -0,0 +1,42 @@
+import { useMemo } from 'react';
+
+import { Vergunning } from '../../../server/services';
+import { defaultDateFormat } from '../../../universal/helpers/date';
+import { getCustomTitleForVergunningWithLicensePlates } from '../../../universal/helpers/vergunningen';
+import { AppState } from '../../../universal/types';
+import { CaseType } from '../../../universal/types/vergunningen';
+import { addTitleLinkComponent } from '../../components';
+
+const titleTransformMap: Record<string, unknown> = {
+  [CaseType.TouringcarJaarontheffing]:
+    getCustomTitleForVergunningWithLicensePlates,
+  [CaseType.TouringcarDagontheffing]:
+    getCustomTitleForVergunningWithLicensePlates,
+  [CaseType.EigenParkeerplaats]: getCustomTitleForVergunningWithLicensePlates,
+  [CaseType.EigenParkeerplaatsOpheffen]:
+    getCustomTitleForVergunningWithLicensePlates,
+};
+
+export function useVergunningenTransformed(
+  VERGUNNINGEN: AppState['VERGUNNINGEN']
+) {
+  const vergunningen: Vergunning[] = useMemo(() => {
+    if (!VERGUNNINGEN.content?.length) {
+      return [];
+    }
+    const items: Vergunning[] = VERGUNNINGEN.content
+      .filter((x) => x)
+      .map((item) => {
+        const transformer = titleTransformMap[item.caseType];
+        return {
+          ...item,
+          title:
+            typeof transformer === 'function' ? transformer(item) : item.title,
+          dateRequest: defaultDateFormat(item.dateRequest),
+        };
+      });
+    return addTitleLinkComponent(items, 'identifier');
+  }, [VERGUNNINGEN.content]);
+
+  return vergunningen;
+}
