50c277496a686c3475a28ac2959ec4fadf06412f
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-12-17 18:47:35 +0100

    Add firstName, lastName to type

--- src/server/services/afis/afis-business-partner.ts
+++ src/server/services/afis/afis-business-partner.ts
@@ -10,6 +10,7 @@ import {
   AfisBusinessPartnerEmailSource,
   AfisBusinessPartnerPhone,
   AfisBusinessPartnerPhoneSource,
+  BusinessPartnerId,
   BusinessPartnerIdPayload,
 } from './afis-types';
 import { FeatureToggle } from '../../../universal/config/feature-toggles';
@@ -35,7 +36,8 @@ function transformBusinessPartnerAddressResponse(
     const includeLand = true;
     return {
       id: addressEntry.AddressID,
-      address: getFullAddress(
+      address: addressEntry,
+      fullAddress: getFullAddress(
         {
           straatnaam: addressEntry.StreetName,
           huisnummer: addressEntry.HouseNumber?.toString() ?? '',
@@ -55,7 +57,7 @@ function transformBusinessPartnerAddressResponse(
 
 async function fetchBusinessPartnerAddress(
   requestID: RequestID,
-  businessPartnerId: string
+  businessPartnerId: BusinessPartnerId
 ): Promise<ApiResponse<AfisBusinessPartnerAddress>> {
   const additionalConfig: DataRequestConfig = {
     transformResponse: transformBusinessPartnerAddressResponse,
@@ -83,6 +85,8 @@ function transformBusinessPartnerFullNameResponse(
   if (businessPartnerEntry) {
     const transformedResponse: AfisBusinessPartnerDetails = {
       fullName: businessPartnerEntry.BusinessPartnerFullName ?? null,
+      firstName: businessPartnerEntry.FirstName ?? null,
+      lastName: businessPartnerEntry.LastName ?? null,
     };
 
     return transformedResponse;
@@ -91,14 +95,14 @@ function transformBusinessPartnerFullNameResponse(
   return null;
 }
 
-async function fetchBusinessPartnerFullName(
+async function fetchBusinessPartner(
   requestID: RequestID,
-  businessPartnerId: string
-) {
+  businessPartnerId: BusinessPartnerId
+): Promise<ApiResponse<AfisBusinessPartnerDetails | null>> {
   const additionalConfig: DataRequestConfig = {
     transformResponse: transformBusinessPartnerFullNameResponse,
     formatUrl(config) {
-      return `${config.url}/API/ZAPI_BUSINESS_PARTNER_DET_SRV/A_BusinessPartner?$filter=BusinessPartner eq '${businessPartnerId}'&$select=BusinessPartnerFullName`;
+      return `${config.url}/API/ZAPI_BUSINESS_PARTNER_DET_SRV/A_BusinessPartner?$filter=BusinessPartner eq '${businessPartnerId}'&$select=BusinessPartnerFullName,FirstName,LastName`;
     },
   };
 
@@ -187,7 +191,7 @@ export async function fetchAfisBusinessPartnerDetails(
   requestID: RequestID,
   payload: BusinessPartnerIdPayload
 ): Promise<ApiSuccessResponse<AfisBusinessPartnerDetailsTransformed>> {
-  const fullNameRequest = fetchBusinessPartnerFullName(
+  const fullNameRequest = fetchBusinessPartner(
     requestID,
     payload.businessPartnerId
   );
