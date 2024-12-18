34afdd4a1bb464602f44aba2fd9d5f10187fab88
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-12-18 14:13:19 +0100

    Add temp log

--- src/client/pages/Varen/Varen.tsx
+++ src/client/pages/Varen/Varen.tsx
@@ -1,3 +1,7 @@
+import { useAppStateGetter } from '../../hooks/useAppState';
+
 export default function Varen() {
+  const { VAREN } = useAppStateGetter();
+  console.log(VAREN);
   return 'FAKE';
 }
