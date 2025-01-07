2a8d730f5d2a2cb4b5f7fa3782e3c5bc82c49db4
Roan Paulus <roan.paulus@amsterdam.nl>
2025-01-07 17:03:06 +0100

    added mock route

--- mocks/routes/gpass.js
+++ mocks/routes/gpass.js
@@ -1,5 +1,3 @@
-const UID = require('uid-safe');
-
 const settings = require('../settings');
 const RESPONSES = {
   PASHOUDER: require('../fixtures/gpass-pashouders.json'),
@@ -64,6 +62,42 @@ module.exports = [
         options: {
           status: 200,
           body: [{ toBeDeterminedFields: 'Unknown' }],
+        },
+      },
+    ],
+  },
+  {
+    id: 'post-toggle-stadspas',
+    url: `${settings.MOCK_BASE_PATH}/gpass/rest/sales/v1/togglepas/:pasId`,
+    method: 'POST',
+    variants: [
+      {
+        id: 'standard',
+        type: 'json',
+        options: {
+          status: 200,
+          body: {
+            id: 999999,
+            pasnummer: 6099999999999,
+            pasnummer_volledig: '6064366099999999999',
+            categorie: 'Minima stadspas',
+            categorie_code: 'M',
+            expiry_date: '2025-01-07T15:55:10.796Z',
+            actief: false,
+            budgetten_actief: true,
+            heeft_budget: true,
+            vervangen: false,
+            passoort: { id: 99, naam: 'Digitale Stadspas' },
+            originele_pas: {
+              id: 999999,
+              pasnummer: 6099999999999,
+              pasnummer_volledig: '6064366099999999999',
+              categorie: 'Minima stadspas',
+              categorie_code: 'M',
+              passoort: { id: 99, naam: 'Digitale Stadspas' },
+            },
+            budgetten: [],
+          },
         },
       },
     ],
