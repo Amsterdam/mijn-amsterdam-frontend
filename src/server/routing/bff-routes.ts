811232b87f3d31a116585ae1887210fd8d689036
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-13 11:11:48 +0100

    Add BFF sign request norification endpoint

--- src/server/routing/bff-routes.ts
+++ src/server/routing/bff-routes.ts
@@ -26,6 +26,8 @@ export const BffEndpoints = {
   AFIS_EMANDATES_SIGN_REQUEST_URL: '/services/afis/e-mandates/sign-request-url',
   AFIS_EMANDATES_SIGN_REQUEST_STATUS:
     '/services/afis/e-mandates/sign-request-status',
+  AFIS_EMANDATES_SIGN_REQUEST_STATUS_NOTIFICATION:
+    '/services/afis/e-mandates/sign-request-notification',
   AFIS_EMANDATES_SIGN_REQUEST_RETURNTO:
     '/services/afis/e-mandates/sign-request-callback',
   AFIS_FACTUREN:
