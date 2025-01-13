b3e27b5c5d1478588635a4fd07f88fa52ac3ff1a
Roan Paulus <roan.paulus@amsterdam.nl>
2025-01-13 16:40:57 +0100

    stopped updating whole appstate. Using atom instead

---
+++ src/client/pages/HLI/HLI.hooks.tsx
@@ -0,0 +1,28 @@
+import { atom, useRecoilState } from 'recoil';
+
+import { useAppStateGetter } from '../../hooks/useAppState';
+
+type StadspasActiefByID = {
+  [id: string]: boolean;
+};
+
+const stadspasActiefAtom = atom<StadspasActiefByID>({
+  key: 'stadspasActief',
+  default: {},
+});
+
+export function useStadspassen() {
+  const { HLI } = useAppStateGetter();
+  const [stadspasActief, setStadspassenActiefStatus] =
+    useRecoilState(stadspasActiefAtom);
+
+  const stadspassen = (HLI.content?.stadspas || []).map((pas) => {
+    const stadspas = {
+      ...pas,
+      actief: stadspasActief[pas.id] ?? true,
+    };
+    return stadspas;
+  });
+
+  return [stadspassen, setStadspassenActiefStatus] as const;
+}
