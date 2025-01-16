1e55bdd81ec23d776f8b13e33bd050824852fdc8
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-16 14:29:28 +0100

    Add tip

--- src/server/services/tips/tips-content.ts
+++ src/server/services/tips/tips-content.ts
@@ -396,4 +396,25 @@ export const tips: Tip[] = [
       to: 'https://www.amsterdam.nl/verkeer-vervoer/kinderen-gratis-openbaar-vervoer/#h24924a59-2782-410a-b9fd-d56aedabbdf2',
     },
   },
+  {
+    id: 'mijn-59',
+    owner: '',
+    dateActiveStart: '2025-01-01',
+    dateActiveEnd: '2025-12-31',
+    active: true,
+    priority: 1,
+    datePublished: '2025-01-16',
+    title: '€300 korting op witgoed',
+    thema: Themas.HLI,
+    profileTypes: ['private'],
+    description:
+      'Met je Stadspas krijg je € 300,- korting op een nieuwe energiezuinige wasmachine of koelkast. Dit helpt je om geld te besparen op je energierekening.',
+    predicates: [is18OrOlder, hasStadspasGroeneStip],
+    reason:
+      'U ziet deze tip omdat u ouder bent dan 18 jaar en woonachtig bent in Amsterdam en een Stadspas heeft.',
+    link: {
+      title: 'Meer informatie en aanvraagformulier',
+      to: 'https://www.amsterdam.nl/stadspas/stadspasacties/korting-energiezuinige-wasmachine/',
+    },
+  },
 ];
