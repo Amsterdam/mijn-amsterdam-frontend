3fc6e81c8034db047149ff3942beb24af7f40d82
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-17 18:11:26 +0100

    Add welcome heading

--- src/client/pages/Dashboard/Dashboard.tsx
+++ src/client/pages/Dashboard/Dashboard.tsx
@@ -8,10 +8,9 @@ import {
 } from '@amsterdam/design-system-react';
 import { generatePath, useHistory } from 'react-router-dom';
 
-import styles from './Dashboard.module.scss';
+import { WelcomeHeading } from './WelcomHeading';
 import { AppRoutes } from '../../../universal/config/routes';
 import { isLoading } from '../../../universal/helpers/api';
-import { getFullName } from '../../../universal/helpers/brp';
 import { LoadingContent, Page } from '../../components';
 import { MaRouterLink } from '../../components/MaLink/MaLink';
 import MyAreaDashboard from '../../components/MyArea/MyAreaDashboard';
@@ -20,7 +19,6 @@ import { MyThemasPanel } from '../../components/MyThemasPanel/MyThemasPanel';
 import { usePhoneScreen } from '../../hooks/media.hook';
 import { useAppStateGetter } from '../../hooks/useAppState';
 import { useAppStateNotifications } from '../../hooks/useNotifications';
-import { useProfileTypeValue } from '../../hooks/useProfileType';
 import { useThemaMenuItems } from '../../hooks/useThemaMenuItems';
 
 const MAX_NOTIFICATIONS_VISIBLE = 6;
@@ -38,8 +36,6 @@ export function Dashboard() {
   const { items: myThemaItems, isLoading: isMyThemasLoading } =
     useThemaMenuItems();
 
-  const profileType = useProfileTypeValue();
-
   useEffect(() => {
     if (history.location.search) {
       history.replace(history.location.pathname);
@@ -50,16 +46,8 @@ export function Dashboard() {
     <Page>
       <Screen>
         <Grid>
-          <Grid.Cell span="all">
-            <div className={styles.Welcome}>
-              <Heading className={styles.WelcomeHeading} level={3}>
-                Goedemorgen,
-                <br />{' '}
-                {BRP.content?.persoon ? getFullName(BRP.content?.persoon) : ''}
-              </Heading>
-            </div>
-          </Grid.Cell>
-          <Grid.Cell start={1} span={6}>
+          <WelcomeHeading />
+          <Grid.Cell start={2} span={6}>
             <Heading level={2} className="ams-mb--sm">
               Recente berichten{' '}
               {total > notifications.length && (
@@ -91,7 +79,7 @@ export function Dashboard() {
                 })}
             </OrderedList>
           </Grid.Cell>
-          <Grid.Cell start={7} span={6}>
+          <Grid.Cell start={8} span={5}>
             <Heading level={2} className="ams-mb--sm">
               Mijn thema&apos;s
             </Heading>
