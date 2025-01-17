7f5b5e053b604c9352d67703a11b888a3d5d8fe2
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-17 18:11:26 +0100

    Add welcome heading

---
+++ src/client/components/MainHeader/ProfileName.tsx
@@ -0,0 +1,30 @@
+import { isLoading } from '../../../universal/helpers/api';
+import { getFullName } from '../../../universal/helpers/brp';
+import { useAppStateGetter } from '../../hooks/useAppState';
+import { useProfileTypeValue } from '../../hooks/useProfileType';
+import LoadingContent from '../LoadingContent/LoadingContent';
+
+export function ProfileName() {
+  const { BRP, KVK } = useAppStateGetter();
+  const profileType = useProfileTypeValue();
+
+  const persoon = BRP.content?.persoon;
+
+  const labelCommercial =
+    KVK.content?.onderneming.handelsnaam || 'Mijn onderneming';
+  const labelPrivate = persoon?.opgemaakteNaam
+    ? persoon.opgemaakteNaam
+    : persoon?.voornamen
+      ? getFullName(persoon)
+      : 'Mijn gegevens';
+  return (
+    <>
+      {profileType === 'private' && !isLoading(BRP) && labelPrivate}
+      {profileType === 'commercial' && !isLoading(KVK) && labelCommercial}
+      {((profileType === 'commercial' && isLoading(KVK)) ||
+        (profileType === 'private' && isLoading(BRP))) && (
+        <LoadingContent barConfig={[['200px', '20px', '0']]} />
+      )}
+    </>
+  );
+}
