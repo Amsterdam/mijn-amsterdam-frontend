57b44012aa7dd2bee02bdf848ec69e46c22b0d45
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-17 17:35:59 +0100

    Refactor Main Menu, remove components

---
+++ src/client/components/MainHeader/useMainHeaderData.hook.ts
@@ -0,0 +1,191 @@
+import { useMemo, useState, useEffect, useCallback, useRef } from 'react';
+
+import { useSpring } from '@react-spring/web';
+import { useLocation, useHistory } from 'react-router-dom';
+
+import { mainMenuItems, isMenuItemVisible } from './MainHeader.constants';
+import { AppRoutes } from '../../../universal/config/routes';
+import { getApiErrors } from '../../config/api';
+import { ThemaTitles } from '../../config/thema';
+import { usePhoneScreen } from '../../hooks/media.hook';
+import { useAppStateGetter } from '../../hooks/useAppState';
+import { useProfileTypeValue } from '../../hooks/useProfileType';
+import { useTermReplacement } from '../../hooks/useTermReplacement';
+import { useThemaMenuItems } from '../../hooks/useThemaMenuItems';
+import {
+  AmsMainMenuClassname,
+  BurgerMenuToggleBtnId,
+} from '../MainMenu/MainMenu';
+import { SearchEntry } from '../Search/search-config';
+import { useSearchOnPage } from '../Search/useSearch';
+
+function isTargetWithinMenu(target: any) {
+  const LinkContainer = document.querySelector(`.${AmsMainMenuClassname}`);
+  const BurgerMenuToggleButton = document.getElementById(BurgerMenuToggleBtnId);
+  return (
+    LinkContainer?.contains(target) || BurgerMenuToggleButton?.contains(target)
+  );
+}
+
+export function useMainHeaderData() {
+  const appState = useAppStateGetter();
+  const errors = useMemo(() => getApiErrors(appState), [appState]);
+  const hasErrors = !!errors.length;
+  const termReplace = useTermReplacement();
+  const [isBurgerMenuVisible, toggleBurgerMenu] = useState<boolean | undefined>(
+    undefined
+  );
+  const { items: myThemasMenuItems } = useThemaMenuItems();
+  const location = useLocation();
+  const profileType = useProfileTypeValue();
+  const { isSearchActive, setSearchActive, isDisplayLiveSearch } =
+    useSearchOnPage();
+
+  const fadeStyles = useSpring({
+    config: { duration: 100 },
+    from: { opacity: 0, display: 'none' },
+    to: {
+      opacity: isBurgerMenuVisible ? 1 : 0,
+      display: isBurgerMenuVisible ? 'block' : 'none',
+    },
+  });
+  const isPhoneScreen = usePhoneScreen();
+
+  // Bind click outside and tab navigation interaction
+  useEffect(() => {
+    const onTab = (event: KeyboardEvent) => {
+      const isMenuTarget = isTargetWithinMenu(event.target);
+      if (event.key === 'Tab') {
+        if (isBurgerMenuVisible === true && !isMenuTarget) {
+          toggleBurgerMenu(false);
+        } else if (isBurgerMenuVisible === false && isMenuTarget) {
+          toggleBurgerMenu(true);
+        }
+      }
+    };
+
+    const onClickOutsideBurgermenu = (event?: MouseEvent) => {
+      if (isBurgerMenuVisible === true && !isTargetWithinMenu(event?.target)) {
+        toggleBurgerMenu(false);
+      }
+    };
+
+    document.addEventListener('keyup', onTab);
+    document.addEventListener('click', onClickOutsideBurgermenu);
+    return () => {
+      document.removeEventListener('keyup', onTab);
+      document.removeEventListener('click', onClickOutsideBurgermenu);
+    };
+  }, [isBurgerMenuVisible]);
+
+  // Close menu on click escape key
+  useEffect(() => {
+    const onEscape = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        const activeElement = document.activeElement as HTMLElement;
+        toggleBurgerMenu(false);
+        activeElement?.blur();
+        return;
+      }
+    };
+    if (!isBurgerMenuVisible) return;
+
+    document.addEventListener('keydown', onEscape);
+    return () => {
+      document.removeEventListener('keydown', onEscape);
+    };
+  }, [isBurgerMenuVisible]);
+
+  // Hides small screen menu on route change
+  useEffect(() => {
+    toggleBurgerMenu(false);
+  }, [location.pathname]);
+
+  const replaceResultUrl = useCallback((result: SearchEntry) => {
+    return result.url.startsWith(AppRoutes.BUURT);
+  }, []);
+
+  const menuItems = useMemo(() => {
+    return mainMenuItems
+      .filter((menuItem) => isMenuItemVisible(profileType, menuItem))
+      .map((item) => {
+        let menuItem = item;
+        if (menuItem.title === ThemaTitles.BUURT && profileType !== 'private') {
+          menuItem = {
+            ...menuItem,
+            title: termReplace(menuItem.title),
+          };
+        }
+
+        return menuItem;
+      });
+  }, [myThemasMenuItems, profileType, termReplace]);
+
+  const backdropRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const adjustOverlay = () => {
+      if (!menuRef.current) {
+        return;
+      }
+
+      // Add 10 pixels to the height to make sure the overlay is not visible when the menu is changing is size
+      const PADDING_HEIGHT = 10;
+      let menuHeight = menuRef.current?.offsetHeight + PADDING_HEIGHT;
+      const scrollTop = window.scrollY;
+
+      if (scrollTop >= menuHeight) {
+        menuHeight = 0;
+      }
+      if (backdropRef.current && isBurgerMenuVisible) {
+        backdropRef.current.style.top = `${menuHeight}px`;
+        backdropRef.current.style.height = `calc(100vh - ${menuHeight}px)`;
+      }
+    };
+
+    adjustOverlay();
+    window.addEventListener('resize', adjustOverlay);
+    window.addEventListener('scroll', adjustOverlay);
+    window.addEventListener('touchmove', adjustOverlay);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener('resize', adjustOverlay);
+      window.removeEventListener('scroll', adjustOverlay);
+      window.removeEventListener('touchmove', adjustOverlay);
+    };
+  }, [isBurgerMenuVisible]);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const h1Element = document.querySelector('h1');
+    const goToHomepage = () => {
+      history.push(AppRoutes.HOME);
+    };
+    if (h1Element?.textContent === 'Mijn Amsterdam') {
+      h1Element.addEventListener('click', goToHomepage);
+    }
+    return () => {
+      h1Element?.removeEventListener('click', goToHomepage);
+    };
+  }, []);
+
+  return {
+    backdropRef,
+    errors,
+    fadeStyles,
+    hasErrors,
+    isBurgerMenuVisible,
+    isDisplayLiveSearch,
+    isPhoneScreen,
+    isSearchActive,
+    menuItems,
+    menuRef,
+    myThemasMenuItems,
+    replaceResultUrl,
+    setSearchActive,
+    toggleBurgerMenu,
+  };
+}
--- src/client/components/MainMenu/MainMenu.tsx
+++ src/client/components/MainMenu/MainMenu.tsx
@@ -3,8 +3,10 @@ import { Heading, MegaMenu } from '@amsterdam/design-system-react';
 import styles from './MainMenu.module.scss';
 import { ThemaMenuItemTransformed } from '../../config/thema';
 import { MenuItem } from '../MainHeader/MainHeader.constants';
-import { SecondaryLinks } from '../MainHeader/SecondaryLinks';
 import { MaLink, MaRouterLink } from '../MaLink/MaLink';
+
+export const AmsMainMenuClassname = 'ma-main-header';
+export const BurgerMenuToggleBtnId = 'BurgerMenuToggleBtn';
 
 type MainMenuProps = {
   menuItems: MenuItem[];
@@ -14,60 +16,42 @@ type MainMenuProps = {
 
 export function MainMenu({ menuItems, themas, isPhoneScreen }: MainMenuProps) {
   return (
-    <MegaMenu>
-      <div className={styles.menu}>
-        <nav>
-          <Heading level={3} size="level-3">
-            Thema’s
-          </Heading>
-          <MegaMenu.ListCategory>
-            {themas.map((thema, index) => {
-              return thema.rel === 'external' ? (
-                <MaLink
-                  key={thema.id}
-                  href={thema.to}
-                  maVariant="noDefaultUnderline"
-                  rel="noreferrer"
-                  className={styles.menuItem}
-                >
-                  {thema.title}
-                </MaLink>
-              ) : (
-                <MaRouterLink
-                  key={thema.id}
-                  href={thema.to}
-                  maVariant="noDefaultUnderline"
-                  className={styles.menuItem}
-                >
-                  {thema.title}
-                </MaRouterLink>
-              );
-            })}
-          </MegaMenu.ListCategory>
-        </nav>
-        <nav>
-          <Heading level={3} size="level-3">
-            Categorieën
-          </Heading>
-          <MegaMenu.ListCategory>
-            {menuItems.map((item) => (
-              <MaRouterLink
-                key={item.id}
-                href={item.to}
-                maVariant="noDefaultUnderline"
+    <MegaMenu className={styles.MainMenu}>
+      <nav>
+        <Heading level={3}>Thema&apos;s</Heading>
+        <MegaMenu.ListCategory>
+          {themas.map((thema) => {
+            const LinkComponent =
+              thema.rel === 'external' ? MaLink : MaRouterLink;
+            return (
+              <LinkComponent
+                key={thema.id}
+                href={thema.to}
+                maVariant="fatNoUnderline"
+                rel={thema.rel === 'external' ? 'noreferrer' : undefined}
                 className={styles.menuItem}
               >
-                {item.title}
-              </MaRouterLink>
-            ))}
-          </MegaMenu.ListCategory>
-        </nav>
-        {isPhoneScreen && (
-          <nav>
-            <SecondaryLinks showIcons />
-          </nav>
-        )}
-      </div>
+                {thema.title}
+              </LinkComponent>
+            );
+          })}
+        </MegaMenu.ListCategory>
+      </nav>
+      <nav>
+        <Heading level={3}>Categorieën</Heading>
+        <MegaMenu.ListCategory>
+          {menuItems.map((item) => (
+            <MaRouterLink
+              key={item.id}
+              href={item.to}
+              maVariant="noDefaultUnderline"
+              className={styles.menuItem}
+            >
+              {item.title}
+            </MaRouterLink>
+          ))}
+        </MegaMenu.ListCategory>
+      </nav>
     </MegaMenu>
   );
 }
