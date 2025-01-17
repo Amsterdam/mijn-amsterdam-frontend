d4eb4f284d12d8373c5eadd4420d74e38a5c6ffe
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-17 22:09:40 +0100

    Add PageHeadingV2 component, add PageV2 components

--- src/client/pages/MyNotifications/MyNotifications.tsx
+++ src/client/pages/MyNotifications/MyNotifications.tsx
@@ -1,22 +1,16 @@
 import { useEffect, useMemo } from 'react';
 
-import { OrderedList } from '@amsterdam/design-system-react';
+import { Grid, OrderedList } from '@amsterdam/design-system-react';
 import classNames from 'classnames';
 import { generatePath, useHistory, useParams } from 'react-router-dom';
 
 import styles from './MyNotifications.module.scss';
 import { AppRoutes } from '../../../universal/config/routes';
 import { isError, isLoading } from '../../../universal/helpers/api';
-import {
-  ErrorAlert,
-  ThemaIcon,
-  DetailPage,
-  PageContent,
-  PageHeading,
-  Pagination,
-  LoadingContent,
-} from '../../components';
+import { ErrorAlert, Pagination, LoadingContent } from '../../components';
 import { MyNotification } from '../../components/MyNotification/MyNotification';
+import { DetailPageV2, PageContentV2 } from '../../components/Page/Page';
+import { PageHeadingV2 } from '../../components/PageHeading/PageHeadingV2';
 import { useAppStateGetter } from '../../hooks/useAppState';
 import { useAppStateNotifications } from '../../hooks/useNotifications';
 
@@ -51,71 +45,68 @@ export function MyNotificationsPage() {
   }, [currentPage]);
 
   return (
-    <DetailPage className={styles.MyNotifications}>
-      <PageHeading
-        backLink={{
-          to: AppRoutes.HOME,
-          title: 'Home',
-        }}
-        className={styles.MainHeader}
-        icon={<ThemaIcon />}
-      >
-        Actueel
-      </PageHeading>
-      <PageContent>
-        {isError(NOTIFICATIONS) && (
-          <ErrorAlert>
-            Niet alle updates kunnen op dit moment worden getoond.
-          </ErrorAlert>
-        )}
-      </PageContent>
-      {total > PAGE_SIZE && (
-        <PageContent>
-          <Pagination
-            className={styles.Pagination}
-            totalCount={total}
-            pageSize={PAGE_SIZE}
-            currentPage={currentPage}
-            onPageClick={(page) => {
-              history.replace(generatePath(AppRoutes.NOTIFICATIONS, { page }));
-            }}
-          />
-        </PageContent>
-      )}
-      <OrderedList markers={false}>
-        {isLoading(NOTIFICATIONS) && (
-          <OrderedList.Item>
-            <LoadingContent />
-          </OrderedList.Item>
-        )}
-        {!isLoading(NOTIFICATIONS) &&
-          notificationsPaginated.map((notification, index) => {
-            return (
-              <OrderedList.Item
-                key={`${notification.thema}-${notification.id}-${index}`}
-                className={classNames(styles.MyNotificationItem, 'ams-mb--sm')}
-              >
-                <MyNotification
-                  notification={notification}
-                  trackCategory="Dashboard / Actueel"
-                />
+    <DetailPageV2 className={styles.MyNotifications}>
+      <PageContentV2>
+        <PageHeadingV2 backLink={AppRoutes.HOME}>Actueel</PageHeadingV2>
+        <Grid.Cell span="all">
+          {isError(NOTIFICATIONS) && (
+            <ErrorAlert>
+              Niet alle updates kunnen op dit moment worden getoond.
+            </ErrorAlert>
+          )}
+          {total > PAGE_SIZE && (
+            <Pagination
+              className={styles.Pagination}
+              totalCount={total}
+              pageSize={PAGE_SIZE}
+              currentPage={currentPage}
+              onPageClick={(page) => {
+                history.replace(
+                  generatePath(AppRoutes.NOTIFICATIONS, { page })
+                );
+              }}
+            />
+          )}
+
+          <OrderedList markers={false}>
+            {isLoading(NOTIFICATIONS) && (
+              <OrderedList.Item>
+                <LoadingContent />
               </OrderedList.Item>
-            );
-          })}
-      </OrderedList>
-      {total > PAGE_SIZE && (
-        <PageContent>
-          <Pagination
-            className={styles.Pagination}
-            totalCount={total}
-            pageSize={PAGE_SIZE}
-            currentPage={currentPage}
-            onPageClick={(page) => {
-              history.replace(generatePath(AppRoutes.NOTIFICATIONS, { page }));
-            }}
-          />
-        </PageContent>
-      )}
-    </DetailPage>
+            )}
+            {!isLoading(NOTIFICATIONS) &&
+              notificationsPaginated.map((notification, index) => {
+                return (
+                  <OrderedList.Item
+                    key={`${notification.thema}-${notification.id}-${index}`}
+                    className={classNames(
+                      styles.MyNotificationItem,
+                      'ams-mb--sm'
+                    )}
+                  >
+                    <MyNotification
+                      notification={notification}
+                      trackCategory="Dashboard / Actueel"
+                    />
+                  </OrderedList.Item>
+                );
+              })}
+          </OrderedList>
+          {total > PAGE_SIZE && (
+            <Pagination
+              className={styles.Pagination}
+              totalCount={total}
+              pageSize={PAGE_SIZE}
+              currentPage={currentPage}
+              onPageClick={(page) => {
+                history.replace(
+                  generatePath(AppRoutes.NOTIFICATIONS, { page })
+                );
+              }}
+            />
+          )}
+        </Grid.Cell>
+      </PageContentV2>
+    </DetailPageV2>
   );
 }
