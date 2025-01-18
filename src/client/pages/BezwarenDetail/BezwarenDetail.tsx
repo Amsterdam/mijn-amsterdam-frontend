9e1febccecc8b1ca0004105b35592f8af082796a
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-18 10:26:32 +0100

    Bezwaren new components

--- src/client/pages/BezwarenDetail/BezwarenDetail.tsx
+++ src/client/pages/BezwarenDetail/BezwarenDetail.tsx
@@ -8,15 +8,7 @@ import { AppRoutes } from '../../../universal/config/routes';
 import { isError, isLoading } from '../../../universal/helpers/api';
 import { defaultDateFormat } from '../../../universal/helpers/date';
 import { uniqueArray } from '../../../universal/helpers/utils';
-import {
-  DetailPage,
-  ErrorAlert,
-  InfoDetail,
-  LoadingContent,
-  PageContent,
-  PageHeading,
-  ThemaIcon,
-} from '../../components';
+import { ErrorAlert, InfoDetail, LoadingContent } from '../../components';
 import DocumentList from '../../components/DocumentList/DocumentList';
 import {
   InfoDetailGroup,
@@ -24,8 +16,14 @@ import {
 } from '../../components/InfoDetail/InfoDetail';
 import { TextClamp } from '../../components/InfoDetail/TextClamp';
 import { BarConfig } from '../../components/LoadingContent/LoadingContent';
+import {
+  DetailPageV2,
+  PageContentCell,
+  PageContentV2,
+} from '../../components/Page/Page';
+import { PageHeadingV2 } from '../../components/PageHeading/PageHeadingV2';
 import { BFFApiUrls } from '../../config/api';
-import { BagThemas, ThemaTitles } from '../../config/thema';
+import { BagThemas } from '../../config/thema';
 import { usePhoneScreen } from '../../hooks/media.hook';
 import { useAppStateBagApi, useAppStateGetter } from '../../hooks/useAppState';
 
@@ -58,47 +56,53 @@ function BezwarenDetailPartial({ uuidEncrypted }: BezwarenDetailPartialProps) {
 
   return (
     <>
-      <PageContent>
-        {documentCategories.length > 0 && (
-          <>
-            {documentCategories.map((category) => {
-              const docs = documents.filter((d) => d.dossiertype === category);
-              return (
-                <InfoDetailGroup
-                  key={category}
-                  label={
-                    <div className={styles.DocumentListHeader}>
-                      <InfoDetailHeading
-                        label={`Document${
-                          documents.length > 1 ? 'en' : ''
-                        } ${category.toLowerCase()}`}
-                      />
-                      {!isSmallScreen && (
-                        <span className={styles.DocumentListHeader_Date}>
-                          Datum
-                        </span>
-                      )}
-                    </div>
-                  }
-                >
-                  <DocumentList documents={docs} showDatePublished />
-                </InfoDetailGroup>
-              );
-            })}
-          </>
+      <PageContentV2>
+        <PageContentCell>
+          {documentCategories.length > 0 && (
+            <>
+              {documentCategories.map((category) => {
+                const docs = documents.filter(
+                  (d) => d.dossiertype === category
+                );
+                return (
+                  <InfoDetailGroup
+                    key={category}
+                    label={
+                      <div className={styles.DocumentListHeader}>
+                        <InfoDetailHeading
+                          label={`Document${
+                            documents.length > 1 ? 'en' : ''
+                          } ${category.toLowerCase()}`}
+                        />
+                        {!isSmallScreen && (
+                          <span className={styles.DocumentListHeader_Date}>
+                            Datum
+                          </span>
+                        )}
+                      </div>
+                    }
+                  >
+                    <DocumentList documents={docs} showDatePublished />
+                  </InfoDetailGroup>
+                );
+              })}
+            </>
+          )}
+          {isLoading(bezwaarDetailApiResponse) && (
+            <>
+              <Heading level={4} size="level-4">
+                Status
+              </Heading>
+              <LoadingContent barConfig={loadingContentBarConfig2} />
+            </>
+          )}
+        </PageContentCell>
+        {!!bezwaarDetail && (
+          <PageContentCell>
+            <BezwarenStatusLines id={uuidEncrypted} statussen={statussen} />
+          </PageContentCell>
         )}
-        {isLoading(bezwaarDetailApiResponse) && (
-          <>
-            <Heading level={4} size="level-4">
-              Status
-            </Heading>
-            <LoadingContent barConfig={loadingContentBarConfig2} />
-          </>
-        )}
-      </PageContent>
-      {!!bezwaarDetail && (
-        <BezwarenStatusLines id={uuidEncrypted} statussen={statussen} />
-      )}
+      </PageContentV2>
     </>
   );
 }
@@ -111,70 +115,70 @@ function BezwarenDetail() {
   const noContent = !isLoading(BEZWAREN) && !bezwaar;
 
   return (
-    <DetailPage>
-      <PageHeading
-        icon={<ThemaIcon />}
-        backLink={{
-          to: AppRoutes.BEZWAREN,
-          title: ThemaTitles.BEZWAREN,
-        }}
-        isLoading={isLoading(BEZWAREN)}
-      >
-        {bezwaar?.identificatie ?? 'Bezwaar'}
-      </PageHeading>
-
-      <PageContent>
-        {(isError(BEZWAREN) || noContent) && (
-          <ErrorAlert>We kunnen op dit moment geen gegevens tonen.</ErrorAlert>
-        )}
-        {!!bezwaar && (
-          <>
-            {bezwaar.omschrijving && (
-              <InfoDetail label="Onderwerp" value={bezwaar.omschrijving} />
-            )}
-            {bezwaar.toelichting && (
-              <InfoDetail
-                label="Reden bezwaar"
-                value={
-                  <TextClamp tagName="span" minHeight="100px" maxHeight="200px">
-                    {bezwaar.toelichting}
-                  </TextClamp>
-                }
-              />
-            )}
-            {bezwaar.primairbesluit && bezwaar.primairbesluitdatum && (
-              <InfoDetailGroup>
+    <DetailPageV2>
+      <PageContentV2>
+        <PageHeadingV2 backLink={AppRoutes.BEZWAREN}>
+          {bezwaar?.identificatie ?? 'Bezwaar'}
+        </PageHeadingV2>
+        <PageContentCell>
+          {(isError(BEZWAREN) || noContent) && (
+            <ErrorAlert>
+              We kunnen op dit moment geen gegevens tonen.
+            </ErrorAlert>
+          )}
+          {!!bezwaar && (
+            <>
+              {bezwaar.omschrijving && (
+                <InfoDetail label="Onderwerp" value={bezwaar.omschrijving} />
+              )}
+              {bezwaar.toelichting && (
                 <InfoDetail
-                  label="Besluit waartegen u bezwaar maakt"
-                  value={bezwaar.primairbesluit}
+                  label="Reden bezwaar"
+                  value={
+                    <TextClamp
+                      tagName="span"
+                      minHeight="100px"
+                      maxHeight="200px"
+                    >
+                      {bezwaar.toelichting}
+                    </TextClamp>
+                  }
                 />
-                <InfoDetail
-                  label="Datum"
-                  value={defaultDateFormat(bezwaar.primairbesluitdatum)}
-                />
-              </InfoDetailGroup>
-            )}
-            {bezwaar.einddatum && bezwaar.resultaat && (
-              <InfoDetailGroup>
-                <InfoDetail
-                  label="Resultaat bezwaar"
-                  value={bezwaar.resultaat}
-                />
-                {!!bezwaar.datumResultaat && (
+              )}
+              {bezwaar.primairbesluit && bezwaar.primairbesluitdatum && (
+                <InfoDetailGroup>
+                  <InfoDetail
+                    label="Besluit waartegen u bezwaar maakt"
+                    value={bezwaar.primairbesluit}
+                  />
                   <InfoDetail
                     label="Datum"
-                    value={defaultDateFormat(bezwaar.datumResultaat)}
+                    value={defaultDateFormat(bezwaar.primairbesluitdatum)}
                   />
-                )}
-              </InfoDetailGroup>
-            )}
-          </>
-        )}
-      </PageContent>
+                </InfoDetailGroup>
+              )}
+              {bezwaar.einddatum && bezwaar.resultaat && (
+                <InfoDetailGroup>
+                  <InfoDetail
+                    label="Resultaat bezwaar"
+                    value={bezwaar.resultaat}
+                  />
+                  {!!bezwaar.datumResultaat && (
+                    <InfoDetail
+                      label="Datum"
+                      value={defaultDateFormat(bezwaar.datumResultaat)}
+                    />
+                  )}
+                </InfoDetailGroup>
+              )}
+            </>
+          )}
+        </PageContentCell>
+      </PageContentV2>
       {!!bezwaar?.uuidEncrypted && (
         <BezwarenDetailPartial uuidEncrypted={bezwaar.uuidEncrypted} />
       )}
-    </DetailPage>
+    </DetailPageV2>
   );
 }
 
