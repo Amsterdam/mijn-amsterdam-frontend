cb56f755ed96d107eb237c27a7fd9c4d1549351b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-19 11:35:29 +0100

    Add some logic to welcome

--- src/client/pages/Dashboard/WelcomHeading.tsx
+++ src/client/pages/Dashboard/WelcomHeading.tsx
@@ -1,13 +1,42 @@
+import styles from './WelcomeHeading.module.scss';
 import { ProfileName } from '../../components/MainHeader/ProfileName';
 import { PageHeadingV2 } from '../../components/PageHeading/PageHeadingV2';
 
 const HELLO = 'Welkom,';
+const NOON = 12;
+const EVENING = 18;
+const NIGHT = 5;
 
 export function WelcomeHeading() {
+  const hours = new Date().getHours();
+  const isMorning = hours < NOON && hours >= NIGHT;
+  const isEvening = hours >= EVENING;
+  const isNight = hours < NIGHT;
+  const isAfternoon = hours >= NOON && hours < EVENING;
+
+  let hello = HELLO;
+
+  switch (true) {
+    case isMorning:
+      hello = 'Goedemorgen,';
+      break;
+    case isAfternoon:
+      hello = 'Goedemiddag,';
+      break;
+    case isEvening:
+      hello = 'Goedenavond,';
+      break;
+    case isNight:
+      hello = 'Goedenacht,';
+      break;
+  }
+
   return (
     <PageHeadingV2 showBacklink={false}>
-      {HELLO}
-      <br /> <ProfileName />
+      {hello}{' '}
+      <span className={styles.ProfileNameWrap}>
+        <ProfileName />
+      </span>
     </PageHeadingV2>
   );
 }
