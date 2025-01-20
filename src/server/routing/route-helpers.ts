f58b0b266799097dd0ad1b914ec0486dcde71c07
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-20 13:17:39 +0100

    Refine types
    # Conflicts:
    #	src/server/routing/route-helpers.ts
    
    # Conflicts:
    #	src/server/routing/route-helpers.ts

--- src/server/routing/route-helpers.ts
+++ src/server/routing/route-helpers.ts
@@ -58,7 +58,7 @@ export function generateFullApiUrlBFF(
 /** Sets the right statuscode and sends a response. */
 export function sendResponse(
   res: Response,
-  apiResponse: ApiResponse_DEPRECATED<any>
+  apiResponse: ApiResponse_DEPRECATED<unknown>
 ) {
   if (apiResponse.status === 'ERROR') {
     res.status(
@@ -107,7 +107,7 @@ export function sendMessage(
   res: Response,
   id: string,
   event: string = 'message',
-  data?: any
+  data?: object | string | number | null
 ) {
   const doStringify = typeof data !== 'string';
   const payload = doStringify ? JSON.stringify(data) : data;
