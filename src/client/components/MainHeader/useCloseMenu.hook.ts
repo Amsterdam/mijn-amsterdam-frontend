c606367bcf961065aea19097359e1f62823b9f93
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-22 20:44:37 +0100

    Close Main Header menu on other interaction

---
+++ src/client/components/MainHeader/useCloseMenu.hook.ts
@@ -0,0 +1,46 @@
+import { useCallback, useEffect } from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+import { useKeyUp } from '../../hooks/useKey';
+import { useSearchActive } from '../Search/useSearch';
+
+function getCloseButtonNode() {
+  const button = document.querySelector(
+    'button[aria-expanded=true].ams-header__mega-menu-button'
+  );
+  return button as HTMLButtonElement;
+}
+
+export function useCloseMenu() {
+  const closeMenu = useCallback(() => {
+    getCloseButtonNode()?.click();
+  }, []);
+
+  const location = useLocation();
+  const [isSearchActive] = useSearchActive();
+
+  useEffect(() => {
+    if (isSearchActive) {
+      closeMenu();
+    }
+  }, [isSearchActive, closeMenu]);
+
+  useEffect(() => {
+    closeMenu();
+  }, [location.pathname, closeMenu]);
+
+  const keyHandler = useCallback(
+    (event: KeyboardEvent) => {
+      const isEscape = event.key === 'Escape';
+      if (isEscape) {
+        closeMenu();
+      }
+    },
+    [closeMenu]
+  );
+
+  useKeyUp(keyHandler);
+
+  return closeMenu;
+}
