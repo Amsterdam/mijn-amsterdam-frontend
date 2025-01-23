647f340cfa0a6874fe12398b02b1e36a027ad7a4
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-23 16:16:50 +0100

    Mijn-9980-bug geblokkeerde passen worden eruit gefiltert wut tf (#1725)
    * Fix test
    
    * Reset mocks?!

--- src/server/services/hli/stadspas.test.ts
+++ src/server/services/hli/stadspas.test.ts
@@ -39,7 +39,7 @@ function createStadspasHouderResponse() {
 
 function createPas(
   actief: boolean,
-  // eslint-disable-next-line no-magic-numbers
+
   pasnummer: number = 777777777777,
   securitycode: string = '012345'
 ) {
@@ -128,6 +128,7 @@ describe('stadspas services', () => {
 
   afterAll(() => {
     vi.restoreAllMocks();
+    vi.resetAllMocks();
   });
 
   test('stadspas-zorgned-service', async () => {
@@ -252,7 +253,7 @@ describe('stadspas services', () => {
       .reply(200, pashouderResponse);
     remoteApi
       .get(/\/stadspas\/rest\/sales\/v1\/pas\//)
-      .times(3)
+      .times(6)
       .matchHeader('authorization', `AppBearer ${FAKE_API_KEY},0363000123-123`)
       .reply(200, pasResponse);
 
@@ -262,7 +263,10 @@ describe('stadspas services', () => {
         administratienummer: '0363000123-123',
         stadspassen: [
           createTransformedPas('Vadertje', 'A'),
+          createTransformedPas('Vadertje', 'A'),
           createTransformedPas('Moedertje', 'B'),
+          createTransformedPas('Moedertje', 'B'),
+          createTransformedPas('Kindje', 'C'),
           createTransformedPas('Kindje', 'C'),
         ],
       },
