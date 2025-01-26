5f7c59117bbe6331fd0117694c1b50716466f251
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-26 12:44:18 +0100

    Fixed widths on large screens

--- src/client/pages/Krefia/Krefia.tsx
+++ src/client/pages/Krefia/Krefia.tsx
@@ -1,5 +1,6 @@
 import { Paragraph } from '@amsterdam/design-system-react';
 
+import styles from './Krefia.module.scss';
 import { useKrefiaThemaData } from './useKrefiaThemaData.hook';
 import type { KrefiaDeepLink } from '../../../server/services';
 import { entries } from '../../../universal/helpers/utils';
@@ -51,6 +52,7 @@ export function KrefiaThemaPagina() {
           title={title}
           zaken={deepLinks.filter(filter)}
           displayProps={displayProps}
+          className={styles.KrefiaTable}
         />
       );
     }
