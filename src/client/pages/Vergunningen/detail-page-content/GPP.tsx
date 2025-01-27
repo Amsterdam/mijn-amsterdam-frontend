8180a3228a52bf97661b61cce3d797ef903f3d7f
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-27 22:18:06 +0100

    Add detail page content
    # Conflicts:
    #	src/client/pages/Vergunningen/VergunningDetail.tsx

--- src/client/pages/Vergunningen/detail-page-content/GPP.tsx
+++ src/client/pages/Vergunningen/detail-page-content/GPP.tsx
@@ -1,14 +1,11 @@
 import { getRows } from './fields-config';
-import {
-  GPP,
-  VergunningFrontendV2,
-} from '../../../../server/services/vergunningen/config-and-types';
+import { VergunningFrontendV2 } from '../../../../server/services/vergunningen/config-and-types';
 import { Datalist } from '../../../components/Datalist/Datalist';
 
 export function GPPContent({
   vergunning,
 }: {
-  vergunning: VergunningFrontendV2<GPP>;
+  vergunning: VergunningFrontendV2;
 }) {
   const rows = getRows(vergunning, [
     'identifier',
