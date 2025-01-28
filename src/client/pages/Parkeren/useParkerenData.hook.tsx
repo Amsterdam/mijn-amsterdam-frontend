b029e979646b44ac66f7852dd8fa283f53351680
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-28 22:58:52 +0100

    Add title link

--- src/client/pages/Parkeren/useParkerenData.hook.tsx
+++ src/client/pages/Parkeren/useParkerenData.hook.tsx
@@ -1,12 +1,18 @@
 import { linkListItems, routes, tableConfig } from './Parkeren-thema-config';
+import { DecosParkeerVergunning } from '../../../server/services/parkeren/config-and-types';
+import { VergunningFrontend } from '../../../server/services/vergunningen/config-and-types';
 import { isError, isLoading } from '../../../universal/helpers/api';
+import { addLinkElementToProperty } from '../../components/Table/TableV2';
 import { ThemaTitles } from '../../config/thema';
 import { useAppStateGetter } from '../../hooks/useAppState';
 
 export function useParkerenData() {
   const { PARKEREN } = useAppStateGetter();
-  const vergunningen = PARKEREN.content?.vergunningen ?? [];
   const hasMijnParkerenVergunningen = !!PARKEREN.content?.isKnown;
+
+  const vergunningen = addLinkElementToProperty<
+    VergunningFrontend<DecosParkeerVergunning>
+  >(PARKEREN.content?.vergunningen ?? [], 'identifier', true);
 
   return {
     title: ThemaTitles.PARKEREN,
