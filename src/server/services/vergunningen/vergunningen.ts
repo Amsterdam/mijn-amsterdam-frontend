ef6517eb9ed23aba5030a1f031d2732467cd46ad
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-28 09:11:12 +0100

    Change route set-up

--- src/server/services/vergunningen/vergunningen.ts
+++ src/server/services/vergunningen/vergunningen.ts
@@ -45,7 +45,7 @@ export function transformVergunningFrontend<V extends DecosZaakBase>(
     ),
     link: {
       to: generatePath(appRoute, {
-        title: slug(vergunning.caseType, {
+        caseType: slug(vergunning.caseType, {
           lower: true,
         }),
         id: vergunning.id,
