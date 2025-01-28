0742da44fd3f1c015ee5039279cb7753f82a33d1
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-28 23:46:29 +0100

    Separate concerns

--- src/client/pages/Parkeren/detail-page-content/BZP.tsx
+++ src/client/pages/Parkeren/detail-page-content/BZP.tsx
@@ -1,14 +1,14 @@
-import type { BZP as BZPVergunning } from '../../../../server/services/vergunningen/vergunningen';
+import type { BZP } from '../../../../server/services/parkeren/config-and-types';
 import { defaultDateFormat } from '../../../../universal/helpers/date';
 import InfoDetail, {
   InfoDetailGroup,
 } from '../../../components/InfoDetail/InfoDetail';
 
-export function BZP({ vergunning }: { vergunning: BZPVergunning }) {
+export function BZP({ vergunning }: { vergunning: BZP }) {
   return (
     <>
       <InfoDetail label="Kenmerk" value={vergunning?.identifier || '-'} />
-      <InfoDetail label="Kenteken" value={vergunning.kenteken || '-'} />
+      <InfoDetail label="Kenteken" value={vergunning.kentekens || '-'} />
       {!!vergunning.dateStart &&
         !!vergunning.dateEnd &&
         vergunning.decision === 'Verleend' &&
