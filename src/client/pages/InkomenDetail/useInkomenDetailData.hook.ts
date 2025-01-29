c5530e054bee626f5a5250b3689e0c1e5ea615c7
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-29 14:55:28 +0100

    Conver inkomen detail pagina’s

---
+++ src/client/pages/InkomenDetail/useInkomenDetailData.hook.ts
@@ -0,0 +1,22 @@
+import { useParams } from 'react-router-dom';
+
+import { isError, isLoading } from '../../../universal/helpers/api';
+import { useAppStateGetter } from '../../hooks/useAppState';
+
+export function useInkomenDetailData(
+  stateKey: 'WPI_TOZO' | 'WPI_BBZ' | 'WPI_AANVRAGEN' | 'WPI_TONK'
+) {
+  const appState = useAppStateGetter();
+  const STATE = appState[stateKey];
+  const zaken = Array.isArray(STATE.content) ? STATE.content : [];
+
+  const { id } = useParams<{ id: string }>();
+  const zaak = zaken.find((item) => item.id === id);
+
+  return {
+    zaak,
+    zaken,
+    isLoading: isLoading(STATE),
+    isError: isError(STATE),
+  };
+}
