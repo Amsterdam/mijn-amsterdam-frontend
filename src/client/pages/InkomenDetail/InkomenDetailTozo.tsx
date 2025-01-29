dff17bc87d0b23da84e11d47cc59ad024a8bdefa
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-29 14:55:28 +0100

    Conver inkomen detail pagina’s

--- src/client/pages/InkomenDetail/InkomenDetailTozo.tsx
+++ src/client/pages/InkomenDetail/InkomenDetailTozo.tsx
@@ -1,42 +1,40 @@
-import { useCallback } from 'react';
-
 import { LinkList, Paragraph } from '@amsterdam/design-system-react';
 
+import { useInkomenDetailData } from './useInkomenDetailData.hook';
+import { WpiRequestProcess } from '../../../server/services/wpi/wpi-types';
 import { PageContentCell } from '../../components/Page/Page';
 import { ExternalUrls } from '../../config/app';
-import StatusDetail, { StatusSourceItem } from '../StatusDetail/StatusDetail';
+import { routes } from '../Afis/Afis-thema-config';
+import ThemaDetailPagina from '../ThemaPagina/ThemaDetailPagina';
 
-export default function InkomenDetailTozo() {
-  const pageContent = useCallback(
-    (_isLoading: boolean, inkomenItem: StatusSourceItem) => {
-      return (
-        <PageContentCell spanWide={6}>
-          <Paragraph className="ams-mb--sm">
-            Hieronder ziet u de status van uw aanvraag voor de{' '}
-            {inkomenItem?.about || 'Tozo'}. Als u meerdere aanvragen voor de{' '}
-            {inkomenItem?.about || 'Tozo'} hebt gedaan, dan krijgt u 1 besluit
-            als antwoord op al uw aanvragen voor de{' '}
-            {inkomenItem?.about || 'Tozo'}. Het duurt maximaal 3 werkdagen
-            voordat uw documenten over de {inkomenItem?.about || 'Tozo'} in Mijn
-            Amsterdam staan.
-          </Paragraph>
-          <LinkList>
-            <LinkList.Link rel="noreferrer" href={ExternalUrls.WPI_TOZO}>
-              Meer informatie over de Tozo
-            </LinkList.Link>
-          </LinkList>
-        </PageContentCell>
-      );
-    },
-    []
+export function InkomenDetailTOZO() {
+  const { isLoading, isError, zaak } = useInkomenDetailData('WPI_TOZO');
+  const pageContentTop = (
+    <PageContentCell spanWide={6}>
+      <Paragraph className="ams-mb--sm">
+        Hieronder ziet u de status van uw aanvraag voor de{' '}
+        {zaak?.about || 'Tozo'}. Als u meerdere aanvragen voor de{' '}
+        {zaak?.about || 'Tozo'} hebt gedaan, dan krijgt u 1 besluit als antwoord
+        op al uw aanvragen voor de {zaak?.about || 'Tozo'}. Het duurt maximaal 3
+        werkdagen voordat uw documenten over de {zaak?.about || 'Tozo'} in Mijn
+        Amsterdam staan.
+      </Paragraph>
+      <LinkList>
+        <LinkList.Link rel="noreferrer" href={ExternalUrls.WPI_TOZO}>
+          Meer informatie over de Tozo
+        </LinkList.Link>
+      </LinkList>
+    </PageContentCell>
   );
 
   return (
-    <StatusDetail
-      thema="INKOMEN"
-      stateKey="WPI_TOZO"
-      pageContent={pageContent}
-      statusLabel={(statusItem) => `${statusItem?.about}-aanvraag`}
+    <ThemaDetailPagina<WpiRequestProcess>
+      title={zaak?.title || 'TOZO aanvraag'}
+      zaak={zaak}
+      isError={isError}
+      isLoading={isLoading}
+      pageContentTop={pageContentTop}
+      backLink={routes.themaPage}
     />
   );
 }
