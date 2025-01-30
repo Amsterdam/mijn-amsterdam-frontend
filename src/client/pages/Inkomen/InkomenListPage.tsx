8ea64d202658137574181d2895f4f81c11457060
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-30 10:15:19 +0100

    Inkomen upgrade

---
+++ src/client/pages/Inkomen/InkomenListPage.tsx
@@ -0,0 +1,27 @@
+import { useParams } from 'react-router-dom';
+
+import { ListPageParamKind } from './Inkomen-thema-config';
+import { useInkomenThemaData } from './useInkomenThemaData.hook';
+import { ListPagePaginated } from '../../components/ListPagePaginated/ListPagePaginated';
+
+export function InkomenLijstPagina() {
+  const { kind } = useParams<{
+    kind: Exclude<ListPageParamKind, 'jaaropgaven' | 'specificaties'>;
+  }>();
+  const { zaken, tableConfig, routes, isLoadingWpi, isErrorWpi } =
+    useInkomenThemaData();
+  const listPageTableConfig = tableConfig[kind];
+
+  return (
+    <ListPagePaginated
+      items={zaken.filter(listPageTableConfig.filter)}
+      title={listPageTableConfig.title}
+      appRoute={routes.listPage}
+      appRouteParams={{ kind }}
+      appRouteBack={routes.themaPage}
+      displayProps={listPageTableConfig.displayProps}
+      isLoading={isLoadingWpi}
+      isError={isErrorWpi}
+    />
+  );
+}
