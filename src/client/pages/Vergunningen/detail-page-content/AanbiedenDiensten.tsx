d564af6cc91120773a2be6236dcf9e00a062772c
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-31 20:50:14 +0100

    Add vergunningen transformers

--- src/client/pages/Vergunningen/detail-page-content/AanbiedenDiensten.tsx
+++ src/client/pages/Vergunningen/detail-page-content/AanbiedenDiensten.tsx
@@ -1,11 +1,12 @@
 import { getRows } from './fields-config';
+import { GPP } from '../../../../server/services/parkeren/config-and-types';
 import { VergunningFrontend } from '../../../../server/services/vergunningen/config-and-types';
 import { Datalist } from '../../../components/Datalist/Datalist';
 
 export function AanbiedenDienstenContent({
   vergunning,
 }: {
-  vergunning: VergunningFrontend;
+  vergunning: VergunningFrontend<GPP>;
 }) {
   const rows = getRows(vergunning, ['identifier', 'decision']);
 
