7662d837003c345fbbf4be6ac7df8dcccac1bf10
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-01-31 20:50:14 +0100

    Add vergunningen transformers

--- src/client/pages/Vergunningen/detail-page-content/EvenementMelding.tsx
+++ src/client/pages/Vergunningen/detail-page-content/EvenementMelding.tsx
@@ -1,49 +1,27 @@
-import { Location } from './Location';
+import { commonTransformers, getRows } from './fields-config';
 import type {
   EvenementMelding,
   VergunningFrontend,
 } from '../../../../server/services/vergunningen/config-and-types';
-import { defaultDateFormat } from '../../../../universal/helpers/date';
-import InfoDetail, {
-  InfoDetailGroup,
-} from '../../../components/InfoDetail/InfoDetail';
+import { Datalist } from '../../../components/Datalist/Datalist';
 
 export function EvenementMelding({
   vergunning,
 }: {
-  vergunning: VergunningFrontend;
+  vergunning: VergunningFrontend<EvenementMelding>;
 }) {
-  const vergunningData = vergunning as VergunningFrontend<EvenementMelding>;
-  return (
-    <>
-      <InfoDetail label="Kenmerk" value={vergunningData.identifier || '-'} />
-      {!!vergunningData.location && (
-        <Location location={vergunningData.location} />
-      )}
+  const rows = getRows(vergunning, [
+    'identifier',
+    'location',
+    {
+      onFromTo: (vergunning: VergunningFrontend<EvenementMelding>) => {
+        return vergunning.decision === 'Verleend'
+          ? commonTransformers.onFromTo(vergunning)
+          : null;
+      },
+    },
+    'decision',
+  ]);
 
-      <InfoDetailGroup>
-        <InfoDetail
-          label="Op"
-          value={
-            vergunningData.dateStart
-              ? defaultDateFormat(vergunningData.dateStart)
-              : '-'
-          }
-        />
-        <InfoDetail
-          label="Van"
-          value={
-            vergunningData.timeStart ? `${vergunningData.timeStart} uur` : '-'
-          }
-        />
-        <InfoDetail
-          label="Tot"
-          value={vergunningData.timeEnd ? `${vergunningData.timeEnd} uur` : '-'}
-        />
-      </InfoDetailGroup>
-      {!!vergunningData.decision && (
-        <InfoDetail label="Resultaat" value={vergunningData.decision} />
-      )}
-    </>
-  );
+  return <Datalist rows={rows} />;
 }
