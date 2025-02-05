8bbe84b32952556fb3e073ee159ea644ce8d8a1b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-02-05 17:11:12 +0100

    Pass on statuscode

--- src/server/services/shared/document-download-route-handler.ts
+++ src/server/services/shared/document-download-route-handler.ts
@@ -75,7 +75,11 @@ export function downloadDocumentRouteHandler(
           documentResponse.status === 'POSTPONE'
         ) {
           return res
-            .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+            .status(
+              'code' in documentResponse && documentResponse.code
+                ? documentResponse.code
+                : HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR
+            )
             .send(documentResponse);
         }
 
