49acd251a3e82d44be9a825278e7219cb65a5dcc
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-02-07 09:20:32 +0100

    Update test

--- src/server/services/afis/afis-facturen.test.ts
+++ src/server/services/afis/afis-facturen.test.ts
@@ -707,22 +707,17 @@ describe('afis-facturen', async () => {
 
   describe('getFactuurDocumentId returns correct factuurDocumentId', () => {
     const properID = '1234567890';
-    const properID2 = '123456789055555555';
     const oldID = '98765432';
-    const migratedID = `100000${oldID}2024`;
+    const migratedID = `00${oldID}`;
 
     test('Returns same ID', () => {
       expect(forTesting.getFactuurDocumentId(properID)).toBe(properID);
     });
 
-    test('Returns same ID 2', () => {
-      expect(forTesting.getFactuurDocumentId(properID2)).toBe(properID2);
-    });
-
     test('Converts oldID to migratedID', () => {
       const rs = forTesting.getFactuurDocumentId(oldID);
       expect(rs).toBe(migratedID);
-      expect(rs.length).toBe(18);
+      expect(rs.length).toBe(10);
     });
   });
 
