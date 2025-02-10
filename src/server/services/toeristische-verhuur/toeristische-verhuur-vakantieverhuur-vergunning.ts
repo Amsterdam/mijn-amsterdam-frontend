263a4975c72ad67a7a62404ecf36e3e4ff3a3262
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-02-10 16:24:21 +0100

    Generalize status steps for (decos) vergunningen

--- src/server/services/toeristische-verhuur/toeristische-verhuur-vakantieverhuur-vergunning.ts
+++ src/server/services/toeristische-verhuur/toeristische-verhuur-vakantieverhuur-vergunning.ts
@@ -1,72 +1,15 @@
 import {
-  DecosVakantieverhuurVergunningaanvraag,
+  VakantieverhuurVergunning,
   VakantieverhuurVergunningaanvraag,
 } from './toeristische-verhuur-config-and-types';
 import { AppRoutes } from '../../../universal/config/routes';
 import { apiSuccessResult } from '../../../universal/helpers/api';
-import { StatusLineItem } from '../../../universal/types/App.types';
 import { AuthProfileAndToken } from '../../auth/auth-types';
 import {
   fetchDecosZaken,
   transformDecosZaakFrontend,
 } from '../decos/decos-service';
-import { VergunningFrontend } from '../vergunningen/config-and-types';
-
-function getVergunningStatussen(vergunning: VergunningFrontend) {
-  const isAfgehandeld =
-    vergunning.status === 'Afgehandeld' || !!vergunning.decision;
-  const isIngetrokken = vergunning.decision === 'Ingetrokken';
-  const isVerlopen =
-    vergunning.status === 'Verlopen' ||
-    (vergunning.dateEnd && new Date(vergunning.dateEnd) <= new Date());
-
-  const statusOntvangen: StatusLineItem = {
-    id: 'step-1',
-    status: 'Ontvangen',
-    datePublished: vergunning.dateRequest,
-    isActive: false,
-    isChecked: true,
-  };
-
-  const statusInBehandeling: StatusLineItem = {
-    id: 'step-2',
-    status: 'In behandeling',
-    datePublished: vergunning.dateRequest,
-    isActive: false,
-    isChecked: true,
-  };
-
-  const statusAfgehandeld: StatusLineItem = {
-    id: 'step-3',
-    status: 'Afgehandeld',
-    datePublished: vergunning.dateDecision ?? vergunning.dateRequest ?? '',
-    description: '',
-    isActive: isAfgehandeld && !(isIngetrokken || isVerlopen),
-    isChecked: isAfgehandeld,
-  };
-
-  const statussen = [statusOntvangen, statusInBehandeling, statusAfgehandeld];
-
-  if (isVerlopen || isIngetrokken) {
-    const statusGewijzigd: StatusLineItem = {
-      id: 'step-4',
-      status: isIngetrokken ? 'Ingetrokken' : 'Verlopen',
-      datePublished:
-        (isVerlopen && !isIngetrokken
-          ? (vergunning.dateDecision ?? vergunning.dateEnd)
-          : vergunning.dateDecision) ?? '',
-      description: isIngetrokken
-        ? `Wij hebben uw ${vergunning.title} ingetrokken.`
-        : `Uw ${vergunning.title} is verlopen.`,
-      isActive: true,
-      isChecked: true,
-    };
-
-    statussen.push(statusGewijzigd);
-  }
-
-  return statussen;
-}
+import { getStatusSteps } from '../vergunningen/vergunningen-status-steps';
 
 export async function fetchVakantieverhuurVergunningen(
   requestID: RequestID,
@@ -78,7 +21,7 @@ export async function fetchVakantieverhuurVergunningen(
 
   if (response.status === 'OK') {
     const decosVergunningen = response.content;
-    const vergunningenFrontend: VergunningFrontend<DecosVakantieverhuurVergunningaanvraag>[] =
+    const vergunningenFrontend: VakantieverhuurVergunning[] =
       decosVergunningen.map((vergunning) => {
         const vergunningTransformed = transformDecosZaakFrontend(
           authProfileAndToken.profile.sid,
@@ -88,7 +31,7 @@ export async function fetchVakantieverhuurVergunningen(
 
         return {
           ...vergunningTransformed,
-          steps: getVergunningStatussen(vergunningTransformed),
+          steps: getStatusSteps(vergunningTransformed),
         };
       });
     return apiSuccessResult(vergunningenFrontend);
