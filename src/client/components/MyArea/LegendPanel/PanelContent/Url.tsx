ba3074504eb6da4067644c7fb3969b1cd9500ce0
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-02-14 13:05:31 +0100

    Update area components

--- src/client/components/MyArea/LegendPanel/PanelContent/Url.tsx
+++ src/client/components/MyArea/LegendPanel/PanelContent/Url.tsx
@@ -1,31 +1,20 @@
 import { Link } from '@amsterdam/design-system-react';
 
-import InfoDetail from '../../../InfoDetail/InfoDetail';
-
 interface UrlProps {
   url: string;
   label?: string;
   urlTitle?: string;
 }
 
-export default function Url({
-  url,
-  urlTitle,
-  label = 'Meer informatie',
-}: UrlProps) {
+export default function Url({ url, urlTitle }: UrlProps) {
   const theUrl =
     url.startsWith('www.') ||
     (!url.startsWith('http') && !url.startsWith('mailto'))
       ? `https://${url}`
       : url;
   return (
-    <InfoDetail
-      label={label}
-      value={
-        <Link rel="noopener noreferrer" href={theUrl}>
-          {urlTitle || url}
-        </Link>
-      }
-    />
+    <Link rel="noopener noreferrer" href={theUrl}>
+      {urlTitle || url}
+    </Link>
   );
 }
