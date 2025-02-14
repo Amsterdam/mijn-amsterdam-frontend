5a415e4111d36e5f96806aed3ed4f6a63460f2bb
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-02-14 13:05:31 +0100

    Update area components

--- src/client/components/MyArea/LegendPanel/PanelContent/Laadpalen.tsx
+++ src/client/components/MyArea/LegendPanel/PanelContent/Laadpalen.tsx
@@ -4,15 +4,16 @@ import GenericBase from './GenericBase';
 import styles from './Laadpalen.module.scss';
 import { laadpaalValueConfig } from '../../../../../universal/config/myarea-datasets';
 import { getFullAddress } from '../../../../../universal/helpers/brp';
+import { Unshaped } from '../../../../../universal/types';
 import {
   IconConnectorTypeCCS,
   IconConnectorTypeChademo,
   IconConnectorTypeMennekes,
 } from '../../../../assets/icons';
-import InfoDetail from '../../../InfoDetail/InfoDetail';
+import { Datalist } from '../../../Datalist/Datalist';
 
 type Props = {
-  panelItem: any;
+  panelItem: Unshaped;
   datasetId: string;
 };
 
@@ -48,47 +49,45 @@ export default function MyAreaPanelContentLaadpalen({
 
   return (
     <GenericBase title={panelItem.name} supTitle="Laadpalen">
-      {!!panelItem.connector_type && (
-        <InfoDetail
-          label={`Connector type${connectorTypes.length > 1 ? 's' : ''}`}
-          className={styles.Icons}
-          value={
-            icons && values ? (
-              <>
-                {icons.map((Icon: any, index: number) => (
-                  <React.Fragment key={index}>
-                    <Icon className={styles.Icon} />{' '}
-                    {Array.isArray(values) && values[index]}
-                  </React.Fragment>
-                ))}
-              </>
-            ) : null
-          }
-          valueWrapperElement={'div'}
-        />
-      )}
-      <InfoDetail
-        label="Adres"
-        value={getFullAddress(
+      <Datalist
+        rows={[
           {
-            straatnaam: panelItem.street,
-            huisnummer: panelItem.housenumber,
-            woonplaatsNaam: panelItem.city,
-            postcode: panelItem.postalcode,
-            huisnummertoevoeging: panelItem.housenumber_addition,
-            huisletter: panelItem.housenumber_letter,
+            label: `Connector type${connectorTypes.length > 1 ? 's' : ''}`,
+            content:
+              icons && values ? (
+                <>
+                  {icons.map((Icon: any, index: number) => (
+                    <React.Fragment key={index}>
+                      <Icon className={styles.Icon} />{' '}
+                      {Array.isArray(values) && values[index]}
+                    </React.Fragment>
+                  ))}
+                </>
+              ) : null,
           },
-          true
-        )}
+          {
+            label: 'Adres',
+            content: getFullAddress(
+              {
+                straatnaam: panelItem.street,
+                huisnummer: panelItem.housenumber,
+                woonplaatsNaam: panelItem.city,
+                postcode: panelItem.postalcode,
+                huisnummertoevoeging: panelItem.housenumber_addition,
+                huisletter: panelItem.housenumber_letter,
+              },
+              true
+            ),
+          },
+          { label: 'Snellader', content: panelItem.snellader },
+          {
+            label: 'Maximum wattage',
+            content: `${panelItem.charging_cap_max}Kwh`,
+            isVisible: !!panelItem.charging_cap_max,
+          },
+          { label: 'Provider', content: panelItem.provider },
+        ]}
       />
-      <InfoDetail label={'Snellader'} value={panelItem.snellader} />
-      {!!panelItem.charging_cap_max && (
-        <InfoDetail
-          label="Maximum wattage"
-          value={`${panelItem.charging_cap_max}Kwh`}
-        />
-      )}
-      <InfoDetail label="Provider" value={panelItem.provider} />
     </GenericBase>
   );
 }
