f30489bee93315d4a48c14fd244cead91fd006f8
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-02-24 19:00:33 +0100

    Polishing, focus trap, doc title functions
    # Conflicts:
    #	src/client/config/thema.ts
    #	src/client/pages/Varen/Varen-thema-config.ts

--- src/client/components/LinkToListPage/LinkToListPage.tsx
+++ src/client/components/LinkToListPage/LinkToListPage.tsx
@@ -7,6 +7,7 @@ interface LinkToListPageProps {
   count: number;
   route: string;
   label?: string;
+  linkTitle?: string;
   params?: Record<string, any>;
   threshold?: number;
   translateX?: string;
@@ -14,6 +15,7 @@ interface LinkToListPageProps {
 
 export function LinkToListPage({
   label = 'Toon meer',
+  linkTitle,
   threshold = MAX_TABLE_ROWS_ON_THEMA_PAGINA,
   count,
   route,
@@ -21,7 +23,11 @@ export function LinkToListPage({
 }: LinkToListPageProps) {
   const routeGenerated = generatePath(route, params);
   return count > threshold ? (
-    <MaRouterLink maVariant="noDefaultUnderline" href={routeGenerated}>
+    <MaRouterLink
+      title={linkTitle}
+      maVariant="noDefaultUnderline"
+      href={routeGenerated}
+    >
       {label}
     </MaRouterLink>
   ) : null;
