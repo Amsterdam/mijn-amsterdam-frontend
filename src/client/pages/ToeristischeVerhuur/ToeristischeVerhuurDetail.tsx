dcee35b3ffac12591735d297bc54b373ec78f0b6
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-02-27 09:24:08 +0100

    Gelijktrekken data
    # Conflicts:
    #	src/client/pages/Vergunningen/VergunningDetail.tsx
    #	src/server/services/vergunningen/vergunningen.ts
    
    # Conflicts:
    #	src/server/services/vergunningen/vergunningen.ts
    
    # Conflicts:
    #	src/server/services/vergunningen/config-and-types.ts
    #	src/server/services/vergunningen/vergunningen.ts
    
    # Conflicts:
    #	mocks/routes/decos.js

--- src/client/pages/ToeristischeVerhuur/ToeristischeVerhuurDetail.tsx
+++ src/client/pages/ToeristischeVerhuur/ToeristischeVerhuurDetail.tsx
@@ -1,33 +1,44 @@
+import { useMemo } from 'react';
+
 import { Link, Paragraph } from '@amsterdam/design-system-react';
 import { useParams } from 'react-router-dom';
 
+import { THEMA_DETAIL_TITLE_DEFAULT } from './toeristischeVerhuur-thema-config';
 import styles from './ToeristischeVerhuurDetail.module.scss';
-import { useToeristischeVerhuurThemaData } from './useToeristischeVerhuur.hook';
-import { ToeristischeVerhuurVergunning } from '../../../server/services/toeristische-verhuur/toeristische-verhuur-config-and-types';
+import {
+  BBVergunningFrontend,
+  ToeristischeVerhuurVergunningFrontend,
+  useToeristischeVerhuurThemaData,
+  VakantieverhuurVergunningFrontend,
+} from './useToeristischeVerhuur.hook';
+import { VakantieverhuurVergunningaanvraagIF } from '../../../server/services/toeristische-verhuur/toeristische-verhuur-config-and-types';
+import { AppRoutes } from '../../../universal/config/routes';
 import { getFullAddress, getFullName } from '../../../universal/helpers/brp';
 import { Datalist, Row, RowSet } from '../../components/Datalist/Datalist';
 import DocumentListV2 from '../../components/DocumentList/DocumentListV2';
 import { AddressDisplayAndModal } from '../../components/LocationModal/LocationModal';
 import { PageContentCell } from '../../components/Page/Page';
 import { useAppStateGetter } from '../../hooks/useAppState';
+import NotFound from '../NotFound/NotFound';
 import ThemaDetailPagina from '../ThemaPagina/ThemaDetailPagina';
+import { useVergunningDetailData } from '../Vergunningen/detail-page-content/useVergunningDetailData.hook';
 
 function getMailBody(
-  vergunning: ToeristischeVerhuurVergunning,
+  vergunning: ToeristischeVerhuurVergunningFrontend,
   fullName: string,
   fullAddress: string
 ) {
-  return `Geachte heer/mevrouw,%0D%0A%0D%0AHierbij verzoek ik u om het [document type] document van de vergunning met zaaknummer ${vergunning.zaaknummer} op te sturen.%0D%0A%0D%0AMet vriendelijke groet,%0D%0A%0D%0A${fullName}%0D%0A%0D%0A${fullAddress}`;
+  return `Geachte heer/mevrouw,%0D%0A%0D%0AHierbij verzoek ik u om het [document type] document van de vergunning met zaaknummer ${vergunning.identifier} op te sturen.%0D%0A%0D%0AMet vriendelijke groet,%0D%0A%0D%0A${fullName}%0D%0A%0D%0A${fullAddress}`;
 }
 
-function getMailSubject(vergunning: ToeristischeVerhuurVergunning) {
-  return `${vergunning.zaaknummer} - Document opvragen`;
+function getMailSubject(vergunning: ToeristischeVerhuurVergunningFrontend) {
+  return `${vergunning.identifier} - Document opvragen`;
 }
 
 function DocumentInfo({
   vergunning,
 }: {
-  vergunning: ToeristischeVerhuurVergunning;
+  vergunning: ToeristischeVerhuurVergunningFrontend;
 }) {
   const INCLUDE_WOONPLAATS = true;
   const INCLUDE_LAND = false;
@@ -60,7 +71,7 @@ function DocumentInfo({
 }
 
 interface VerhuurDocumentListProps {
-  vergunning: ToeristischeVerhuurVergunning;
+  vergunning: ToeristischeVerhuurVergunningFrontend;
 }
 
 function VerhuurDocumentList({ vergunning }: VerhuurDocumentListProps) {
@@ -79,14 +90,14 @@ function VerhuurDocumentList({ vergunning }: VerhuurDocumentListProps) {
 }
 
 type DetailPageContentProps = {
-  vergunning: ToeristischeVerhuurVergunning;
+  vergunning: ToeristischeVerhuurVergunningFrontend;
 };
 
 function DetailPageContent({ vergunning }: DetailPageContentProps) {
   const rows: Array<Row | RowSet> = [
     {
       label: 'Gemeentelijk zaaknummer',
-      content: vergunning.zaaknummer,
+      content: vergunning.identifier,
     },
     {
       rows: [
@@ -101,21 +112,21 @@ function DetailPageContent({ vergunning }: DetailPageContentProps) {
           className: styles.VanTot_Col2,
         },
       ],
-      isVisible: vergunning.result === 'Verleend',
+      isVisible: vergunning.decision === 'Verleend',
     },
     {
       label: 'Adres',
       content: (
         <>
-          <AddressDisplayAndModal address={vergunning.adres ?? ''} />
+          <AddressDisplayAndModal address={vergunning.location ?? ''} />
         </>
       ),
-      isVisible: !!vergunning.adres,
+      isVisible: !!vergunning.location,
     },
     {
       label: 'Resultaat',
-      content: vergunning.result,
-      isVisible: !!(vergunning.result && vergunning.dateDecision),
+      content: vergunning.decision,
+      isVisible: !!(vergunning.decision && vergunning.dateDecision),
     },
   ];
 
@@ -158,25 +169,96 @@ function DetailPageContent({ vergunning }: DetailPageContentProps) {
   );
 }
 
-export function ToeristischeVerhuurDetail() {
-  const { vergunningen, isError, isLoading, title, routes } =
-    useToeristischeVerhuurThemaData();
-  const { id } = useParams<{ id: string }>();
-  const vergunning = vergunningen?.find((item) => item.id === id) ?? null;
-
+function ToeristischeVerhuurDetail<
+  V extends ToeristischeVerhuurVergunningFrontend,
+>({
+  vergunning,
+  backLink,
+  isError,
+  isLoading,
+}: {
+  vergunning: V | null;
+  backLink: string;
+  isError: boolean;
+  isLoading: boolean;
+}) {
   return (
-    <ThemaDetailPagina<ToeristischeVerhuurVergunning>
-      title={vergunning?.title ?? 'Vergunning toeristische verhuur'}
+    <ThemaDetailPagina<V>
+      title={vergunning?.title ?? THEMA_DETAIL_TITLE_DEFAULT}
       zaak={vergunning}
       isError={isError}
       isLoading={isLoading}
       pageContentTop={
         vergunning && <DetailPageContent vergunning={vergunning} />
       }
-      backLink={routes.themaPage}
+      backLink={backLink}
       documentPathForTracking={(document) =>
         `/downloads/toeristische-verhuur/vergunning/${vergunning?.title}/${document.title.split(/\n/)[0]}`
       }
     />
   );
+}
+
+function VakantieVerhuurVergunningDetail() {
+  const {
+    vergunningDetail,
+    isError,
+    isLoading,
+    isLoadingThemaData,
+    vergunningDocuments,
+  } = useVergunningDetailData<VakantieverhuurVergunningaanvraagIF>();
+
+  const vergunning: VakantieverhuurVergunningFrontend | null = useMemo(() => {
+    if (vergunningDetail !== null) {
+      return {
+        ...vergunningDetail,
+        documents: vergunningDocuments,
+      };
+    }
+    return null;
+  }, [vergunningDetail, vergunningDocuments]);
+
+  return (
+    <ToeristischeVerhuurDetail<VakantieverhuurVergunningFrontend>
+      vergunning={vergunning}
+      backLink={AppRoutes.TOERISTISCHE_VERHUUR}
+      isLoading={isLoading || isLoadingThemaData}
+      isError={isError}
+    />
+  );
+}
+
+function BedAndBreakfastVergunningDetail() {
+  const { vergunningen, isError, isLoading, routes } =
+    useToeristischeVerhuurThemaData();
+  const { id } = useParams<{ id: string }>();
+
+  let vergunning = null;
+
+  for (const v of vergunningen) {
+    if (v.title === 'Vergunning bed & breakfast' && id === v.id) {
+      vergunning = v;
+    }
+  }
+
+  return (
+    <ToeristischeVerhuurDetail<BBVergunningFrontend>
+      vergunning={vergunning}
+      backLink={routes.themaPage}
+      isLoading={isLoading}
+      isError={isError}
+    />
+  );
+}
+
+export function ToeristischeVerhuurDetailPagina() {
+  const { caseType } = useParams<{ caseType: string }>();
+
+  if (caseType == 'vakantieverhuur-vergunningsaanvraag') {
+    return <VakantieVerhuurVergunningDetail />;
+  }
+  if (caseType == 'bed-and-breakfast') {
+    return <BedAndBreakfastVergunningDetail />;
+  }
+  return <NotFound />;
 }
