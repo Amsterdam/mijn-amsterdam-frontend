06fb0c6130dfa89cad8007e31047696d0042046d
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-02-27 14:10:21 +0100

    Remove default arg

--- src/server/services/decos/config-and-types.ts
+++ src/server/services/decos/config-and-types.ts
@@ -143,7 +143,7 @@ export const MA_DECISION_DEFAULT = 'Zie besluit';
 
 export type ZakenFilter = (zaak: DecosZaakBase) => boolean;
 
-export type DecosFieldTransformer<T extends DecosZaakBase = DecosZaakBase> = {
+export type DecosFieldTransformer<T extends DecosZaakBase> = {
   name: keyof T;
   transform?: (input: any) => DecosFieldValue;
 };
