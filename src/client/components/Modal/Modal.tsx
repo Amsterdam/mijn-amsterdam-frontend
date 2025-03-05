df5446cfbbb485ec79314f3dd7561f65a969cfc6
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-03-05 15:42:46 +0100

    Remove portal
    # Conflicts:
    #	src/client/components/Modal/Modal.tsx

--- src/client/components/Modal/Modal.tsx
+++ src/client/components/Modal/Modal.tsx
@@ -1,12 +1,10 @@
-import { KeyboardEventHandler, ReactNode, useEffect, useRef } from 'react';
+import { ReactNode, useRef } from 'react';
 
 import { Dialog } from '@amsterdam/design-system-react';
 import classnames from 'classnames';
-import ReactDOM from 'react-dom';
 
 import styles from './Modal.module.scss';
 import { ComponentChildren } from '../../../universal/types';
-import { useModalRoot } from '../../hooks/modalRoot.hook';
 
 interface ModalProps {
   children: ComponentChildren;
@@ -17,7 +15,6 @@ interface ModalProps {
   onClose?: () => void;
   title?: string;
   showCloseButton?: boolean;
-  onKeyUp?: KeyboardEventHandler<HTMLDialogElement>;
 }
 
 export function Modal({
@@ -29,42 +26,30 @@ export function Modal({
   title,
   showCloseButton = true,
   onClose,
-  onKeyUp,
 }: ModalProps) {
-  const dialogRef = useRef<HTMLDialogElement>(null);
+  const dialogEl = useRef(null);
   const marginTop = window.scrollY;
-  const appendToElement = useModalRoot(document.getElementById('modal-root')!);
 
-  useEffect(() => {
-    if (isOpen) {
-      dialogRef.current?.focus();
-    }
-  }, [isOpen]);
+  return isOpen ? (
+    <div className={styles.ModalContainer}>
+      <div className={styles.Modal} onClick={onClose} />
 
-  return isOpen
-    ? ReactDOM.createPortal(
-        <div className={styles.ModalContainer}>
-          <div className={styles.Modal} onClick={onClose} />
-
-          <Dialog
-            ref={dialogRef}
-            onClose={onClose}
-            onKeyUp={onKeyUp}
-            open
-            heading={title ?? ''}
-            closeButtonLabel={closeButtonLabel}
-            footer={actions}
-            style={{ transform: `translateY(${marginTop}px)` }}
-            className={classnames(
-              styles.Dialog,
-              !showCloseButton && styles.DialogWithoutCloseButton,
-              className
-            )}
-          >
-            {children}
-          </Dialog>
-        </div>,
-        appendToElement
-      )
-    : null;
+      <Dialog
+        ref={dialogEl}
+        onClose={onClose}
+        open
+        heading={title ?? ''}
+        closeButtonLabel={closeButtonLabel}
+        footer={actions}
+        style={{ transform: `translateY(${marginTop}px)` }}
+        className={classnames(
+          styles.Dialog,
+          !showCloseButton && styles.DialogWithoutCloseButton,
+          className
+        )}
+      >
+        {children}
+      </Dialog>
+    </div>
+  ) : null;
 }
