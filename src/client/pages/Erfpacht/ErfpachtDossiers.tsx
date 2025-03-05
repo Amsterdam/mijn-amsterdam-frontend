5fe8e9123385246410aa1552d48265aa0b34858f
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-03-05 11:49:01 +0100

    Update Erfpacht components

--- src/client/pages/Erfpacht/ErfpachtDossiers.tsx
+++ src/client/pages/Erfpacht/ErfpachtDossiers.tsx
@@ -1,24 +1,24 @@
 import { listPageParamKind } from './Erfpacht-thema-config';
 import { useErfpachtV2Data } from './erfpachtData.hook';
-import { AppRoutes } from '../../../universal/config/routes';
-import { isError, isLoading } from '../../../universal/helpers/api';
 import { ListPagePaginated } from '../../components/ListPagePaginated/ListPagePaginated';
 
 export function ErfpachtDossiers() {
-  const { ERFPACHTv2, dossiers, tableConfig } = useErfpachtV2Data();
+  const { isLoading, isError, dossiers, tableConfig, routes } =
+    useErfpachtV2Data();
 
-  const tableConfigDossiers = tableConfig?.[listPageParamKind.dossiers];
+  const tableConfigDossiers = tableConfig?.[listPageParamKind.erfpachtRechten];
+  console.log('tableConfigDossiers', tableConfigDossiers);
   const displayPropsDossiers = tableConfigDossiers?.displayProps ?? {};
 
   return (
     <ListPagePaginated
       items={dossiers}
-      title={`Alle ${titleDossiers?.toLowerCase()}`}
-      appRoute={AppRoutes['ERFPACHTv2/DOSSIERS']}
-      appRouteBack={AppRoutes.ERFPACHTv2}
+      title={tableConfigDossiers?.title ?? 'Erfpachtrechten'}
+      appRoute={tableConfigDossiers?.listPageRoute ?? ''}
+      appRouteBack={routes.themaPage}
       displayProps={displayPropsDossiers}
-      isLoading={isLoading(ERFPACHTv2)}
-      isError={isError(ERFPACHTv2)}
+      isLoading={isLoading}
+      isError={isError}
     />
   );
 }
