155bcdd37b3a0e33858bfddbeb9429e74b1529e2
Terry van Walen <t.van.walen@amsterdam.nl>
2025-03-11 17:23:33 +0100

    Test Varen list page

---
+++ src/client/pages/Varen/VarenList.test.tsx
@@ -0,0 +1,121 @@
+import { render } from '@testing-library/react';
+import Mockdate from 'mockdate';
+import { generatePath } from 'react-router-dom';
+import { MutableSnapshot } from 'recoil';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+
+import {
+  VarenFrontend,
+  VarenVergunningExploitatieType,
+} from '../../../server/services/varen/config-and-types';
+import { AppRoutes } from '../../../universal/config/routes';
+import { jsonCopy } from '../../../universal/helpers/utils';
+import { AppState } from '../../../universal/types/App.types';
+import { appStateAtom } from '../../hooks/useAppState';
+import MockApp from '../MockApp';
+import { VarenList } from './VarenList';
+
+type ExploitatieAanvraag = VarenFrontend<VarenVergunningExploitatieType>;
+const exploitatieDecision: ExploitatieAanvraag = {
+  id: 'Z-24-0000001',
+  identifier: 'Z/24/0000001',
+  caseType: 'Varen vergunning exploitatie',
+  title: 'Varen vergunning exploitatie',
+  status: 'Besluit',
+  decision: 'Verleend',
+  processed: true,
+  vesselName: 'BootjeVanBerend',
+  dateDecisionFormatted: '10 november 2023',
+  link: {
+    to: '/passagiers-en-beroepsvaart/vergunning/varen-vergunning-exploitatie/Z-24-0000001',
+    title: 'Bekijk hoe het met uw aanvraag staat',
+  },
+} as unknown as ExploitatieAanvraag;
+
+const varenContent: AppState['VAREN']['content'] = [exploitatieDecision];
+
+const getTestState = (content: VarenFrontend[] = varenContent): AppState =>
+  jsonCopy({
+    VAREN: {
+      content: content,
+      status: 'OK',
+    },
+  });
+
+function initializeState(snapshot: MutableSnapshot, state: AppState) {
+  snapshot.set(appStateAtom, state);
+}
+
+describe('<VarenList />', () => {
+  function Component({ state }: { state: AppState }) {
+    return (
+      <MockApp
+        routePath={AppRoutes['VAREN/LIST']}
+        routeEntry={generatePath(AppRoutes['VAREN/LIST'], {
+          kind: 'actieve-vergunningen',
+          page: 1,
+        })}
+        component={VarenList}
+        initializeState={(snap) => initializeState(snap, state)}
+      />
+    );
+  }
+
+  beforeAll(() => {
+    Mockdate.set('2025-03-04');
+  });
+
+  afterAll(() => {
+    Mockdate.reset();
+  });
+
+  it('Shows the expected title on the page', () => {
+    const screen = render(<Component state={getTestState([])} />);
+    expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+      'Actieve vergunningen'
+    );
+  });
+
+  it('Shows the expected title on the page', () => {
+    const screen = render(<Component state={getTestState([])} />);
+    expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+      'Actieve vergunningen'
+    );
+  });
+
+  it('Shows the expected rows in the tables', () => {
+    const screen = render(
+      <Component
+        state={getTestState(
+          [
+            exploitatieDecision,
+            exploitatieDecision,
+            exploitatieDecision,
+            exploitatieDecision,
+          ].map((vergunning, index) => ({ ...vergunning, id: `${index}` }))
+        )}
+      />
+    );
+    expect(screen.getByText('Naam vaartuig')).toBeInTheDocument();
+    expect(screen.getAllByText('BootjeVanBerend').length).toBe(4);
+
+    expect(screen.getByText('Omschrijving')).toBeInTheDocument();
+    expect(screen.getAllByText('Varen vergunning exploitatie').length).toBe(4);
+
+    expect(screen.getByText('Datum besluit')).toBeInTheDocument();
+    expect(screen.getAllByText('10 november 2023').length).toBe(4);
+
+    expect(screen.getByText('Resultaat')).toBeInTheDocument();
+    expect(screen.getAllByText('Verleend').length).toBe(4);
+  });
+
+  it('Naam vaartuig links to the corresponding aanvraag or vergunning', () => {
+    const screen = render(
+      <Component state={getTestState([exploitatieDecision])} />
+    );
+
+    expect(screen.getByText('BootjeVanBerend').getAttribute('href')).toContain(
+      'Z-24-0000001'
+    );
+  });
+});
