130761a22301a7450a8dc19d14c4ef58e38a06f3
Terry van Walen <t.van.walen@amsterdam.nl>
2025-03-20 14:16:31 +0100

    Add tests for varen notifications

---
+++ src/server/services/varen/varen-notifications.test.ts
@@ -0,0 +1,185 @@
+import MockDate from 'mockdate';
+import { describe, expect, it } from 'vitest';
+
+import {
+  VarenFrontend,
+  VarenRegistratieRederType,
+  VarenVergunningExploitatieType,
+} from './config-and-types';
+import { fetchVarenNotifications } from './varen-notifications';
+import {
+  apiErrorResult,
+  apiSuccessResult,
+} from '../../../universal/helpers/api';
+import { AuthProfileAndToken } from '../../auth/auth-types';
+import * as decos from '../decos/decos-service';
+
+const exploitatieBase_ = {
+  vesselName: 'boatName',
+  id: 'Z-25-0000001',
+  identifier: 'Z/25/0000001',
+  key: 'ABCDEF0123456789ABCDEF0123456789',
+  caseType: 'Varen vergunning exploitatie',
+  title: 'Varen vergunning exploitatie',
+  status: 'In behandeling',
+  decision: 'Nog niet bekend',
+  processed: false,
+  dateRequest: '2025-01-01T00:00:00',
+  statusDates: [],
+  termijnDates: [],
+} satisfies Partial<VarenFrontend>;
+const exploitatieBase =
+  exploitatieBase_ as unknown as VarenVergunningExploitatieType;
+
+describe('Notifications', () => {
+  const authProfileAndToken: AuthProfileAndToken = {
+    profile: { authMethod: 'digid', profileType: 'private', id: '', sid: '' },
+    token: 'xxxxxx',
+  };
+
+  MockDate.set('2025-01-20');
+
+  it('should respond with an error response if fetchDecosZaken returns an error', async () => {
+    vi.spyOn(decos, 'fetchDecosZaken').mockResolvedValueOnce(
+      apiErrorResult('Error', null)
+    );
+
+    const response = await fetchVarenNotifications('x0', authProfileAndToken);
+    const errorResponse = {
+      content: null,
+      message: 'Error fetching Varen zaken data',
+      status: 'ERROR',
+    };
+    expect(response).toStrictEqual(errorResponse);
+  });
+
+  it('should only show most recent notification for every zaak', async () => {
+    const zaakRecent = exploitatieBase;
+
+    const zaakOlderThen3Months = {
+      ...exploitatieBase,
+      dateRequest: '2024-09-01T00:00:00',
+    };
+
+    vi.spyOn(decos, 'fetchDecosZaken').mockResolvedValueOnce(
+      apiSuccessResult([zaakRecent, zaakOlderThen3Months])
+    );
+
+    const { content } = await fetchVarenNotifications(
+      'x3',
+      authProfileAndToken
+    );
+
+    expect(content?.notifications).toHaveLength(1);
+    expect(content?.notifications[0].datePublished).toBe('2025-01-01T00:00:00');
+  });
+
+  it('should show a notification for registratie reder', async () => {
+    const rederRegistratie_ = {
+      id: 'Z-25-0000001',
+      caseType: 'Varen registratie reder',
+      dateRequest: '2025-01-01T00:00:00',
+      termijnDates: [],
+      statusDates: [],
+    } satisfies Partial<VarenRegistratieRederType>;
+    const rederRegistratie =
+      rederRegistratie_ as unknown as VarenRegistratieRederType;
+
+    vi.spyOn(decos, 'fetchDecosZaken').mockResolvedValueOnce(
+      apiSuccessResult([rederRegistratie])
+    );
+
+    const response = await fetchVarenNotifications('x1', authProfileAndToken);
+    const successResponse = {
+      status: 'OK',
+      content: {
+        notifications: [
+          {
+            thema: 'VAREN',
+            id: 'varen-Z-25-0000001-reder-notification',
+            datePublished: '01 januari 2025',
+            title: 'Reder geregistreerd',
+            description: 'U heeft zich geregistreerd.',
+            link: {
+              title: 'Bekijk details',
+              to: '/passagiers-en-beroepsvaart',
+            },
+          },
+        ],
+      },
+    };
+    expect(response).toStrictEqual(successResponse);
+  });
+
+  it('should show a notification for every zaak', async () => {
+    const zaakInProgress = {
+      ...exploitatieBase,
+    } as unknown as VarenVergunningExploitatieType;
+
+    const zaakMeerInformatie = {
+      ...exploitatieBase,
+      termijnDates: [
+        {
+          status: 'Meer informatie nodig',
+          dateStart: '2025-01-17T00:00:00',
+          dateEnd: '2025-01-30T00:00:00',
+        },
+      ],
+    } as unknown as VarenVergunningExploitatieType;
+
+    const zaakDecision = {
+      ...exploitatieBase,
+      processed: true,
+      dateDecision: '2025-01-20T00:00:00',
+    } as unknown as VarenVergunningExploitatieType;
+
+    vi.spyOn(decos, 'fetchDecosZaken').mockResolvedValueOnce(
+      apiSuccessResult([zaakInProgress, zaakMeerInformatie, zaakDecision])
+    );
+
+    const response = await fetchVarenNotifications('x2', authProfileAndToken);
+    const successResponse = {
+      status: 'OK',
+      content: {
+        notifications: [
+          {
+            thema: 'VAREN',
+            id: 'varen-Z-25-0000001-inbehandeling-notification',
+            title: 'Aanvraag Varen vergunning exploitatie in behandeling',
+            description: 'Wij hebben uw aanvraag in behandeling genomen.',
+            datePublished: '2025-01-01T00:00:00',
+            link: {
+              title: 'Bekijk details',
+              to: '/passagiers-en-beroepsvaart/vergunning/Varen%20vergunning%20exploitatie/Z-25-0000001',
+            },
+          },
+          {
+            thema: 'VAREN',
+            id: 'varen-Z-25-0000001-meerinformatienodig-notification',
+            title:
+              'Meer informatie nodig omtrent uw Varen vergunning exploitatie aanvraag',
+            description:
+              'Er is meer informatie nodig om de aanvraag verder te kunnen verwerken.',
+            datePublished: '2025-01-17T00:00:00',
+            link: {
+              title: 'Bekijk details',
+              to: '/passagiers-en-beroepsvaart/vergunning/Varen%20vergunning%20exploitatie/Z-25-0000001',
+            },
+          },
+          {
+            thema: 'VAREN',
+            id: 'varen-Z-25-0000001-afgehandeld-notification',
+            title: 'Aanvraag Varen vergunning exploitatie afgehandeld',
+            description: 'Wij hebben uw aanvraag afgehandeld.',
+            datePublished: '2025-01-20T00:00:00',
+            link: {
+              title: 'Bekijk details',
+              to: '/passagiers-en-beroepsvaart',
+            },
+          },
+        ],
+      },
+    };
+    expect(response).toStrictEqual(successResponse);
+  });
+});
