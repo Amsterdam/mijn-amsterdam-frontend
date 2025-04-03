d0f80a9df3a55a14059f6ef7283cad698171dd86
Roan Paulus <roan.paulus@amsterdam.nl>
2025-04-03 14:24:51 +0200

    Factor generic statusline functions out

--- src/server/services/wmo/wmo.ts
+++ src/server/services/wmo/wmo.ts
@@ -20,6 +20,7 @@ import {
 import { WMOVoorzieningFrontend } from './wmo-config-and-types';
 import { wmoStatusLineItemsConfig } from './wmo-status-line-items';
 import { fetchZorgnedAanvragenWMO } from './wmo-zorgned-service';
+import { getLatestStatus, getLatestStatusDate } from '../../statusline';
 
 function getDocuments(
   sessionID: SessionID,
@@ -48,33 +49,6 @@ function getDocuments(
   }
 
   return [];
-}
-
-function getLatestStatusStep(steps: StatusLineItem[]): StatusLineItem | null {
-  const active = steps.findLast((step) => step.isActive);
-  if (active) {
-    return active;
-  }
-  const checked = steps.findLast((step) => step.isChecked);
-  if (checked) {
-    return checked;
-  }
-  return null;
-}
-
-function getLatestStatus(steps: StatusLineItem[]) {
-  return getLatestStatusStep(steps)?.status ?? 'Onbekend';
-}
-
-function getLatestStatusDate(
-  steps: StatusLineItem[],
-  doTransformDate: boolean = false
-) {
-  const date = getLatestStatusStep(steps)?.datePublished;
-  if (date && doTransformDate) {
-    return defaultDateFormat(date);
-  }
-  return date || '-';
 }
 
 function transformVoorzieningenForFrontend(
