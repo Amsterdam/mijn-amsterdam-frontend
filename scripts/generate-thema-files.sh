0fd7246c1a86375dd575748e607c59c6f7644c9b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-04-17 15:33:41 +0200

    Delete scriptjes

--- scripts/generate-thema-files.sh
+++
@@ -1,7 +0,0 @@
-LIST="Krefia,Horeca,AVG,Parkeren,Bodem,Bezwaren,Zorg,Vergunningen,Varen,Afval,HLI,Burgerzaken,ToeristischeVerhuur,Erfpacht,Klachten"
-
-IFS=',' read -r -a ITEMS <<< "$LIST"
-
-for ITEM in "${ITEMS[@]}"; do
-  node scripts/generate-thema.js --id $ITEM --title $ITEM --zaakType "${ITEM} Zaak" --private --commercial --config core,render
-done
