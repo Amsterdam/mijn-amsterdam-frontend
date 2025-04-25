e20393e18fe5114fe1d2b20de4fb2ca2bef1e09c
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-04-25 08:58:47 +0200

    MIJN-11442-Chore - Refactor bezwaren
    # Conflicts:
    #	src/client/AppState.ts

--- src/client/AppState.ts
+++ src/client/AppState.ts
@@ -4,7 +4,6 @@ import { FeatureToggle } from '../universal/config/feature-toggles';
 import { apiPristineResult } from '../universal/helpers/api';
 import { AppState } from '../universal/types/App.types';
 import { featureToggle as featureToggleAVG } from './pages/Thema/AVG/AVG-thema-config';
-import { featureToggle as featureToggleAVG } from './pages/Thema/AVG/AVG-thema-config';
 import { featureToggle as featureToggleBezwaren } from './pages/Thema/Bezwaren/Bezwaren-thema-config';
 import { zorgnedLeerlingenvervoerActive } from './pages/Thema/Jeugd/Jeugd-thema-config';
 
