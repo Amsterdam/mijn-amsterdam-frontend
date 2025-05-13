40c08f8fda72a387a40b78ce95865e62833bfc81
Terry van Walen <TerryvanWalen@users.noreply.github.com>
2025-05-13 13:18:42 +0200

    Mijn-11629-chore time in verlopen statustrein (#1935)
    Co-authored-by: Tim van Oostrom <t.van.oostrom@amsterdam.nl>

--- src/client/pages/Thema/Vergunningen/Vergunningen-helpers.ts
+++ src/client/pages/Thema/Vergunningen/Vergunningen-helpers.ts
@@ -1,3 +1,8 @@
+import type {
+  WithDateEnd,
+  WithTimeRange,
+} from '../../../../server/services/decos/decos-types';
+import { dateTimeFormatYear } from '../../../../universal/helpers/date';
 import type { ZaakDetail } from '../../../../universal/types/App.types';
 
 export type VergunningAanvraag = ZaakDetail & {
@@ -20,4 +25,13 @@ export function isVergunningExpired<T extends VergunningExpirable>(
   return vergunning.steps.some(
     (step) => step.status === 'Verlopen' && step.isActive
   );
+}
+
+export function dateTimeEndFormatted<
+  T extends Partial<WithTimeRange & WithDateEnd>,
+>(zaak: T) {
+  if (zaak.dateEnd && zaak.timeEnd) {
+    return dateTimeFormatYear(`${zaak.dateEnd.split('T')[0]}T${zaak.timeEnd}`);
+  }
+  return zaak.dateEndFormatted;
 }
