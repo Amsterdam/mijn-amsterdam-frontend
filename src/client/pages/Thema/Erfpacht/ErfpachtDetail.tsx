c80a8de745899bfa7a33ebcf5ed6275c15909fe5
Roan Paulus <roan.paulus@amsterdam.nl>
2025-05-13 16:57:14 +0200

    replace facturen tables with disclaimer where to find your 'openstaande facturen'

--- src/client/pages/Thema/Erfpacht/ErfpachtDetail.tsx
+++ src/client/pages/Thema/Erfpacht/ErfpachtDetail.tsx
@@ -2,9 +2,7 @@ import { DataTableBijzondereBepalingen } from './DossierDetail/DatalistBijzonder
 import { DatalistGeneral } from './DossierDetail/DatalistGeneral';
 import { DatalistJuridisch } from './DossierDetail/DatalistJuridisch';
 import { DatalistsFinancieel } from './DossierDetail/DatalistsFinancieel';
-import { DataTableFacturen } from './DossierDetail/DataTableFacturen';
 import { useDossierDetaiLData as useDossierDetailData } from './DossierDetail/erfpachtDossierData.hook';
-import styles from './ErfpachtDetail.module.scss';
 import { CollapsiblePanel } from '../../../components/CollapsiblePanel/CollapsiblePanel';
 import { PageContentCell } from '../../../components/Page/Page';
 import ThemaDetailPagina from '../../../components/Thema/ThemaDetailPagina';
@@ -18,7 +16,6 @@ export function ErfpachtDetail() {
     isErrorThemaData,
     isLoadingThemaData,
     relatieCode,
-    displayPropsDossierFacturen,
     breadcrumbs,
     title,
     routeConfig,
@@ -54,17 +51,6 @@ export function ErfpachtDetail() {
               <PageContentCell>
                 <CollapsiblePanel title={dossier.titelKopFinancieel}>
                   <DatalistsFinancieel dossier={dossier} />
-                </CollapsiblePanel>
-              </PageContentCell>
-
-              <PageContentCell className={styles.Section}>
-                <CollapsiblePanel title="Facturen">
-                  <DataTableFacturen
-                    dossier={dossier}
-                    relatieCode={relatieCode}
-                    isLoading={isLoading || isLoadingThemaData}
-                    displayProps={displayPropsDossierFacturen}
-                  />
                 </CollapsiblePanel>
               </PageContentCell>
             </>
