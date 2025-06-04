42d594cc690e8d390cc2d85709bcc80cbc4291c6
Roan Paulus <roan.paulus@amsterdam.nl>
2025-06-04 15:37:24 +0200

    remove name in welcome message for commercial users

--- src/client/pages/Dashboard/WelcomHeading.tsx
+++ src/client/pages/Dashboard/WelcomHeading.tsx
@@ -1,5 +1,6 @@
 import styles from './WelcomeHeading.module.scss';
 import { ProfileName } from '../../components/MainHeader/ProfileName';
+import { useProfileTypeValue } from '../../hooks/useProfileType';
 
 const NOON = 12;
 const EVENING = 18;
@@ -12,31 +13,40 @@ export function WelcomeHeading() {
   const isNight = hours < NIGHT;
   const isAfternoon = hours >= NOON && hours < EVENING;
 
-  let greetingMsgStart = 'Welkom,';
+  let greetingMsgStart = 'Welkom';
 
   switch (true) {
     case isMorning:
-      greetingMsgStart = 'Goedemorgen,';
+      greetingMsgStart = 'Goedemorgen';
       break;
     case isAfternoon:
-      greetingMsgStart = 'Goedemiddag,';
+      greetingMsgStart = 'Goedemiddag';
       break;
     case isEvening:
-      greetingMsgStart = 'Goedenavond,';
+      greetingMsgStart = 'Goedenavond';
       break;
     case isNight:
-      greetingMsgStart = 'Goedenacht,';
+      greetingMsgStart = 'Goedenacht';
       break;
   }
 
+  const profileType = useProfileTypeValue();
+
   return (
     <h2 className={styles.WelcomeHeading}>
-      {greetingMsgStart}{' '}
-      <ProfileName
-        fallbackName="Bezoeker"
-        loaderBarConfig={[['400px', '40px', '0']]}
-        preferVoornaam
-      />
+      {greetingMsgStart}
+      {profileType === 'commercial' ? (
+        ''
+      ) : (
+        <>
+          {', '}
+          <ProfileName
+            fallbackName="Bezoeker"
+            loaderBarConfig={[['400px', '40px', '0']]}
+            preferVoornaam
+          />
+        </>
+      )}
     </h2>
   );
 }
