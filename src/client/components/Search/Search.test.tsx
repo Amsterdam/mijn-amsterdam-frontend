04d0b0ec69c9d366f28cd69b972cca6e51fde156
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-06-11 14:19:25 +0200

    Add tests

--- src/client/components/Search/Search.test.tsx
+++ src/client/components/Search/Search.test.tsx
@@ -11,6 +11,37 @@ import { bffApi } from '../../../testing/utils';
 import { AppState } from '../../../universal/types/App.types';
 import { appStateAtom, appStateReadyAtom } from '../../hooks/useAppState';
 
+const appStateMock = {
+  VERGUNNINGEN: {
+    status: 'OK',
+    content: [
+      {
+        caseType: 'GPK',
+        title: 'Europse gehandicaptenparkeerkaart (GPK)',
+        identifier: 'Z/000/000008',
+        status: 'Ontvangen',
+        description: 'Amstel 1 GPK aanvraag',
+        link: {
+          to: '/vergunningen/gpk/1726584505',
+          title: 'Bekijk hoe het met uw aanvraag staat',
+        },
+      },
+    ],
+  },
+} as unknown as AppState;
+
+const mocks = vi.hoisted(() => ({
+  navigate: vi.fn(),
+}));
+
+vi.mock('react-router', async (importOriginal) => {
+  const actual: object = await importOriginal();
+  return {
+    ...actual,
+    useNavigate: () => mocks.navigate,
+  };
+});
+
 describe('<Search />', () => {
   beforeEach(() => {
     bffApi.get('/services/search-config').reply(200, { content: remoteConfig });
@@ -36,6 +67,7 @@ describe('<Search />', () => {
 
   afterEach(() => {
     nock.cleanAll();
+    mocks.navigate.mockClear();
   });
 
   test('Render search placeholder busy', async () => {
@@ -69,31 +101,63 @@ describe('<Search />', () => {
     await screen.findByPlaceholderText('Zoeken naar...');
   });
 
-  test('Enter search text', async () => {
+  test('Enter search text and click result', async () => {
+    const user = userEvent.setup();
+    const onFinishCallback = vi.fn();
+
+    const screen = render(
+      <BrowserRouter>
+        <RecoilRoot
+          initializeState={(snapshot) => {
+            snapshot.set(appStateAtom, appStateMock);
+            snapshot.set(appStateReadyAtom, true);
+          }}
+        >
+          <Search onFinish={onFinishCallback} />
+        </RecoilRoot>
+      </BrowserRouter>
+    );
+
+    const input = await screen.getByPlaceholderText('Zoeken naar...');
+
+    await user.keyboard('Dashboard');
+
+    expect(screen.getByDisplayValue('Dashboard')).toBeInTheDocument();
+
+    expect(await screen.findByText('Dashboard')).toBeInTheDocument();
+    expect(screen.getByText('Home -')).toBeInTheDocument();
+
+    await user.tripleClick(input);
+
+    await user.keyboard('gehandicaptenparkeerkaart');
+
+    expect(input).toHaveValue('gehandicaptenparkeerkaart');
+
+    const vergunning = await screen.findByText('Z/000/000008');
+    expect(vergunning).toBeInTheDocument();
+
+    await user.click(vergunning);
+    expect(mocks.navigate).toHaveBeenCalledWith(
+      '/vergunningen/gpk/1726584505',
+      {
+        state: {
+          from: '/',
+          pageType: 'none',
+        },
+      }
+    );
+
+    expect(onFinishCallback).toHaveBeenCalledWith('Resultaat geklikt');
+  });
+
+  test('Clear search input', async () => {
     const user = userEvent.setup();
 
     const screen = render(
       <BrowserRouter>
         <RecoilRoot
           initializeState={(snapshot) => {
-            snapshot.set(appStateAtom, {
-              VERGUNNINGEN: {
-                status: 'OK',
-                content: [
-                  {
-                    caseType: 'GPK',
-                    title: 'Europse gehandicaptenparkeerkaart (GPK)',
-                    identifier: 'Z/000/000008',
-                    status: 'Ontvangen',
-                    description: 'Amstel 1 GPK aanvraag',
-                    link: {
-                      to: '/vergunningen/gpk/1726584505',
-                      title: 'Bekijk hoe het met uw aanvraag staat',
-                    },
-                  },
-                ],
-              },
-            } as unknown as AppState);
+            snapshot.set(appStateAtom, appStateMock);
             snapshot.set(appStateReadyAtom, true);
           }}
         >
@@ -108,14 +172,47 @@ describe('<Search />', () => {
 
     expect(input).toHaveValue('gehandicaptenparkeerkaart');
 
-    expect(await screen.findByText('Z/000/000008')).toBeInTheDocument();
+    const vergunning = await screen.findByText('Z/000/000008');
+    expect(vergunning).toBeInTheDocument();
 
-    await user.tripleClick(input);
-    await user.keyboard('Dashboard');
+    const clearButton = screen.getByLabelText('Verwijder zoekopdracht');
+    expect(clearButton).toBeInTheDocument();
+    await user.click(clearButton);
+    expect(input).toHaveValue('');
+    expect(screen.queryByText('Z/000/000008')).not.toBeInTheDocument();
+  });
 
-    expect(screen.getByDisplayValue('Dashboard')).toBeInTheDocument();
+  test('Navigates to search page', async () => {
+    const user = userEvent.setup();
 
-    expect(await screen.findByText('Dashboard')).toBeInTheDocument();
-    expect(screen.getByText('Home -')).toBeInTheDocument();
+    const screen = render(
+      <BrowserRouter>
+        <RecoilRoot
+          initializeState={(snapshot) => {
+            snapshot.set(appStateAtom, appStateMock);
+            snapshot.set(appStateReadyAtom, true);
+          }}
+        >
+          <Search />
+        </RecoilRoot>
+      </BrowserRouter>
+    );
+
+    const input = await screen.getByPlaceholderText('Zoeken naar...');
+
+    await user.keyboard('gehandicaptenparkeerkaart');
+
+    expect(input).toHaveValue('gehandicaptenparkeerkaart');
+
+    const vergunning = await screen.findByText('Z/000/000008');
+    expect(vergunning).toBeInTheDocument();
+
+    const submitButton = screen.getByLabelText('Verstuur zoekopdracht');
+    expect(submitButton).toBeInTheDocument();
+    await user.click(submitButton);
+
+    expect(mocks.navigate).toHaveBeenCalledWith(
+      '/zoeken?term=gehandicaptenparkeerkaart'
+    );
   });
 });
