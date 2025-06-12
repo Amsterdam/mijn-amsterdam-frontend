1d53aefb490af57c5dad272043fdf7f132bd1abf
Roan Paulus <roan.paulus@amsterdam.nl>
2025-06-12 15:10:11 +0200

    wip

--- src/server/services/parkeren/parkeren-egis-service.ts
+++ src/server/services/parkeren/parkeren-egis-service.ts
@@ -1,12 +1,17 @@
 import FormData from 'form-data';
+import * as jose from 'jose';
 
 import {
   ActivePermitSourceResponse,
   ClientProductDetailsSourceResponse,
 } from './config-and-types';
 import { AuthProfileAndToken } from '../../auth/auth-types';
+import { ONE_MINUTE_MS } from '../../config/app';
+import { getFromEnv } from '../../helpers/env';
 import { getApiConfig } from '../../helpers/source-api-helpers';
 import { requestData } from '../../helpers/source-api-request';
+import { logger } from '../../logging';
+import { IS_PRODUCTION } from '../../../universal/config/env';
 
 export async function fetchSSOURL(authProfileAndToken: AuthProfileAndToken) {
   const config = getApiConfig('PARKEREN_FRONTOFFICE', {
@@ -59,9 +64,19 @@ export async function hasPermitsOrPermitRequests(
       ? 'private'
       : 'company';
 
-  const jweTokenResponse = await fetchJWEToken(authProfileAndToken);
-  if (jweTokenResponse.status !== 'OK' || !jweTokenResponse.content) {
-    return true;
+  let JWT;
+  if (IS_PRODUCTION) {
+    const token = await createJWEToken(authProfileAndToken.profile.id);
+    if (!token) {
+      return true;
+    }
+    JWT = token;
+  } else {
+    const jweTokenResponse = await fetchJWEToken(authProfileAndToken);
+    if (jweTokenResponse.status !== 'OK' || !jweTokenResponse.content) {
+      return true;
+    }
+    JWT = jweTokenResponse.content;
   }
 
   const [clientProductsResponse, permitRequestsResponse] = await Promise.all([
@@ -71,7 +86,7 @@ export async function hasPermitsOrPermitRequests(
           `${config.url}/v1/${userType}/client_product_details`,
         method: 'POST',
         data: {
-          token: jweTokenResponse.content,
+          token: JWT,
         },
       })
     ),
@@ -81,7 +96,7 @@ export async function hasPermitsOrPermitRequests(
           `${config.url}/v1/${userType}/active_permit_request`,
         method: 'POST',
         data: {
-          token: jweTokenResponse.content,
+          token: JWT,
         },
       })
     ),
@@ -93,6 +108,37 @@ export async function hasPermitsOrPermitRequests(
     !!clientProductsResponse.content.data.length ||
     !!permitRequestsResponse.content.data.length
   );
+}
+
+async function createJWEToken(bsn: string): Promise<string | null> {
+  const JWK_SharedKey = Buffer.from(
+    getFromEnv('TODO_BFF_PARKEREN_JWTKEY', true)!
+  ).toString('base64');
+
+  const EXPIRY = ONE_MINUTE_MS * 60;
+
+  try {
+    const accessToken = await new jose.SignJWT({
+      iat: Date.now(),
+      exp: Date.now() + EXPIRY,
+      iss: 'mijn.amsterdam.nl',
+      aud: 'TODO: parkeren audience??',
+      bsn,
+    })
+      .setProtectedHeader({
+        alg: 'A256GCMKW',
+        enc: 'A256CBC-HS512',
+        zip: 'DEF',
+      })
+      .setIssuedAt()
+      .setExpirationTime('2h')
+      .sign(Uint8Array.from(JWK_SharedKey));
+    return accessToken;
+  } catch (err) {
+    logger.error(err);
+  }
+
+  return null;
 }
 
 export const forTesting = { fetchJWEToken };
