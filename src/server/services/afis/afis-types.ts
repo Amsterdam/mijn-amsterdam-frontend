ca42422b806157764c462c0db56144684441c739
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-06-16 11:09:22 +0200

    Add E-mandates initial
    # Conflicts:
    #	.env.local.template

--- src/server/services/afis/afis-types.ts
+++ src/server/services/afis/afis-types.ts
@@ -2,6 +2,24 @@ import Decimal from 'decimal.js';
 
 import { LinkProps } from '../../../universal/types/App.types';
 
+export type AfisApiFeedResponseSource<T> = {
+  feed?: {
+    count?: number;
+    entry?: Array<{
+      content?: {
+        properties?: T;
+      };
+    }>;
+  };
+};
+
+export type XmlNullable<T extends Record<string, unknown>> = {
+  [key in keyof T]: { '@null': true } | T[key];
+};
+
+// Business partner
+// ================
+
 type JaOfNee = 'Ja' | 'Nee';
 
 /** Business partner private response from external AFIS API.
@@ -35,28 +53,6 @@ export type AfisBusinessPartnerCommercialResponseSource = {
   Record:
     | AfisBusinessPartnerRecordCommercial
     | AfisBusinessPartnerRecordCommercial[];
-};
-
-export type AfisFacturenByStateResponse = {
-  [key in AfisFactuurState]?: AfisFacturenResponse | null;
-};
-
-export type AfisThemaResponse = {
-  isKnown: boolean;
-  businessPartnerIdEncrypted: string | null;
-  businessPartnerId?: string | null;
-  facturen?: AfisFacturenByStateResponse | null;
-};
-
-export type AfisApiFeedResponseSource<T> = {
-  feed?: {
-    count?: number;
-    entry?: Array<{
-      content?: {
-        properties?: T;
-      };
-    }>;
-  };
 };
 
 export type AfisBusinessPartnerAddressSource = {
@@ -110,69 +106,8 @@ export type AfisBusinessPartnerDetailsTransformed = {
   address?: string | null;
 };
 
-export type AfisFactuurState = 'open' | 'afgehandeld' | 'overgedragen';
-
-export type AfisFacturenResponse = {
-  count: number;
-  facturen: AfisFactuur[];
-};
-
-export type AfisFacturenParams = {
-  state: AfisFactuurState | 'deelbetalingen';
-  businessPartnerID: string;
-  top?: string;
-};
-
-export type AfisFactuur = {
-  id: string;
-  afzender: string;
-  datePublished: string | null;
-  datePublishedFormatted: string | null;
-  paymentDueDate: string;
-  paymentDueDateFormatted: string;
-  debtClearingDate: string | null;
-  debtClearingDateFormatted: string | null;
-  amountPayed: string;
-  amountPayedFormatted: string;
-  amountOriginal: string;
-  amountOriginalFormatted: string;
-  factuurNummer: string;
-  factuurDocumentId: string;
-  status: AfisFactuurStatus;
-  paylink: string | null;
-  documentDownloadLink: string | null;
-  statusDescription: string;
-  link: LinkProps;
-};
-
-export type AfisFactuurStatus =
-  | 'openstaand'
-  | 'automatische-incasso'
-  | 'in-dispuut'
-  | 'gedeeltelijke-betaling'
-  | 'handmatig-betalen'
-  | 'overgedragen-aan-belastingen'
-  | 'geld-terug'
-  | 'betaald'
-  | 'geannuleerd'
-  | 'herinnering'
-  | 'onbekend';
-
-export type AfisInvoicesSource =
-  AfisApiFeedResponseSource<AfisFactuurPropertiesSource>;
-
-export type AfisInvoicesPartialPaymentsSource = AfisApiFeedResponseSource<
-  Pick<
-    AfisFactuurPropertiesSource,
-    'AmountInBalanceTransacCrcy' | 'InvoiceReference'
-  >
->;
-
-export type AccountingDocumentType = string;
-
-export type AfisFactuurDeelbetalingen = {
-  [factuurNummer: string]: Decimal;
-};
+// Facturen
+// ================
 
 /** Extra property information
  *  ==========================
@@ -201,6 +136,88 @@ export type AfisFactuurPropertiesSource = {
   SEPAMandate: string;
 };
 
+export type AfisInvoicesSource =
+  AfisApiFeedResponseSource<AfisFactuurPropertiesSource>;
+
+export type AfisFactuurStatus =
+  | 'openstaand'
+  | 'automatische-incasso'
+  | 'in-dispuut'
+  | 'gedeeltelijke-betaling'
+  | 'handmatig-betalen'
+  | 'overgedragen-aan-belastingen'
+  | 'geld-terug'
+  | 'betaald'
+  | 'geannuleerd'
+  | 'herinnering'
+  | 'onbekend';
+
+export type AfisFactuur = {
+  id: string;
+  afzender: string;
+  datePublished: string | null;
+  datePublishedFormatted: string | null;
+  paymentDueDate: string;
+  paymentDueDateFormatted: string;
+  debtClearingDate: string | null;
+  debtClearingDateFormatted: string | null;
+  amountPayed: string;
+  amountPayedFormatted: string;
+  amountOriginal: string;
+  amountOriginalFormatted: string;
+  factuurNummer: string;
+  factuurDocumentId: string;
+  status: AfisFactuurStatus;
+  paylink: string | null;
+  documentDownloadLink: string | null;
+  statusDescription: string;
+  link: LinkProps;
+};
+
+export type AfisInvoicesPartialPaymentsSource = AfisApiFeedResponseSource<
+  Pick<
+    AfisFactuurPropertiesSource,
+    'AmountInBalanceTransacCrcy' | 'InvoiceReference'
+  >
+>;
+
+// Facturen / Deelbetalingen
+// =========================
+export type AfisFactuurDeelbetalingen = {
+  [factuurNummer: string]: Decimal;
+};
+
+export type AfisFactuurState = 'open' | 'afgehandeld' | 'overgedragen';
+
+export type AfisFacturenResponse = {
+  count: number;
+  facturen: AfisFactuur[];
+};
+
+export type AfisFacturenByStateResponse = {
+  [key in AfisFactuurState]?: AfisFacturenResponse | null;
+};
+
+export type AfisThemaResponse = {
+  isKnown: boolean;
+  businessPartnerIdEncrypted: string | null;
+  businessPartnerId?:
+    | AfisBusinessPartnerDetailsTransformed['businessPartnerId']
+    | null;
+  facturen?: AfisFacturenByStateResponse | null;
+};
+
+export type AfisFacturenParams = {
+  state: AfisFactuurState | 'deelbetalingen';
+  businessPartnerID: AfisBusinessPartnerDetailsTransformed['businessPartnerId'];
+  top?: string;
+};
+
+// Documents / PDF's
+// =================
+
+export type AccountingDocumentType = string;
+
 export type AfisArcDocID = AfisDocumentIDPropertiesSource['ArcDocId'];
 
 export type AfisDocumentIDSource =
@@ -217,6 +234,72 @@ export type AfisDocumentDownloadSource = {
   };
 };
 
-export type XmlNullable<T extends Record<string, unknown>> = {
-  [key in keyof T]: { '@null': true } | T[key];
+// E-Mandates
+// =================
+
+export type AfisEMandateSource = {
+  ABC: string;
+};
+
+export type AfisEMandatesResponseDataSource =
+  AfisApiFeedResponseSource<AfisEMandateSource>;
+
+export type AfisEMandateFrontend = {
+  id: AfisEMandateSource['ABC'];
+  dateCreated: string;
+  dateCreatedFormatted: string;
+  incassantName: string;
+  incassantIBAN: string;
+};
+
+export type AfisEMandatesResponseFrontend = AfisEMandateFrontend[];
+
+export type AfisEMandatePayloadBase = Readonly<{
+  SEPAMandateApplication: string;
+  Recipient: string;
+  RecipientType: string;
+  SenderType: string;
+  Creditor: string;
+}>;
+
+export type AfisEMandatePayloadCreate = {
+  // Business partner
+  SenderExternalID: AfisBusinessPartnerDetailsTransformed['businessPartnerId'];
+
+  // Mandate
+  SEPAMandateReferenceType: string;
+  SEPAMandateReference: string;
+  SEPAMandateStatus: string;
+  ValidityStartDate: string;
+  ValidityEndDate: string;
+  SEPASignatureCityName: string;
+  SEPASignatureDate: string;
+
+  // Debtor
+  // SenderType: string;
+  Sender: string;
+  SenderLastName: string;
+  SenderFirstName: string;
+  SenderStreetName: string;
+  SenderHouseNumber: string;
+  SenderPostalCode: string;
+  SenderCityName: string;
+  SenderCountry: string;
+  SenderIBAN: string;
+  SenderBankSWIFTCode?: string;
+
+  // Creditor
+  // RecipientType: string;
+  Recipient: string;
+  RecipientName1: string;
+  // RecipientName2: string;
+  RecipientStreetName: string;
+  RecipientHouseNumber: string;
+  RecipientPostalCode: string;
+  RecipientCityName: string;
+  RecipientCountry: string;
+};
+
+export type AfisEMandatePayloadUpdate = {
+  SenderExternalID: AfisBusinessPartnerDetailsTransformed['businessPartnerId'];
 };
