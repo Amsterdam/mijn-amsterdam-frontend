871c53f5fbdb1ee55dad47f85f195463923bb4fc
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-06-16 11:20:22 +0200

    Add ActionButton component
    # Conflicts:
    #	src/client/pages/Thema/Afis/AfisEmandateActionButtons.module.scss
    #	src/client/pages/Thema/Afis/AfisEmandateActionButtons.tsx

---
+++ src/client/pages/Thema/Afis/AfisEmandateActionButtons.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect } from 'react';
+
+import { Button, Icon } from '@amsterdam/design-system-react';
+
+import styles from './AfisEmandateActionUrls.module.scss';
+import {
+  AfisEMandateFrontend,
+  AfisEMandateSignRequestResponse,
+} from '../../../server/services/afis/afis-types';
+import { ApiResponse } from '../../../universal/helpers/api';
+import { IconAlert } from '../../assets/icons';
+import { Spinner } from '../../components/Spinner/Spinner';
+import { ApiState, useDataApi } from '../../hooks/api/useDataApi';
+
+type AfisEMandateActionUrlProps = {
+  eMandate: AfisEMandateFrontend;
+};
+
+type ApiActionProps<T> = {
+  api: ApiState<ApiResponse<T> | null>;
+  fetch: () => void;
+  label: React.ReactNode;
+  errorMessage: React.ReactNode;
+};
+
+function ApiActionButton<T>({
+  api,
+  fetch,
+  label,
+  errorMessage,
+}: ApiActionProps<T>) {
+  return (
+    <span className={api.isError ? styles.DocumentDownloadError : ''}>
+      <Button variant="tertiary" onClick={() => fetch()}>
+        {api.isLoading && <Spinner />}
+        {!api.isLoading && api.isError && (
+          <Icon className={styles.LinkIcon} svg={IconAlert} size="level-5" />
+        )}
+        {label}
+      </Button>
+      {api.isLoading && <span className={styles.DownloadInfo}>Bezig...</span>}
+      {api.isError && (
+        <span className={styles.DownloadInfo}>{errorMessage}</span>
+      )}
+    </span>
+  );
+}
+
+export function AfisEMandateActionUrls({
+  eMandate,
+}: AfisEMandateActionUrlProps) {
+  const [redirectUrlApi, fetchRedirectUrl] =
+    useDataApi<ApiResponse<AfisEMandateSignRequestResponse> | null>(
+      {
+        postpone: true,
+      },
+      null
+    );
+
+  useEffect(() => {
+    if (redirectUrlApi.data?.content?.redirectUrl) {
+      window.location.href = redirectUrlApi.data.content.redirectUrl;
+    }
+  }, [redirectUrlApi.data]);
+
+  const [statusChangeApi, fetchStatusChange] =
+    useDataApi<ApiResponse<AfisEMandateSignRequestResponse> | null>(
+      {
+        postpone: true,
+      },
+      null
+    );
+
+  useEffect(() => {
+    if (redirectUrlApi.data?.status === 'OK') {
+      alert('Update eMandate');
+    }
+  }, [statusChangeApi.data]);
+
+  return (
+    <>
+      {eMandate.signRequestUrl && (
+        <ApiActionButton
+          api={redirectUrlApi}
+          fetch={() => fetchRedirectUrl({ url: eMandate.signRequestUrl })}
+          label={eMandate.status === '1' ? 'Wijzigen' : 'Activeren'}
+          errorMessage="Er is iets misgegaan bij het ophalen van de link"
+        />
+      )}
+      {eMandate.statusChangeUrl && (
+        <ApiActionButton
+          api={statusChangeApi}
+          fetch={() => fetchStatusChange({ url: eMandate.statusChangeUrl })}
+          label={eMandate.status === '1' ? 'Stopzetten' : 'Activeren'}
+          errorMessage="Er is iets misgegaan bij het veranderen van de status"
+        />
+      )}
+    </>
+  );
+}
