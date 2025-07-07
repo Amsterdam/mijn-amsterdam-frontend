e214fe2cb59ed066e36d0e50fef46a97a18bf202
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-07-07 15:10:35 +0200

    Typos

--- src/client/pages/Thema/Afis/AfisEmandateDateAdjust.tsx
+++ src/client/pages/Thema/Afis/AfisEmandateDateAdjust.tsx
@@ -61,7 +61,7 @@ function DateAdjustModal({
               setDateAdjustModal(false);
             }}
           >
-            terug
+            Terug
           </Button>
         </ActionGroup>
       }
