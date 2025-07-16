981333dd2b8210140d9528289229f5e6974e9a7b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-07-16 13:33:05 +0200

    Add pending status to multiple screens

--- src/client/pages/Thema/Afis/AfisBetaalVoorkeuren.tsx
+++ src/client/pages/Thema/Afis/AfisBetaalVoorkeuren.tsx
@@ -2,6 +2,7 @@ import { Grid, Heading, Link, Paragraph } from '@amsterdam/design-system-react';
 
 import { featureToggle } from './Afis-thema-config';
 import styles from './AfisBetaalVoorkeuren.module.scss';
+import { EmandateRefetchInterval } from './AfisEMandateDetail';
 import {
   useAfisBetaalVoorkeurenData,
   useAfisEMandatesData,
@@ -101,6 +102,8 @@ export function AfisBetaalVoorkeuren() {
     eMandateTableConfig,
     hasEMandatesError,
     isLoadingEMandates,
+    statusNotification: { ibansPendingActivation },
+    refetchEMandates,
   } = useAfisEMandatesData();
 
   const isLoadingAllAPis =
@@ -172,6 +175,9 @@ export function AfisBetaalVoorkeuren() {
         isLoading={!!(isLoadingBusinessPartnerDetails || isThemaPaginaLoading)}
         startCollapsed={featureToggle.afisEMandatesActive}
       />
+      {!!ibansPendingActivation.length && (
+        <EmandateRefetchInterval fetch={refetchEMandates} />
+      )}
       {eMandatesTable}
     </>
   );
