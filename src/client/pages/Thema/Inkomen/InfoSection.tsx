45a5598f57411b93bc5273e9137d6618810d7111
Roan Paulus <123072030+RoanPaulus@users.noreply.github.com>
2025-08-26 10:15:15 +0200

    MIJN-11094-feature-toggle-verwerken-in-uitlegpagina (#2101)

--- src/client/pages/Thema/Inkomen/InfoSection.tsx
+++ src/client/pages/Thema/Inkomen/InfoSection.tsx
@@ -7,8 +7,9 @@ export const inkomenSectionProps: SectionProps = {
   listItems: [
     'Uw aanvraag voor een bijstandsuitkering of bijstand voor zelfstandigen (Bbz)',
     'De uitkeringsspecificaties en jaaropgaven van uw bijstandsuitkering of bijstand voor zelfstandigen (Bbz)',
-    'Uw aanvraag voor de Tijdelijke overbruggingsregeling zelfstandig ondernemers (Tozo 1, 2, 3 en 4)',
+    'Uw aanvraag voor de Tijdelijke overbruggingsregeling zelfstandig ondernemers (Tozo 1, 2, 3, 4 en 5)',
     'Uw aanvraag voor de Tijdelijke Ondersteuning Noodzakelijke Kosten (TONK)',
     'Uw aanvraag voor de Inkomensvoorziening oudere en gedeeltelijk arbeidsongeschikte gewezen zelfstandigen (IOAZ)',
   ],
+  active: true,
 };
