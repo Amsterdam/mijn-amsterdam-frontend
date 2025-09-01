cf12c317b3df5d9b196e862e5d60686c2ae8f53c
Terry van Walen <t.van.walen@amsterdam.nl>
2025-09-01 10:09:07 +0200

    Remove duplicate types from bedAndBreakfast

--- src/server/services/toeristische-verhuur/bed-and-breakfast/bed-and-breakfast-pb-zaken.ts
+++ src/server/services/toeristische-verhuur/bed-and-breakfast/bed-and-breakfast-pb-zaken.ts
@@ -1,19 +1,10 @@
 import {
   BedAndBreakfastType,
   caseTypeBedAndBreakfast,
+  documentNamenMA_PB,
 } from './bed-and-breakfast-types';
 import { SELECT_FIELDS_TRANSFORM_BASE } from '../../powerbrowser/powerbrowser-field-transformers';
 import { PowerBrowserZaakTransformer } from '../../powerbrowser/powerbrowser-types';
-
-export const bbDocumentNamesMA = {
-  TOEKENNING: 'Besluit toekenning',
-  VERLENGING: 'Besluit verlenging beslistermijn',
-  WEIGERING: 'Besluit weigering',
-  BUITEN_BEHANDELING: 'Besluit Buiten behandeling',
-  INTREKKING: 'Besluit intrekking',
-  MEER_INFORMATIE: 'Verzoek aanvullende gegevens',
-  SAMENVATTING: 'Samenvatting aanvraagformulier',
-} as const;
 
 export const BedAndBreakfastZaakTransformer: PowerBrowserZaakTransformer<BedAndBreakfastType> =
   {
@@ -24,30 +15,7 @@ export const BedAndBreakfastZaakTransformer: PowerBrowserZaakTransformer<BedAndB
       !!pbRecordField.text &&
       pbRecordField.text?.includes(caseTypeBedAndBreakfast.BedAndBreakfast),
     transformFields: SELECT_FIELDS_TRANSFORM_BASE,
-    transformDoclinks: {
-      [bbDocumentNamesMA.TOEKENNING]: [
-        'BB Besluit vergunning bed and breakfast',
-        'BB Besluit van rechtswege',
-      ],
-      [bbDocumentNamesMA.VERLENGING]: ['BB Besluit verlenging beslistermijn'],
-      [bbDocumentNamesMA.BUITEN_BEHANDELING]: [
-        'BB Besluit buiten behandeling stellen',
-        'BB buiten behandeling stellen',
-      ],
-      [bbDocumentNamesMA.WEIGERING]: [
-        'Besluit weigering',
-        'BB Besluit weigeren vergunning',
-        'BB Besluit weigeren vergunning quotum',
-        'Besluit B&B weigering zonder overgangsrecht',
-      ],
-      [bbDocumentNamesMA.INTREKKING]: [
-        'Intrekken vergunning',
-        'BB Intrekkingsbesluit nav niet voldoen aan voorwaarden',
-        'BB Intrekkingsbesluit op eigen verzoek',
-      ],
-      [bbDocumentNamesMA.MEER_INFORMATIE]: ['BB Verzoek aanvullende gegevens'],
-      [bbDocumentNamesMA.SAMENVATTING]: ['Samenvatting'],
-    },
+    transformDoclinks: documentNamenMA_PB,
   };
 
 export const powerBrowserZaakTransformers = [BedAndBreakfastZaakTransformer];
