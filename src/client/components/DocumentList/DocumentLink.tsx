9dc99c96ae5672cdde57edb42f46b3ec185d2759
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-09-03 16:17:15 +0200

    Refactor out Axios p1

--- src/client/components/DocumentList/DocumentLink.tsx
+++ src/client/components/DocumentList/DocumentLink.tsx
@@ -2,13 +2,13 @@ import { useCallback, useState } from 'react';
 
 import { Icon } from '@amsterdam/design-system-react';
 import { AlertIcon, DownloadIcon } from '@amsterdam/design-system-react-icons';
-import { HttpStatusCode } from 'axios';
 import classnames from 'classnames';
 
 import styles from './DocumentLink.module.scss';
 import type { GenericDocument } from '../../../universal/types/App.types';
 import { captureException } from '../../helpers/monitoring';
 import { trackDownload } from '../../hooks/analytics.hook';
+import { HttpStatusCode } from '../../hooks/api/useDataApi-v2';
 import { useProfileTypeValue } from '../../hooks/useProfileType';
 import { MaLink } from '../MaLink/MaLink';
 import { Spinner } from '../Spinner/Spinner';
