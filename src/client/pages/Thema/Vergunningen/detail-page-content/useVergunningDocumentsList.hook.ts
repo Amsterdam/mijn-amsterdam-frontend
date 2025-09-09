ee5affa51db9a278e5e71c28e71ce97a93192923
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-09-09 13:05:06 +0200

    use api hook

--- src/client/pages/Thema/Vergunningen/detail-page-content/useVergunningDocumentsList.hook.ts
+++ src/client/pages/Thema/Vergunningen/detail-page-content/useVergunningDocumentsList.hook.ts
@@ -1,30 +1,19 @@
-import useSWR from 'swr';
+import { useEffect } from 'react';
 
-import { FIFTEEN_MINUTES_MS } from '../../../../../universal/config/app';
-import { ApiResponse } from '../../../../../universal/helpers/api';
 import { GenericDocument } from '../../../../../universal/types/App.types';
+import { createApiHook } from '../../../../hooks/api/useDataApi-v2';
 
-async function fetchDocuments(url: string) {
-  const res = await fetch(url, { credentials: 'include' });
+const useVergunningenDocumentsApi = createApiHook<GenericDocument[]>();
 
-  if (!res.ok) {
-    throw new Error('An error occurred while fetching the data.');
-  }
+export function useVergunningDocumentList(url?: string) {
+  const api = useVergunningenDocumentsApi();
 
-  return res.json();
-}
+  useEffect(() => {
+    if (api.isPristine && url) {
+      api.fetch(url);
+    }
+  }, [api.fetch, api.isPristine, url]);
 
-export function useVergunningDocumentList(
-  url?: string,
-  dedupingInterval: number = FIFTEEN_MINUTES_MS
-) {
-  const {
-    data: documentsResponse,
-    isLoading,
-    error,
-  } = useSWR<ApiResponse<GenericDocument[]>>(url, fetchDocuments, {
-    dedupingInterval,
-  });
-  const documents = documentsResponse?.content ?? [];
-  return { documents, isLoading, isError: !!error };
+  const documents = api.data?.content ?? [];
+  return { documents, isLoading: api.isLoading, isError: api.isError };
 }
