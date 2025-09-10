bb4a6869cc41715ac21af68205f687e66bd715d2
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-09-10 16:37:37 +0200

    Fix

--- src/client/components/MainFooter/MainFooter.tsx
+++ src/client/components/MainFooter/MainFooter.tsx
@@ -79,7 +79,7 @@ export function MainFooter() {
       </PageFooter.Spotlight>
 
       <PageFooter.Menu>
-        {footer?.content?.bottomLinks.map((link) => {
+        {data?.content?.bottomLinks.map((link) => {
           return (
             <PageFooter.MenuLink key={link.label} href={link.url}>
               {link.label}
