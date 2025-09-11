1789f6f201805f9966478540642b4f1363262f47
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-09-11 10:31:37 +0200

    Rm comment

--- src/server/helpers/source-api-helpers.test.ts
+++ src/server/helpers/source-api-helpers.test.ts
@@ -11,7 +11,6 @@ describe('Config', () => {
     const config2 = getApiConfig('ERFPACHT');
 
     expect(config).not.toStrictEqual(config2);
-    // Because we are using a stack trace to generate a cache key, the value can change if the underlying code changes.
     expect(config.cacheKey_UNSAFE).toBe('ERFPACHT-foo');
 
     expect('Accept' in config2.headers!).toBe(false);
