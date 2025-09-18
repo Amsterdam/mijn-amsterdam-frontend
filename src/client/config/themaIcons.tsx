bd33a511078594217c5b233f8888b21526ae70ec
dandianass908-bit <a.dandi@amsterdam.nl>
2025-09-18 13:03:25 +0200

    useProfileData.hook.ts:
    - Id: themaId hernoemd naar themaId: themaConfig.id
    
    useProfileThemaData.hook.tsx:
    
    - id: themaId hernoemd naar themaId: themaConfig.id

--- src/client/config/themaIcons.tsx
+++ src/client/config/themaIcons.tsx
@@ -1,6 +1,6 @@
 import { BellIcon, SearchIcon } from '@amsterdam/design-system-react-icons';
 
-import { type ThemaID } from '../../universal/config/thema';
+import { type ThemaID } from '../../universal/config/thema-config.ts';
 import { SVGComponent } from '../../universal/types/App.types';
 import { IconInkomenSVWI } from '../assets/icons';
 
