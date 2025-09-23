79ed58fe9b0445857780a17accb9b0c57c21bed2
YacineHallo <y.hesseling@amsterdam.nl>
2025-09-23 13:47:51 +0200

    Aparte constante in GeneralInfo voor Bodem gemaakt en InfoSection uitgeschakeld

--- src/client/pages/Thema/Bodem/InfoSection.tsx
+++ src/client/pages/Thema/Bodem/InfoSection.tsx
@@ -1,9 +1,9 @@
-import { themaConfig } from './Bodem-thema-config';
-import { SectionProps } from '../../GeneralInfo/GeneralInfo';
+// import { themaConfig } from './Bodem-thema-config';
+// import { SectionProps } from '../../GeneralInfo/GeneralInfo';
 
-export const bodemsectionProps: SectionProps = {
-  id: themaConfig.id,
-  title: themaConfig.title,
-  listItems: ["Uw aanvraag voor 'lood in de bodem-check'"],
-  active: themaConfig.featureToggle.themaActive,
-};
+// export const bodemsectionProps: SectionProps = {
+//   id: themaConfig.id,
+//   title: themaConfig.title,
+//   listItems: ["Uw aanvraag voor 'lood in de bodem-check'"],
+//   active: themaConfig.featureToggle.themaActive,
+// };
