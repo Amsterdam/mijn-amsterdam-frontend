4130c3e69db058929960460935a86fe59af9f6cf
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-09-24 11:25:20 +0200

    Simplify authenticated route handling

--- src/server/services/wmo/wmo-route-handlers.ts
+++ src/server/services/wmo/wmo-route-handlers.ts
@@ -1,5 +1,13 @@
+import { Request } from 'express';
+
 import { AuthProfileAndToken } from '../../auth/auth-types';
-import { fetchDocument } from '../zorgned/zorgned-service';
+import {
+  sendResponse,
+  type ResponseAuthenticated,
+} from '../../routing/route-helpers';
+import { fetchAllDocuments, fetchDocument } from '../zorgned/zorgned-service';
+
+const ZORGNED_JZD_API_CONFIG_KEY = 'ZORGNED_JZD';
 
 export async function fetchZorgnedJZDDocument(
   authProfileAndToken: AuthProfileAndToken,
@@ -7,8 +15,19 @@ export async function fetchZorgnedJZDDocument(
 ) {
   const response = fetchDocument(
     authProfileAndToken.profile.id,
-    'ZORGNED_JZD',
+    ZORGNED_JZD_API_CONFIG_KEY,
     documentId
   );
   return response;
+}
+
+export async function fetchZorgnedJZDDocuments(
+  req: Request,
+  res: ResponseAuthenticated
+) {
+  const response = await fetchAllDocuments(res.locals.userID, {
+    zorgnedApiConfigKey: ZORGNED_JZD_API_CONFIG_KEY,
+  });
+
+  return sendResponse(res, response);
 }
