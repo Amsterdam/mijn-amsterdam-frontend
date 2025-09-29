043611a64a370afe0da95fbd714f7b0acf7c89cc
dandianass908-bit <a.dandi@amsterdam.nl>
2025-09-29 10:17:02 +0200

    Het uitcommenten van Infosection.tsx voor bodem want het wordt niet meer gebruikt

--- src/client/pages/Thema/Bodem/InfoSection.tsx
+++ src/client/pages/Thema/Bodem/InfoSection.tsx
@@ -1,9 +1,9 @@
-import { themaConfig } from './Bodem-thema-config';
-import { SectionProps as Section } from '../../GeneralInfo/GeneralInfo';
+// import { themaConfig } from './Bodem-thema-config';
+// import { SectionProps as Section } from '../../GeneralInfo/GeneralInfo';
 
-export const bodemsectionProps: Section = {
-  id: themaConfig.id,
-  title: themaConfig.title,
-  listItems: ["Uw aanvraag voor 'lood in de bodem-check'"],
-  active: themaConfig.featureToggle.themaActive, // aanpassen op basis van bodem-thema-config.ts featuretoggle
-};
+// export const bodemsectionProps: Section = {
+//   id: themaConfig.id,
+//   title: themaConfig.title,
+//   listItems: ["Uw aanvraag voor 'lood in de bodem-check'"],
+//   active: themaConfig.featureToggle.themaActive, // aanpassen op basis van bodem-thema-config.ts featuretoggle
+// };
