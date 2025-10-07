6ed084a4b0f5120b61e22a3b864a24fdebe9de09
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-10-07 15:09:08 +0200

    Add ontvangers

--- src/client/pages/Thema/HLI/HLI-thema-config.ts
+++ src/client/pages/Thema/HLI/HLI-thema-config.ts
@@ -13,7 +13,7 @@ const MAX_TABLE_ROWS_ON_THEMA_PAGINA_EERDER = MAX_TABLE_ROWS_ON_THEMA_PAGINA;
 const displayPropsHuidigeRegelingen: DisplayProps<HLIRegelingFrontend> = {
   props: {
     detailLinkComponent: 'Regeling',
-    betrokkenen: 'Ontvangers',
+    ...(!IS_PRODUCTION && { betrokkenen: 'Ontvangers' }),
   },
   colWidths: {
     large: ['80%', '20%'],
