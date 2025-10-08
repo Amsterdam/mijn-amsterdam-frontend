bc260878add52ff346c88d76c651ed652a66ef1a
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-10-08 21:20:51 +0200

    Add input validation
    # Conflicts:
    #	package.json
    #	pnpm-lock.yaml

--- src/server/services/wmo/wmo-router-external-consumer.ts
+++ src/server/services/wmo/wmo-router-external-consumer.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
+import * as z from 'zod';
 
 import {
   fetchWmoVoorzieningenCompact,
   type FetchWmoVoorzieningFilter,
 } from './wmo';
+import { ZodValidators } from '../../helpers/validation';
 import { ExternalConsumerEndpoints } from '../../routing/bff-routes';
 import {
   createBFFRouter,
@@ -26,14 +28,25 @@ const isActueleUitgevoerdeWoonruimteAanpassing: FetchWmoVoorzieningFilter = (
   voorziening.isActueel &&
   steps.some((step) => step.status === WRA_STEP_STATUS && step.isActive);
 
+const apiInput = z.object({
+  bsn: ZodValidators.BSN,
+});
+
 wmoRouterPrivateNetwork.post(
   ExternalConsumerEndpoints.private.WMO_VOORZIENINGEN,
   async (req: Request, res: Response) => {
-    // TODO: zod validation
     const bsn = req.body?.bsn as BSN;
 
-    if (!bsn) {
-      return sendBadRequest(res, 'BSN is required', null);
+    try {
+      apiInput.parse(req.body);
+    } catch (error) {
+      let inputError = 'Invalid input';
+
+      if (error instanceof z.ZodError) {
+        inputError = error.issues.map((e) => e.message).join(', ');
+      }
+
+      return sendBadRequest(res, inputError);
     }
 
     const response = await fetchWmoVoorzieningenCompact(bsn, {
