8532bfb836e03fc245824ea428a3b00ed70e1bdf
Roan Paulus <roan.paulus@amsterdam.nl>
2025-10-08 14:13:22 +0200

    remove infosection

--- src/client/pages/Thema/Bodem/InfoSection.tsx
+++
@@ -1,9 +0,0 @@
-import { featureToggle, themaId, themaTitle } from './Bodem-thema-config';
-import { SectionProps } from '../../GeneralInfo/GeneralInfo';
-
-export const bodemsectionProps: SectionProps = {
-  id: themaId,
-  title: themaTitle,
-  listItems: ["Uw aanvraag voor 'lood in de bodem-check'"],
-  active: featureToggle.BodemActive,
-};
