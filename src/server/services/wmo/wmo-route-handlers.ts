c905b9960f92134ea06b9f542583f5bde894fb1f
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-10-09 12:50:14 +0200

    Move route handler

--- src/server/services/wmo/wmo-route-handlers.ts
+++ src/server/services/wmo/wmo-route-handlers.ts
@@ -1,8 +1,12 @@
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import z from 'zod';
 
+import { fetchActueleWRAVoorzieningenCompact } from './wmo';
 import { AuthProfileAndToken } from '../../auth/auth-types';
+import { ZodValidators } from '../../helpers/validation';
 import {
   sendResponse,
+  sendBadRequestInvalidInput,
   type ResponseAuthenticated,
 } from '../../routing/route-helpers';
 import {
@@ -43,6 +47,25 @@ export async function fetchZorgnedJZDAanvragen(
   const response = await fetchAanvragenRaw(res.locals.userID, {
     zorgnedApiConfigKey: ZORGNED_JZD_API_CONFIG_KEY,
   });
+
+  return sendResponse(res, response);
+}
+
+const voorzieningenRequestInput = z.object({
+  bsn: ZodValidators.BSN,
+});
+
+export async function handleVoorzieningenRequest(req: Request, res: Response) {
+  // Validate the request body so we can be sure it has the correct shape and values.
+  let validatedRequestBody;
+  try {
+    validatedRequestBody = voorzieningenRequestInput.parse(req.body);
+  } catch (error) {
+    return sendBadRequestInvalidInput(res, error);
+  }
+
+  const bsn = validatedRequestBody.bsn;
+  const response = await fetchActueleWRAVoorzieningenCompact(bsn);
 
   return sendResponse(res, response);
 }
