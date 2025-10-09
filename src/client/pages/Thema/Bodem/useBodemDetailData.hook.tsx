b021e90095c08ac4709cdbad0e5c271c2da9d1b6
YacineHallo <y.hesseling@amsterdam.nl>
2025-10-09 10:10:27 +0200

    RouteConfig uit useBodemDetail verwijderd

--- src/client/pages/Thema/Bodem/useBodemDetailData.hook.tsx
+++ src/client/pages/Thema/Bodem/useBodemDetailData.hook.tsx
@@ -4,18 +4,17 @@ import { themaConfig } from './Bodem-thema-config';
 import { useBodemData } from './useBodemData.hook';
 
 export function useBodemDetailData() {
-  const { items, isLoading, isError, breadcrumbs,  } = useBodemData();
+  const { items, isLoading, isError, breadcrumbs } = useBodemData();
   const { id } = useParams<{ id: string }>();
 
   const meting = items.find((meting) => meting.kenmerk === id) ?? null;
 
   return {
     id: themaConfig.id,
-    title: themaConfig.detailPage.title?? undefined,
+    title: themaConfig.detailPage.title ?? undefined,
     meting,
     isLoading,
     isError,
     breadcrumbs,
-    routeConfig: themaConfig.listPage.route,
   };
 }
