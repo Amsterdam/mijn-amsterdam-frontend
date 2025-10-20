a0d2539e0c7c4175977c08594d6b9e64fedf1e1f
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-10-20 18:20:42 +0200

    Add comment

--- src/server/services/afis/afis-service-config.ts
+++ src/server/services/afis/afis-service-config.ts
@@ -10,12 +10,12 @@ export const featureToggle = {
 
 export const routes = {
   private: {
-    // Afis E-Mandates
     // This endpoint is reached over the private network from the EnableU network.
+    // It's used to notify us about the status of e-mandate signing requests. Sent by the Payment Service Provider.
+    // The requesting side is allways a server side request from the PSP.
     AFIS_EMANDATE_SIGN_REQUEST_STATUS_NOTIFY: `/services/afis/e-mandates/sign-request-status-notify`,
   },
   protected: {
-    // AFIS
     AFIS_BUSINESSPARTNER: '/services/afis/businesspartner',
     AFIS_EMANDATES: '/services/afis/e-mandates',
     AFIS_EMANDATES_STATUS_CHANGE: '/services/afis/e-mandates/change-status',
