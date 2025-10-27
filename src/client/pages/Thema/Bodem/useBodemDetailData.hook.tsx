004b4055cb95157c5344a1c3ca134d395646287b
dandianass908-bit <a.dandi@amsterdam.nl>
2025-10-27 10:59:38 +0100

    UseBodemDetailData.hook.tsx:
    Ik heb de title bij de detailpagina weten te fixen door de title aan te passen naar title: themaConfig.detailpage.title waardoor ik nu in plaats van Bodem 'Lood in bodem-check' zie staan. Deze aanpassing moest ik bij de return doen.

--- src/client/pages/Thema/Bodem/useBodemDetailData.hook.tsx
+++ src/client/pages/Thema/Bodem/useBodemDetailData.hook.tsx
@@ -1,24 +1,18 @@
 import { useParams } from 'react-router';
 
+import { themaConfig } from './Bodem-thema-config';
 import { useBodemData } from './useBodemData.hook';
 
 export function useBodemDetailData() {
-  const {
-    themaId,
-    title,
-    items,
-    isLoading,
-    isError,
-    breadcrumbs,
-    detailPageConfig,
-  } = useBodemData();
+  const { themaId, items, isLoading, isError, breadcrumbs, detailPageConfig } =
+    useBodemData();
   const { id } = useParams<{ id: string }>();
 
   const meting = items.find((meting) => meting.identifier === id) ?? null;
 
   return {
     themaId,
-    title,
+    title: themaConfig.detailPage.title,
     meting,
     isLoading,
     isError,
