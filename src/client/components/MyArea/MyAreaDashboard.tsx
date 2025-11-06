3b5d7f21d33c9a8ded6fb074c1218a0b9c8a9c8b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-11-06 12:55:45 +0100

    Fix loading issue

--- src/client/components/MyArea/MyAreaDashboard.tsx
+++ src/client/components/MyArea/MyAreaDashboard.tsx
@@ -19,7 +19,7 @@ export function MyAreaDashboard() {
   const isPrivate = profileType === 'private';
   // Check if the Map is nearly scrolled into view
   const mokum = isPrivate ? isMokum(BRP.content) : isMokum(KVK.content);
-  const isLoadingBrpKvk = isLoading(BRP) || isLoading(KVK);
+  const isLoadingBrpKvk = isPrivate ? isLoading(BRP) : isLoading(KVK);
   return (
     <div ref={ref} className={styles.DashboardMapContainer}>
       <MyAreaLoader isDashboard={true} />
