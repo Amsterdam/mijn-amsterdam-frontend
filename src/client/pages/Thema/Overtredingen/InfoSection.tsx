754f2d64e71e24830ea8659f837c47415e19b468
dandianass908-bit <dandianass908@gmail.com>
2025-11-10 12:59:37 +0100

    MIJN-12063-BUG - Corrigeer naamgeving InfoSection naar InfoSection_DEPRECATED (#2217)
    Het mergen van de bugfix naar main.

--- src/client/pages/Thema/Overtredingen/InfoSection.tsx
+++ src/client/pages/Thema/Overtredingen/InfoSection.tsx
@@ -3,9 +3,9 @@ import {
   themaId,
   themaTitle,
 } from './Overtredingen-thema-config';
-import { InfoSection } from '../../GeneralInfo/GeneralInfo';
+import { InfoSection_DEPRECATED } from '../../GeneralInfo/GeneralInfo';
 
-export const overtredingensectionProps: InfoSection = {
+export const overtredingensectionProps: InfoSection_DEPRECATED = {
   id: themaId,
   title: themaTitle,
   listItems: ['Inzien van uw overtredingen in de milieuzone'],
