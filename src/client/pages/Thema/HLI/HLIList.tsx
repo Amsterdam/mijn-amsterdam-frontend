64e202624939870c0bff480ea127d7e088cf4646
dandianass908-bit <a.dandi@amsterdam.nl>
2025-11-11 14:38:14 +0100

    HLIList.tsx:
    -  Het verwijderen van routeConfig en het aanpassen van routeConfig.detailPage naar themaConfig.detailPage.route

--- src/client/pages/Thema/HLI/HLIList.tsx
+++ src/client/pages/Thema/HLI/HLIList.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router';
 
-import { ListPageParamKind } from './HLI-thema-config';
+import { ListPageParamKind, themaConfig } from './HLI-thema-config';
 import { HistoricItemsMention } from './HLIThema';
 import { useHliThemaData } from './useHliThemaData';
 import { ListPagePaginated } from '../../../components/ListPagePaginated/ListPagePaginated';
@@ -11,16 +11,9 @@ export function HLIList() {
   const { kind = 'huidige-regelingen' } = useParams<{
     kind: ListPageParamKind;
   }>();
-  const {
-    themaId,
-    regelingen,
-    tableConfig,
-    isLoading,
-    isError,
-    breadcrumbs,
-    routeConfig,
-  } = useHliThemaData();
-  useHTMLDocumentTitle(routeConfig.detailPage);
+  const { themaId, regelingen, tableConfig, isLoading, isError, breadcrumbs } =
+    useHliThemaData();
+  useHTMLDocumentTitle(themaConfig.detailPage.route);
 
   const { filter, sort, title, displayProps, listPageRoute } =
     tableConfig[kind];
