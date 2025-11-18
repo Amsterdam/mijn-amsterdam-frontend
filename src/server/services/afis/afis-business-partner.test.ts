91399e9c85eb2d466256dbca0d5435c606c3a70b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-11-18 12:48:53 +0100

    Update matchers

--- src/server/services/afis/afis-business-partner.test.ts
+++ src/server/services/afis/afis-business-partner.test.ts
@@ -120,33 +120,30 @@ describe('Afis Business Partner services', () => {
       businessPartnerId: GENERIC_ID,
     });
 
-    expect(response).toMatchInlineSnapshot(`
-      {
-        "content": {
-          "address": {
-            "AddressID": 430844,
-            "CityName": "Leiden",
-            "Country": "NL",
-            "HouseNumber": 20,
-            "HouseNumberSupplementText": "",
-            "PostalCode": "2311 VW",
-            "Region": "",
-            "StreetName": "Rembrandtstraat",
-            "StreetPrefixName": "",
-            "StreetSuffixName": "",
-          },
-          "businessPartnerId": "12346789",
-          "email": "xxmail@arjanappel.nl",
-          "firstName": null,
-          "fullAddress": "Rembrandtstraat 20
-      2311 VW Leiden",
-          "fullName": "Taxon Expeditions BV",
-          "id": 430844,
-          "lastName": null,
+    expect(response).toStrictEqual({
+      content: {
+        address: {
+          AddressID: 430844,
+          CityName: 'Leiden',
+          Country: 'NL',
+          HouseNumber: 20,
+          HouseNumberSupplementText: '',
+          PostalCode: '2311 VW',
+          Region: '',
+          StreetName: 'Rembrandtstraat',
+          StreetPrefixName: '',
+          StreetSuffixName: '',
         },
-        "status": "OK",
-      }
-    `);
+        businessPartnerId: '12346789',
+        email: 'xxmail@arjanappel.nl',
+        firstName: null,
+        fullAddress: 'Rembrandtstraat 20\n2311 VW Leiden',
+        fullName: 'Taxon Expeditions BV',
+        id: 430844,
+        lastName: null,
+      },
+      status: 'OK',
+    });
   });
 
   it('returns an error when there is an error fetching the business partner details', async () => {
@@ -156,13 +153,12 @@ describe('Afis Business Partner services', () => {
       businessPartnerId: GENERIC_ID,
     });
 
-    expect(response).toMatchInlineSnapshot(`
-      {
-        "content": null,
-        "message": "Could not get full name or address for business partner 12346789",
-        "status": "ERROR",
-      }
-    `);
+    expect(response).toStrictEqual({
+      content: null,
+      message:
+        'Could not get full name or address for business partner 12346789',
+      status: 'ERROR',
+    });
   });
 
   it('Omits email and phone properties when the business partner details data quality is not sufficient', async () => {
@@ -194,31 +190,31 @@ describe('Afis Business Partner services', () => {
       businessPartnerId: GENERIC_ID,
     });
 
-    expect(response).toMatchInlineSnapshot(`
-    {
-      "content": {
-        "address": null,
-        "businessPartnerId": "12346789",
-        "firstName": null,
-        "fullAddress": "onbekend adres",
-        "fullName": null,
-        "id": undefined,
-        "lastName": null,
+    expect(response).toStrictEqual({
+      content: {
+        address: null,
+        businessPartnerId: '12346789',
+        firstName: null,
+        fullAddress: 'onbekend adres',
+        fullName: null,
+        id: undefined,
+        lastName: null,
       },
-      "failedDependencies": {
-        "email": {
-          "content": null,
-          "message": "Could not get email, missing required query param addressId",
-          "status": "ERROR",
+      failedDependencies: {
+        email: {
+          content: null,
+          message:
+            'Could not get email, missing required query param addressId',
+          status: 'ERROR',
         },
-        "phone": {
-          "content": null,
-          "message": "Could not get phone, missing required query param addressId",
-          "status": "ERROR",
+        phone: {
+          content: null,
+          message:
+            'Could not get phone, missing required query param addressId',
+          status: 'ERROR',
         },
       },
-      "status": "OK",
-    }
-  `);
+      status: 'OK',
+    });
   });
 });
