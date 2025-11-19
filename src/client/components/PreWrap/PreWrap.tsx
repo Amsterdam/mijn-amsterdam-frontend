6d30702d4a1a12128ceb84d8c8da80d529086ea7
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-11-19 09:15:06 +0100

    Fix import

--- src/client/components/PreWrap/PreWrap.tsx
+++ src/client/components/PreWrap/PreWrap.tsx
@@ -1,4 +1,4 @@
-import styles from './PreWrap.module.css';
+import styles from './PreWrap.module.scss';
 
 export function PreWrap({ children }: { children: React.ReactNode }) {
   return <span className={styles.PreWrap}>{children}</span>;
