88f02a0b631ba19ff2975720b8cee15b482ad036
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-11-19 10:18:27 +0100

    Change comment

--- src/server/services/hr-kvk/hr-kvk-service-config.ts
+++ src/server/services/hr-kvk/hr-kvk-service-config.ts
@@ -1,5 +1,5 @@
 export const featureToggle = {
-  expandHeeftAlsEigenaarHrNps: false, // TODO: Enable if we have the correct autorisation
+  expandHeeftAlsEigenaarHrNps: false, // TODO: Enable if we have the correct authorization
 };
 export const MACExpandScopes = [
   'heeftAlsEigenaarHrNnp',
