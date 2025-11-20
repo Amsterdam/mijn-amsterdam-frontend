237fafe3331ee25022849619ab43b4f3b8e0c400
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-11-20 15:36:34 +0100

    Add info page

--- src/client/pages/Burgerzaken/Burgerzaken.tsx
+++ src/client/pages/Burgerzaken/Burgerzaken.tsx
@@ -11,18 +11,29 @@ export function Burgerzaken() {
   return (
     <TextPageV2>
       <PageContentV2 span={8}>
-        <PageHeadingV2>Paspoort en ID-Kaart</PageHeadingV2>
+        <PageHeadingV2>Paspoort en ID-kaart</PageHeadingV2>
         <PageContentCell>
           <Paragraph className="ams-mb-m">
-            Het is in Mijn Amsterdam helaas niet meer mogelijk om uw paspoort of
-            ID-kaart in te zien.
+            U kunt uw paspoort of ID-kaart niet meer bekijken in Mijn Amsterdam.
+            <br />
+            Ga naar{' '}
+            <Link
+              rel="noopener noreferrer"
+              href="https://mijn.overheid.nl/identiteit/persoonsgegevens/reisdocumenten"
+            >
+              MijnOverheid
+            </Link>{' '}
+            om gegevens van uw paspoort of ID-kaart te zien.
           </Paragraph>
           <Paragraph>
-            Voor meer informatie over uw paspoort of ID-kaart{' '}
-            <Link href="https://www.amsterdam.nl/burgerzaken/paspoort-id-kaart-rijbewijs/">
-              gaat u naar de pagina op Amsterdam.nl
-            </Link>
-            .
+            Op{' '}
+            <Link
+              rel="noopener noreferrer"
+              href="https://www.amsterdam.nl/burgerzaken/paspoort-id-kaart-rijbewijs/"
+            >
+              Paspoort en ID-kaart
+            </Link>{' '}
+            leest u meer over deze reisdocumenten.
           </Paragraph>
         </PageContentCell>
       </PageContentV2>
