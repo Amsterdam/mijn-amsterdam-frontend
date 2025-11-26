741d60bacf50ba70b92b9be08688ade399ad4f18
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-11-26 09:18:17 +0100

    Remove facturen from types, add more safety

--- src/server/services/erfpacht/erfpacht.test.ts
+++ src/server/services/erfpacht/erfpacht.test.ts
@@ -1,7 +1,6 @@
 import { describe, expect, test } from 'vitest';
 
 import { fetchErfpacht, fetchErfpachtDossiersDetail } from './erfpacht';
-import type { ErfpachtDossiersResponseSource } from './erfpacht-types';
 import ERFPACHT_DOSSIERINFO_DETAILS from '../../../../mocks/fixtures/erfpacht-v2-dossierinfo-bsn.json';
 import ERFPACHT_DOSSIERS from '../../../../mocks/fixtures/erfpacht-v2-dossiers.json';
 import ERFPACHT_ERFPACHTER from '../../../../mocks/fixtures/erfpacht-v2-erfpachter.json';
@@ -38,25 +37,6 @@ describe('simple-connect/erfpacht', () => {
 
     const responseContent = await fetchErfpacht(authProfileAndToken);
     expect(responseContent).toMatchSnapshot();
-  });
-
-  test('fetchErfpacht: dossiers - does not include openstaandeFacturen property if not present in source data.', async () => {
-    remoteApi
-      .get('/erfpacht/vernise/api/erfpachter')
-      .reply(200, { erfpachter: true, relationCode: '123-abc' });
-
-    const dossiersWithoutOpenstaandeFacturen = structuredClone(
-      ERFPACHT_DOSSIERS
-    ) as ErfpachtDossiersResponseSource;
-    delete dossiersWithoutOpenstaandeFacturen.openstaandeFacturen;
-
-    remoteApi
-      .get('/erfpacht/vernise/api/dossierinfo')
-      .reply(200, dossiersWithoutOpenstaandeFacturen);
-
-    const responseContent = await fetchErfpacht(authProfileAndToken);
-    expect(responseContent.content).not.toBeFalsy();
-    expect(responseContent.content).not.toHaveProperty('openstaandeFacturen');
   });
 
   test('fetchErfpacht: dossier detail', async () => {
