d46c4e6cad286a01810857fe6c238e9d6c96b1ec
dandianass908-bit <a.dandi@amsterdam.nl>
2025-11-27 04:44:31 +0100

    stadspas-gpass-service.ts:
    het omzetten van themaConfig.featureToggle.hliThemaStadspasBlokkerenActive, naar  !themaConfig.featureToggle.stadspas.hliThemaStadspasBlokkerenActive,

--- src/server/services/hli/stadspas-gpass-service.ts
+++ src/server/services/hli/stadspas-gpass-service.ts
@@ -522,7 +522,8 @@ export async function mutateGpassSetPasIsBlockedState(
     formatUrl: ({ url }) => `${url}/rest/sales/v1/togglepas/${passNumber}`,
     headers: getHeaders(administratienummer),
     enableCache: false,
-    postponeFetch: !themaConfig.featureToggle.hliThemaStadspasBlokkerenActive,
+    postponeFetch:
+      !themaConfig.featureToggle.stadspas.hliThemaStadspasBlokkerenActive,
     transformResponse: transformPassIsBlockedResponse,
   });
 
