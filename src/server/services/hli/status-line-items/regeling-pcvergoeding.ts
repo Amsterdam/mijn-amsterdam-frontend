b90db97679b0accc434f146cddc0d95a80f7f367
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-12-22 11:47:15 +0100

    Add env var

--- src/server/services/hli/status-line-items/regeling-pcvergoeding.ts
+++ src/server/services/hli/status-line-items/regeling-pcvergoeding.ts
@@ -4,6 +4,7 @@ import { getBetrokkenKinderenDescription } from './generic';
 import { defaultDateFormat } from '../../../../universal/helpers/date';
 import { lowercaseFirstLetter } from '../../../../universal/helpers/text';
 import { sortAlpha, splitBy } from '../../../../universal/helpers/utils';
+import { getFromEnv } from '../../../helpers/env';
 import {
   ZorgnedAanvraagWithRelatedPersonsTransformed,
   ZorgnedStatusLineItemTransformerConfig,
@@ -21,7 +22,8 @@ export const AV_PCVC = 'AV-PCVC'; // PC Tegoed Voortgezet Onderwijs.
 export const AV_PCVZIL = 'AV-PCVZIL';
 export const AV_PCVTG = 'AV-PCVTG';
 
-const PCTEGOED_2026_CODES_ACTIVE = '2026-01-01';
+const DATE_PCTEGOED_2026_CODES_ACTIVE =
+  getFromEnv('BFF_DATE_PCTEGOED_2026_CODES_ACTIVE') || '2026-01-01';
 
 const avCodes = {
   PC: {
@@ -170,7 +172,8 @@ export function filterCombineUpcPcvData(
   const baseRegelingIdWithVerzilvering: string[] = [];
   const [aanvragenAfter2026, aanvragenBefore2026] = splitBy(
     aanvragen,
-    (aanvraag) => isBefore(aanvraag.datumBesluit, PCTEGOED_2026_CODES_ACTIVE)
+    (aanvraag) =>
+      isBefore(aanvraag.datumBesluit, DATE_PCTEGOED_2026_CODES_ACTIVE)
   );
   const aanvragen_ =
     filterOutRedundantPcVergoedingsAanvragenWhenWorkShopNietGevolgd(
