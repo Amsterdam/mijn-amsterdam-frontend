16e7b05274e2cf23759bfa67104b2f90a5de127f
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-01-06 17:17:06 +0100

    MIJN-12440-Bug-change-entrypoint

--- scripts/docker-entrypoint-bff.sh
+++ scripts/docker-entrypoint-bff.sh
@@ -18,14 +18,12 @@ DESTINATION_FOLDER="/home/site/wwwroot/App_Data/jobs/triggered/"
 # Check if destination folder exists
 if [ ! -d "$DESTINATION_FOLDER" ]; then
   echo "Error: Destination folder '$DESTINATION_FOLDER' does not exist."
-  exit 1
+else
+  echo "Merging contents into existing folder '$DESTINATION_FOLDER'..."
+  # https://unix.stackexchange.com/a/149504
+  rsync -hvrPt "$SOURCE_FOLDER/" "$DESTINATION_FOLDER/"
+  echo "Successfully merged contents of '$SOURCE_FOLDER' into '$DESTINATION_FOLDER'."
 fi
-
-# https://unix.stackexchange.com/a/149504
-rsync -hvrPt "$SOURCE_FOLDER/" "$DESTINATION_FOLDER/"
-
-echo "Successfully merged contents of '$SOURCE_FOLDER' into '$DESTINATION_FOLDER'."
-
 
 npm run bff-api:serve-build
 
