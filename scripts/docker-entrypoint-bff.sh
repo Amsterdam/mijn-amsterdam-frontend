4ac2c7c190014bd4e61b3fc55398088dcd58ba4b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-01-07 16:57:51 +0100

    Make warning

--- scripts/docker-entrypoint-bff.sh
+++ scripts/docker-entrypoint-bff.sh
@@ -17,7 +17,7 @@ DESTINATION_FOLDER="/home/site/wwwroot/App_Data/jobs/triggered/"
 
 # Check if destination folder exists
 if [ ! -d "$DESTINATION_FOLDER" ]; then
-  echo "Error: Webjobs destination folder '$DESTINATION_FOLDER' does not exist." >&2
+  echo "Warning: Webjobs destination folder '$DESTINATION_FOLDER' does not exist."
 else
   echo "Merging contents into existing Webjobs folder '$DESTINATION_FOLDER'..."
   # https://unix.stackexchange.com/a/149504
