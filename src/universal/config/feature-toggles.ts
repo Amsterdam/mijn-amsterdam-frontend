a5e5c7807964dd10573a1152246ec9eb92c5ab85
Terry van Walen <t.van.walen@amsterdam.nl>
2026-02-02 17:23:54 +0100

    Enable featureToggle for vth vergunning for powerbrowser on acceptatie

--- src/universal/config/feature-toggles.ts
+++ src/universal/config/feature-toggles.ts
@@ -1,4 +1,4 @@
-import { IS_PRODUCTION } from './env';
+import { IS_PRODUCTION, IS_TEST } from './env';
 
 /**
  * @deprecated
@@ -86,5 +86,5 @@ export const FeatureToggle = {
   cobrowseIsActive: true,
 
   // VTH vergunningen move from decos to powerbrowser (ligplaats/kameromzettingen/woningvormingen)
-  VTHOnPowerbrowserActive: false,
+  VTHOnPowerbrowserActive: !(IS_PRODUCTION || IS_TEST),
 } as const;
