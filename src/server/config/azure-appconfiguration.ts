7d6416ea56fb19262f2f85437eafda6bccd24075
Roan Paulus <roan.paulus@amsterdam.nl>
2026-02-06 18:24:31 +0100

    rename env, add to template, mock featuremanager

--- src/server/config/azure-appconfiguration.ts
+++ src/server/config/azure-appconfiguration.ts
@@ -7,12 +7,27 @@ import {
   ConfigurationMapFeatureFlagProvider,
 } from '@microsoft/feature-management';
 
+import { IS_DEVELOPMENT } from '../../universal/config/env';
+
 let featureManager: FeatureManager | undefined;
 const REFRESH_INTERVAL_MS = 5000;
 
+const DISABLED_DEVELOPMENT_FEATURES: string[] = [];
+
 export async function startAppConfiguration() {
-  const endpoint = process.env.AZURE_APPCONFIG_CONNECTION_STRING;
-  const appConfig = await load(endpoint, {
+  const connectionString = process.env.APPCONFIGURATION_CONNECTION_STRING;
+  if (!connectionString) {
+    if (IS_DEVELOPMENT) {
+      featureManager = {
+        isEnabled: isEnabledMock,
+      } as unknown as FeatureManager;
+      return;
+    }
+    throw Error(
+      'Environment variable APPCONFIGURATION_CONNECTION_STRING is not defined'
+    );
+  }
+  const appConfig = await load(connectionString, {
     featureFlagOptions: {
       enabled: true,
       refresh: {
@@ -21,11 +36,9 @@ export async function startAppConfiguration() {
       },
     },
   });
-
   featureManager = new FeatureManager(
     new ConfigurationMapFeatureFlagProvider(appConfig)
   );
-
   while (true) {
     await appConfig.refresh();
     await new Promise((resolve) => setTimeout(resolve, REFRESH_INTERVAL_MS));
@@ -37,4 +50,8 @@ export function getFeatureManager(): FeatureManager {
     throw Error('No featureManager defined, call startAppConfiguration first.');
   }
   return featureManager;
+}
+
+async function isEnabledMock(featureName: string): Promise<boolean> {
+  return !DISABLED_DEVELOPMENT_FEATURES.includes(featureName);
 }
