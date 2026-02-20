9b0074a1b0375d5132aaa8ef14f226cfbec100c3
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-02-20 10:53:13 +0100

    Update mocks api

--- mocks.config.js
+++ mocks.config.js
@@ -6,7 +6,7 @@ const ProfileTypeHandler = require('./mocks/handlers/ProfileTypeHandler.js');
 
 module.exports = {
   // Log level. Can be one of silly, debug, verbose, info, warn or error
-  log: 'info',
+  log: 'silly',
   config: {
     //allowUnknownArguments: false,
   },
