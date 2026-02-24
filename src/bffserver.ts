59c3f3a316ed88e65ac1187143a07c6c57be3ab8
Roan Paulus <roan.paulus@amsterdam.nl>
2026-02-24 13:40:27 +0100

    start in app.ts

--- src/bffserver.ts
+++ src/bffserver.ts
@@ -1,10 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./mijnamsterdam.d.ts" />
 import '../src/server/helpers/load-env';
-import { startAppConfiguration } from './server/config/azure-appconfiguration';
 
-(async function () {
-  await startAppConfiguration();
-  // Compilation entrypoint
-  import('./server/app');
-})();
+// Compilation entrypoint
+import('./server/app');
