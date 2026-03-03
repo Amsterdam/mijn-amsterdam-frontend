79bebdd6a7f84b4b4a3014e729e6d5110946c53c
YacineHallo <y.hesseling@amsterdam.nl>
2026-03-03 13:24:26 +0100

    themaConfi de Base afgerond

--- src/client/pages/Thema/Vergunningen/Vergunningen-thema-config.ts
+++ src/client/pages/Thema/Vergunningen/Vergunningen-thema-config.ts
@@ -79,18 +79,7 @@ export type ListPageParamKind = (typeof listPageParamKind)[ListPageParamKey];
 const THEMA_ID = 'VERGUNNINGEN';
 const THEMA_TITLE = 'Vergunningen en ontheffingen';
 
-type VergunningenThemaConfig = Pick<
-  ThemaConfigBase,
-  | 'id'
-  | 'title'
-  | 'profileTypes'
-  | 'redactedScope'
-  | 'featureToggle'
-  | 'route'
-  | 'pageLinks'
-> & {
-  id: typeof THEMA_ID;
-};
+type VergunningenThemaConfig = ThemaConfigBase<typeof THEMA_ID>;
 
 export const themaConfig: VergunningenThemaConfig = {
   id: THEMA_ID,
@@ -107,6 +96,41 @@ export const themaConfig: VergunningenThemaConfig = {
     {
       to: 'https://www.amsterdam.nl/ondernemen/vergunningen/wevos/',
       title: 'Ontheffing RVV en TVM aanvragen',
+    },
+  ],
+  uitlegPageSections: [
+    {
+      title: THEMA_TITLE,
+      listItems: [
+        {
+          text: 'Uw aanvraag voor een ontheffing of vergunning voor de volgende activiteiten:',
+
+          listItems: [
+            'Ergens rijden of stilstaan waar dat normaal niet mag (RVV en e-RVV)',
+            'Straat tijdelijk afsluiten of afzetten (TVM)',
+            'Object neerzetten op parkeervak, straat of stoep (Objectvergunning)',
+            'Parkeervakken reserveren (TVM)',
+            'Tijdelijk toegang krijgen tot gebied dat is afgesloten met paaltjes (RVV)',
+            'Werkzaamheden uitvoeren op tijden dat het normaal niet mag (Nachtwerkontheffing)',
+            'Filmen (Filmmelding)',
+            'Fietsen en/of fietsenrekken verwijderen',
+          ],
+        },
+        'Uw aanvraag of kentekenwijziging voor een RVV-ontheffing Sloterweg',
+        'Uw aanvraag voor een gehandicaptenparkeerkaart (GPK) of een vaste gehandicaptenparkeerplaats (GPP)',
+        'Uw aanvraag voor een ontheffing touringcar',
+        'Uw aanvraag voor een ontheffing zwaar verkeer',
+        'Uw aanvraag voor een ontheffing blauwe zone',
+        'Uw evenementvergunning of evenementmelding',
+        'Uw aanvraag voor een splitsingsvergunning',
+        'Uw aanvraag voor kamerverhuur (omzettingsvergunning)',
+        'Uw aanvraag vergunning straatartiest, draaiorgel of het aanbieden van diensten op straat',
+        'Uw aanvraag ontheffing verspreiden reclamemateriaal (sampling)',
+        'Uw aanvraag voor een vergunning voor onttrekken, samenvoegen en vormen van woonruimte',
+        'Uw aanvraag voor een ligplaatsvergunning',
+        'Uw aanvraag voor een eigen parkeerplaats voor huisartsen, verloskundigen en consuls',
+        'Uw aanvraag voor een vergunning exploitatie horecabedrijf',
+      ],
     },
   ],
 };
