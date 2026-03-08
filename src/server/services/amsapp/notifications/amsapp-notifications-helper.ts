3d66f3839bf8741c0fbddc01c99f5fbed141e6e2
Terry van Walen <t.van.walen@amsterdam.nl>
2026-03-08 20:07:00 +0100

    Restructure notifications slightly

--- src/server/services/amsapp/notifications/amsapp-notifications-helper.ts
+++ src/server/services/amsapp/notifications/amsapp-notifications-helper.ts
@@ -1,3 +1,27 @@
+import UID from 'uid-safe';
+
+import type {
+  AuthProfile,
+  AuthProfileAndToken,
+} from '../../../auth/auth-types';
+
 const SEVEN_DAYS_IN_MS = 7 * 24 * 60 * 60 * 1000;
 export const getSevenDaysAgoISOString = () =>
   new Date(Date.now() - SEVEN_DAYS_IN_MS).toISOString();
+
+export function getAuthProfileAndTokenWithoutSession(
+  profileId: AuthProfile['id']
+): AuthProfileAndToken {
+  const BYTE_LENGTH = 16;
+  const authProfileAndToken = {
+    profile: {
+      authMethod: 'digid',
+      profileType: 'private',
+      sid: `overridden-${UID.sync(BYTE_LENGTH)}}`,
+      id: profileId,
+    } as const,
+    token: 'notprovided',
+    expiresAtMilliseconds: 1,
+  };
+  return authProfileAndToken;
+}
