c71b25844fed7ac7520b7fbcdfdf824ed543b765
Roan Paulus <roan.paulus@amsterdam.nl>
2026-03-16 13:58:25 +0100

    refactor and add tests

--- src/client/components/DocumentList/DocumentLink.test.tsx
+++ src/client/components/DocumentList/DocumentLink.test.tsx
@@ -11,46 +11,55 @@ import { trackDownload } from '../../hooks/analytics.hook';
 
 vi.mock('../../hooks/analytics.hook');
 
-const DOCS: Record<string, GenericDocument> = {
-  uitkeringSpecificatieA: {
-    datePublished: '2019-03-23T00:00:00+01:00',
-    id: '24078091',
-    title: 'Uitkeringsspecificatie',
-    url: `${bffApiHost}/wpi/document?id=24078091&isBulk=false&isDms=false`,
-  },
-  uitkeringSpecificatieB: {
-    datePublished: '2014-01-24T00:00:00+01:00',
-    id: '30364921',
-    title: 'Uitkeringsspecificatie',
-    url: `${bffApiHost}/wpi/document?id=30364921&isBulk=false&isDms=false`,
-  },
+const downloadDocumentPath =
+  '/wpi/document?id=24078091&isBulk=false&isDms=false';
+
+const docWithoutExtension: GenericDocument = {
+  datePublished: '2019-03-23T00:00:00+01:00',
+  id: '24078091',
+  title: 'Uitkeringsspecificatie',
+  url: `${bffApiHost}${downloadDocumentPath}`,
+};
+
+const docx: GenericDocument = {
+  datePublished: '2019-03-23T00:00:00+01:00',
+  id: '24078091',
+  title: 'Uitkeringsspecificatie',
+  url: `${bffApiHost}${downloadDocumentPath}`,
+  filename: '12345.docx',
 };
 
 beforeEach(() => {
   vi.resetAllMocks();
 });
-test('Handles multiple file extensions', () => {});
+
+test('Displays file extension if specified', () => {
+  const screen = render(<DocumentLink document={docx} />);
+  screen.getByText(new RegExp('docx'));
+});
+
+test('Displays no file extension when not specified', () => {
+  const screen = render(<DocumentLink document={docWithoutExtension} />);
+  screen.getByText(docWithoutExtension.title);
+});
 
 test('Clicking a link fires tracking call', async () => {
   const user = userEvent.setup();
   (trackDownload as Mock).mockReturnValue(null);
 
-  bffApi
-    .get('/wpi/document?id=24078091&isBulk=false&isDms=false')
-    .reply(200, 'x');
+  bffApi.get(downloadDocumentPath).reply(200, 'x');
 
-  const doc = DOCS.uitkeringSpecificatieA;
-  render(<DocumentLink document={doc} />);
+  render(<DocumentLink document={docWithoutExtension} />);
 
-  const downloadLink = screen.getByText(doc.title);
+  const downloadLink = screen.getByText(docWithoutExtension.title);
 
   await user.click(downloadLink);
   await waitFor(() =>
     expect(trackDownload).toHaveBeenCalledWith(
-      doc.title,
+      docWithoutExtension.title,
       'pdf',
       // The additional leading / is representing window.location.pathname
-      '//downloads/' + doc.title + '.pdf',
+      '//downloads/' + docWithoutExtension.title + '.pdf',
       'private'
     )
   );
@@ -59,27 +68,24 @@ test('Clicking a link fires tracking call', async () => {
 test('trackPath function is used to create the link sent to the tracking call', async () => {
   const user = userEvent.setup();
   (trackDownload as Mock).mockReturnValue(null);
-  const doc = DOCS.uitkeringSpecificatieA;
 
-  bffApi
-    .get('/wpi/document?id=24078091&isBulk=false&isDms=false')
-    .reply(200, 'x');
+  bffApi.get(downloadDocumentPath).reply(200, 'x');
 
   render(
     <DocumentLink
-      document={doc}
+      document={docWithoutExtension}
       trackPath={() => {
         return '/compleet/ander/pad.pdf';
       }}
     />
   );
 
-  const downloadLink = screen.getByText(doc.title);
+  const downloadLink = screen.getByText(docWithoutExtension.title);
   await user.click(downloadLink);
 
   await waitFor(() =>
     expect(trackDownload).toHaveBeenCalledWith(
-      doc.title,
+      docWithoutExtension.title,
       'pdf',
       // The additional leading / is representing window.location.pathname
       '/compleet/ander/pad.pdf',
@@ -92,15 +98,12 @@ test('Clicking a link does not fire a tracking call when the link returns a 404 
   const user = userEvent.setup();
 
   const captureException = vi.spyOn(Monitoring, 'captureException');
-  const doc = DOCS.uitkeringSpecificatieA;
 
-  bffApi
-    .get('/wpi/document?id=24078091&isBulk=false&isDms=false')
-    .reply(404, 'not found');
+  bffApi.get(downloadDocumentPath).reply(404, 'not found');
 
-  render(<DocumentLink document={doc} />);
+  render(<DocumentLink document={docWithoutExtension} />);
 
-  const downloadLink = screen.getAllByText(doc.title)[0];
+  const downloadLink = screen.getAllByText(docWithoutExtension.title)[0];
   await user.click(downloadLink);
 
   await waitFor(() => expect(trackDownload).not.toHaveBeenCalled());
@@ -110,8 +113,8 @@ test('Clicking a link does not fire a tracking call when the link returns a 404 
       new Error(`Failed to download document. Code: 404`),
       {
         properties: {
-          title: doc.title,
-          url: doc.url,
+          title: docWithoutExtension.title,
+          url: docWithoutExtension.url,
         },
       }
     )
