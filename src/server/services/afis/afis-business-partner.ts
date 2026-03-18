08d362738732c4f88311c6d70b0cd3d9c4b15aa9
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-03-18 17:22:20 +0100

    Add additional information
    # Conflicts:
    #	src/server/services/afis/afis-business-partner.ts

--- src/server/services/afis/afis-business-partner.ts
+++ src/server/services/afis/afis-business-partner.ts
@@ -26,6 +26,7 @@ import type {
   ApiResponse_DEPRECATED} from '../../../universal/helpers/api.ts';
 import {
   apiErrorResult,
+  ApiResponse_DEPRECATED,
   apiSuccessResult,
   getFailedDependencies,
   getSettledResult,
@@ -273,6 +274,8 @@ export async function createBusinessPartnerBankAccount(
     IBAN: payload.iban,
     BankAccount: iban.accountNumber,
     BankCountryKey: iban.countryCode ?? '',
+    CollectionAuthInd: true,
+    BankAccountReferenceText: `Bankrekening toegevoegd via Mijn Amsterdam voor E-Manaat afdeling ${payload.creditorName ?? 'onbekend'}.`,
   };
 
   const additionalConfig: DataRequestConfig = {
@@ -294,19 +297,26 @@ export async function createBusinessPartnerBankAccount(
 export async function fetchCheckIfIBANexists(
   IBAN: AfisBusinessPartnerBankAccount['IBAN'],
   businessPartnerID: BusinessPartnerId
-): Promise<ApiResponse<boolean>> {
+): Promise<ApiResponse<AfisEMandateBankAccountExistsResponse>> {
   const additionalConfig: DataRequestConfig = {
     formatUrl(config) {
-      return `${config.url}/API/ZAPI_BUSINESS_PARTNER_DET_SRV/A_BusinessPartnerBank?$filter=IBAN eq '${IBAN}' and BusinessPartner eq '${businessPartnerID}'`;
+      return `${config.url}/API/ZAPI_BUSINESS_PARTNER_DET_SRV/A_BusinessPartnerBank?$filter=IBAN eq '${IBAN}' and BusinessPartner eq '${businessPartnerID}'&$orderBy=BankIdentification desc`;
     },
     transformResponse(
       response: AfisApiFeedResponseSource<AfisBusinessPartnerBankAccount>
     ) {
-      return !!getFeedEntryProperties(response).length;
+      return {
+        exists: !!getFeedEntryProperties(response).length,
+        eMandateCollectionEnabled: !!getFeedEntryProperties(response).findLast(
+          (entry) => entry.CollectionAuthInd === true
+        ),
+      };
     },
   };
 
   const businessPartnerRequestConfig = await getAfisApiConfig(additionalConfig);
 
-  return requestData<boolean>(businessPartnerRequestConfig);
+  return requestData<AfisEMandateBankAccountExistsResponse>(
+    businessPartnerRequestConfig
+  );
 }
