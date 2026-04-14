a1ce0394a0c10fe1654610acf916f6041c5084ef
Terry van Walen <t.van.walen@amsterdam.nl>
2026-04-14 10:46:47 +0200

    Remove unused file

--- src/server/services/vergunningen/vergunningen-service-config.ts
+++
@@ -1,6 +0,0 @@
-import { isEnabled } from '../../config/azure-appconfiguration.ts';
-
-export const featureToggle = {
-  // VTH zaken are fetched from Powerbrowser instead of Decos
-  vthOnPowerbrowserActive: isEnabled('VERGUNNINGEN.VTHOnPowerbrowserActive'),
-};
