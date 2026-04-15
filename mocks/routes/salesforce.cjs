cbeeac87c4ccc7cf087f6b4eab669f95cdf14961
Roan Paulus <roan.paulus@amsterdam.nl>
2026-04-15 16:05:44 +0200

    add mock data

--- mocks/routes/salesforce.cjs
+++ mocks/routes/salesforce.cjs
@@ -1,9 +1,10 @@
-const SALESFORCE_RESPONSE = require('../fixtures/salesforce-contactmomenten.json');
+const KLANTCONTACTEN_RESPONSE = require('../fixtures/salesforce-contactmomenten.json');
+const AFSPRAKEN_STADSLOKET_RESPONSE = require('../fixtures/salesforce-afspraken-stadsloken.json');
 const settings = require('../settings.cjs');
 
 module.exports = [
   {
-    id: 'get-salesforce',
+    id: 'get-salesforce-klantcontacten',
     url: `${settings.MOCK_BASE_PATH}/salesforce/contactmomenten/services/apexrest/klantinteracties/v1.0/klantcontacten/`,
     method: 'GET',
     variants: [
@@ -12,7 +13,22 @@ module.exports = [
         type: 'json',
         options: {
           status: 200,
-          body: SALESFORCE_RESPONSE,
+          body: KLANTCONTACTEN_RESPONSE,
+        },
+      },
+    ],
+  },
+  {
+    id: 'get-salesforce-afspraken-stadsloket',
+    url: `${settings.MOCK_BASE_PATH}/salesforce/contactmomenten/services/apexrest/klantinteracties/v1.0/appointments`,
+    method: 'GET',
+    variants: [
+      {
+        id: 'standard',
+        type: 'json',
+        options: {
+          status: 200,
+          body: AFSPRAKEN_STADSLOKET_RESPONSE,
         },
       },
     ],
