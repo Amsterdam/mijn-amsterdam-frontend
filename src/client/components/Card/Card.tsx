9b99cb942ca1f59c9d6f153c849a345d3b83a507
Roan Paulus <roan.paulus@amsterdam.nl>
2026-04-22 16:45:23 +0200

    first iteration for both mobile and desktop

--- src/client/components/Card/Card.tsx
+++ src/client/components/Card/Card.tsx
@@ -12,16 +12,25 @@ type CardProps = {
 
 export function Card({ title, icon, children, actionRightside }: CardProps) {
   const isSmallScreen = useSmallScreen();
+  const isLargeScreen = !isSmallScreen;
+
   const icon_ = icon && <Icon svg={icon} size={'heading-2'}></Icon>;
+  const heading = <Heading level={3}>{title}</Heading>;
   return (
     <div
       className={'ams-mb-m'}
       style={{ paddingTop: '16px', borderTop: '2px solid #EEEEEE' }}
     >
       <Row gap={'large'}>
-        {isSmallScreen || icon_}
+        {isLargeScreen && icon_}
         <Column style={{ width: isSmallScreen ? '' : '800px' }}>
-          <Heading level={3}>{title}</Heading>
+          {isLargeScreen && heading}
+          {isSmallScreen && (
+            <Row>
+              {icon_}
+              {heading}
+            </Row>
+          )}
           {children}
           {isSmallScreen && actionRightside}
         </Column>
