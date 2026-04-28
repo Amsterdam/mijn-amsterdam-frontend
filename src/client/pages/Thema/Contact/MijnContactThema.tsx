8c24fe96b2a9568f43ae447cf9000a576bfa0261
Roan Paulus <roan.paulus@amsterdam.nl>
2026-04-28 17:39:30 +0200

    wip: ical

--- src/client/pages/Thema/Contact/MijnContactThema.tsx
+++ src/client/pages/Thema/Contact/MijnContactThema.tsx
@@ -13,6 +13,92 @@ import ThemaPagina from '../../../components/Thema/ThemaPagina.tsx';
 import ThemaPaginaTable from '../../../components/Thema/ThemaPaginaTable.tsx';
 import { useHTMLDocumentTitle } from '../../../hooks/useHTMLDocumentTitle.ts';
 
+import React, { type ReactNode } from 'react';
+
+type ICALProps = {
+  children?: ReactNode;
+  // Format: use toICALDateTimeString(): YYYYMMDDTHHMMSSZ
+  start: Date;
+  end: Date;
+  uid: string;
+  summary: string;
+  description: string;
+  // Free form location field. Example: Amsterdam City Hall, Amstel 1, 1011 PN Amsterdam, Netherlands, Room 101
+  location: string;
+};
+
+function leftPadWithZero(num: string): string {
+  return num.length === 1 ? `0${num}` : num;
+}
+
+function toICALDateTimeString(date: Date): string {
+  const year = date.getUTCFullYear().toString();
+  const month = leftPadWithZero(date.getUTCMonth().toString());
+  const day = leftPadWithZero(date.getUTCDate().toString());
+
+  const hours = leftPadWithZero(date.getUTCHours().toString());
+  const minutes = leftPadWithZero(date.getUTCMinutes().toString());
+  const seconds = leftPadWithZero(date.getUTCSeconds().toString());
+
+  return `${year}${month}${day}T${hours}${minutes}${seconds}Z`;
+}
+
+function LinkICAL({
+  children,
+  start,
+  end,
+  uid,
+  summary,
+  description,
+  location,
+}: ICALProps) {
+  function handleDownload(event: React.MouseEvent<HTMLAnchorElement>) {
+    event.preventDefault();
+
+    const icalContent = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Amsterdam//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:${uid}
+DTSTAMP:${toICALDateTimeString(new Date())}
+DTSTART:${toICALDateTimeString(start)}
+DTEND:${toICALDateTimeString(end)}
+SUMMARY:${summary}
+DESCRIPTION:${description}
+LOCATION:${location}
+END:VEVENT
+END:VCALENDAR`;
+
+    const blob = new Blob([icalContent], {
+      type: 'text/calendar;charset=utf-8',
+    });
+
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'event.ics';
+
+    // Trigger the download
+    link.click();
+
+    // Clean up the URL object
+    URL.revokeObjectURL(url);
+  }
+
+  return (
+    <div>
+      <a href="#" onClick={handleDownload}>
+        {children}
+      </a>
+    </div>
+  );
+}
+
+function setHourMinutes(date: Date, hourMinutes: string): void {
+  const [hours, minutes] = hourMinutes.split(':');
+  date.setHours(parseInt(hours, 10), parseInt(minutes, 10));
+}
+
 export function MijnContactThema() {
   const {
     id,
@@ -44,6 +130,10 @@ export function MijnContactThema() {
   );
 
   const appointmentCards = data?.appointments.map((a) => {
+    const startTime = new Date(a.appointmentDate);
+    setHourMinutes(startTime, a.startTime);
+    const endTime = new Date(a.appointmentDate);
+    setHourMinutes(endTime, a.endTime);
     return (
       <div key={a.caseReference}>
         <Card
@@ -57,9 +147,16 @@ export function MijnContactThema() {
         >
           <Paragraph>{`Datum, ${a.appointmentDateFormatted}, ${a.startTime}-${a.endTime} uur`}</Paragraph>
           <Paragraph>{`Locatie Stadsloket ${a.location.name}, ${a.location.street}`}</Paragraph>
-          <Paragraph className="ams-mb-s">
+          <LinkICAL
+            start={startTime}
+            end={endTime}
+            uid={a.caseReference}
+            summary={`Afspraak voor ${a.subject}`}
+            description={`Referentienummer: ${a.caseReference}`}
+            location={`Stadsloket ${a.location.name}, ${a.location.street}, ${a.location.postalCode} ${a.location.city}, Nederland`}
+          >
             Voeg toe aan uw privé agenda
-          </Paragraph>
+          </LinkICAL>
           <MaRouterLink
             maVariant="noUnderline"
             href={generatePath(themaConfig.appointmentQRCode.route.path, {
