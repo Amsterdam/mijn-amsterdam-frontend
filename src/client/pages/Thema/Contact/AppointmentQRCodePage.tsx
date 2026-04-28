d477c76d7dbf7a30fd5490f3da37c4c0bff30344
Roan Paulus <roan.paulus@amsterdam.nl>
2026-04-28 11:00:21 +0200

    fill placeholder appointment text

--- src/client/pages/Thema/Contact/AppointmentQRCodePage.tsx
+++ src/client/pages/Thema/Contact/AppointmentQRCodePage.tsx
@@ -1,40 +1,51 @@
-import { useHTMLDocumentTitle } from '../../../hooks/useHTMLDocumentTitle.ts';
-import { useKlantcontactData } from './useKlantcontactData.hook.tsx';
-import { useThemaBreadcrumbs } from '../../../hooks/useThemaMenuItems.ts';
-import { PageContentCell, PageV2 } from '../../../components/Page/Page.tsx';
 import { Paragraph } from '@amsterdam/design-system-react';
-
 import QRCode from 'react-qr-code';
-import ErrorAlert from '../../../components/Alert/Alert.tsx';
 import { useParams } from 'react-router';
 
 import styles from './AppointmentQRCodePage.module.scss';
+import { useKlantcontactData } from './useKlantcontactData.hook.tsx';
+import ErrorAlert from '../../../components/Alert/Alert.tsx';
+import { PageContentCell, PageV2 } from '../../../components/Page/Page.tsx';
+import { useHTMLDocumentTitle } from '../../../hooks/useHTMLDocumentTitle.ts';
+import { useThemaBreadcrumbs } from '../../../hooks/useThemaMenuItems.ts';
 
 export function AppointmentQRCodePage() {
-  const { id, themaConfig } = useKlantcontactData();
+  const { id, themaConfig, data } = useKlantcontactData();
   const routeConfig = themaConfig.appointmentQRCode.route;
   useThemaBreadcrumbs(id);
   useHTMLDocumentTitle(routeConfig);
 
   const { qrcode } = useParams();
 
-  // RP TODO: location data, replace all.
-  const stadsloketName = 'PLACEHOLDER';
+  const a = data?.appointments.find((a) => {
+    return a.qrCode === qrcode;
+  });
+
+  const stadsloketName = a?.location.name;
   const paragraphMargin = 'ams-mb-m';
   return (
     <>
       <PageV2 heading={`Uw afspraak bij stadsloket ${stadsloketName}`}>
         <PageContentCell>
-          <Paragraph className={paragraphMargin}>
-            {`U heeft een afspraak bij het stadsloket Stadsloket ${stadsloketName} Buikslotermeerplein 2000, 1025 XL Amsterdam.`}
-          </Paragraph>
-          <Paragraph className={paragraphMargin}>
-            <b>{`Datum, 4 augustus 2025, 13-14 uur`}</b>
-          </Paragraph>
-          <Paragraph className={paragraphMargin}>
-            Scan deze QR code op het stadsloket zodat de medewerker weet dat u
-            op het stadsloket aanwezig bent.
-          </Paragraph>
+          {a ? (
+            <>
+              <Paragraph className={paragraphMargin}>
+                {`U heeft een afspraak bij het stadsloket Stadsloket ${stadsloketName}, ${a.location.street}, ${a.location.postalCode} ${a.location.city}.`}
+              </Paragraph>
+              <Paragraph className={paragraphMargin}>
+                <b>{`Datum, ${a.appointmentDateFormatted}, ${a.startTime}-${a.endTime} uur`}</b>
+              </Paragraph>
+              <Paragraph className={paragraphMargin}>
+                Scan deze QR code op het stadsloket zodat de medewerker weet dat
+                u op het stadsloket aanwezig bent.
+              </Paragraph>
+            </>
+          ) : (
+            <ErrorAlert>
+              Er ging iets fout met het opzoeken van uw afspraak, probeer het
+              later opnieuw.
+            </ErrorAlert>
+          )}
         </PageContentCell>
         <PageContentCell className={styles['center-elements']}>
           {qrcode ? (
