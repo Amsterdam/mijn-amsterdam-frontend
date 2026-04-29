446ff303070a1060043dee7d6959c994101346e4
Roan Paulus <123072030+RoanPaulus@users.noreply.github.com>
2026-04-29 13:12:41 +0200

    Update mocks/routes/salesforce.cjs
    Co-authored-by: Tim van Oostrom <t.van.oostrom@amsterdam.nl>

--- mocks/routes/salesforce.cjs
+++ mocks/routes/salesforce.cjs
@@ -4,7 +4,7 @@ const settings = require('../settings.cjs');
 
 module.exports = [
   {
-    id: 'get-salesforce-klantcontacten',
+    id: 'get-salesforce-contactmomenten',
     url: `${settings.MOCK_BASE_PATH}/salesforce/contactmomenten/services/apexrest/klantinteracties/v1.0/klantcontacten/`,
     method: 'GET',
     variants: [
