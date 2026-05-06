bf5262e58addde09204e692150024535221ca2ad
Roan Paulus <roan.paulus@amsterdam.nl>
2026-05-06 13:22:50 +0200

    refactor, improve readabilty for small/large screens

--- src/client/components/AfspraakCard/AfspraakCard.tsx
+++ src/client/components/AfspraakCard/AfspraakCard.tsx
@@ -24,7 +24,6 @@ type AfspraakCardProps = {
 
 export function AfspraakCard({ afspraak, className }: AfspraakCardProps) {
   const isSmallScreen = useSmallScreen();
-  const isLargeScreen = !isSmallScreen;
 
   const TitleHeading: React.FC<{ className?: string }> = ({ className }) => (
     <Heading level={3} className={className ?? ''}>
@@ -32,29 +31,52 @@ export function AfspraakCard({ afspraak, className }: AfspraakCardProps) {
     </Heading>
   );
 
-  const cancellationLink = (
-    <MaLink
-      className={isSmallScreen ? styles.CancellationLink : ''}
-      href={afspraak.cancellationLink}
-    >
+  const CancellationLink: React.FC<{ className?: string }> = ({
+    className,
+  }) => (
+    <MaLink className={className} href={afspraak.cancellationLink}>
       Annuleren
     </MaLink>
   );
 
   const icon = <Icon svg={PersonAtDeskIcon} size="heading-2"></Icon>;
 
-  return (
-    <Row className={className} gap="large">
-      {isLargeScreen && icon}
-      <Column
-        className={
-          isLargeScreen
-            ? styles.ColumnLargeScreen /* Make sure `cancellationLink` is always aligned */
-            : ''
-        }
+  const body = (
+    <>
+      <Paragraph>{afspraak.displayDate}</Paragraph>
+      {/* Without this div the LocationModal is not clickable. */}
+      <div>
+        <LocationModal
+          address={`${afspraak.location.street}`}
+          className={classNames(
+            'ams-link',
+            maLinkStyles.MaLink,
+            styles.LocationModalLink
+          )}
+        >
+          {`Locatie Stadsloket ${afspraak.location.name}, ${afspraak.location.street}`}
+        </LocationModal>
+      </div>
+      <CalendarLink
+        className={classNames(styles.Block, 'ams-mb-s')}
+        icsData={{
+          start: afspraak.startDate,
+          end: afspraak.endDate,
+          uid: `afspraak-stadsloket-${afspraak.caseReference}`,
+          summary: `Afspraak voor ${afspraak.subject}`,
+          description: `Referentienummer: ${afspraak.caseReference}`,
+          location: `Stadsloket ${afspraak.location.name}, ${afspraak.location.street}, ${afspraak.location.postalCode} ${afspraak.location.city}, Nederland`,
+        }}
       >
-        {isLargeScreen && <TitleHeading></TitleHeading>}
-        {isSmallScreen && (
+        Voeg toe aan uw privé agenda
+      </CalendarLink>
+    </>
+  );
+
+  if (isSmallScreen) {
+    return (
+      <Row className={className}>
+        <Column>
           <Row>
             {
               <TitleHeading
@@ -63,42 +85,36 @@ export function AfspraakCard({ afspraak, className }: AfspraakCardProps) {
             }
             {icon}
           </Row>
-        )}
-        <Paragraph>{afspraak.displayDate}</Paragraph>
-        {/* Without this div the LocationModal is not clickable. */}
-        <div>
-          <LocationModal
-            address={`${afspraak.location.street}`}
-            className={classNames(
-              'ams-link',
-              maLinkStyles.MaLink,
-              styles.LocationModalLink
-            )}
-          >
-            {`Locatie Stadsloket ${afspraak.location.name}, ${afspraak.location.street}`}
-          </LocationModal>
-        </div>
-        <CalendarLink
-          className={classNames(styles.Block, 'ams-mb-s')}
-          icsData={{
-            start: afspraak.startDate,
-            end: afspraak.endDate,
-            uid: `afspraak-stadsloket-${afspraak.caseReference}`,
-            summary: `Afspraak voor ${afspraak.subject}`,
-            description: `Referentienummer: ${afspraak.caseReference}`,
-            location: `Stadsloket ${afspraak.location.name}, ${afspraak.location.street}, ${afspraak.location.postalCode} ${afspraak.location.city}, Nederland`,
-          }}
-        >
-          Voeg toe aan uw privé agenda
-        </CalendarLink>
+          {body}
+          <Row alignVertical="center">
+            <MaRouterLink maVariant="noUnderline" href={afspraak.qrCodeHref}>
+              <Button variant="secondary">Toon QR code</Button>
+            </MaRouterLink>
+            <CancellationLink
+              className={styles.SmallScreenCancellationLink}
+            ></CancellationLink>
+          </Row>
+        </Column>
+      </Row>
+    );
+  }
+  return (
+    <Row className={className} gap="large">
+      {icon}
+      <Column
+        className={
+          styles.ColumnLargeScreen /* Make sure `cancellationLink` is always in the same spot for alignment. */
+        }
+      >
+        <TitleHeading></TitleHeading>
+        {body}
         <Row alignVertical="center">
           <MaRouterLink maVariant="noUnderline" href={afspraak.qrCodeHref}>
             <Button variant="secondary">Toon QR code</Button>
           </MaRouterLink>
-          {isSmallScreen && cancellationLink}
         </Row>
       </Column>
-      {isLargeScreen && cancellationLink}
+      <CancellationLink></CancellationLink>
     </Row>
   );
 }
