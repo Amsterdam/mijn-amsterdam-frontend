36e9e6b1b910c50b59c33c42b8afa8f738824812
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-05-21 16:20:34 +0200

    Use own logger

--- src/mocks-server/logger.ts
+++ src/mocks-server/logger.ts
@@ -1,3 +1,13 @@
-import { logger as appLogger } from '../server/logging.ts';
+import pino from 'pino';
 
-export const logger = appLogger;
+const LOG_LEVEL = process.env.LOG_LEVEL;
+
+const options: pino.LoggerOptions = {
+  enabled: !!LOG_LEVEL,
+  level: LOG_LEVEL,
+  transport: {
+    target: 'pino-pretty',
+  },
+};
+
+export const logger = pino(options);
