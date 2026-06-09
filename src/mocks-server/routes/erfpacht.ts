81f115b53ae8d66f89a5fee033a0b5df03049c09
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-06-09 12:32:47 +0200

    Update routes

--- src/mocks-server/routes/erfpacht.ts
+++ src/mocks-server/routes/erfpacht.ts
@@ -1,6 +1,9 @@
-import ERFPACHT_V2_DOSSIER_INFO_DETAILS from '../fixtures/erfpacht-v2-dossierinfo-bsn.json' with { type: 'json' };
-import ERFPACHT_V2_DOSSIERS from '../fixtures/erfpacht-v2-dossiers.json' with { type: 'json' };
-import ERFPACHT_V2_ERFPACHTER from '../fixtures/erfpacht-v2-erfpachter.json' with { type: 'json' };
+import ERFPACHT_V2_DOSSIER_INFO_DETAILS from '../fixtures/erfpacht/erfpacht-v2-dossierinfo-bsn.json' with { type: 'json' };
+import ERFPACHT_V2_DOSSIERS from '../fixtures/erfpacht/erfpacht-v2-dossiers.json' with { type: 'json' };
+import ERFPACHT_V2_ERFPACHTER from '../fixtures/erfpacht/erfpacht-v2-erfpachter.json' with { type: 'json' };
+import ERFPACHT_ZAAK_DETAIL from '../fixtures/erfpacht/erfpacht-zaak-detail.json' with { type: 'json' };
+import ERFPACHT_ZAAK_INFO from '../fixtures/erfpacht/erfpacht-zaak-info.json' with { type: 'json' };
+import ERFPACHT_ZAAK_STATUSSEN from '../fixtures/erfpacht/erfpacht-zaak-statussen.json' with { type: 'json' };
 import { MOCK_BASE_PATH } from '../settings.ts';
 import type { MockRouteDefinition } from '../types.ts';
 
@@ -38,6 +41,36 @@ export const routes: MockRouteDefinition[] = [
           dossierNummer,
         });
       },
+    },
+  },
+  {
+    id: 'get-erfpacht-zaakinfo',
+    url: `${MOCK_BASE_PATH}/erfpachtv2/vernise/api/zaakinfo`,
+    method: 'GET',
+    handler: {
+      type: 'json',
+      status: 200,
+      body: ERFPACHT_ZAAK_INFO,
+    },
+  },
+  {
+    id: 'get-erfpacht-zaak-detail',
+    url: `${MOCK_BASE_PATH}/erfpachtv2/vernise/api/ozgv/zaak/:uuid`,
+    method: 'GET',
+    handler: {
+      type: 'json',
+      status: 200,
+      body: ERFPACHT_ZAAK_DETAIL,
+    },
+  },
+  {
+    id: 'get-erfpacht-zaak-statussen',
+    url: `${MOCK_BASE_PATH}/erfpachtv2/vernise/api/ozgv/statussen`,
+    method: 'GET',
+    handler: {
+      type: 'json',
+      status: 200,
+      body: ERFPACHT_ZAAK_STATUSSEN,
     },
   },
 ];
