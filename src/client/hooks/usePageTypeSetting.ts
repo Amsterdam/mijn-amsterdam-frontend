637ab5245146787e95c63710d511c2eac6b13692
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-06-23 10:27:34 +0200

    Add to dep array

--- src/client/hooks/usePageTypeSetting.ts
+++ src/client/hooks/usePageTypeSetting.ts
@@ -21,7 +21,7 @@ export function usePageTypeSetting(pageTypeRequested: PageTypeSetting) {
     return () => {
       setPageType('none');
     };
-  }, [pageTypeRequested]);
+  }, [pageTypeRequested, setPageType]);
 
   return pageType;
 }
