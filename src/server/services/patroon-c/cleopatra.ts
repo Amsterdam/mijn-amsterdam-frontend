490ba2bc97e205626c7c54a1eef6b259a9f92f1b
Roan Paulus <roan.paulus@amsterdam.nl>
2026-06-26 12:32:08 +0200

    Transfer to new jose

--- src/server/services/patroon-c/cleopatra.ts
+++ src/server/services/patroon-c/cleopatra.ts
@@ -1,4 +1,5 @@
-import jose from 'node-jose';
+import type { KeyLike } from 'jose';
+import { importX509, importJWK, EncryptJWT, type JWK } from 'jose';
 
 import type { ApiPatternResponseA } from './api-service.ts';
 import { fetchService } from './api-service.ts';
@@ -20,33 +21,34 @@ import {
 } from '../../helpers/source-api-helpers.ts';
 import { logger } from '../../logging.ts';
 
-const DEV_KEY = {
+const alg = 'RSA-OAEP-256';
+
+const DEV_KEY: JWK = {
   kty: 'RSA',
   kid: 'xxx',
   x5t: '8zMcftkkR9RYIvhvu4UrfAo0IaM',
   n: 'uTBrbjUH8Ry0A-dt9v0NJon3HPfo-b1ZKrW1eLUUChOUVop1qhlRck_IMx1o3zVETqe_nOmwHp4yC6NQNUfYWnHQfu858Zr-zfDpI1h1cjlgv5pdorJDxTDydNMwvYQIbbeblKWavGWaz7Pq3SK_AwACxIlcoJ09wNPKi2E3zSQBI0RpVQcYs38mtCa4iOT5DEbn7gibBVbtXxOC3NeWvhMXEJVeqAXWA0M2Df1qHGLo1gLund1hDR4rTQf4h62PSpVp8G3Hoo1zGpZlJJJo1RB2JNKqCIZkjs3f80ZhEP9_IIC0CgZdsyqpltf4ygsG_TycxgEuiAy1bVrDSy82xaZleV1hvjHrnlIAHNTfnNebmCMPzahrcExDjSMQbXWHWd00r5cs2E1YAv9iYox-MFflaUN0tto76GhsASnDC_V40mWLaRFEymIfanIgLZFtViW2kCBQJOEUtWrC3weLx_iTQQtvbSPhx-ayQQCpfKU_vShfkxUeqAcf5yoZDae3uqGoo-biCZMFDFo5i74biwvz-AQ5lRtsMRgQCpGDaHjQJ6P7pJeQlhrQhMwxvePGaUW0XcJ0l3C4YuNtLLHFtbxw3DieQRMbAkmpSzideOg9tIesCbAca8EvNipB3PowQ4TpZXPkK9HxsWL681_YRcu-QxCT79hhQJ9FDPbqbes',
   e: 'AQAB',
+  alg,
 };
 
-function getPublicKey() {
-  const keystore = jose.JWK.createKeyStore();
-  let certContent;
-
-  try {
-    if (IS_TAP) {
+function getPublicKey(): Promise<KeyLike | Uint8Array<ArrayBufferLike> | null> {
+  if (IS_TAP) {
+    let certContent;
+    try {
       certContent = getCert('BFF_CLEOPATRA_PUBLIC_KEY_CERT');
+    } catch (error) {
+      logger.error(error, 'Error getting public key');
     }
-  } catch (error) {
-    logger.error(error, 'Error getting public key');
+
+    if (certContent) {
+      return importX509(certContent, alg);
+    }
+
+    return Promise.resolve(null);
   }
 
-  const pemPubKey = !IS_TAP
-    ? keystore.add(DEV_KEY, 'json')
-    : certContent
-      ? keystore.add(certContent, 'pem')
-      : Promise.resolve(undefined);
-
-  return pemPubKey;
+  return importJWK(DEV_KEY, alg);
 }
 
 export function getJSONRequestPayload(
@@ -65,25 +67,13 @@ export function getJSONRequestPayload(
 
 export async function encryptPayload(payload: CleopatraRequestPayloadString) {
   const key = await getPublicKey();
-
-  if (key) {
-    return jose.JWE.createEncrypt(
-      {
-        format: 'flattened',
-        fields: {
-          alg: 'RSA-OAEP-256',
-          enc: 'A256CBC-HS512',
-          typ: 'JWE',
-          kid: key.kid,
-        },
-      },
-      key
-    )
-      .update(payload)
-      .final();
+  if (!key) {
+    return null;
   }
-
-  return null;
+  const jwt = new EncryptJWT({ payload })
+    .setProtectedHeader({ alg, enc: 'A256CBC-HS512', typ: 'JWE', kid: 'xxx' })
+    .encrypt(key);
+  return jwt;
 }
 
 interface CleopatraMessage {
