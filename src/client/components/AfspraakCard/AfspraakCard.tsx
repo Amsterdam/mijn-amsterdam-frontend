73685ce2301eb04eaddc5451e873d20ea567a696
jasper-ams <j.massink@amsterdam.nl>
2026-07-09 15:04:06 +0200

    refactor(afspraakcard): standaardiseer structuur

--- src/client/components/AfspraakCard/AfspraakCard.tsx
+++ src/client/components/AfspraakCard/AfspraakCard.tsx
@@ -3,9 +3,10 @@ import {
   Heading,
   Icon,
   Paragraph,
+  Row,
+  Column,
 } from '@amsterdam/design-system-react';
 import { PersonAtDeskIcon } from '@amsterdam/design-system-react-icons';
-import classNames from 'classnames';
 import QRCode from 'react-qr-code';
 
 import styles from './AfspraakCard.module.scss';
@@ -26,67 +27,74 @@ export function AfspraakCard({
   compact,
 }: AfspraakCardProps) {
   return (
-    <article className={classNames(styles.AfspraakCard, className)}>
-      <Icon
-        className={styles.Icon}
-        svg={PersonAtDeskIcon}
-        hidden
-        size="heading-2"
-      />
-      <Heading level={3} size="level-3">
-        {afspraak.subject}
-      </Heading>
-      <Paragraph>
-        Datum:{' '}
-        <time dateTime={afspraak.dateStart}>{afspraak.displayDateTime}</time>
-      </Paragraph>
-      <Paragraph>
-        Locatie: Stadsloket {afspraak.location.name}, {afspraak.location.street}
-      </Paragraph>
-      {!compact && (
-        <>
-          <ActionGroup style={{ paddingTop: 'var(--ams-space-s)' }}>
-            <ModalAndButton
-              modal={{
-                title: `QR code - Stadsloket ${afspraak.location.name}`,
-              }}
-              buttonVariant="secondary"
-              buttonLabel="Toon QR code"
-            >
-              <>
-                <QRCode
-                  size={256}
-                  value={afspraak.qrCode}
-                  className="ams-mb-s"
+    <article className={className}>
+      <Row>
+        <Icon
+          className={styles.Icon}
+          svg={PersonAtDeskIcon}
+          hidden
+          size="heading-2"
+        />
+        <Column>
+          <Heading level={3} size="level-3">
+            {afspraak.subject}
+          </Heading>
+          <Paragraph>
+            Datum:{' '}
+            <time dateTime={afspraak.dateStart}>
+              {afspraak.displayDateTime}
+            </time>
+          </Paragraph>
+          <Paragraph>
+            Locatie: Stadsloket {afspraak.location.name},{' '}
+            {afspraak.location.street}
+          </Paragraph>
+          {!compact && (
+            <>
+              <ActionGroup style={{ paddingTop: 'var(--ams-space-s)' }}>
+                <ModalAndButton
+                  modal={{
+                    title: `QR code - Stadsloket ${afspraak.location.name}`,
+                  }}
+                  buttonVariant="secondary"
+                  buttonLabel="Toon QR code"
+                >
+                  <>
+                    <QRCode
+                      size={256}
+                      value={afspraak.qrCode}
+                      className="ams-mb-s"
+                    />
+                    <Paragraph className="ams-mb-l">
+                      Scan deze QR code op het stadsloket zodat de medewerker
+                      weet dat u op het stadsloket aanwezig bent.
+                    </Paragraph>
+                  </>
+                </ModalAndButton>
+                <LocationModal
+                  address={afspraak.location.street ?? afspraak.location.name}
+                  buttonLabel={'Toon op kaart'}
                 />
-                <Paragraph className="ams-mb-l">
-                  Scan deze QR code op het stadsloket zodat de medewerker weet
-                  dat u op het stadsloket aanwezig bent.
-                </Paragraph>
-              </>
-            </ModalAndButton>
-            <LocationModal
-              address={afspraak.location.street ?? afspraak.location.name}
-              buttonLabel={'Toon op kaart'}
-            />
-            <MaButtonLink
-              variant="tertiary"
-              href={afspraak.icsLink.to}
-              download={afspraak.icsLink.download}
-              rel="noopener noreferrer"
-            >
-              Voeg toe aan uw agenda
-            </MaButtonLink>
-            <MaButtonLink
-              variant="tertiary"
-              rel="noopener noreferrer"
-              href={afspraak.cancellationLink}
-            >
-              Annuleren
-            </MaButtonLink>
-          </ActionGroup>
-        </>
-      )}
+                <MaButtonLink
+                  variant="tertiary"
+                  href={afspraak.icsLink.to}
+                  download={afspraak.icsLink.download}
+                  rel="noopener noreferrer"
+                >
+                  Voeg toe aan uw agenda
+                </MaButtonLink>
+                <MaButtonLink
+                  variant="tertiary"
+                  rel="noopener noreferrer"
+                  href={afspraak.cancellationLink}
+                >
+                  Annuleren
+                </MaButtonLink>
+              </ActionGroup>
+            </>
+          )}
+        </Column>
+      </Row>
     </article>
   );
 }
