3377775a19f03abc3a1235fe91f4b49cb5a8d4cc
jasper-ams <j.massink@amsterdam.nl>
2026-07-09 14:47:28 +0200

    feat(afspraakcard): move buttons

--- src/client/components/AfspraakCard/AfspraakCard.tsx
+++ src/client/components/AfspraakCard/AfspraakCard.tsx
@@ -1,4 +1,9 @@
-import { Heading, Icon, Link, Paragraph } from '@amsterdam/design-system-react';
+import {
+  ActionGroup,
+  Heading,
+  Icon,
+  Paragraph,
+} from '@amsterdam/design-system-react';
 import { PersonAtDeskIcon } from '@amsterdam/design-system-react-icons';
 import classNames from 'classnames';
 import QRCode from 'react-qr-code';
@@ -6,7 +11,7 @@ import QRCode from 'react-qr-code';
 import styles from './AfspraakCard.module.scss';
 import type { AfspraakFrontend } from '../../../server/services/klantcontact/klantcontact.types.ts';
 import { LocationModal } from '../LocationModal/LocationModal.tsx';
-import { MaRouterLink } from '../MaLink/MaLink.tsx';
+import { MaButtonLink, MaRouterLink } from '../MaLink/MaLink.tsx';
 import { ModalAndButton } from '../Modal/Modal.tsx';
 
 type AfspraakCardProps = {
@@ -20,25 +25,6 @@ export function AfspraakCard({
   className,
   compact,
 }: AfspraakCardProps) {
-  const locatie = `Locatie Stadsloket ${afspraak.location.name}, ${afspraak.location.street}`;
-  const details = (
-    <Paragraph>
-      <time dateTime={afspraak.dateStart}>
-        Datum, {afspraak.displayDateTime}
-      </time>
-      {compact && (
-        <>
-          <br />
-          {locatie}
-          <br />
-          <MaRouterLink href={afspraak.link.to}>
-            {afspraak.link.title}
-          </MaRouterLink>
-        </>
-      )}
-    </Paragraph>
-  );
-
   return (
     <article className={classNames(styles.AfspraakCard, className)}>
       <Icon
@@ -50,51 +36,55 @@ export function AfspraakCard({
       <Heading level={3} size="level-3">
         {afspraak.subject}
       </Heading>
-      {details}
-
+      <Paragraph>
+        Datum:{' '}
+        <time dateTime={afspraak.dateStart}>{afspraak.displayDateTime}</time>
+      </Paragraph>
+      <Paragraph>
+        Locatie: Stadsloket {afspraak.location.name}, {afspraak.location.street}
+      </Paragraph>
       {!compact && (
         <>
-          <LocationModal
-            address={afspraak.location.street ?? afspraak.location.name}
-            buttonVariant="ma-link-like"
-            buttonLabel={locatie}
-            buttonClassName={styles.LocationLink}
-          />
-          <br />
-          <Link
-            download={afspraak.icsLink.download}
-            href={afspraak.icsLink.to}
-            rel="noopener noreferrer"
-            className={classNames(styles.ICSLink, 'ams-mb-m')}
-          >
-            {afspraak.icsLink.title}
-          </Link>
-          <br />
-          <span className={styles.CancellationLinkContainer}>
-            <Link
+          <ActionGroup style={{ paddingTop: 'var(--ams-space-s)' }}>
+            <ModalAndButton
+              modal={{
+                title: `QR code - Stadsloket ${afspraak.location.name}`,
+              }}
+              buttonVariant="secondary"
+              buttonLabel="Toon QR code"
+            >
+              <>
+                <QRCode
+                  size={256}
+                  value={afspraak.qrCode}
+                  className="ams-mb-s"
+                />
+                <Paragraph className="ams-mb-l">
+                  Scan deze QR code op het stadsloket zodat de medewerker weet
+                  dat u op het stadsloket aanwezig bent.
+                </Paragraph>
+              </>
+            </ModalAndButton>
+            <LocationModal
+              address={afspraak.location.street ?? afspraak.location.name}
+              buttonLabel={'Toon op kaart'}
+            />
+            <MaButtonLink
+              variant="tertiary"
+              href={afspraak.icsLink.to}
+              download={afspraak.icsLink.download}
               rel="noopener noreferrer"
-              className={classNames(styles.CancellationLink, 'ams-mb-m')}
+            >
+              Voeg toe aan uw agenda
+            </MaButtonLink>
+            <MaButtonLink
+              variant="tertiary"
+              rel="noopener noreferrer"
               href={afspraak.cancellationLink}
             >
               Annuleren
-            </Link>
-            <br />
-          </span>
-          <ModalAndButton
-            modal={{
-              title: `QR code - Stadsloket ${afspraak.location.name}`,
-            }}
-            buttonVariant="secondary"
-            buttonLabel="Toon QR code"
-          >
-            <>
-              <QRCode size={256} value={afspraak.qrCode} className="ams-mb-s" />{' '}
-              <Paragraph className="ams-mb-l">
-                Scan deze QR code op het stadsloket zodat de medewerker weet dat
-                u op het stadsloket aanwezig bent.
-              </Paragraph>
-            </>
-          </ModalAndButton>
+            </MaButtonLink>
+          </ActionGroup>
         </>
       )}
     </article>
