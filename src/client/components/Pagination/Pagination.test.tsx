bb716002475af1654628995eccce748351923311
Terry van Walen <TerryvanWalen@users.noreply.github.com>
2026-07-16 11:26:17 +0200

    MIJN-13134-BUG: Paginering contactmomenten longterm gracefull handler (#2673)

--- src/client/components/Pagination/Pagination.test.tsx
+++ src/client/components/Pagination/Pagination.test.tsx
@@ -7,6 +7,7 @@ import { PaginationV2 } from './PaginationV2.tsx';
 const mocks = vi.hoisted(() => {
   return {
     navigate: vi.fn(),
+    IS_PRODUCTION: false,
   };
 });
 
@@ -18,7 +19,21 @@ vi.mock('react-router', async (importOriginal) => {
   };
 });
 
+vi.mock('../../../universal/config/env.ts', async (importActual) => {
+  return {
+    ...(await importActual()),
+    get IS_PRODUCTION() {
+      return mocks.IS_PRODUCTION;
+    },
+  };
+});
+
 describe('<Pagination />', () => {
+  beforeEach(() => {
+    mocks.navigate.mockReset();
+    mocks.IS_PRODUCTION = false;
+  });
+
   it('Renders without crashing', async () => {
     const user = userEvent.setup();
 
@@ -50,5 +65,68 @@ describe('<Pagination />', () => {
     expect(screen.getAllByRole('link')[2]?.innerText).toBe('Pagina 2');
     await user.click(screen.getAllByRole('link')[3]);
     expect(mocks.navigate).toHaveBeenCalledWith('/thema/lijst/3');
+  });
+
+  it('supports routes ending with optional :page? in production', async () => {
+    const user = userEvent.setup();
+    mocks.IS_PRODUCTION = true;
+
+    render(
+      <PaginationV2
+        totalCount={10}
+        pageSize={2}
+        currentPage={1}
+        path="/mijn-contact/contactmomenten/:page?"
+      />
+    );
+
+    await user.click(screen.getAllByRole('link')[1]);
+    expect(mocks.navigate).toHaveBeenCalledWith(
+      '/mijn-contact/contactmomenten/2'
+    );
+  });
+
+  it('throws for optional :page? outside production', () => {
+    mocks.IS_PRODUCTION = false;
+    expect(() =>
+      render(
+        <PaginationV2
+          totalCount={10}
+          pageSize={2}
+          currentPage={1}
+          path="/mijn-contact/contactmomenten/:page?"
+        />
+      )
+    ).toThrow(
+      "Unparsed router path encountered: '/mijn-contact/contactmomenten/:page?'"
+    );
+  });
+
+  it('throws for non-optional param :page', () => {
+    mocks.IS_PRODUCTION = true;
+    expect(() =>
+      render(
+        <PaginationV2
+          totalCount={10}
+          pageSize={2}
+          currentPage={1}
+          path="/mijn-contact/:page"
+        />
+      )
+    ).toThrow("Unparsed router path encountered: '/mijn-contact/:page'");
+  });
+
+  it('throws for optional params other than :page?', () => {
+    mocks.IS_PRODUCTION = true;
+    expect(() =>
+      render(
+        <PaginationV2
+          totalCount={10}
+          pageSize={2}
+          currentPage={1}
+          path="/mijn-contact/:kind?"
+        />
+      )
+    ).toThrow("Unparsed router path encountered: '/mijn-contact/:kind?'");
   });
 });
