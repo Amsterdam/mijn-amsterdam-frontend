2891919872c0a4f200570cd2d7724de35b2c1d5a
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-08-18 17:17:53 +0200

    Change label based on count

--- src/client/pages/Thema/Afis/AfisEMandateDetail.tsx
+++ src/client/pages/Thema/Afis/AfisEMandateDetail.tsx
@@ -34,7 +34,7 @@ function EmandateHistorySectionSmallScreen({
   return (
     <section>
       <Heading id="eerdere-emandaten" level={3} className="ams-mb-m">
-        Eerdere E-Mandaten
+        Eerdere incassomachtiging{eMandateHistory.length > 0 && 'en'}
       </Heading>
       {eMandateHistory.map((historyItem) => {
         return (
