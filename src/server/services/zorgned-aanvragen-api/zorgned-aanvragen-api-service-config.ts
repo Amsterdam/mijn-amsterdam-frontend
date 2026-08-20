608d5a0c870f92f4fc940d402fb5de0d71d8be56
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-08-20 11:16:28 +0200

    Remove feature toggle

--- src/server/services/zorgned-aanvragen-api/zorgned-aanvragen-api-service-config.ts
+++ src/server/services/zorgned-aanvragen-api/zorgned-aanvragen-api-service-config.ts
@@ -1,25 +1,9 @@
-import { isEnabled } from '../../config/azure-appconfiguration.ts';
-
-export const featureToggle = {
-  router: {
-    private: {
-      isEnabled: true,
-    },
-  },
-  service: {
-    fetchWmo: {
-      addMaAanvragenApiProps: isEnabled(
-        'WMO.fetchWmo.addMaVoorzieningenApiProps'
-      ),
-    },
-  },
-} as const;
-
 // The role is generic and not specific to WMO, because the same role is used for all ZorgNed aanvragen clients (WMO, LLV, HLI).
 export const OAUTH_ROLE_ZORGNED_AANVRAGEN = 'wmo.aanvragen' as const;
 
 export const routes = {
   private: {
+    // Legacy route definition. Used by Forms team.
     VOORZIENINGEN_JZD: `/services/jzd/voorzieningen`,
     AANVRAGEN: `/services/zorgned/aanvragen`,
     AANVRAAG_DETAIL: `/services/zorgned/aanvraag`,
