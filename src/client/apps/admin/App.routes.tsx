626e38143d4c2db647c8c85e2475c09e2d132a21
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-08-25 16:21:43 +0200

    Fix routes

--- src/client/apps/admin/App.routes.tsx
+++ src/client/apps/admin/App.routes.tsx
@@ -1,16 +1,9 @@
 import { matchPath } from 'react-router';
 
-import { AdminAccountRoutes } from './Pages/Account/Account-render-config.tsx';
-import { AdminHomeRoutes } from './Pages/Home/Home-render-config.tsx';
-import { AdminUserFeedbackRoutes } from './Pages/UserFeedback/UserFeedback-render-config.tsx';
-import type { ApplicationRouteConfig } from '../../../universal/types/App.types.ts';
+import type { ApplicationRouteConfig } from '../../../universal/types/thema-types.ts';
 import { ApplicationRoutes } from '../../components/ApplicationRoutes/ApplicationRoutes.tsx';
 
-const routeComponents: ApplicationRouteConfig[] = [
-  AdminHomeRoutes,
-  AdminAccountRoutes,
-  AdminUserFeedbackRoutes,
-].flat();
+const routeComponents: ApplicationRouteConfig[] = [].flat();
 
 export const privateRoutes = routeComponents.filter(
   (config) => config.private !== false
