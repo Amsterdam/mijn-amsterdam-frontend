0d67fbcc7696d9f6d2e65bd2e4a4289cabaef3b3
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-08-26 10:36:52 +0200

    Add Home page

---
+++ src/client/apps/admin/Pages/Home/Home-render-config.tsx
@@ -0,0 +1,21 @@
+import { themaConfig } from './Home-thema-config.ts';
+import { Home, HomePublic } from './Home.tsx';
+import type { ApplicationRouteConfig } from '../../../../../universal/types/App.types.ts';
+
+export const AdminHomeRoutes = [
+  {
+    route: themaConfig.route.path,
+    Component: Home,
+    isActive: themaConfig.featureToggle.active,
+    public: false,
+    props: {
+      index: true,
+    },
+  },
+  {
+    route: '/',
+    Component: HomePublic,
+    isActive: themaConfig.featureToggle.active,
+    public: true,
+  },
+] as const satisfies readonly ApplicationRouteConfig[];
