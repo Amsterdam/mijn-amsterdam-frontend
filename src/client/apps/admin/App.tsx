eb8799d170bfb47375aeb3ca4368adf0f9502daa
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-08-26 10:36:45 +0200

    Add App

--- src/client/apps/admin/App.tsx
+++ src/client/apps/admin/App.tsx
@@ -1,3 +1,90 @@
+import { Grid, PageHeader } from '@amsterdam/design-system-react';
+import { BrowserRouter } from 'react-router';
+
+import { PrivateRoutes, PublicRoutes } from './App.routes.tsx';
+import { AppTabs } from './AppTabs.tsx';
+import { AUTH_API_URL, LOGIN_URL, LOGOUT_URL } from './config/api.ts';
+// import { themaConfig as themaConfigAccount } from './Pages/Account/Account-thema-config.ts';
+// import type { AccountData } from '../../../server/services/admin/admin-account.types.ts';
+import { MaRouterLink } from '../../components/MaLink/MaLink.tsx';
+import { PageContentCell } from '../../components/Page/Page.tsx';
+import { useSessionApi } from '../../hooks/api/useSessionApi.ts';
+
+function AppLanding() {
+  const session = useSessionApi(AUTH_API_URL);
+  const { isAuthenticated, isDirty } = session;
+  // const { fetch: fetchAccountData, data } = useBffApi<AccountData>(
+  //   BFFApiUrls.ACCOUNT,
+  //   {
+  //     fetchImmediately: false,
+  //   }
+  // );
+
+  // useEffect(() => {
+  //   if (isAuthenticated) {
+  //     void fetchAccountData();
+  //   }
+  // }, [fetchAccountData, isAuthenticated]);
+
+  // We don't want to show the app content until we know whether the user is authenticated or not,
+  // to prevent flashing of the wrong content.
+  // Therefore, we return null while the session is still loading (dirty).
+  if (!isDirty) {
+    return null;
+  }
+
+  const welcomeLoader = document.getElementById('loader');
+  if (welcomeLoader) {
+    welcomeLoader.remove();
+  }
+
+  return (
+    <>
+      <PageHeader
+        brandName="Mijn Amsterdam - backoffice"
+        logoLink="/admin"
+        menuItems={
+          isAuthenticated ? (
+            <>
+              <PageHeader.MenuLink
+                linkComponent={MaRouterLink}
+                fixed
+                // href={themaConfigAccount.route.path}
+              >
+                {/* {data?.content?.username ?? 'Account'} */}Account
+              </PageHeader.MenuLink>
+              <PageHeader.MenuLink fixed href={LOGOUT_URL}>
+                Uitloggen
+              </PageHeader.MenuLink>
+            </>
+          ) : (
+            <PageHeader.MenuLink fixed href={LOGIN_URL}>
+              Inloggen
+            </PageHeader.MenuLink>
+          )
+        }
+      />
+
+      {isAuthenticated ? (
+        <>
+          <Grid>
+            <PageContentCell>
+              <AppTabs />
+            </PageContentCell>
+          </Grid>
+          <PrivateRoutes />
+        </>
+      ) : (
+        <PublicRoutes />
+      )}
+    </>
+  );
+}
+
 export function App() {
-  return 'Hello admin';
+  return (
+    <BrowserRouter basename="/admin">
+      <AppLanding />
+    </BrowserRouter>
+  );
 }
