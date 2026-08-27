dafd1d301baad0bb4e4eb0594409a9fc2e0b25f9
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-08-27 08:12:03 +0200

    Encrypt the api token

--- src/server/services/admin/admin-account.types.ts
+++ src/server/services/admin/admin-account.types.ts
@@ -2,10 +2,19 @@ import z from 'zod';
 
 const MAX_JIRA_API_TOKEN_LENGTH = 512;
 
+type ApiTokenEncrypted = string;
+type ApiTokenDecrypted = string;
+
+export type AccountRow = {
+  username: string;
+  jiraApiToken: ApiTokenEncrypted | '';
+  lastSignInDate: Date;
+};
+
 export type AccountData = {
   username: string;
   lastSignInDate: string;
-  jiraApiToken: string;
+  jiraApiToken: ApiTokenDecrypted;
 };
 
 export const accountUpdateInput = z
