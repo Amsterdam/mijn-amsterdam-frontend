1dfdf3b7d4e6a37b8883b3444920ecc3628cf0e5
jasper-ams <j.massink@amsterdam.nl>
2026-09-08 14:31:11 +0200

    update notififcatie

--- src/server/services/wonen/zwd-notifications.ts
+++ src/server/services/wonen/zwd-notifications.ts
@@ -5,6 +5,7 @@ import {
   apiSuccessResult,
   type ApiResponse,
 } from '../../../universal/helpers/api.ts';
+import { lowercaseFirstLetter } from '../../../universal/helpers/text.ts';
 import type { MyNotification } from '../../../universal/types/App.types.ts';
 import type { AuthProfileAndToken } from '../../auth/auth-types.ts';
 
@@ -12,19 +13,27 @@ export function transformZWDCasesToNotifications(
   cases: VvEDataFrontend['cases']
 ): MyNotification[] {
   return cases
-    .filter((zaak) => !!zaak.updated)
-    .map((zaak) => ({
-      id: `wonen-zwd-${zaak.id}-notification`,
-      themaID: themaConfig.BRP.id,
-      themaTitle: themaConfig.BRP.title,
-      title: zaak.adviceType,
-      description: zaak.homeownerAssociation.name,
-      datePublished: zaak.updated,
-      link: {
-        to: themaConfig.BRP.detailPageVvE.route.path,
-        title: 'Bekijk uw aanvraag',
-      },
-    }));
+    .filter((zaak) => zaak.adviceType === 'Energieadvies')
+    .map((zaak) => {
+      const zaakStatusText = lowercaseFirstLetter(zaak.status);
+
+      return {
+        id: `wonen-zwd-${zaak.id}-notification`,
+        themaID: themaConfig.BRP.id,
+        themaTitle: themaConfig.BRP.title,
+        title: 'Aanvraag verduurzamingsadvies VVE ' + zaakStatusText,
+        description:
+          'Wij hebbben de aanvraag voor verduurzamingsadvies van de VVE ' +
+          zaak.homeownerAssociation.name +
+          ' ' +
+          zaakStatusText,
+        datePublished: zaak.updated,
+        link: {
+          to: themaConfig.BRP.detailPageVvE.route.path,
+          title: 'Bekijk uw aanvraag',
+        },
+      };
+    });
 }
 
 export async function fetchZWDNotifications(
