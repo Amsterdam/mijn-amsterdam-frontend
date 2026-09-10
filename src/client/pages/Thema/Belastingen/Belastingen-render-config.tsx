b76ec80ba7ee4dd8d42ced21cdc2abc89f61ae03
YacineHallo <y.hesseling@amsterdam.nl>
2026-09-10 14:05:16 +0200

    render aangepast

--- src/client/pages/Thema/Belastingen/Belastingen-render-config.tsx
+++ src/client/pages/Thema/Belastingen/Belastingen-render-config.tsx
@@ -1,19 +1,15 @@
-import { themaConfig } from './Belastingen-thema-config.ts';
+import {
+  getBelastingenSSOUrl,
+  themaConfig,
+} from './Belastingen-thema-config.ts';
 import { default as BelastingenIcon } from './BelastingenIcon.svg?react';
 import { isLoading } from '../../../../universal/helpers/api.ts';
-import { type AppState } from '../../../../universal/types/App.types.ts';
 import { type ThemaMenuItem } from '../../../config/thema-types.ts';
 
 export const menuItem: ThemaMenuItem = {
   title: themaConfig.title,
   id: themaConfig.id,
-  to: (_appState: AppState, profileType?: string) => {
-    const path =
-      profileType === 'commercial'
-        ? '/eherkenning.saml.php?start'
-        : '/digid.saml.php?start';
-    return `${themaConfig.route.path + path}`;
-  },
+  to: getBelastingenSSOUrl,
   profileTypes: themaConfig.profileTypes,
   redactedScope: themaConfig.redactedScope,
   isActive(appState, profileType) {
