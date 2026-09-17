a83d19a9fee87df3ccd88cbf4b025552e1597693
Evi Wammes <e.wammes@amsterdam.nl>
2026-09-17 14:41:05 +0200

    Small refactor of MyAreaDashboard

--- src/client/components/MyArea/MyAreaDashboard.tsx
+++ src/client/components/MyArea/MyAreaDashboard.tsx
@@ -6,9 +6,9 @@ import { NavLink } from 'react-router';
 import { routeConfig, themaTitle } from './MyArea-thema-config.ts';
 import styles from './MyAreaDashboard.module.scss';
 import { MyAreaLoader } from './MyAreaLoader.tsx';
-import { isLoading } from '../../../universal/helpers/api.ts';
 import { isMokum } from '../../../universal/helpers/brp.ts';
 import { useAppStateGetter } from '../../hooks/useAppStateStore.ts';
+import { useIsLoading } from '../../hooks/useIsLoading.ts';
 import { useProfileTypeValue } from '../../hooks/useProfileType.ts';
 import { LoadingContent } from '../LoadingContent/LoadingContent.tsx';
 
@@ -17,31 +17,25 @@ export function MyAreaDashboard() {
   const ref = useRef<HTMLDivElement | null>(null);
   const { BRP, KVK } = useAppStateGetter();
   const isPrivate = profileType === 'private';
+  const isLoading = useIsLoading(isPrivate ? BRP : KVK);
+
   // Check if the Map is nearly scrolled into view
   const mokum = isPrivate ? isMokum(BRP.content) : isMokum(KVK.content);
-  const isLoadingBrpKvk = isPrivate ? isLoading(BRP) : isLoading(KVK);
+
+  const paragraphText = mokum
+    ? 'Klik voor een overzicht van gemeentelijke informatie rond uw adres.'
+    : 'Uw adres kan niet worden getoond in Mijn Amsterdam.';
+
   return (
     <div ref={ref} className={styles.DashboardMapContainer}>
       <MyAreaLoader isDashboard={true} />
       <NavLink className={styles.NavLink} to={routeConfig.themaPage.path}>
         <span className={styles.NavLinkContentWrap}>
           <Heading level={3}>{themaTitle}</Heading>
-          {isLoadingBrpKvk && (
+          {isLoading ? (
             <LoadingContent barConfig={[['200px', '30px', '20px']]} />
-          )}
-          {!isLoadingBrpKvk && (
-            <>
-              {!mokum ? (
-                <Paragraph>
-                  Uw adres kan niet worden getoond in Mijn Amsterdam.
-                </Paragraph>
-              ) : (
-                <Paragraph>
-                  Klik voor een overzicht van gemeentelijke informatie rond uw
-                  adres.
-                </Paragraph>
-              )}
-            </>
+          ) : (
+            <Paragraph>{paragraphText}</Paragraph>
           )}
         </span>
       </NavLink>
