a9c8e0cfc67a7c14d449494531ceb6f10406eb20
Evi Wammes <e.wammes@amsterdam.nl>
2026-09-18 15:06:31 +0200

    Remove double check for the featureToggle as we already filter it out

--- src/client/components/MainMenu/MainMenu.constants.ts
+++ src/client/components/MainMenu/MainMenu.constants.ts
@@ -1,12 +1,11 @@
 import { dashboardMenuItem } from '../../apps/bob/pages/Dashboard/Dashboard-routes.ts';
 import { myNotificationsMenuItem } from '../../apps/bob/pages/MyNotifications/MyNotifications-routes.ts';
-import { featureToggle } from '../../apps/bob/pages/MyTips/MyTips-config.ts';
 import { myTipsMenuItem } from '../../apps/bob/pages/MyTips/MyTips-routes.ts';
 import { menuCategoryItem as buurtMenuItem } from '../MyArea/MyArea-routes.ts';
 
 export const categoryMenuItems = [
   buurtMenuItem,
   myNotificationsMenuItem,
-  ...(featureToggle.newTipsDesign ? [myTipsMenuItem] : []),
+  myTipsMenuItem,
   dashboardMenuItem,
 ] as const;
