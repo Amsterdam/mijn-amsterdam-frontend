1ede0cc7ae011bd7e89e5348895f0480dd1c7954
Evi Wammes <e.wammes@amsterdam.nl>
2026-09-22 08:34:06 +0200

    Rectify import order after merge conflict

--- src/client/apps/bob/pages/Thema/Belastingen/Belastingen-render-config.tsx
+++ src/client/apps/bob/pages/Thema/Belastingen/Belastingen-render-config.tsx
@@ -3,9 +3,8 @@ import {
   themaConfig,
 } from './Belastingen-thema-config.ts';
 import { default as BelastingenIcon } from './BelastingenIcon.svg?react';
-import { isLoading } from '../../../../../helpers/api.ts';
-import { type AppState } from '../../../../../../universal/types/App.types.ts';
 import { type ThemaMenuItem } from '../../../../../../universal/types/thema-types.ts';
+import { isLoading } from '../../../../../helpers/api.ts';
 
 export const menuItem: ThemaMenuItem = {
   title: themaConfig.title,
