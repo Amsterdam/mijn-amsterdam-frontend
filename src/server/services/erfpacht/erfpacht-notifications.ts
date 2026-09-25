e95edcccf36a7123ef8a19e1f00c6d45db950b0d
Terry van Walen <t.van.walen@amsterdam.nl>
2026-09-25 16:30:44 +0200

    Remove aanvraag from aanvraag afgerond notification title

--- src/server/services/erfpacht/erfpacht-notifications.ts
+++ src/server/services/erfpacht/erfpacht-notifications.ts
@@ -34,13 +34,14 @@ export function getSubStatusNotificationTitle(
       return `Aanvraag ${caseType} gereed voor behandeling`;
     case ZAAK_STATUS_SOURCE.AANBIEDING:
       return `Aanvraag ${caseType} aanbieding verstuurd`;
+    case ZAAK_STATUS_SOURCE.AANVRAAG_AFGEROND:
+      return `Aanvraag ${caseType} afgerond`;
 
     case ZAAK_STATUS_SOURCE.INDICATIE_VERSTUURD:
     case ZAAK_STATUS_SOURCE.ACCEPTATIE_ONTVANGEN:
     case ZAAK_STATUS_SOURCE.BESLUIT_VERSTUURD:
     case ZAAK_STATUS_SOURCE.AKTE_GEPASSEERD:
     case ZAAK_STATUS_SOURCE.BEHANDELING:
-    case ZAAK_STATUS_SOURCE.AANVRAAG_AFGEROND:
       return `Aanvraag ${caseType} ${zaakOmschrijving}`;
     default:
       captureException(`Unknown status for ${caseType}: ${zaakOmschrijving}`, {
